ac48afeabee073e5a78b08e3596f797d65dcff2f
rele <rele@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-07-28 11:39:22 +0000

    Performance
    git-svn-id: http://svn.vuze.com/public/client@141 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core/SHA1Hasher.java
+++ trunk/azureus2/src/org/gudy/azureus2/core/SHA1Hasher.java
@@ -15,30 +15,26 @@ import java.security.MessageDigest;
  * @author  TdC_VgA
  */
 public class SHA1Hasher {
-	private MessageDigest _messageDigest = null;
-    
-    /** Creates a new instance of SHA1Hasher */
-    public SHA1Hasher() throws java.security.NoSuchAlgorithmException
-    {
-		_messageDigest = MessageDigest.getInstance("SHA-1");
-    }
-    
-    public byte[] calculateHash(byte[] bytes){
-		byte[] hash = null;				
-		_messageDigest.update(bytes);
-		return hash = _messageDigest.digest();
-    }
-    
-    public byte[] calculateHash(ByteBuffer buffer) {
-    byte[] hash = null;
+  private MessageDigest _messageDigest = null;
+
+  /** Creates a new instance of SHA1Hasher */
+  public SHA1Hasher() throws java.security.NoSuchAlgorithmException {
+    _messageDigest = MessageDigest.getInstance("SHA-1");
+  }
+
+  public byte[] calculateHash(byte[] bytes) {
+    _messageDigest.update(bytes);
+    return _messageDigest.digest();
+  }
+
+  public byte[] calculateHash(ByteBuffer buffer) {
     //Temp stores the current buffer's position
     int position = buffer.position();
-    while(buffer.hasRemaining())
-    {        
-     _messageDigest.update(buffer.get());
+    while (buffer.hasRemaining()) {
+      _messageDigest.update(buffer.get());
     }
     //Restores the buffer's position
     buffer.position(position);
-    return hash = _messageDigest.digest();
-    }
+    return _messageDigest.digest();
+  }
 }
