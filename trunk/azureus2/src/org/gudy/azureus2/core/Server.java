881a81515fa08be3eb6139942f3666997dfa4c08
rele <rele@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-07-28 11:31:44 +0000

    high and low port recognition
    git-svn-id: http://svn.vuze.com/public/client@140 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core/Server.java
+++ trunk/azureus2/src/org/gudy/azureus2/core/Server.java
@@ -18,7 +18,7 @@ public class Server extends Thread
   public static final int evtNewConnection = 1;
   public static final int evtErrors = 2;
   
-  private static final int MAX_CONNECTIONS = 50;
+//  private static final int MAX_CONNECTIONS = 50;
   private int port;
   private ServerSocketChannel sck;
   private boolean bContinue;
@@ -30,28 +30,26 @@ public class Server extends Thread
     super("Bt Server");
     //Will create a Server on any socket from 6881 to 6889
     int lowPort = ConfigurationManager.getInstance().getIntParameter("Low Port",6881);
+    int highPort = ConfigurationManager.getInstance().getIntParameter("High Port",6889);
+    lowPort = Math.min(lowPort, highPort);
+    highPort = Math.max(lowPort, highPort);
     port = lowPort;
     sck = null;
     bContinue = true;
-    int highPort = ConfigurationManager.getInstance().getIntParameter("High Port",6889);
-    while(sck == null && port <= highPort)
-    {
+    while (sck == null && port <= highPort) {
       try {
-        sck = ServerSocketChannel.open(); 
+        sck = ServerSocketChannel.open();
         sck.socket().bind(new InetSocketAddress(port));
-      } catch(Exception e) {
+      } catch (Exception e) {
         Logger.getLogger().log(componentID,evtErrors,Logger.ERROR,"BT Server was unable to bind port " + port + ", reason : " + e);
         port++;
         sck = null;
       }
     }
     
-    if(sck != null)
-    {
-        Logger.getLogger().log(componentID,evtLyfeCycle,Logger.INFORMATION,"BT Server is bound on port " + port);
-    }     
-    else
-    {
+    if (sck != null) {
+      Logger.getLogger().log(componentID,evtLyfeCycle,Logger.INFORMATION,"BT Server is bound on port " + port);
+    } else {
       Logger.getLogger().log(componentID,evtLyfeCycle,Logger.INFORMATION,"BT was unable to bind on a port from "+ lowPort + " to " + highPort);            
       port = 0;
     }
@@ -62,17 +60,14 @@ public class Server extends Thread
     try {
       sck.configureBlocking(false);
       Logger.getLogger().log(componentID,evtLyfeCycle,Logger.INFORMATION,"BT Server is ready to accept incoming connections");
-      while(bContinue)
-      { 
+      while (bContinue) {
         SocketChannel sckClient = sck.accept();
-        if(sckClient != null)
-        {
-            Logger.getLogger().log(componentID,evtNewConnection,Logger.INFORMATION,"BT Server has accepted an incoming connection from : " + sckClient.socket().getInetAddress().getHostAddress());
-            sckClient.configureBlocking(false);
-            manager.addPeer(sckClient);
-        }            
-        else
-            Thread.sleep(50);          
+        if (sckClient != null) {
+          Logger.getLogger().log(componentID,evtNewConnection,Logger.INFORMATION,"BT Server has accepted an incoming connection from : " + sckClient.socket().getInetAddress().getHostAddress());
+          sckClient.configureBlocking(false);
+          manager.addPeer(sckClient);
+        } else
+          Thread.sleep(50);
       }
     } catch (Exception e)  {               
       Logger.getLogger().log(componentID,evtErrors,Logger.ERROR,"BT Server has catched an error : " + e);            
