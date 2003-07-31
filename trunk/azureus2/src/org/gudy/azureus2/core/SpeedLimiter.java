94f8f13bafe3756fa885865f3a29533d7a4f55b9
rele <rele@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-07-31 00:01:28 +0000

    improved performance and less memory usage
    git-svn-id: http://svn.vuze.com/public/client@181 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core/SpeedLimiter.java
+++ trunk/azureus2/src/org/gudy/azureus2/core/SpeedLimiter.java
@@ -32,7 +32,14 @@ public class SpeedLimiter {
 
   //The List of current uploaders
   private List uploaders;
-
+  
+  private int[] sortedUploadersHighPriorityInts = new int[1000];
+  private PeerSocket[] sortedUploadersHighPriorityPeerSockets = new PeerSocket[1000];
+  private int[] sortedUploadersLowPriorityInts = new int[1000];
+  private PeerSocket[] sortedUploadersLowPriorityPeerSockets = new PeerSocket[1000];
+  
+  private int i, j, maxUpload, smallestIndex, sortedUploadersHighPriorityIndex, sortedUploadersLowPriorityIndex;
+  
   /**
    * Private constructor for SpeedLimiter
    *
@@ -90,12 +97,117 @@ public class SpeedLimiter {
     return (this.limit != 0 && uploaders.contains(wt));
   }
 
+  public int getLimitPer100ms(PeerSocket wt) {
+
+    if (this.uploaders.size() == 0)
+      return 0;
+
+    sortedUploadersHighPriorityIndex = 0;
+    sortedUploadersLowPriorityIndex = 0;
+
+    //We construct a TreeMap to sort all writeThread according to their up speed.
+    synchronized (uploaders) {
+      for (i = 0; i < uploaders.size(); i++) {
+        PeerSocket wti = (PeerSocket) uploaders.get(i);
+        maxUpload = wti.getMaxUpload();
+        if (wti.getDownloadPriority() == DownloadManager.HIGH_PRIORITY) {
+          for(j=0; j < sortedUploadersHighPriorityIndex; j++)
+            if(sortedUploadersHighPriorityInts[j] == maxUpload) {
+              maxUpload++;
+              j = -1;
+            }
+          sortedUploadersHighPriorityInts[sortedUploadersHighPriorityIndex] = maxUpload;
+          sortedUploadersHighPriorityPeerSockets[sortedUploadersHighPriorityIndex++] = wti;
+        }
+        else {
+          for(j=0; j < sortedUploadersLowPriorityIndex; j++)
+            if(sortedUploadersLowPriorityInts[j] == maxUpload) {
+              maxUpload++;
+              j = -1;
+            }
+          sortedUploadersLowPriorityInts[sortedUploadersLowPriorityIndex] = maxUpload;
+          sortedUploadersLowPriorityPeerSockets[sortedUploadersLowPriorityIndex++] = wti;
+        }
+      }
+    }
+
+    for (j = 0; j < sortedUploadersHighPriorityIndex - 1; j++) {
+      smallestIndex = j;
+      for (i = j + 1; i < sortedUploadersHighPriorityIndex; i++) {
+        if (sortedUploadersHighPriorityInts[i] < sortedUploadersHighPriorityInts[smallestIndex])
+          smallestIndex = i;
+      }
+      if(j != smallestIndex) {
+        sortedUploadersHighPriorityInts[sortedUploadersHighPriorityIndex] = sortedUploadersHighPriorityInts[j];
+        sortedUploadersHighPriorityPeerSockets[sortedUploadersHighPriorityIndex] = sortedUploadersHighPriorityPeerSockets[j];
+        sortedUploadersHighPriorityInts[j] = sortedUploadersHighPriorityInts[smallestIndex];
+        sortedUploadersHighPriorityPeerSockets[j] = sortedUploadersHighPriorityPeerSockets[smallestIndex];
+        sortedUploadersHighPriorityInts[smallestIndex] = sortedUploadersHighPriorityInts[sortedUploadersHighPriorityIndex];
+        sortedUploadersHighPriorityPeerSockets[smallestIndex] = sortedUploadersHighPriorityPeerSockets[sortedUploadersHighPriorityIndex];
+      }
+    }
+
+    for (j = 0; j < sortedUploadersLowPriorityIndex - 1; j++) {
+      smallestIndex = j;
+      for (i = j + 1; i < sortedUploadersLowPriorityIndex; i++) {
+        if (sortedUploadersLowPriorityInts[i] < sortedUploadersLowPriorityInts[smallestIndex])
+          smallestIndex = i;
+      }
+      if(j != smallestIndex) {
+        sortedUploadersLowPriorityInts[sortedUploadersLowPriorityIndex] = sortedUploadersLowPriorityInts[j];
+        sortedUploadersLowPriorityPeerSockets[sortedUploadersLowPriorityIndex] = sortedUploadersLowPriorityPeerSockets[j];
+        sortedUploadersLowPriorityInts[j] = sortedUploadersLowPriorityInts[smallestIndex];
+        sortedUploadersLowPriorityPeerSockets[j] = sortedUploadersLowPriorityPeerSockets[smallestIndex];
+        sortedUploadersLowPriorityInts[smallestIndex] = sortedUploadersLowPriorityInts[sortedUploadersLowPriorityIndex];
+        sortedUploadersLowPriorityPeerSockets[smallestIndex] = sortedUploadersLowPriorityPeerSockets[sortedUploadersLowPriorityIndex];
+      }
+    }
+
+    //System.out.println(sortedUploadersHighPriority.size() + " : " + sortedUploadersLowPriority.size());
+
+
+    int toBeAllocated = this.limit / 10;
+    int peersToBeAllocated = sortedUploadersHighPriorityIndex;
+    int allowed;
+    boolean found = false;
+    for (i = 0; i < sortedUploadersHighPriorityIndex; i++) {
+      if (sortedUploadersHighPriorityPeerSockets[i] == wt) {
+        found = true;
+        break;
+      }
+      allowed = toBeAllocated / peersToBeAllocated;
+      maxUpload = sortedUploadersHighPriorityPeerSockets[i].getMaxUpload();
+      toBeAllocated -= allowed > maxUpload ? maxUpload : allowed;
+      peersToBeAllocated--;
+    }
+    if (!found) {
+      peersToBeAllocated = sortedUploadersLowPriorityIndex;
+      for (i = 0; i < sortedUploadersLowPriorityIndex; i++) {
+        if (sortedUploadersLowPriorityPeerSockets[i] == wt) {
+          found = true;
+          break;
+        }
+        allowed = toBeAllocated / peersToBeAllocated;
+        maxUpload = sortedUploadersLowPriorityPeerSockets[i].getMaxUpload();
+        toBeAllocated -= allowed > maxUpload ? maxUpload : allowed;
+        peersToBeAllocated--;
+      }
+    }
+
+    allowed = toBeAllocated / peersToBeAllocated;
+    maxUpload = wt.getMaxUpload();
+    int result = allowed > maxUpload ? maxUpload : allowed;
+    //Logger.getLogger().log(0,0,Logger.ERROR,"Allocated for 100ms :" + result);
+//    getLimitPer100ms(wt, result);
+    return result;
+  }
+
   /**
    * This method returns the amount of data a thread may upload
    * over a period of 100ms.
    * @return number of bytes allowed for 100 ms
    */
-  public int getLimitPer100ms(PeerSocket wt) {
+  public int getLimitPer100msOld(PeerSocket wt, int otherResult) {
 
     if (this.uploaders.size() == 0)
       return 0;
