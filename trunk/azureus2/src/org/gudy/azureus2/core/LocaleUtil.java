403b2a523906fac22f7b7de834283eaa056ecb91
rele <rele@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-08-19 19:13:04 +0000

    optimized and better readable; unicode letters are still discarded
    git-svn-id: http://svn.vuze.com/public/client@269 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core/LocaleUtil.java
+++ trunk/azureus2/src/org/gudy/azureus2/core/LocaleUtil.java
@@ -1,7 +1,6 @@
 package org.gudy.azureus2.core;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.CharBuffer;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
@@ -10,55 +9,47 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LocaleUtil {
-    
+
+  private static final String systemEncoding = System.getProperty("file.encoding");
+  private static final CharsetDecoder systemDecoder = Charset.forName(systemEncoding).newDecoder();
+
 	private static final String[] charset = {
-		System.getProperty("file.encoding"),
+    systemEncoding,
 		"Big5","EUC-JP","EUC-KR","GB18030","GBK","ISO-2022-JP","ISO-2022-KR",
 		"Shift_JIS","KOI8-R","UTF-8","windows-1251","ISO-8859-1"
 	};    
+
+  private static final CharsetDecoder[] charsetDecoder = new CharsetDecoder[charset.length];
+
+  static {
+    for (int i = 0; i < charset.length; i++) {
+      try {
+        charsetDecoder[i] = Charset.forName(charset[i]).newDecoder();
+      } catch (Exception ignore) {
+      }
+    }
+  }
 
   public static String getCharsetString(byte[] array) throws UnsupportedEncodingException {
     return new String(array, getCharset(array));
   }
 
 	public static String getCharset(byte[] array) {
+    Candidate[] candidates = new Candidate[charset.length];
+    for (int i = 0; i < charset.length; i++) {
+      candidates[i] = new Candidate();
+      try {
+        candidates[i].name = charsetDecoder[i].decode(ByteBuffer.wrap(array)).toString();
+        candidates[i].charset = charset[i];
+      } catch (Exception ignore) {
+      }
+    }
 
-		class Candidate implements Comparable {
-			String name;
-			String charset;
+    String defaultString = candidates[0].name;
 
-			public int compareTo(Object o) {
-				Candidate candidate = (Candidate)o;
-				if (candidate.name.hashCode()==name.hashCode() &&
-					candidate.charset.hashCode()==charset.hashCode()) {
-					return 0;
-				}
-				if (name.length() < candidate.name.length()) {
-					return -1;
-				} 
-				return 1;
-			}
-		}
-
-		List candidateList  = new ArrayList();
-		for (int i=0;i<charset.length;i++) {
-			try {
-				Charset cset = Charset.forName(charset[i]);
-				CharsetDecoder decoder = cset.newDecoder();
-				CharBuffer cb=decoder.decode(ByteBuffer.wrap(array));
-				Candidate candidate = new Candidate();
-				candidate.name=cb.toString();
-				candidate.charset=charset[i];
-				candidateList.add(candidate);
-			}
-			catch (Exception e) {
-			}
-		}
-
-		Object [] candidates = candidateList.toArray();
 		Arrays.sort(candidates);
 
-		int minlength = ((Candidate)candidates[0]).name.length();
+		int minlength = candidates[0].name.length();
 		
 		/*
 		int filterCount = 1;
@@ -68,17 +59,6 @@ public class LocaleUtil {
 		}
 		*/
 
-		// Compute default file.encoding candidate string
-		String defaultString = null;
-		try {
-			Charset cset = Charset.forName(System.getProperty("file.encoding"));
-			CharsetDecoder decoder = cset.newDecoder();
-			CharBuffer cb=decoder.decode(ByteBuffer.wrap(array));
-			defaultString = cb.toString();
-		}
-		catch (Exception e) {
-		}
-
 		/*
 		for (int i=0;i<filterCount;i++) {
 			System.out.println(((Candidate)candidates[i]).charset);
@@ -87,12 +67,29 @@ public class LocaleUtil {
        
 		// If the default string length == minlength assumes that
 		// the array encoding is from default charset 
-		if (defaultString !=null && defaultString.length()==minlength) {
-			return System.getProperty("file.encoding");
+		if (defaultString != null && defaultString.length() == minlength) {
+			return systemEncoding;
 		}
 
 		// Here it is assumed the shorter the string, the more likely is 
 		// the correct charset
-		return ((Candidate)candidates[0]).charset; 
+		return candidates[0].charset; 
 	}    
+
+  private static class Candidate implements Comparable {
+    String name;
+    String charset;
+
+    public int compareTo(Object o) {
+      Candidate candidate = (Candidate)o;
+      if (candidate.name.hashCode()==name.hashCode() &&
+        candidate.charset.hashCode()==charset.hashCode()) {
+        return 0;
+      }
+      if (name.length() < candidate.name.length()) {
+        return -1;
+      } 
+      return 1;
+    }
+  }
 }
