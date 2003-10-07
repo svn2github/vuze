d3f234391a257b46e9bb27811be3a68b3bdfc018
fatal_2 <fatal_2@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-10-07 14:06:34 +0000

    more functionality added..
    git-svn-id: http://svn.vuze.com/public/client@588 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/beditor/TreeTest.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/beditor/TreeTest.java
@@ -26,11 +26,13 @@ import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Decorations;
@@ -49,39 +51,56 @@ import org.gudy.azureus2.ui.swt.ImageRepository;
 /**
  * @author Paul Duran
  *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * Simple class to show how the BEditor tree should/could work
  */
 public class TreeTest {
 	
-	private Tree theTree;
-	private Composite treeComposite;
-	private boolean showArrayIndices;
-	private boolean isEditable;
-		
-	public TreeTest(Decorations parent) throws FileNotFoundException
+	private final Tree theTree;
+	private final Composite form;
+	private final Composite treeComposite;
+	private final Composite editorComposite;
+	private final Display display;
+	private boolean showArrayIndices = false;
+	private final Map renderInfo;
+	
+	public TreeTest(Display display, Decorations parent, boolean editable) throws FileNotFoundException
 	{
-		//		FormLayout layout = new FormLayout();
-
-//		RowLayout layout = new RowLayout(SWT.HORIZONTAL);
-//		layout.pack = false;
-//		layout.justify = true;
-//		layout.marginLeft = 3;
-//		layout.marginRight = 3;
-//		layout.marginTop = 3;
-//		layout.marginBottom = 3;
-		FillLayout layout = new FillLayout();
-		treeComposite = new Composite(parent, SWT.NORMAL);
-		treeComposite.setLayout(new FillLayout());
-		treeComposite.setSize(400,500);	
+		this.display = display;
+		
+		final FillLayout layout = new FillLayout();
+		renderInfo = new HashMap();
+		form = new SashForm(parent, SWT.HORIZONTAL);
+				
+		treeComposite = new Composite(form, SWT.NONE);
+		treeComposite.setLayout(layout);
+		
 		theTree = new Tree(treeComposite, SWT.MULTI);
-		setupContextMenu(parent, theTree);
+		if( editable )
+			setupContextMenu(parent, theTree);
 
+		editorComposite = new Composite(form, SWT.NONE);
+		if( editable )	
+		{
+		} else {
+		}
 	}
 	public Composite getPanel()
 	{
-		return treeComposite;
+		return form;
 	}
+	private void renderItem( String key, TreeItem item )
+	{
+		RenderInfo info = (RenderInfo) renderInfo.get(key);
+		if( info == null)
+			return;
+		if( info.foreground != -1 ) {
+			item.setForeground(display.getSystemColor(info.foreground));
+		}
+		if( info.background != -1 ) {
+			item.setBackground(display.getSystemColor(info.background));
+		}
+	}
+	
 	/**
 	 * attaches some context menu items to the specified tree item.
 	 * @param item
@@ -165,19 +184,33 @@ public class TreeTest {
 	 */
 	private void addDictionary( TreeItem parent, Map theData )
 	{		
-//		setupBranchNode(parent);
 		List sortedKeys = new ArrayList(theData.keySet());		
 		Collections.sort(sortedKeys);
 		for (Iterator iter = sortedKeys.iterator(); iter.hasNext();) {
 			String key = (String)iter.next();
 			Object val = theData.get(key);
-//			System.out.println("key: " + entry.getKey() + " . value: " + entry.getValue() + " of class: " + entry.getValue().getClass());
 			TreeItem item = new TreeItem(parent, SWT.NORMAL);
-			fillItem(item, key, val);
+			addItem(item, key, val);
 		}
 	}
-	private void fillItem( TreeItem item, String key, Object val)
+	private void addList( TreeItem parent, List list )
 	{
+		int index = 0;
+		for (Iterator iterator = list.iterator(); iterator.hasNext(); ) 
+		{
+			Object iterVal = iterator.next();
+			TreeItem child = new TreeItem(parent, SWT.NORMAL);
+			if( showArrayIndices )
+				addItem(child, "" + index, iterVal );
+			else
+				addItem(child, null, iterVal);
+					
+			index++;
+		}
+	}
+	private void addItem( TreeItem item, String key, Object val)
+	{
+		renderItem(key,item);
 		String prefix = (key != null ? key + ": " : "");
 		if( key == null)
 			key = "";
@@ -190,6 +223,7 @@ public class TreeTest {
 //			System.out.println("its a byte array!");
 			byte []data = (byte [])val;
 			if( data.length > 1000 ) {
+				item.setImage(ImageRepository.getImage("data"));
 				item.setText(prefix + "(large byte array)");
 			} else {
 				try {
@@ -205,26 +239,13 @@ public class TreeTest {
 			item.setImage(ImageRepository.getImage("dict"));
 			item.setData("type", Map.class);
 			addDictionary( item, (Map)val);
-			item.setExpanded(true);
+//			item.setExpanded(true);
 		} else if( val instanceof List ) {
 			item.setText(key);
 			item.setImage(ImageRepository.getImage("list"));
 			item.setData("type", List.class);
-			int index = 0;
-			for (Iterator iterator = ((List)val).iterator(); iterator.hasNext(); ) 
-			{
-//				item.setImage(new Image(display, "d:/down.gif")); 
-				Object iterVal = iterator.next();
-				
-				TreeItem child = new TreeItem(item, SWT.NORMAL);
-				if( showArrayIndices )
-					fillItem(child, "" + index, iterVal );
-				else
-					fillItem(child, null, iterVal);
-					
-				item.setExpanded(true);
-				index++;
-			}
+			addList( item, (List)val );
+//			item.setExpanded(true);
 		}
 	}
 	public void addMetaData(String fileName) throws FileNotFoundException
@@ -255,7 +276,7 @@ public class TreeTest {
 		TreeItem item = new TreeItem(theTree, SWT.NORMAL);
 		item.setText(title);
 		item.setImage(ImageRepository.getImage("root"));
-		addDictionary( item, root );
+		addItem( item, title, root );
 		item.setExpanded(true);
 		treeComposite.pack();
 	}
@@ -273,39 +294,66 @@ public class TreeTest {
 		}
 		display.dispose();
 	}	
+	public void setRenderInfo( String keyName, RenderInfo info )
+	{
+		this.renderInfo.put(keyName, info);
+	}
 	public void setShowArrayIndices(boolean newValue)
 	{
 		this.showArrayIndices = newValue;
 	}
-	public void setEditable(boolean editable)
-	{
-		this.isEditable = editable;
-	}
 	public static void main(String args[]) throws FileNotFoundException
 	{
-		Shell mainWindow;
-		Display display;
-		
-		System.out.println("starting");
-		display = new Display();
-		ImageRepository.loadImages(display);
-		mainWindow = new Shell(display);
-		Layout layout = new FillLayout();
-		mainWindow.setLayout(layout);
-		mainWindow.setText("SWT Test");
-
-		String fileName;
+		final String fileName;
 		if( args.length > 0 )
 			fileName = args[0];
 		else 
 			fileName = "d:/test2.torrent";
-		TreeTest test = new TreeTest(mainWindow);
+
+
+		final Shell mainWindow;
+		final Display display;
+		
+//		System.out.println("starting");
+		display = new Display();
+		mainWindow = new Shell(display);
+		Layout layout = new FillLayout();
+		
+		ImageRepository.loadImages(display);
+		mainWindow.setLayout(layout);
+		mainWindow.setText("SWT Test");
+
+		TreeTest test = new TreeTest(display, mainWindow, false);
+		final String []specialKeys = new String[] {
+			"comment",
+			"created by",
+			"announce",
+			"creation date",
+			"hash value",
+			"name",
+		};
+		RenderInfo info = new RenderInfo( SWT.COLOR_BLUE, -1 );
+		for (int i = 0; i < specialKeys.length; i++) {
+			test.setRenderInfo(specialKeys[i], info);
+		}
+
 		test.addMetaData(fileName);
 		test.addMetaData(fileName, "Another One");
 		mainWindow.pack();
+		mainWindow.setSize(400,500);	
 		mainWindow.open();		
-
+		
 		waitForClose(mainWindow,display);
 	}
-
+	public static class RenderInfo
+	{
+		// -1 for default
+		public int foreground;
+		public int background;
+		public RenderInfo( int fgcolor, int bgcolor )
+		{
+			this.foreground = fgcolor;
+			this.background = bgcolor;
+		}
+	}
 }
