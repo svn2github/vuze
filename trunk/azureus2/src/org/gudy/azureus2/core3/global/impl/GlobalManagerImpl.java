7e311e45bf976ea820c057432c256ee87cc5f32d
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-11-11 18:27:19 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@969 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
@@ -32,8 +32,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
