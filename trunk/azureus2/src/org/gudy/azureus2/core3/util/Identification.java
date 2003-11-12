6d41abe804bbea203960abc9e6427c58a2174702
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-11-12 10:16:27 +0000

    Now identifies Shadow client versions.
    git-svn-id: http://svn.vuze.com/public/client@984 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/Identification.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Identification.java
@@ -19,16 +19,30 @@ public class Identification {
    * Decodes the given peerID, returning an identification string.
    */  
   public static String decode(byte[] peerID) {
+    final boolean DEBUG = false;
     String generic = MessageText.getString("PeerSocket.generic");
     
     try {
 
+      if (DEBUG) System.out.println(new String(peerID, 0, 20, Constants.BYTE_ENCODING));
+      
       String azureus     = new String(peerID, 1, 2, Constants.BYTE_ENCODING);
       String old_azureus = new String(peerID, 5, 7, Constants.BYTE_ENCODING);
       String shadow      = new String(peerID, 0, 1, Constants.BYTE_ENCODING);
     
       if (shadow.equals("S")) {
-        return "Shadow";
+        
+        if (peerID[8] == (byte)0) return "Shadow";
+        
+        String version = new String(peerID, 1, 3, Constants.BYTE_ENCODING);
+        String name = "Shadow ";
+        
+        for (int i = 0; i < 2; i++) {
+          name = name.concat(version.charAt(i) + ".");
+        }
+        name = name + version.charAt(2);
+        
+        return name;
       }
       
       if (old_azureus.equals("Azureus")) return "Azureus";
