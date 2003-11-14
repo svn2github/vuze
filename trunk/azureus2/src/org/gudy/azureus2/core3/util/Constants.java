ee20dc796710091d4fa7591ac66dd8f3285d131b
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-11-14 13:12:57 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@1053 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
@@ -22,6 +22,6 @@ public class Constants {
   public static final String INFINITY_STRING	= "\u221E"; // "oo";
   
   public static final String AZUREUS_NAME	    = "Azureus";
-  public static final String AZUREUS_VERSION  = "2.0.4.0";
-  public static final byte[] VERSION_ID       = ("-" + "AZ" + "2040" + "-").getBytes();  //MUST be 8 chars long!
+  public static final String AZUREUS_VERSION  = "2.0.4.1_CVS";
+  public static final byte[] VERSION_ID       = ("-" + "AZ" + "2041" + "-").getBytes();  //MUST be 8 chars long!
 }
