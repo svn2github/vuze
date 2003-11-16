4e7307311049a6a8c050b78c5b707e186f787211
epall <epall@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-11-16 20:11:29 +0000

    Initial version
    git-svn-id: http://svn.vuze.com/public/client@1099 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/config2/BooleanConfigProperty.java
@@ -0,0 +1,18 @@
+/*
+ * Created on Nov 16, 2003
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.gudy.azureus2.plugins.config2;
+
+/**
+ * @author Eric Allen
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public interface BooleanConfigProperty extends ConfigProperty
+{
+//	public boolean getValue();
+}
