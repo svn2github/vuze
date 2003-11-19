28025e1e9acee72c333f8c6c662b4e15bad18764
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-11-19 13:59:32 +0000

    Debug helper class.
    git-svn-id: http://svn.vuze.com/public/client@1170 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Debug.java
@@ -0,0 +1,39 @@
+/*
+ * Created on Nov 19, 2003
+ * Created by nolar
+ *
+ */
+package org.gudy.azureus2.core3.util;
+
+/**
+ * Debug-assisting class.
+ */
+public class Debug {
+  
+  
+  /**
+   * Prints out the given debug message to System.out,
+   * prefixed by the calling class name, method and
+   * line number.
+   */
+  public static void out(String debug_message) {
+    String header = "DEBUG::";
+    String className;
+    String methodName;
+    int lineNumber;
+    
+    try {
+      throw new Exception();
+    }
+    catch (Exception e) {
+      StackTraceElement st = e.getStackTrace()[1];
+      className = st.getClassName() + "::";
+      methodName = st.getMethodName() + "::";
+      lineNumber = st.getLineNumber();
+    }
+    
+    System.out.println(header + className + methodName + lineNumber + ":");
+    System.out.println("  " + debug_message + "\n");
+  }
+
+}
