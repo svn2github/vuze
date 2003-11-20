327d7e7af4b17efd4f6819e7bc77fcd64e669f07
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-11-20 12:30:55 +0000

    Cleanup.
    git-svn-id: http://svn.vuze.com/public/client@1189 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/TimeFormater.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/TimeFormater.java
@@ -12,18 +12,20 @@ public class TimeFormater {
 
     public static String format(long time)
     {
+      if (time > 864000) return Constants.INFINITY_STRING;
+      
       int secs = (int) time % 60;
       int mins = (int) (time / 60) % 60;
       int hours = (int) (time /3600) % 24;
       int days = (int) (time / 86400);
       
       String result = "";
-      if(days > 0) result = days + "d ";
-      if(hours > 0 || days > 0) result += hours + "h ";
-      if((mins > 0 || hours > 0 || days > 0) && mins < 10) result += "0" + mins + "' ";
-      if((mins > 0 || hours > 0 || days > 0) && mins >= 10) result += mins + "' ";
-      if((secs > 0 || mins > 0 || hours > 0 || days > 0) && secs < 10) result += "0" + secs + "\" ";
-      if((secs > 0 || mins > 0 || hours > 0 || days > 0) && secs >= 10) result += secs + "\" ";
+      if (days > 0) result = days + "d ";
+      if (hours > 0 || days > 0) result += hours + "h ";
+      if ((days == 0) && ((mins > 0 || hours > 0) && mins < 10)) result += "0" + mins + "m ";
+      if ((days == 0) && ((mins > 0 || hours > 0) && mins >= 10)) result += mins + "m ";
+      if ((hours == 0 && days == 0) && ((secs > 0 || mins > 0) && secs < 10)) result += "0" + secs + "s ";
+      if ((hours == 0 && days == 0) && ((secs > 0 || mins > 0) && secs >= 10)) result += secs + "s ";
       
       return result;
     }
