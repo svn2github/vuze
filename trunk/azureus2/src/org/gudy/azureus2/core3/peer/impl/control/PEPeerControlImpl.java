2d11f62b001b066ecbbf41412f92820cd08cbaf5
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-11-27 20:12:11 +0000

    Added a treshold to the 'getRarestPieces' method (set a 10 for now)
    git-svn-id: http://svn.vuze.com/public/client@1359 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/peer/impl/control/PEPeerControlImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/peer/impl/control/PEPeerControlImpl.java
@@ -836,6 +836,7 @@ PEPeerControlImpl
 
     //We add a 90 % range
     pieceMinAvailability = ((100+rangePercent) * pieceMinAvailability) / 100;
+    if(pieceMinAvailability > 10 && pieceMinAvailability < 999) pieceMinAvailability = 999;
     //For all pieces
     for (int i = 0; i < _nbPieces; i++) {
       //If we're not downloading it, if it's not downloaded, and if it's available from that peer
