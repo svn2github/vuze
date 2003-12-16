c543ffb6e6145880dbd8157102bd463d70962386
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2003-12-16 00:21:58 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@1659 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/components/graphics/SpeedGraphic.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/components/graphics/SpeedGraphic.java
@@ -32,6 +32,8 @@ import org.gudy.azureus2.ui.swt.MainWindow;
 public class SpeedGraphic extends ScaledGraphic {
   
   private int max = 1;
+  private int average = 0;
+  private int nbValues = 0;
   
   private int[] values;
   private int currentPosition;
@@ -58,9 +60,15 @@ public class SpeedGraphic extends ScaledGraphic {
 	      max = value;
 	      scale.setMax(max);
 	    }
+	    average += value - values[currentPosition];
 	    values[currentPosition++] = value;
+	    
+	    if(nbValues < 2000)
+	      nbValues++;
+	    
 	    if(currentPosition >= values.length)
 	      currentPosition = 0;
+	    
     }
   }
   
@@ -91,18 +99,46 @@ public class SpeedGraphic extends ScaledGraphic {
         return;
       GC gcImage = new GC(super.bufferImage);        
       
-      gcImage.setForeground(MainWindow.blues[3]);
+      
+      int oldAverage = 0;
       for(int x = 0 ; x < bounds.width - 71 ; x++) {
         int position = currentPosition - x;
         if(position < 0)
           position+= 2000;
         int value = values[position];
         int xDraw = bounds.width - 71 - x;
-        int height = bounds.height - scale.getScaledValue(value) - 2;       
-        gcImage.drawLine(xDraw,bounds.height - 2,xDraw, height);        
+        int height = bounds.height - scale.getScaledValue(value) - 2;
+        
+        gcImage.setForeground(MainWindow.blues[3]);
+        gcImage.drawLine(xDraw,bounds.height - 2,xDraw, height);
+        
+        int average = computeAverage(position);
+        if(x > 6) {
+          int h1 = bounds.height - scale.getScaledValue(average) - 2;
+          int h2 = bounds.height - scale.getScaledValue(oldAverage) - 2;
+          gcImage.setForeground(MainWindow.red);
+          gcImage.drawLine(xDraw,h1,xDraw+1, h2);
+        }
+        oldAverage = average;
       }      
+      
+      
       gcImage.dispose();
     }
+  }
+  
+  protected int computeAverage(int position) {
+    int sum = 0;
+    for(int i = -5 ; i < 6 ; i++) {
+      int pos = position + i;
+      if (pos < 0)
+        pos += 2000;
+      if(pos >= 2000)
+        pos -= 2000;
+      sum += values[pos];
+    }
+    return (sum / 11);
+    
   }
   
   
