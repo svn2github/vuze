912b5713948b3c1650650f4c75c23fea6b9eab8f
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-01-08 06:35:26 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@1850 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
@@ -114,7 +114,7 @@ public class GlobalManagerImpl
             DownloadManager manager = (DownloadManager) managers.get(i);
             
             //temp debug check to see if DiskManager's write thread is dying
-            if (loopFactor % 180 == 0) {
+            if (loopFactor % 300 == 0) {
               DiskManager dm = manager.getDiskManager();
               if (dm != null && dm.getState() != DiskManager.INITIALIZING) {
                 if (!dm.isWriteThreadRunning()) {
