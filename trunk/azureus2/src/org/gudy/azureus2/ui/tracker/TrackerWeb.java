0f56af931135454a2f06d6ab9e70652135775da3
iamchrist <iamchrist@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-01-08 06:10:56 +0000

    Pagenation Complete and Runtime Configurable!! :D:D  w00t!  first feature done!!  :D:D
    git-svn-id: http://svn.vuze.com/public/client@1849 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/tracker/TrackerWeb.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/tracker/TrackerWeb.java
@@ -36,6 +36,10 @@ import org.gudy.azureus2.plugins.torrent.*;
 import org.gudy.azureus2.plugins.tracker.*;
 import org.gudy.azureus2.plugins.tracker.web.*;
 
+//Parg, if this is a nono, find me another way.
+import org.gudy.azureus2.core3.config.*;
+//Parg.  see below for more.
+
 import HTML.Template;
 
 public abstract class 
@@ -145,8 +149,8 @@ TrackerWeb
 		int tracker_page = -1;
 		int tracker_last_page = 1;
 		//Todo: get tracker config tab options to set//enable this.
-		int tracker_skip = 0;
-		//int tracker_skip = COConfigurationManager.getIntParameter("Tracker Skip", 0);; // 0 = disabled, range: >0
+		//int tracker_skip = 0;
+		int tracker_skip = COConfigurationManager.getIntParameter("Tracker Skip", 0); // 0 = disabled, range: >0
 		
 		// parse get parms.
 		if ( params != null ){
