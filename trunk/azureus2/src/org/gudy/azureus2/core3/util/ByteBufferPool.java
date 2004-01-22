e36c1fca450462f57acd0f2ce64a753c668c37e1
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-01-22 07:54:34 +0000

    Added automatic buffer pool compaction.
    git-svn-id: http://svn.vuze.com/public/client@1953 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/ByteBufferPool.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/ByteBufferPool.java
@@ -34,11 +34,14 @@ public class ByteBufferPool {
   
   private final Map buffersMap;
   
-  //private int maxFree = 0;
-  //private long bytesAllocated = 0;
-  //private long bytesSent = 0;
-  //private long bytesReturned = 0;
+  private Object poolsLock;
   
+  private Timer compactionTimer;
+  private static final long COMPACTION_CHECK_PERIOD = 10*60*1000; //10 min
+  
+  private static final long MAX_FREE_BYTES = 10*1024*1024; //10 MB
+  
+ 
   
   private ByteBufferPool() {
     buffersMap = new LinkedHashMap(END_POWER - START_POWER + 1);
@@ -49,6 +52,20 @@ public class ByteBufferPool {
         ArrayList bufferPool = new ArrayList();
         buffersMap.put(size, bufferPool);
     }
+    
+    poolsLock = new Object();
+    
+    //initiate periodic timer to check free memory usage
+    compactionTimer = new Timer("BufferPool Checker", 1);
+    compactionTimer.addPeriodicEvent(
+        COMPACTION_CHECK_PERIOD,
+        new TimerEventPerformer() {
+          public void perform( TimerEvent ev ) {
+            checkMemoryUsage();
+          }
+        }
+     );
+
   }
 
   
@@ -58,7 +75,6 @@ public class ByteBufferPool {
   private ByteBuffer allocateNewBuffer(int size) {
     try {
       ByteBuffer buffer = ByteBuffer.allocateDirect(size);
-      //bytesAllocated += size;
       return buffer;
     }
     catch (OutOfMemoryError e) {
@@ -111,13 +127,14 @@ public class ByteBufferPool {
    * create a new one if the pool is empty.
    */
   private ByteBuffer getBuffer(int length) {
-    //System.out.print("requested = " + length);
-    
     Integer reqVal = new Integer(length);
-
-    //loop through the buffer pools to find a buffer big enough
-    Iterator it = buffersMap.keySet().iterator();
-    while (it.hasNext()) {
+    
+    //make sure we don't remove a buffer when running compaction
+    synchronized( poolsLock ) {
+      
+      //loop through the buffer pools to find a buffer big enough
+      Iterator it = buffersMap.keySet().iterator();
+      while (it.hasNext()) {
         Integer keyVal = (Integer)it.next();
 
         //check if the buffers in this pool are big enough
@@ -136,10 +153,10 @@ public class ByteBufferPool {
                 }
             }
             buff.clear();   //scrub the buffer
-            //System.out.println(" , given = " + buff.capacity());
-            //bytesSent += buff.capacity();
             return buff;
         }
+      }
+      
     }
     
     //we should never get here
@@ -165,21 +182,12 @@ public class ByteBufferPool {
   private void free(ByteBuffer buffer) {
     Integer buffSize = new Integer(buffer.capacity());
     
-    //bytesReturned += buffer.capacity();
-    
     ArrayList bufferPool = (ArrayList)buffersMap.get(buffSize);
     
     synchronized (bufferPool) {
     	if (bufferPool != null) {
+        //no need to sync around 'poolsLock', as adding during compaction is ok
     		bufferPool.add(buffer);
-        
-        //System.out.print("returned buffer of " +buffer.capacity());
-        //System.out.print(" , " +bufferPool.size()+ " free buffers in pool");
-        //if (bufferPool.size() > maxFree) maxFree = bufferPool.size();
-        //System.out.println(" , max free buffers ever = " +maxFree);
-        
-        //System.out.println("bytes sent = "+bytesSent+", returned = "+bytesReturned+", allocated = "+bytesAllocated);
-        
     	}
     	else {
         Debug.out("Invalid buffer given; could not find proper buffer pool");
@@ -208,6 +216,102 @@ public class ByteBufferPool {
     System.runFinalization();
     System.gc();
   }
+  
+  
+  /**
+   * Checks memory usage of free buffers in buffer pools,
+   * and calls the compaction method if necessary.
+   */
+  private void checkMemoryUsage() {
+    long bytesUsed = 0;
+    
+    synchronized( poolsLock ) {
+      
+      //count up total bytes used by free buffers
+      Iterator it = buffersMap.keySet().iterator();
+      while (it.hasNext()) {
+        Integer keyVal = (Integer)it.next();
+        ArrayList bufferPool = (ArrayList)buffersMap.get(keyVal);
+      
+        bytesUsed += keyVal.intValue() * bufferPool.size();
+      }
+      
+      //compact buffer pools if they use too much memory
+      if (bytesUsed > MAX_FREE_BYTES) {
+        compactFreeBuffers(bytesUsed);
+      }
+      
+    }
+  }
+  
+  
+  /**
+   * Fairly removes free buffers from the pools to limit memory usage.
+   */
+  private void compactFreeBuffers(long bytes_used) {
+    int numPools = buffersMap.size();
+    long bytesToFree = 0;
+    int maxPoolSize = 0;
+    
+    int[] buffSizes = new int[numPools];
+    int[] poolSizes = new int[numPools];
+    int[] numToFree = new int[numPools];
 
+    
+    //fill size arrays
+    int pos = 0;
+    Iterator it = buffersMap.keySet().iterator();
+    while (it.hasNext()) {
+      Integer keyVal = (Integer)it.next();
+      ArrayList bufferPool = (ArrayList)buffersMap.get(keyVal);
+      
+      buffSizes[pos] = keyVal.intValue();
+      poolSizes[pos] = bufferPool.size();
+      numToFree[pos] = 0;
+      
+      //find initial max value
+      if (poolSizes[pos] > maxPoolSize) maxPoolSize = poolSizes[pos];
+      
+      pos++;
+    }
+    
+    //calculate the number of buffers to free from each pool
+    while( bytesToFree < (bytes_used - MAX_FREE_BYTES) ) {
+      for (int i=0; i < numPools; i++) {
+        //if the pool size is as large as the current max size
+        if (poolSizes[i] == maxPoolSize) {
+          //update counts
+          numToFree[i]++;
+          poolSizes[i]--;
+          bytesToFree += buffSizes[i];
+        }
+      }
+      //reduce max size for next round
+      maxPoolSize--;
+    }
+    
+    //free buffers from the pools
+    pos = 0;
+    it = buffersMap.values().iterator();
+    while (it.hasNext()) {
+      //for each pool
+      ArrayList bufferPool = (ArrayList)it.next();
+      synchronized( bufferPool ) {
+        int size = bufferPool.size();
+        //remove the buffers from the end
+        for (int i=(size - 1); i >= (size - numToFree[pos]); i--) {
+          bufferPool.remove(i);
+        }
+      }
+      
+      pos++;
+    }
+    
+    //force garbage collection if we've free'd more than the max
+    //i.e. we had used more than 2X the max
+    if (bytesToFree > MAX_FREE_BYTES) {
+      runGarbageCollection();
+    }
+  }
 
 }
