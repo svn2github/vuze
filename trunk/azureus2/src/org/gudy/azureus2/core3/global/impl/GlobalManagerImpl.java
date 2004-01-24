5e08ac38fb18e3e6ea99a5fea75f48428daed968
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-01-24 22:36:48 +0000

    Scrape interval to 15min until the code can be redone.
    git-svn-id: http://svn.vuze.com/public/client@1963 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
@@ -161,8 +161,8 @@ public class GlobalManagerImpl
 	        loopFactor++;
 	        determineSaveResumeDataInterval();
 	
-	        //update tracker scrape every 10min
-	        if (loopFactor >= 600) {
+	        //update tracker scrape every 15min - temporary, until scrape code gets reworked
+	        if (loopFactor >= 900) {
 	          loopFactor = 0;
 	          trackerScraper.update();
 	        }
@@ -1065,8 +1065,8 @@ public class GlobalManagerImpl
   	synchronized( checker ){
 	  	if ( !checker.isAlive()){
 	  		
-	  		checker.start();
-	  	}
-  	}
+    checker.start(); 
+  }
+}
   }
 }
