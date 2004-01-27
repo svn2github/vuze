ba7aba0ede098bdc331d72a8ab983f7c3f8559fc
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-01-27 21:30:01 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@1978 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/webplugin/WebPlugin.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/webplugin/WebPlugin.java
@@ -55,7 +55,9 @@ WebPlugin
 	protected LoggerChannel			log;
 	protected Tracker				tracker;
 	
+	protected String				home_page;
 	protected String				file_root;
+	protected String				resource_root;
 	
 	public void 
 	initialize(
@@ -71,7 +73,37 @@ WebPlugin
 	
 		Properties	props = plugin_interface.getPluginProperties();
 		
-		String	home_page = (String)props.get("homepage");
+		home_page = (String)props.get("homepage");
+		
+		if ( home_page != null ){
+			
+			home_page = home_page.trim();
+			
+			if ( home_page.length() == 0 ){
+				
+				home_page = null;
+				
+			}else if (!home_page.startsWith("/" )){
+			
+				home_page = "/" + home_page;
+			}
+		}
+		
+		resource_root = (String)props.get("rootresource");
+		
+		if ( resource_root != null ){
+			
+			resource_root = resource_root.trim();
+			
+			if ( resource_root.length() == 0 ){
+				
+				resource_root = null;
+				
+			}else if ( resource_root.startsWith("/" )){
+			
+				resource_root = resource_root.substring(1);
+			}
+		}
 		
 		String	root_dir	= (String)props.get("rootdir");
 		
@@ -164,44 +196,69 @@ WebPlugin
 		
 		if (url.equals("/")){
 			
-			for (int i=0;i<welcome_files.length;i++){
+			if (home_page != null ){
 				
-				if ( welcome_files[i].exists()){
+				url = home_page;
+				
+			}else{
+			
+				for (int i=0;i<welcome_files.length;i++){
 					
-					url = "/" + welcome_pages[i];
-					
-					break;
-				}
-			}			
+					if ( welcome_files[i].exists()){
+						
+						url = "/" + welcome_pages[i];
+						
+						break;
+					}
+				}	
+			}
+		}
+	
+			// first try file system for data
+		
+		if ( response.useFile( file_root, url )){
+			
+			return( true );
 		}
 		
-		if ( url.endsWith(".class")){
+				// now try jars		
 			
-			String	class_name = url;
+		String	resource_name = url;
+		
+		if (resource_name.startsWith("/")){
 			
-			if (class_name.startsWith("/")){
-				
-				class_name = class_name.substring(1);
-			}
-						
-			InputStream is = WebPlugin.class.getClassLoader().getResourceAsStream( class_name );
+			resource_name = resource_name.substring(1);
+		}
 			
-			System.out.println( class_name + "->" + is );
+		if ( resource_root != null && !resource_name.startsWith( resource_root )){
 			
+			resource_name = resource_root + "/" + resource_name;
+		}
+		
+		int	pos = resource_name.lastIndexOf(".");
+		
+		if ( pos != -1 ){
+			
+			String	type = resource_name.substring( pos+1 );
+		
+			InputStream is = WebPlugin.class.getClassLoader().getResourceAsStream( resource_name );
+		
+			System.out.println( resource_name + "->" + is );
+		
 			if (is != null ){
-				
+			
 				try{
-					response.useStream( "class", is );
-					
+					response.useStream( type, is );
+				
 				}finally{
-					
+				
 					is.close();
 				}
-				
+			
 				return( true );
 			}
 		}
 		
-		return( response.useFile( file_root, url ));
+		return( false );
 	}
 }
