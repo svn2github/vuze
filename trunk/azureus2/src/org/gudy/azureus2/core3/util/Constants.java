2cbb9dd7cadd32f0c274a17c7d9cae70912ab1dd
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-02-02 08:22:55 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@2050 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
@@ -22,6 +22,6 @@ public class Constants {
   public static final String INFINITY_STRING	= "\u221E"; // "oo";
   
   public static final String AZUREUS_NAME	    = "Azureus";
-  public static final String AZUREUS_VERSION  = "2.0.7.0";
-  public static final byte[] VERSION_ID       = ("-" + "AZ" + "2070" + "-").getBytes();  //MUST be 8 chars long!
+  public static final String AZUREUS_VERSION  = "2.0.7.1_CVS";
+  public static final byte[] VERSION_ID       = ("-" + "AZ" + "2071" + "-").getBytes();  //MUST be 8 chars long!
 }
