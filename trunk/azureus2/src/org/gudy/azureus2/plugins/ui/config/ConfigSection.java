1a5838e0f4b97915f5f7102494fba5afef3cc0cf
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-02-03 11:18:07 +0000

    Initial commit
    git-svn-id: http://svn.vuze.com/public/client@2060 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/ui/config/ConfigSection.java
@@ -0,0 +1,79 @@
+/*
+ * File    : ConfigSection.java
+ * Created : 23 jan. 2004
+ * By      : Paper
+ *
+ * Azureus - a Java Bittorrent client
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.gudy.azureus2.plugins.ui.config;
+
+import org.eclipse.swt.widgets.Composite;
+
+/**
+ * Extend this class to add a new configuration panel to the Config view.
+ */
+public interface ConfigSection {
+  /**
+   * Configuration panel will be added to main configuration view area
+   */
+  public static final String SECTION_ROOT = "ConfigView.section.root";
+  /**
+   * Configuration panel will be added to the plugins view area.
+   */
+  public static final String SECTION_PLUGINS = "ConfigView.section.plugins";
+
+  /**
+   * Returns section you want your configuration panel to be.
+   * See SECTION_* constants.
+   */
+  public String configSectionGetParentSection();
+
+
+  /**
+   * Create your own configuration panel here.  It can be anything that inherits
+   * from SWT's Composite class.
+   * Please be mindfull of small screen resolutions
+   *
+   * @param parent The parent of your configuration panel
+   * @return your configuration panel
+   */
+  public Composite configSectionCreate(Composite parent);
+
+  /**
+   * @return The name of the configuration section
+   */
+  public String configSectionGetName();
+
+  /**
+   * @return A string to identify your section.  You can use this ID to add
+   *         subsections to your section.
+   */
+  public String configSectionGetID();
+
+  /**
+   * User selected Save.
+   * All saving of non-plugin tabs have been completed, as well as
+   * saving of plugins that implement org.gudy.azureus2.plugins.ui.config
+   * parameters.
+   */
+  public void configSectionSave();
+
+  /**
+   * Config view is closing
+   */
+  public void configSectionDelete();
+}
