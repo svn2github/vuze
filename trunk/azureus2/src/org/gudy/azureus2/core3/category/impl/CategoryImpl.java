5399f1122aa79b446e03d0579d6037a6c0fe2c82
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-02-11 11:24:56 +0000

    Category classes
    git-svn-id: http://svn.vuze.com/public/client@2144 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/core3/category/impl/CategoryImpl.java
@@ -0,0 +1,123 @@
+/*
+ * File    : CategoryImpl.java
+ * Created : 09 feb. 2004
+ * By      : TuxPaper
+ *
+ * Azureus - a Java Bittorrent client
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.gudy.azureus2.core3.category.impl;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.gudy.azureus2.core3.category.Category;
+import org.gudy.azureus2.core3.category.CategoryListener;
+import org.gudy.azureus2.core3.download.*;
+import org.gudy.azureus2.core3.util.ListenerManager;
+import org.gudy.azureus2.core3.util.ListenerManagerDispatcher;
+import org.gudy.azureus2.core3.logging.*;
+
+public class CategoryImpl implements Category, Comparable {
+  private String sName;
+  private int type;
+	private List managers = new ArrayList();
+
+  private static final int LDT_CATEGORY_DMADDED     = 1;
+  private static final int LDT_CATEGORY_DMREMOVED   = 2;
+	private ListenerManager	category_listeners = ListenerManager.createManager(
+		"CatListenDispatcher",
+		new ListenerManagerDispatcher()
+		{
+			public void
+			dispatch(Object		_listener,
+               int			type,
+               Object		value )
+			{
+				CategoryListener target = (CategoryListener)_listener;
+		
+				if ( type == LDT_CATEGORY_DMADDED )
+					target.downloadManagerAdded((Category) CategoryImpl.this, (DownloadManager)value);
+				else if ( type == LDT_CATEGORY_DMREMOVED )
+					target.downloadManagerRemoved(CategoryImpl.this, (DownloadManager)value);
+			}
+		});
+
+  public CategoryImpl(String sName) {
+    this.sName = sName;
+    this.type = Category.TYPE_USER;
+  }
+
+  public CategoryImpl(String sName, int type) {
+    this.sName = sName;
+    this.type = type;
+  }
+
+	public void addCategoryListener(CategoryListener l) {
+		category_listeners.addListener( l );
+	}
+	
+	public void removeCategoryListener(CategoryListener l) {
+		category_listeners.removeListener( l );
+	}
+
+  public String getName() {
+    return sName;
+  }
+  
+  public int getType() {
+    return type;
+  }
+  
+  public List getDownloadManagers() {
+    return managers;
+  }
+  
+  public void addManager(DownloadManager manager) {
+    if (manager.getCategory() != this) {
+      manager.setCategory(this);
+      // we will be called again by CategoryManager.categoryChange
+      return;
+    }
+    if (!managers.contains(manager)) {
+      managers.add(manager);
+      category_listeners.dispatch(LDT_CATEGORY_DMADDED, manager);
+    }
+  }
+
+  public void removeManager(DownloadManager manager) {
+    if (manager.getCategory() == this) {
+      manager.setCategory(null);
+      // we will be called again by CategoryManager.categoryChange
+      return;
+    }
+    if (managers.contains(manager)) {
+      managers.remove(manager);
+      category_listeners.dispatch( LDT_CATEGORY_DMREMOVED, manager );
+    }
+  }
+
+  public int compareTo(Object b)
+  {
+    boolean aTypeIsUser = type == Category.TYPE_USER;
+    boolean bTypeIsUser = ((Category)b).getType() == Category.TYPE_USER;
+    if (aTypeIsUser == bTypeIsUser)
+      return sName.compareToIgnoreCase(((Category)b).getName());
+    if (aTypeIsUser)
+      return 1;
+    return -1;
+  }
+}
---
+++ trunk/azureus2/src/org/gudy/azureus2/core3/category/impl/CategoryManagerImpl.java
@@ -0,0 +1,254 @@
+/*
+ * File    : CategoryManagerImpl.java
+ * Created : 09 feb. 2004
+ * By      : TuxPaper
+ *
+ * Azureus - a Java Bittorrent client
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.gudy.azureus2.core3.category.impl;
+
+import java.util.Map;
+import java.util.Iterator;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.HashMap;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.gudy.azureus2.core3.category.impl.*;
+import org.gudy.azureus2.core3.category.*;
+import org.gudy.azureus2.core3.util.*;
+import org.gudy.azureus2.core3.download.*;
+import org.gudy.azureus2.core3.util.ListenerManager;
+import org.gudy.azureus2.core3.util.ListenerManagerDispatcher;
+import org.gudy.azureus2.core3.internat.MessageText;
+
+public class CategoryManagerImpl  {
+  private static CategoryManagerImpl catMan;
+  private static Category catAll = null;
+  private static Category catUncategorized = null;
+  private static boolean doneLoading = false;
+
+  private HashMap categories = new HashMap();
+
+  private static final int LDT_CATEGORY_ADDED     = 1;
+  private static final int LDT_CATEGORY_REMOVED   = 2;
+  private ListenerManager category_listeners = ListenerManager.createManager(
+    "CatListenDispatcher",
+    new ListenerManagerDispatcher()
+    {
+      public void
+      dispatch(Object   _listener,
+               int      type,
+               Object   value )
+      {
+        CategoryManagerListener target = (CategoryManagerListener)_listener;
+
+        if ( type == LDT_CATEGORY_ADDED )
+          target.categoryAdded((Category)value);
+        else if ( type == LDT_CATEGORY_REMOVED )
+          target.categoryRemoved((Category)value);
+      }
+    });
+
+
+  public void addCategoryManagerListener(CategoryManagerListener l) {
+    category_listeners.addListener( l );
+  }
+
+  public void removeCategoryManagerListener(CategoryManagerListener l) {
+    category_listeners.removeListener( l );
+  }
+
+  public synchronized static CategoryManagerImpl getInstance() {
+    if (catMan == null)
+      catMan = new CategoryManagerImpl();
+    return catMan;
+  }
+
+  public void loadCategories() {
+    if (doneLoading)
+      return;
+    doneLoading = true;
+
+    FileInputStream fin = null;
+    BufferedInputStream bin = null;
+    Map map = null;
+    ArrayList catNames = new ArrayList();
+    try {
+      //open the file
+      File configFile = FileUtil.getApplicationFile("categories.config");
+      fin = new FileInputStream(configFile);
+      bin = new BufferedInputStream(fin);
+      map = BDecoder.decode(bin);
+
+      List catList = (List) map.get("categories");
+      for (int i = 0; i < catList.size(); i++) {
+        Map mCategory = (Map) catList.get(i);
+        try {
+          String catName = new String((byte[]) mCategory.get("name"), Constants.DEFAULT_ENCODING);
+          catNames.add(catName);
+        }
+        catch (UnsupportedEncodingException e1) {
+          //Do nothing and process next.
+        }
+      }
+    }
+    catch (FileNotFoundException e) {
+      //Do nothing
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    finally {
+      try {
+        if (bin != null)
+          bin.close();
+      }
+      catch (Exception e) {}
+      try {
+        if (fin != null)
+          fin.close();
+      }
+      catch (Exception e) {}
+    }
+
+    if (map != null) {
+      synchronized (categories) {
+        makeSpecialCategories();
+
+        for (int i = 0; i < catNames.size(); i++) {
+          String name = (String)catNames.get(i);
+          Category cat = new CategoryImpl(name);
+          categories.put(name, cat);
+        } // for catNames
+      }
+    }
+  }
+
+  public void saveCategories() {
+    synchronized (categories) {
+      Map map = new HashMap();
+      List list = new ArrayList(categories.size());
+
+      Iterator iter = categories.values().iterator();
+      while (iter.hasNext()) {
+        Category cat = (Category) iter.next();
+
+        // For now we are only putting in 1 thing.  Maybe more later, so we use a map
+        if (cat.getType() == Category.TYPE_USER) {
+          Map catMap = new HashMap();
+          catMap.put("name", cat.getName());
+          list.add(catMap);
+        }
+      }
+      map.put("categories", list);
+
+
+      FileOutputStream fos = null;
+
+      try {
+        //encode the data
+        byte[] torrentData = BEncoder.encode(map);
+
+         File oldFile = FileUtil.getApplicationFile("categories.config");
+         File newFile = FileUtil.getApplicationFile("categories.config.new");
+
+         //write the data out
+        fos = new FileOutputStream(newFile);
+         fos.getChannel().force(true);
+        fos.write(torrentData);
+         fos.flush();
+
+          //close the output stream
+         fos.close();
+         fos = null;
+
+         //delete the old file
+         if ( !oldFile.exists() || oldFile.delete() ) {
+            //rename the new one
+            newFile.renameTo(oldFile);
+         }
+
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+      finally {
+        try {
+          if (fos != null)
+            fos.close();
+        }
+        catch (Exception e) {}
+      }
+    }
+  }
+
+  public Category createCategory(String name) {
+    makeSpecialCategories();
+    Category newCategory = new CategoryImpl(name);
+    categories.put(name, newCategory);
+    saveCategories();
+
+    category_listeners.dispatch( LDT_CATEGORY_ADDED, newCategory );
+    return (Category)categories.get(name);
+  }
+
+  public void removeCategory(Category category) {
+    if (categories.containsKey(category.getName())) {
+      categories.remove(category.getName());
+      category_listeners.dispatch( LDT_CATEGORY_REMOVED, category );
+    }
+  }
+
+  public Category[] getCategories() {
+    if (categories.size() > 0)
+      return (Category[])categories.values().toArray(new Category[categories.size()]);
+    return (new Category[0]);
+  }
+
+  public Category getCategory(String name) {
+    return (Category)categories.get(name);
+  }
+
+  public Category getCategory(int type) {
+    if (type == Category.TYPE_ALL)
+      return catAll;
+    if (type == Category.TYPE_UNCATEGORIZED)
+      return catUncategorized;
+    return null;
+  }
+
+  private void makeSpecialCategories() {
+    if (catAll == null) {
+      catAll = new CategoryImpl(MessageText.getString("Categories.all"), Category.TYPE_ALL);
+      categories.put(MessageText.getString("Categories.all"), catAll);
+    }
+    
+    if (catUncategorized == null) {
+      catUncategorized = new CategoryImpl(MessageText.getString("Categories.uncategorized"), Category.TYPE_UNCATEGORIZED);
+      categories.put(MessageText.getString("Categories.uncategorized"), catUncategorized);
+    }
+  }
+}
