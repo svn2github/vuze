517129a4d5d923dceb95d48e0c6f968bd5aa6bf9
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-02-15 10:41:40 +0000

    Proxy support.
    git-svn-id: http://svn.vuze.com/public/client@2190 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/ConfigView.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/ConfigView.java
@@ -949,11 +949,97 @@ public class ConfigView extends AbstractIView {
     gridData.horizontalSpan = 3;
     new StringParameter(gServer, "Bind IP", "").setLayoutData(gridData); //$NON-NLS-1$ //$NON-NLS-2$
 
+    new Label(gServer, SWT.NULL);
+    new Label(gServer, SWT.NULL);
+    new Label(gServer, SWT.NULL);
+    new Label(gServer, SWT.NULL);
+    
     label = new Label(gServer, SWT.NULL);
     Messages.setLanguageText(label, "ConfigView.label.serverport"); //$NON-NLS-1$
     gridData = new GridData();
     gridData.widthHint = 40;
     new IntParameter(gServer, "TCP.Listen.Port", 6881).setLayoutData(gridData); //$NON-NLS-1$
+    
+    
+    
+    // Sub-Section: Server -> Proxy
+    // ----------------------------
+    Composite cProxy = new Composite(cConfigSection, SWT.NULL);
+
+    TreeItem treeProxy = new TreeItem(treeServer, SWT.NULL);
+    Messages.setLanguageText(treeProxy, "ConfigView.section.proxy");
+    treeProxy.setData("Panel", cProxy);
+
+    gridData = new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL);
+    cProxy.setLayoutData(gridData);
+    layout = new GridLayout();
+    layout.numColumns = 2;
+    cProxy.setLayout(layout);
+    
+    new Label(cProxy, SWT.NULL);
+    
+    label = new Label(cProxy, SWT.NULL);
+    Messages.setLanguageText(label, "ConfigView.section.proxy.description");
+    
+    new Label(cProxy, SWT.NULL);
+    new Label(cProxy, SWT.NULL);
+    
+    BooleanParameter enableProxy = new BooleanParameter(cProxy, "Enable.Proxy", false);
+    label = new Label(cProxy, SWT.NULL);
+    Messages.setLanguageText(label, "ConfigView.section.proxy.enable_proxy");
+    
+    BooleanParameter enableSocks = new BooleanParameter(cProxy, "Enable.SOCKS", true);
+    Label lSocks = new Label(cProxy, SWT.NULL);
+    Messages.setLanguageText(lSocks, "ConfigView.section.proxy.enable_socks");
+    
+    new Label(cProxy, SWT.NULL);
+    new Label(cProxy, SWT.NULL);
+    
+    Label lHost = new Label(cProxy, SWT.NULL);
+    Messages.setLanguageText(lHost, "ConfigView.section.proxy.host");
+    gridData = new GridData();
+    gridData.widthHint = 150;
+    StringParameter pHost = new StringParameter(cProxy, "Proxy.Host", "");
+    pHost.setLayoutData(gridData);
+
+    Label lPort = new Label(cProxy, SWT.NULL);
+    Messages.setLanguageText(lPort, "ConfigView.section.proxy.port");
+    gridData = new GridData();
+    gridData.widthHint = 30;
+    StringParameter pPort = new StringParameter(cProxy, "Proxy.Port", "");
+    pPort.setLayoutData(gridData);
+    
+    new Label(cProxy, SWT.NULL);
+    new Label(cProxy, SWT.NULL);
+    
+    Label lUser = new Label(cProxy, SWT.NULL);
+    Messages.setLanguageText(lUser, "ConfigView.section.proxy.username");
+    gridData = new GridData();
+    gridData.widthHint = 100;
+    StringParameter pUser = new StringParameter(cProxy, "Proxy.Username", "");
+    pUser.setLayoutData(gridData);
+
+    Label lPass = new Label(cProxy, SWT.NULL);
+    Messages.setLanguageText(lPass, "ConfigView.section.proxy.password");
+    gridData = new GridData();
+    gridData.widthHint = 100;
+    StringParameter pPass = new StringParameter(cProxy, "Proxy.Password", "");
+    pPass.setLayoutData(gridData);
+    
+    Control[] controls = new Control[10];
+    controls[0] = lSocks;
+    controls[1] = enableSocks.getControl();
+    controls[2] = lHost;
+    controls[3] = pHost.getControl();
+    controls[4] = lPort;
+    controls[5] = pPort.getControl();
+    controls[6] = lUser;
+    controls[7] = pUser.getControl();
+    controls[8] = lPass;
+    controls[9] = pPass.getControl();
+    IAdditionalActionPerformer proxyButton = new ChangeSelectionActionPerformer(controls);
+    enableProxy.setAdditionalActionPerformer(proxyButton);
+
   }
 
   private void initGroupFile() {
