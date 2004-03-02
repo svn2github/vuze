08ff5f096992646fde0166ec3e198bb15aa06fb8
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-03-02 12:34:46 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@2360 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/upnp/Tests.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/upnp/Tests.java
@@ -20,9 +20,12 @@
  */
 package org.gudy.azureus2.core3.upnp;
 
+import org.cybergarage.upnp.Action;
 import org.cybergarage.upnp.ControlPoint;
 import org.cybergarage.upnp.Device;
 import org.cybergarage.upnp.DeviceList;
+import org.cybergarage.upnp.Service;
+import org.cybergarage.upnp.ServiceList;
 
 /**
  * @author Olivier
@@ -39,7 +42,28 @@ public class Tests {
     for(int i =0 ; i < nRootDevs ; i++) {
      Device dev = rootDevList.getDevice(i);
      String devType = dev.getDeviceType();
-     System.out.println("Found device of type : " + devType);
+     ServiceList services = dev.getServiceList();
+     for(int j=0 ; j < services.size() ; j++) {
+      Service service = services.getService(j);
+      String serviceType = service.getServiceType();
+      System.out.println("Found service : "+ serviceType + " on device " + dev);
+      if(serviceType.equals("WANIPConnection:1") || serviceType.equals("WANPPPConnection:1")) {
+        System.out.println("Found a WANIP or WANPPP Connection");
+        Action action = service.getAction("AddPortMapping");
+        if(action != null) {
+         System.out.println("Service supports the AddPortMapping action");
+         action.setArgumentValue("NewRemoteHost","0");
+         action.setArgumentValue("NewExternalPort","7001");
+         action.setArgumentValue("NewProtocol","TCP");
+         action.setArgumentValue("NewInternalPort","7001");
+         action.setArgumentValue("NewInternalClient","0");
+         action.setArgumentValue("NewEnabled","1");
+         action.setArgumentValue("NewPortMappingDescription","Azureus Port Mapping");
+         action.setArgumentValue("NewLeaseDuration","0");
+         System.out.println(action.postControlAction());
+        }
+      }
+     }
     }
     
     ctrlPoint.stop();
