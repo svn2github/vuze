7180c69359b5258d2fdf1ed1739612f0aa08da2f
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-03-13 20:59:15 +0000

    New Sha-1 . Now the transform method is really, really optimized :)
    git-svn-id: http://svn.vuze.com/public/client@2603 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/test/Sha1AlgorithmHelper.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/test/Sha1AlgorithmHelper.java
@@ -27,9 +27,74 @@ package org.gudy.azureus2.core3.util.test;
 public class Sha1AlgorithmHelper {
   
   public static void main(String args[]) {
-    algorithm2();
+    algorithm2NoShift();
+  }    
+  
+  //SHA1 Algorithm v2 using only 16 ints and changing variable meaning
+  //Over a period of 5 iterations.
+  
+  // A SHA-1 Basic operation can be discribed as:
+  // temp = F(a,b,c,d,e);
+  // e = d ; d = c ; c = G(b) ; b = a ; a = temp;
+  // The Goal of changing the variable meaning is to remove the allocations
+  // made.
+  // In order to do, so let's replace the role of :
+  //
+  // e by d,
+  // d by c,
+  // c by b
+  // b by a
+  // a by e
+  //
+  // If we rewrite the 1st equation, we can see that :
+  // e = F(a,b,c,d,e); and b = G(b);
+  // However, next line will be :
+  // d = F(b,c,d,e,a); and a = G(a);
+  // Next will be :
+  // c = F(c,d,e,a,b); and e = G(e);
+  // b = F(d,e,a,b,c); and d = G(d);
+  // a = F(e,a,b,c,d); and c = G(c);
+  // And we loop to first 'kind' where :
+  // e = F(a,b,c,d,e), and b = G(b);
+  
+  // On 80th iteration (that is for t=79), we've looped 80 = 5 * 16,
+  // so we're back to the classic representation of variables a,b,c,d,e
+  // and we can directly use them to increment the h0,h1,h2,h3,h4 variables.
+  
+  public static void algorithm2NoShift() {
+    String variables = "abcde";
+    int mask = 0x0000000F;
+    for(int t = 0 ; t <= 79 ; t++) {
+      String a = "" + variables.charAt((85-t)%5);
+      String b = "" + variables.charAt((85-t+1)%5);
+      String c = "" + variables.charAt((85-t+2)%5);
+      String d = "" + variables.charAt((85-t+3)%5);
+      String e = "" + variables.charAt((85-t+4)%5);
+      
+      int s = t & mask;
+      if(t >= 16) {
+        System.out.println("w" + s + " = w" + ((s+13) & mask) + " ^ w" + ((s+8) & mask) + " ^ w" + ((s+2) & mask) + " ^ w" + s + "; w" + s + " = (w" + s + " << 1) | (w" + s + " >>> 31) ;");        
+      }
+      System.out.print(e + " += ((" + a + " << 5) | ( " + a + " >>> 27)) + w" + s + " + ");
+      int ft = t / 20;
+      if(ft == 0) {
+        System.out.println("((" + b +" & " + c + ") | ((~" + b +" ) & " + d + ")) + 0x5A827999 ;");
+      }
+      if(ft == 1) {
+        System.out.println("(" + b +" ^ " + c + " ^ " + d + ") + 0x6ED9EBA1 ;");
+      }
+      if(ft == 2) {
+        System.out.println("((" + b +" & " + c + ") | (" + b + " & " + d + ") | (" + c + " & " + d + ")) + 0x8F1BBCDC ;");
+      }
+      if(ft == 3) {
+        System.out.println("(" + b + " ^ " + c + " ^ " + d + ") + 0xCA62C1D6 ;");
+      }
+      System.out.println(b + " = (" + b + " << 30) | (" + b + " >>> 2) ;");
+    }
   }
   
+  
+  //SHA1 Algorithm v2 using only 16 ints (+ a,b,c,d,e and temp)
   public static void algorithm2() {
     int mask = 0x0000000F;
   	for(int t = 0 ; t <= 79 ; t++) {
