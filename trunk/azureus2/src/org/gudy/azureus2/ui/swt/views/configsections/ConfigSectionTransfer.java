a93135c84d63b5640a83d174df77752d6f540d44
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-03-18 21:48:32 +0000

    Changed a little bit the layout in the view
    git-svn-id: http://svn.vuze.com/public/client@2720 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionTransfer.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionTransfer.java
@@ -23,8 +23,10 @@
 package org.gudy.azureus2.ui.swt.views.configsections;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
@@ -50,53 +52,93 @@ public class ConfigSectionTransfer implements ConfigSectionSWT {
   
 
   public Composite configSectionCreate(final Composite parent) {
-    GridData gridData;
-    GridLayout layout;
+    FormData formData;
+    FormLayout layout;
     Label label;
 
     Composite cTransfer = new Composite(parent, SWT.NULL);
 
-    gridData = new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL);
+    GridData gridData = new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL);
     cTransfer.setLayoutData(gridData);
-    layout = new GridLayout();
-    layout.numColumns = 2;
+    layout = new FormLayout();   
+    layout.spacing = 5;
     cTransfer.setLayout(layout);
 
-
-    gridData = new GridData();
-    gridData.widthHint = 30;
-    IntParameter paramMaxUploadSpeed = new IntParameter(cTransfer, "Max Upload Speed KBs", 5, -1, true);
-    paramMaxUploadSpeed.setLayoutData(gridData);
+    
+    IntParameter paramMaxUploadSpeed = new IntParameter(cTransfer, "Max Upload Speed KBs", 5, -1, true);    
+    formData = new FormData();
+    formData.top = new FormAttachment(0);
+    formData.left = new FormAttachment(0);
+    formData.right = new FormAttachment(0,30);
+    paramMaxUploadSpeed.setLayoutData(formData);
+    
     label = new Label(cTransfer, SWT.NULL);
     Messages.setLanguageText(label, "ConfigView.label.maxuploadspeed");
+    formData = new FormData();
+    formData.top = new FormAttachment(0,5);
+    formData.left = new FormAttachment(paramMaxUploadSpeed.getControl());
+    formData.right = new FormAttachment(100);
+    label.setLayoutData(formData);
+    
+    
 
-    gridData = new GridData();
-    gridData.widthHint = 30;
-    new IntParameter(cTransfer, "Max Clients", 100).setLayoutData(gridData);
+    IntParameter paramMaxClients = new IntParameter(cTransfer, "Max Clients", 100);
+    formData = new FormData();
+    formData.top = new FormAttachment(paramMaxUploadSpeed.getControl());
+    formData.left = new FormAttachment(0);
+    formData.right = new FormAttachment(0,30);
+    paramMaxClients.setLayoutData(formData);
+    
     label = new Label(cTransfer, SWT.NULL);
     Messages.setLanguageText(label, "ConfigView.label.maxclients");
-
-    gridData = new GridData();
-    gridData.widthHint = 30;
+    formData = new FormData();
+    formData.top = new FormAttachment(paramMaxUploadSpeed.getControl(),5);
+    formData.left = new FormAttachment(paramMaxClients.getControl());
+    formData.right = new FormAttachment(100);
+    label.setLayoutData(formData);
+    
+    
     IntParameter paramMaxUploads = new IntParameter(cTransfer, "Max Uploads", 2, -1, false); 
-    paramMaxUploads.setLayoutData(gridData);
+    formData = new FormData();
+    formData.top = new FormAttachment(paramMaxClients.getControl());
+    formData.left = new FormAttachment(0);
+    formData.right = new FormAttachment(0,30);
+    paramMaxUploads.setLayoutData(formData);
+
     label = new Label(cTransfer, SWT.NULL);
     Messages.setLanguageText(label, "ConfigView.label.maxuploads");
+    formData = new FormData();
+    formData.top = new FormAttachment(paramMaxClients.getControl(),5);
+    formData.left = new FormAttachment(paramMaxUploads.getControl());
+    formData.right = new FormAttachment(100);
+    label.setLayoutData(formData);
+           
     
+    BooleanParameter slowConnect = new BooleanParameter(cTransfer, "Slow Connect", false, "ConfigView.label.slowconnect");
+    formData = new FormData();
+    formData.top = new FormAttachment(paramMaxUploads.getControl());
+    slowConnect.setLayoutData(formData);    
     
-    Composite cTransfer2 = new Composite(cTransfer, SWT.NULL);
-    layout = new GridLayout();
-    layout.numColumns = 1;
-    cTransfer2.setLayout(layout);
-    cTransfer2.setLayoutData(new GridData());
+    BooleanParameter oldPolling = new BooleanParameter(cTransfer, "Old.Socket.Polling.Style", false, "ConfigView.label.oldpollingstyle");
+    formData = new FormData();
+    formData.top = new FormAttachment(slowConnect.getControl());
+    oldPolling.setLayoutData(formData);
     
-    new BooleanParameter(cTransfer2, "Slow Connect", false, "ConfigView.label.slowconnect");
-    new BooleanParameter(cTransfer2, "Old.Socket.Polling.Style", false, "ConfigView.label.oldpollingstyle");
-    new BooleanParameter(cTransfer2, "Allow Same IP Peers", false, "ConfigView.label.allowsameip");
-    new BooleanParameter(cTransfer2, "Prioritize First Piece", false, "ConfigView.label.prioritizefirstpiece");
+    BooleanParameter allowSameIP = new BooleanParameter(cTransfer, "Allow Same IP Peers", false, "ConfigView.label.allowsameip");
+    formData = new FormData();
+    formData.top = new FormAttachment(oldPolling.getControl());
+    allowSameIP.setLayoutData(formData);
+    
+    BooleanParameter firstPiece = new BooleanParameter(cTransfer, "Prioritize First Piece", false, "ConfigView.label.prioritizefirstpiece");
+    formData = new FormData();
+    formData.top = new FormAttachment(allowSameIP.getControl());
+    firstPiece.setLayoutData(formData);
     
     if(!System.getProperty("os.name").equals("Mac OS X")) {
-      new BooleanParameter(cTransfer2, "Play Download Finished", false, "ConfigView.label.playdownloadfinished");
+      BooleanParameter playSound = new BooleanParameter(cTransfer, "Play Download Finished", false, "ConfigView.label.playdownloadfinished");
+      formData = new FormData();
+      formData.top = new FormAttachment(firstPiece.getControl());
+      playSound.setLayoutData(formData);
     }
     
     return cTransfer;
