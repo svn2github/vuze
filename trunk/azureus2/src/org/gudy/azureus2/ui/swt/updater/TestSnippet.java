f1d1e1bd1625c3a1fa02aa9a1133738377f73432
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-04-04 12:08:40 +0000

    tests with runtime.exec
    git-svn-id: http://svn.vuze.com/public/client@2892 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/updater/TestSnippet.java
@@ -0,0 +1,51 @@
+/*
+ * File    : Test.java
+ * Created : 4 avr. 2004
+ * By      : Olivier
+ * 
+ * Azureus - a Java Bittorrent client
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.gudy.azureus2.ui.swt.updater;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+/**
+ * @author Olivier Chalouhi
+ *
+ */
+public class TestSnippet {
+  
+  public static void main(String args[]) throws Exception{
+    String[] exec =  {"java","-version"};
+    
+    System.out.println("About to exec : " + exec );
+    
+    Process p = Runtime.getRuntime().exec(exec);
+    BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+    
+    int exitCode = p.waitFor();
+    
+    String line = null;
+    while((line = br.readLine()) != null ) {
+      System.out.println(line);
+    }
+
+    System.out.println("Exited with code : " + exitCode);
+  }
+  
+}
