40c9f872528851bc80302884c61a9a1433a6da9d
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-04-13 11:30:08 +0000

    Rename FileImporter -> TorrentFolderWatcher.
    git-svn-id: http://svn.vuze.com/public/client@3006 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/disk/TorrentFolderWatcher.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/disk/TorrentFolderWatcher.java
@@ -1,15 +1,14 @@
 /*
- * Created on 14.12.2003
+ * Created on Dec 14, 2003
+ * Modified Apr 13, 2004 by Alon Rohter
+ * Copyright (C) 2004 Aelitis, All Rights Reserved.
  * 
- * To change the template for this generated file go to Window - Preferences -
- * Java - Code Generation - Code and Comments
  */
+ 
 package org.gudy.azureus2.core3.disk;
 
 import java.io.File;
 import java.io.FilenameFilter;
-import java.util.Collections;
-import java.util.List;
 
 import org.gudy.azureus2.core3.config.COConfigurationManager;
 import org.gudy.azureus2.core3.config.ParameterListener;
@@ -21,8 +20,8 @@ import org.gudy.azureus2.ui.swt.MainWindow;
  * 
  * @author Rene Leonhardt
  */
-public class FileImporter {
-  private static FileImporter instance = new FileImporter();
+public class TorrentFolderWatcher {
+  private static TorrentFolderWatcher instance = new TorrentFolderWatcher();
   private static FolderWatcher folderWatcher = instance.new FolderWatcher();
   private static MainWindow mainWindow = MainWindow.getWindow();
 
@@ -36,7 +35,6 @@ public class FileImporter {
     private boolean finished = true;
     private int waitTime = 60000;
     private boolean startWatchedTorrentsStopped = true;
-    private List lastFileList = Collections.EMPTY_LIST;
     private File watchFolder = null;
     private String watchFolderString = null;
     private FilenameFilter filterTorrents;
@@ -56,7 +54,6 @@ public class FileImporter {
       if(folderToWatchPath != null) {
         File folderToWatch = new File(folderToWatchPath);
         if (folderToWatch.isDirectory() && !folderToWatch.equals(watchFolder)) {
-          lastFileList = Collections.EMPTY_LIST;
           watchFolder = folderToWatch;
           watchFolderString = watchFolder.getAbsolutePath() + File.separator;
           return true;
