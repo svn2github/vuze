0590a1a171e60b663657445fec8fde4f7c3fbad6
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-04-14 00:26:01 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@3014 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/test/SystemClockSpeedup.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/test/SystemClockSpeedup.java
@@ -1,29 +1,12 @@
 /*
- * File    : SystemClockSpeedup.java
- * Created : 10 avr. 2004
- * By      : Olivier
+ * Created on Apr 11, 2004
+ * Created by Alon Rohter
+ * Copyright (C) 2004 Aelitis, All Rights Reserved.
  * 
- * Azureus - a Java Bittorrent client
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details ( see the LICENSE file ).
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package org.gudy.azureus2.core3.util.test;
 
-/**
- * @author Olivier Chalouhi
- *
- */
+
 public class SystemClockSpeedup {
 	public static void main(String[] args) {
 		for (int i=0; i < 20; i++) {
