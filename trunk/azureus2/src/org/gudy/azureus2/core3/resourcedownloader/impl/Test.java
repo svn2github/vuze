10f81f8d4feb2c5b5a7b8286d94afaae85f3ec52
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-04-25 18:00:44 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@3126 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/core3/resourcedownloader/impl/Test.java
@@ -0,0 +1,96 @@
+/*
+ * Created on 25-Apr-2004
+ * Created by Paul Gardner
+ * Copyright (C) 2004 Aelitis, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * AELITIS, SARL au capital de 30,000 euros
+ * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
+ *
+ */
+
+package org.gudy.azureus2.core3.resourcedownloader.impl;
+
+/**
+ * @author parg
+ *
+ */
+
+import java.io.*;
+
+import org.gudy.azureus2.core3.resourcedownloader.*;
+
+public class 
+Test
+	implements ResourceDownloaderListener
+{
+	protected
+	Test()
+	{
+		ResourceDownloader rd = ResourceDownloaderFactory.create( "http://localhost:6969/");
+		
+		rd = ResourceDownloaderFactory.getRetryDownloader( rd, 5 );
+		
+		rd.addListener( this );
+		
+		try{
+			rd.asyncDownload();
+			
+		}catch( Throwable e ){
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public void
+	reportPercentComplete(
+		ResourceDownloader	downloader,
+		int					percentage )
+	{
+		System.out.println( "percent = " + percentage );
+	}
+	
+	public void
+	reportActivity(
+		ResourceDownloader	downloader,
+		String				activity )
+	{
+		System.out.println( "activity = " + activity );
+	}
+	
+	public void
+	completed(
+		ResourceDownloader	downloader,
+		InputStream			data )
+	{
+		System.out.println( "Completed" );
+	}
+	
+	public void
+	failed(
+		ResourceDownloader			downloader,
+		ResourceDownloaderException e )
+	{
+		System.out.println( "Failed");
+		
+		e.printStackTrace();
+	}
+	
+	public static void
+	main(
+		String[]	args )
+	{
+		new Test();
+	}
+}
