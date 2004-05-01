f294cafc96505aca6ea05e3a73b81c25b3388518
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-05-01 20:15:04 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@3204 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/webplugin/remoteui/servlet/RemoteUIServlet.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/webplugin/remoteui/servlet/RemoteUIServlet.java
@@ -121,6 +121,7 @@ RemoteUIServlet
 		"ui/webplugin/remoteui/applet/view/VWConfigView$3.class",
 		"ui/webplugin/remoteui/applet/view/VWConfigView$4.class",
 		"ui/webplugin/remoteui/applet/view/VWConfigView$5.class",
+		"ui/webplugin/remoteui/applet/view/VWConfigView$6.class",
 		"ui/webplugin/remoteui/applet/view/VWConfigView$intValueAdapter.class",
 
 		"ui/webplugin/util/WUJarReader.class",
