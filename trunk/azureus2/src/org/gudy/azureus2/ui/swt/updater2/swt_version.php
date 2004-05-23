83a59f6a9f07ca1579debcf62993b4e4b28ea86d
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-05-23 17:37:46 +0000

    Correct for win32, hopefully for linux too, correct for OS X, but the updater doesn't support (yet) mirrors, and first one is a LAN mirror ;)
    git-svn-id: http://svn.vuze.com/public/client@3458 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/updater2/swt_version.php
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/updater2/swt_version.php
@@ -10,11 +10,11 @@
 	echo($latestVersion . "\n");
 
 	if($platform == "win32") { ?>
-http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M8-win32.zip
+http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M9-win32.zip
 <?php } else if($platform == "motif") { ?>
-http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M8-linux-motif.zip
+http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M9-linux-motif.zip
 <?php } else if($platform == "gtk") { ?>
-http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M8-linux-gtk.zip
+http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M9-linux-gtk.zip
 <?php } else if($platform == "carbon") { ?>
 http://192.168.0.101/project1/swt-3.0M9-macosx-carbon.zip
 http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M9-macosx-carbon.zip
