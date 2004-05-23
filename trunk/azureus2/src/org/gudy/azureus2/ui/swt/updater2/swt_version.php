9cc92811b9dee3495532793fb58f61585ab4782e
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-05-23 17:27:32 +0000

    SWT3.0M9 correct version number, and hopefully tiscali's mirror for GTK,Motif,win32 and carbon (OS X)
    git-svn-id: http://svn.vuze.com/public/client@3455 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/updater2/swt_version.php
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/updater2/swt_version.php
@@ -1,5 +1,5 @@
 <?php
-	$latestVersion = 3068;
+	$latestVersion = 3052;
 
 	if(! isset($platform) || $platform == '')
 		$platform 	= @$_GET['platform'];
@@ -10,11 +10,12 @@
 	echo($latestVersion . "\n");
 
 	if($platform == "win32") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0M8-200403261517/swt-3.0M8-win32.zip
+http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M8-win32.zip
 <?php } else if($platform == "motif") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0M8-200403261517/swt-3.0M8-linux-motif.zip
+http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M8-linux-motif.zip
 <?php } else if($platform == "gtk") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0M8-200403261517/swt-3.0M8-linux-gtk.zip
+http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M8-linux-gtk.zip
 <?php } else if($platform == "carbon") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0M8-200403261517/swt-3.0M8-macosx-carbon.zip
+http://192.168.0.101/project1/swt-3.0M9-macosx-carbon.zip
+http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M9-macosx-carbon.zip
 <?php } ?>
