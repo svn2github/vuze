d89958d3f42bdeb2f3ab9fe806fe2f85000feba9
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-05-24 05:45:59 +0000

    Graphic object - Base object for a Graphic displayable in Azureus GraphicSWT object - Extension for SWT (Future GraphicURI - Extension for a physical image file accessible by URI)
    git-svn-id: http://svn.vuze.com/public/client@3469 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/ui/Graphic.java
@@ -0,0 +1,29 @@
+/*
+ * Created on 2004/May/23
+ * Created by TuxPaper
+ * 
+ * Copyright (C) 2004 Aelitis SARL, All rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ * AELITIS, SARL au capital de 30,000 euros,
+ * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
+ */
+
+package org.gudy.azureus2.plugins.ui;
+
+/** An image to be used in Azureus
+ */
+public interface Graphic {
+}
