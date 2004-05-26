17b809801c32e1636ea2fd3faf37f9c1938869ee
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-05-26 05:39:57 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@3547 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/mytorrents/OnlyCDing4Item.java
@@ -0,0 +1,56 @@
+/*
+ * File    : SecondsSeedingItem.java
+ * Created : 01 feb. 2004
+ * By      : TuxPaper
+ *
+ * Copyright (C) 2004 Aelitis SARL, All rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * AELITIS, SARL au capital de 30,000 euros,
+ * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
+ */
+ 
+package org.gudy.azureus2.ui.swt.views.tableitems.mytorrents;
+
+import org.gudy.azureus2.core3.util.TimeFormater;
+import org.gudy.azureus2.core3.download.DownloadManager;
+import org.gudy.azureus2.plugins.ui.tables.*;
+import org.gudy.azureus2.ui.swt.views.table.utils.CoreTableColumn;
+
+/**
+ *
+ * @author TuxPaper
+ * @since 2.0.8.5
+ */
+public class OnlyCDing4Item
+       extends CoreTableColumn 
+       implements TableCellRefreshListener
+{
+  /** Default Constructor */
+  public OnlyCDing4Item(String sTableID) {
+    super("OnlyCDing4", ALIGN_TRAIL, POSITION_INVISIBLE, 70, sTableID);
+    setRefreshInterval(INTERVAL_LIVE);
+  }
+
+  public void refresh(TableCell cell) {
+    DownloadManager dm = (DownloadManager)cell.getDataSource();
+    long value = (dm == null) ? 0 : dm.getStats().getSecondsOnlySeeding();
+
+    if (!cell.setSortValue(value) && cell.isValid())
+      return;
+
+    cell.setText(TimeFormater.format(value));
+  }
+}
