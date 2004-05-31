97a89885fc072d031f4239bea0dad159c272f9d4
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-05-31 06:26:11 +0000

    .torrent open defaults to config dir.
    git-svn-id: http://svn.vuze.com/public/client@3686 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/TorrentOpener.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/TorrentOpener.java
@@ -330,6 +330,7 @@ public class TorrentOpener {
 
   public static void openTorrent() {
     FileDialog fDialog = new FileDialog(mainWindow, SWT.OPEN | SWT.MULTI);
+    fDialog.setFilterPath(COConfigurationManager.getStringParameter("Default Path"));
     fDialog.setFilterExtensions(new String[] { "*.torrent", "*.tor" }); //$NON-NLS-1$
     fDialog.setFilterNames(new String[] { "*.torrent", "*.tor" }); //$NON-NLS-1$
     fDialog.setText(MessageText.getString("MainWindow.dialog.choose.file")); //$NON-NLS-1$
@@ -342,6 +343,7 @@ public class TorrentOpener {
 
   public static void openTorrentNoDefaultSave(boolean forSeeding) {
     FileDialog fDialog = new FileDialog(mainWindow, SWT.OPEN | SWT.MULTI);
+    fDialog.setFilterPath(COConfigurationManager.getStringParameter("Default Path"));
     fDialog.setFilterExtensions(new String[] { "*.torrent", "*.tor" }); //$NON-NLS-1$
     fDialog.setFilterNames(new String[] { "*.torrent", "*.tor" }); //$NON-NLS-1$
     fDialog.setText(MessageText.getString("MainWindow.dialog.choose.file")); //$NON-NLS-1$
