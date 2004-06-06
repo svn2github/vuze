d5ed824cea8f8353fe5e8a05c01f2183c5ff3f13
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-06-06 06:28:57 +0000

    New Colon formatting: [xx d ]hh:nn:ss
    git-svn-id: http://svn.vuze.com/public/client@3757 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/TimeFormater.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/TimeFormater.java
@@ -27,6 +27,27 @@ public class TimeFormater {
       if ((hours == 0 && days == 0) && ((secs > 0 || mins > 0) && secs < 10)) result += "0" + secs + "s ";
       if ((hours == 0 && days == 0) && ((secs > 0 || mins > 0) && secs >= 10)) result += secs + "s ";
       
+      return result.length() == 0 ? "" : result.substring(0, result.length() - 1);
+    }
+
+    public static String formatColon(long time)
+    {
+      if (time >= Constants.INFINITY_AS_INT) return Constants.INFINITY_STRING;
+      if (time < 0) return "";
+
+      int secs = (int) time % 60;
+      int mins = (int) (time / 60) % 60;
+      int hours = (int) (time /3600) % 24;
+      int days = (int) (time / 86400);
+      
+      String result = "";
+      if (days > 0) result = days + "d ";
+      result += twoDigits(hours) + ":" + twoDigits(mins) + ":" + twoDigits(secs);
+
       return result;
+    }
+    
+    private static String twoDigits(int i) {
+      return (i < 10) ? "0" + i : String.valueOf(i);
     }
 }
