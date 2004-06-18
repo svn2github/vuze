1ed5c6fcc551327f1f7bc7771843f18f1fb99f32
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-06-18 10:04:41 +0000

    use SWT 3.0RC2 for CVS users (correct)
    git-svn-id: http://svn.vuze.com/public/client@3896 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/updater2/swt_version.php
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/updater2/swt_version.php
@@ -3,26 +3,32 @@
 
 	if(! isset($platform) || $platform == '')
 		$platform 	= @$_GET['platform'];
-	
+		
 	if(! isset($platform) || $platform == '')
 		exit();
 		
 	if(! isset($version) || $version == '')
 		$version 	= @$_GET['version'];	
+		
+	if(! isset($version))
+		$version 	= '';	
+	
+	if(substr($version,0, 7) == "2.1.0.1") {
+	
+	echo("3058\n");
+	
+	if($platform == "win32") { ?>
+http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/eclipse-SDK-3.0RC2-win32.zip
+<?php } else if($platform == "motif") { ?>
+http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/eclipse-SDK-3.0RC2-linux-motif.zip
+<?php } else if($platform == "gtk") { ?>
+http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/eclipse-SDK-3.0RC2-linux-gtk.zip
+<?php } else if($platform == "carbon") { ?>
+http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/swt-3.0RC2-macosx-carbon.zip
+<?php		}
+	} else {
 	
 	echo($latestVersion . "\n");
-	
-	if(substr($version, 7) == "2.1.0.1") {
-	if($platform == "win32") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/download.php?dropFile=swt-3.0RC2-win32.zip
-<?php } else if($platform == "motif") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/download.php?dropFile=swt-3.0RC2-linux-motif.zip
-<?php } else if($platform == "gtk") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/download.php?dropFile=swt-3.0RC2-linux-gtk.zip
-<?php } else if($platform == "carbon") { ?>
-http://download2.eclipse.org/downloads/drops/S-3.0RC2-200406111814/download.php?dropFile=swt-3.0RC2-macosx-carbon.zip
-		}
-	} else {
 	
 	if($platform == "win32") { ?>
 http://mirror.tiscali.dk/eclipse/downloads/drops/S-3.0M9-200405211200/swt-3.0M9-win32.zip
