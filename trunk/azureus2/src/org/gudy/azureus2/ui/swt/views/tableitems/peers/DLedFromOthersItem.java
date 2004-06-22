1f1dcd6f5ccd341c168b447f51e706605fb1fdc4
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-06-22 01:51:20 +0000

    Peer View columns: - Bytes the peer has downloaded from others - bytes Up:bytes Down ratio - (bytes you sent them):(bytes they got elsewhere) ratio
    git-svn-id: http://svn.vuze.com/public/client@3941 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/peers/DLedFromOthersItem.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2004 Aelitis SARL, All rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details ( see the LICENSE file ).
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * AELITIS, SARL au capital de 30,000 euros,
+ * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
+ */
+ 
+package org.gudy.azureus2.ui.swt.views.tableitems.peers;
+
+import org.gudy.azureus2.core3.util.DisplayFormatters;
+
+import org.gudy.azureus2.core3.peer.PEPeer;
+import org.gudy.azureus2.plugins.ui.tables.*;
+import org.gudy.azureus2.ui.swt.views.table.utils.CoreTableColumn;
+
+/** Downloaded from others WHILE connected to you
+ *
+ * @author TuxPaper
+ * @since 2.1.0.1
+ */
+public class DLedFromOthersItem
+       extends CoreTableColumn 
+       implements TableCellRefreshListener
+{
+  /** Default Constructor */
+  public DLedFromOthersItem() {
+    super("DLedFromOthers", ALIGN_TRAIL, POSITION_INVISIBLE, 70, TableManager.TABLE_TORRENT_PEERS);
+    setRefreshInterval(INTERVAL_LIVE);
+  }
+
+  public void refresh(TableCell cell) {
+    PEPeer peer = (PEPeer)cell.getDataSource();
+    long value = (peer == null) ? 0 : peer.getStats().getBytesDone() - peer.getStats().getTotalSent();
+    // Just because we sent data doesn't mean the peer has told us the piece is done yet
+    if (value < 0) value = 0;
+
+    if (!cell.setSortValue(value) && cell.isValid())
+      return;
+
+    cell.setText(DisplayFormatters.formatByteCountToKiBEtc(value));
+  }
+}
