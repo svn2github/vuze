22aa89f85a0b4cf60a1c64acbd76d2a5c0179acb
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-09-16 14:19:30 +0000

    support for entering referrer URL when downloading from URL (to fix SN probs for example)
    git-svn-id: http://svn.vuze.com/public/client@4432 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/OpenUrlWindow.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/OpenUrlWindow.java
@@ -34,14 +34,24 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
 import com.aelitis.azureus.core.*;
+import org.gudy.azureus2.core3.config.*;
 import org.gudy.azureus2.core3.internat.MessageText;
 
 /**
  * @author Olivier
  * 
  */
-public class OpenUrlWindow {
-
+public class 
+OpenUrlWindow 
+{
+  protected static String	CONFIG_REFERRER_DEFAULT = "openUrl.referrer.default";
+	
+  protected static String	last_referrer = null;
+  
+  static{
+  	last_referrer = COConfigurationManager.getStringParameter( CONFIG_REFERRER_DEFAULT, "" );
+  }
+  
   public 
   OpenUrlWindow(
   	final AzureusCore	azureus_core,
@@ -55,8 +65,11 @@ public class OpenUrlWindow {
     
     GridData gridData;
     GridLayout layout = new GridLayout();
-    layout.numColumns = 2;
-    shell.setLayout(layout);        
+    layout.numColumns = 3;
+    shell.setLayout(layout);
+    
+    	// URL field
+    
     Label label = new Label(shell, SWT.NULL);
     label.setText(MessageText.getString("openUrl.url"));
     gridData = new GridData();
@@ -65,7 +78,8 @@ public class OpenUrlWindow {
     final Text url = new Text(shell, SWT.BORDER);
 
     gridData = new GridData();//GridData.FILL_HORIZONTAL
-    gridData.widthHint=300;
+    gridData.widthHint=400;
+    gridData.horizontalSpan	= 2;
     url.setLayoutData(gridData);
     if(linkURL == null)
       Utils.setTextLinkFromClipboard(shell, gridData, url);
@@ -73,30 +87,76 @@ public class OpenUrlWindow {
       Utils.setTextLink(shell, gridData, url, linkURL);
     url.setSelection(url.getText().length());
     
+    	// referrer field
+    
+    Label referrer_label = new Label(shell, SWT.NULL);
+    referrer_label.setText(MessageText.getString("openUrl.referrer"));
+    gridData = new GridData();
+    referrer_label.setLayoutData(gridData);
+    
+    final Text referrer_text = new Text(shell, SWT.BORDER);
+
+    gridData = new GridData(GridData.FILL_HORIZONTAL);
+    gridData.widthHint=150;
+	gridData.grabExcessHorizontalSpace = true;
+    referrer_text.setLayoutData(gridData);
+    
+    if ( referrer != null && referrer.length() > 0 ){
+    	
+    	referrer_text.setText( referrer );
+    	
+    }else if ( last_referrer != null ){
+    	
+    	referrer_text.setText( last_referrer );
+    }
+    
+    Label referrer_info = new Label(shell, SWT.NULL);
+    referrer_info.setText(MessageText.getString("openUrl.referrer.info"));
+    
+	// line
+	
+	Label labelSeparator = new Label(shell,SWT.SEPARATOR | SWT.HORIZONTAL);
+	gridData = new GridData(GridData.FILL_HORIZONTAL);
+	gridData.horizontalSpan = 3;
+	labelSeparator.setLayoutData(gridData);
+
+    	// buttons
+    
     Composite panel = new Composite(shell, SWT.NULL);
     layout = new GridLayout();
     layout.numColumns = 3;
     panel.setLayout(layout);        
-    gridData = new GridData();
-    gridData.horizontalSpan = 2;
+    gridData = new GridData(GridData.FILL_HORIZONTAL | GridData.HORIZONTAL_ALIGN_END | GridData.HORIZONTAL_ALIGN_FILL);
+    gridData.horizontalSpan = 3;
+	gridData.grabExcessHorizontalSpace = true;
     panel.setLayoutData(gridData);
+ 	
+    new Label(panel, SWT.NULL);
+    
     Button ok = new Button(panel,SWT.PUSH);
-    gridData = new GridData();
-    gridData.widthHint = 100;    
-    gridData.horizontalSpan = 2;
+    gridData = new GridData(GridData.FILL_HORIZONTAL | GridData.HORIZONTAL_ALIGN_END | GridData.HORIZONTAL_ALIGN_FILL);
+    gridData.widthHint = 70;    
+	gridData.grabExcessHorizontalSpace = true;
     ok.setLayoutData(gridData);
     ok.setText(MessageText.getString("Button.ok"));
     ok.addListener(SWT.Selection,new Listener() {
-      public void handleEvent(Event e) {        
-        new FileDownloadWindow(azureus_core,display,url.getText(), referrer );
+      public void handleEvent(Event e) {     
+      	last_referrer	= referrer_text.getText().trim();
+      	
+      	COConfigurationManager.setParameter( CONFIG_REFERRER_DEFAULT, last_referrer );
+      	COConfigurationManager.save();
+      	
+        new FileDownloadWindow(azureus_core,display,url.getText(), last_referrer );
         shell.dispose();
       }
     }); 
+    
     shell.setDefaultButton (ok);
     
     Button cancel = new Button(panel,SWT.PUSH);
-    gridData = new GridData();
-    gridData.widthHint = 100;
+    gridData = new GridData(GridData.HORIZONTAL_ALIGN_END);
+	gridData.grabExcessHorizontalSpace = false;
+    gridData.widthHint = 70;
     cancel.setLayoutData(gridData);
     cancel.setText(MessageText.getString("Button.cancel"));
     cancel.addListener(SWT.Selection,new Listener() {
