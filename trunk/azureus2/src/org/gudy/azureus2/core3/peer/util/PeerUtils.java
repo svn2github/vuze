e60c5c7bbceddc2e9b7be851012b6232bae3a0be
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-09-22 20:18:01 +0000

    Remove class sync as unnecessary.
    git-svn-id: http://svn.vuze.com/public/client@4493 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/peer/util/PeerUtils.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/peer/util/PeerUtils.java
@@ -7,15 +7,12 @@
 package org.gudy.azureus2.core3.peer.util;
 
 import org.gudy.azureus2.core3.config.*;
-import org.gudy.azureus2.core3.util.AEMonitor;
 
 
 /**
  * Varies peer connection utility methods.
  */
 public class PeerUtils {
-
-   private static final AEMonitor 		class_mon	= new AEMonitor( "PeerUtils:class");
 
    private static final String	CONFIG_MAX_CONN_PER_TORRENT	= "Max.Peer.Connections.Per.Torrent";
    private static final String	CONFIG_MAX_CONN_TOTAL		= "Max.Peer.Connections.Total";
@@ -59,39 +56,32 @@ public class PeerUtils {
    * @return max number of new connections allowed, or -1 if there is no limit
    */
   public static int numNewConnectionsAllowed( byte[] data_id ) {
-  	try{
-	  	class_mon.enter();
-	  	
-	    int curConnPerTorrent = PeerIdentityManager.getIdentityCount( data_id );
-	    int curConnTotal = PeerIdentityManager.getTotalIdentityCount();
+    int curConnPerTorrent = PeerIdentityManager.getIdentityCount( data_id );
+    int curConnTotal = PeerIdentityManager.getTotalIdentityCount();
 	    
-	    int perTorrentAllowed = -1;  //default unlimited
-	    if ( maxConnPerTorrent != 0 ) {  //if limited
-	      int allowed = maxConnPerTorrent - curConnPerTorrent;
-	      if ( allowed < 0 )  allowed = 0;
-	      perTorrentAllowed = allowed;
-	    }
+    int perTorrentAllowed = -1;  //default unlimited
+    if ( maxConnPerTorrent != 0 ) {  //if limited
+      int allowed = maxConnPerTorrent - curConnPerTorrent;
+      if ( allowed < 0 )  allowed = 0;
+      perTorrentAllowed = allowed;
+    }
 	    
-	    int totalAllowed = -1;  //default unlimited
-	    if ( maxConnTotal != 0 ) {  //if limited
-	      int allowed = maxConnTotal - curConnTotal;
-	      if ( allowed < 0 )  allowed = 0;
-	      totalAllowed = allowed;
-	    }
+    int totalAllowed = -1;  //default unlimited
+    if ( maxConnTotal != 0 ) {  //if limited
+      int allowed = maxConnTotal - curConnTotal;
+      if ( allowed < 0 )  allowed = 0;
+      totalAllowed = allowed;
+    }
 	    
-	    int allowed = -1;  //default unlimited
-	    if ( perTorrentAllowed > -1 && totalAllowed > -1 ) {  //if both limited
-	      allowed = Math.min( perTorrentAllowed, totalAllowed );
-	    }
-	    else if ( perTorrentAllowed == -1 || totalAllowed == -1 ) {  //if either unlimited
-	    	allowed = Math.max( perTorrentAllowed, totalAllowed );
-	    }
+    int allowed = -1;  //default unlimited
+    if ( perTorrentAllowed > -1 && totalAllowed > -1 ) {  //if both limited
+      allowed = Math.min( perTorrentAllowed, totalAllowed );
+    }
+    else if ( perTorrentAllowed == -1 || totalAllowed == -1 ) {  //if either unlimited
+      allowed = Math.max( perTorrentAllowed, totalAllowed );
+    }
 	    
-	    return allowed;
-  	}finally{
-  		
-  		class_mon.exit();
-  	}
+    return allowed;
   }
   
 
