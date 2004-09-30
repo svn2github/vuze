3e7c1f19ea295b9e97b2b8c78d2d110b548026d4
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-09-30 07:10:04 +0000

    fix mytracker icon in the same way I fixed mytorrents
    git-svn-id: http://svn.vuze.com/public/client@4555 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/mytracker/NameItem.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/mytracker/NameItem.java
@@ -23,6 +23,7 @@ package org.gudy.azureus2.ui.swt.views.tableitems.mytracker;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.program.Program;
 
+import org.gudy.azureus2.core3.torrent.TOTorrent;
 import org.gudy.azureus2.core3.tracker.host.*;
 import org.gudy.azureus2.core3.util.TorrentUtils;
 import org.gudy.azureus2.ui.swt.ImageRepository;
@@ -49,17 +50,38 @@ public class NameItem
     String name = (item == null) ? "" 
                                  : TorrentUtils.getLocalisedName(item.getTorrent());
     //setText returns true only if the text is updated
+    //setText returns true only if the text is updated
     if (cell.setText(name)) {
-      //in which case we also update the icon
-			int sep = name.lastIndexOf('.');
-			if(sep < 0) sep = 0;
-			
-      name = name.substring(sep);
-      Program program = Program.findProgram(name);
-      Image icon = ImageRepository.getIconFromProgram(program);
-      // cheat for core, since we really know it's a TabeCellImpl and want to use
-      // those special functions not available to Plugins
-      ((TableCellCore)cell).setImage(icon);
+    	
+    	boolean	folder_icon	= false;
+    	
+     			// for non-simple torrents the target is always a directory
+    		
+    	TOTorrent	torrent = item.getTorrent();
+    		
+    	if ( torrent != null && !torrent.isSimpleTorrent()){
+    			
+    		folder_icon	= true;
+    	}
+    	   	
+    	if ( folder_icon ){
+    		
+    		Image icon = ImageRepository.getFolderImage();
+    		
+    		((TableCellCore)cell).setImage(icon);
+    		
+    	}else{
+	      int sep = name.lastIndexOf('.');
+		
+	      if(sep < 0) sep = 0;
+	
+	      name = name.substring(sep);
+	      Program program = Program.findProgram(name);
+	      Image icon = ImageRepository.getIconFromProgram(program);
+	      // cheat for core, since we really know it's a TabeCellImpl and want to use
+	      // those special functions not available to Plugins
+	      ((TableCellCore)cell).setImage(icon);
+    	}
     }
   }
 
