8acb66634a87624c35473df93779467fb0196793
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-10-10 16:30:14 +0000

    Cleanup.
    git-svn-id: http://svn.vuze.com/public/client@4674 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/networkmanager/WriteController.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/networkmanager/WriteController.java
@@ -78,15 +78,17 @@ public class WriteController {
       if( check_high_first ) {
         ready_entity = getNextReadyHighPriorityEntity();
         if( ready_entity != null ) { //ready high entity found
-          boolean success = ready_entity.doWrite();
-          if( !success ) System.out.println( "doWrite0 failed" );
+          if( !ready_entity.doWrite() ) {
+            System.out.println( "doWrite0 failed" );
+          }
           check_high_first = false; //start with normal next round
         }
         else { //none ready in high-priority, so check normal-priority
           ready_entity = getNextReadyNormalPriorityEntity();
           if( ready_entity != null ) { //ready normal entity found
-            boolean success = ready_entity.doWrite();
-            if( !success ) System.out.println( "doWrite1 failed" );
+            if( !ready_entity.doWrite() ) {
+              System.out.println( "doWrite1 failed" );
+            }
           }
           else { //none ready in normal-priority either, so sleep
             try {  Thread.sleep( 10 );   }catch(Exception e) { Debug.printStackTrace(e); }
@@ -97,14 +99,16 @@ public class WriteController {
         check_high_first = true; //start with high next round
         ready_entity = getNextReadyNormalPriorityEntity();
         if( ready_entity != null ) { //ready normal entity found
-          boolean success = ready_entity.doWrite();
-          if( !success ) System.out.println( "doWrite2 failed" );
+          if( !ready_entity.doWrite() ) {
+            System.out.println( "doWrite2 failed" );
+          }
         }
         else { //none ready in normal-priority, so check high-priority
           ready_entity = getNextReadyHighPriorityEntity();
           if( ready_entity != null ) { //ready high entity found
-            boolean success = ready_entity.doWrite();
-            if( !success ) System.out.println( "doWrite3 failed" );
+            if( !ready_entity.doWrite() ) {
+              System.out.println( "doWrite3 failed" );
+            }
             check_high_first = false;  //start with normal again next round
           }
           else { //none ready in high-priority either, so sleep
