9b81ed373a2114bd24883e270bd5da0215f8066f
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-10-19 17:12:49 +0000

    put offset values back to pre-gudy edit ones
    git-svn-id: http://svn.vuze.com/public/client@4778 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/sharing/progress/ProgressWindow.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/sharing/progress/ProgressWindow.java
@@ -196,11 +196,11 @@ ProgressWindow
 
 			
       Rectangle bounds = display.getClientArea();    
-      x0 = bounds.x + bounds.width - 250;
+      x0 = bounds.x + bounds.width - 255;
       x1 = bounds.x + bounds.width;
 
       y0 = bounds.y + bounds.height;
-      y1 = bounds.y + bounds.height - 150;
+      y1 = bounds.y + bounds.height - 155;
 					
 			shell.setLocation(x0,y0);
 		}
