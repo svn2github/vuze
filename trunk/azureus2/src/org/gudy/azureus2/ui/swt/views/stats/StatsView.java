869aa60f0a2bb3ca06ab144c3b44cb259eeeae97
gudy <gudy@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-11-16 14:24:03 +0000

    Should fix the crash on OS X when quitting if the stats view has been opened
    git-svn-id: http://svn.vuze.com/public/client@5074 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/stats/StatsView.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/stats/StatsView.java
@@ -29,6 +29,7 @@ import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.gudy.azureus2.core3.global.GlobalManager;
 import org.gudy.azureus2.core3.internat.MessageText;
+import org.gudy.azureus2.core3.util.Constants;
 import org.gudy.azureus2.core3.util.Debug;
 import org.gudy.azureus2.ui.swt.Messages;
 import org.gudy.azureus2.ui.swt.Utils;
@@ -159,6 +160,20 @@ public class StatsView extends AbstractIView {
   public void delete() {
     updateThread.stopIt();
     MainWindow.getWindow().setStats(null);    
+    
+    //Don't ask me why, but without this an exception is thrown further
+    // (in folder.dispose() )
+    //TODO : Investigate to see if it's a platform (OSX-Carbon) BUG, and report to SWT team.
+    if(Constants.isOSX) {
+      if(folder != null && !folder.isDisposed()) {
+        TabItem[] items = folder.getItems();
+        for(int i=0 ; i < items.length ; i++) {
+          if (!items[i].isDisposed())
+            items[i].dispose();
+        }
+      }
+    }
+    
     viewActivity.delete();
     viewStats.delete();
     viewCache.delete();
