9422a3f7e2a86576cc043c9c2db081bb14a6dc2e
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-12-02 09:41:46 +0000

    fixed UDP scrape, it was returning the wrong value for incomplete
    git-svn-id: http://svn.vuze.com/public/client@5202 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/tracker/client/classic/TrackerStatus.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/tracker/client/classic/TrackerStatus.java
@@ -712,8 +712,8 @@ public class TrackerStatus {
 							
 							byte[][]	hashes 		= scrape_reply.getHashes();
 							int[]		complete 	= scrape_reply.getComplete();
-							int[]		downloaded 	= scrape_reply.getComplete();
-							int[]		incomplete 	= scrape_reply.getComplete();
+							int[]		downloaded 	= scrape_reply.getDownloaded();
+							int[]		incomplete 	= scrape_reply.getIncomplete();
 							
 							Map	files = new ByteEncodedKeyHashMap();
 							
