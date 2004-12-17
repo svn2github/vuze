e15f332ad97533f8c101ccc338801d1fc442586c
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-12-17 12:13:39 +0000

    Decrease default total connection limit to 250.
    git-svn-id: http://svn.vuze.com/public/client@5317 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/config/COConfigurationManager.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/config/COConfigurationManager.java
@@ -33,7 +33,7 @@ COConfigurationManager
 	public static final int CONFIG_DEFAULT_MIN_MAX_UPLOAD_SPEED			= 5;
 	public static final int CONFIG_DEFAULT_MAX_DOWNLOAD_SPEED			= 0;
 	public static final int	CONFIG_DEFAULT_MAX_CONNECTIONS_PER_TORRENT	= 100;
-	public static final int	CONFIG_DEFAULT_MAX_CONNECTIONS_GLOBAL		= 1000;
+	public static final int	CONFIG_DEFAULT_MAX_CONNECTIONS_GLOBAL		= 250;
 	
 	public static void
 	initialise()
