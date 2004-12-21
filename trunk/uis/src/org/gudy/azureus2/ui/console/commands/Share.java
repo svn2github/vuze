6f4169c30b283ca118c1bae81334a0bbd9fe541f
fatal_2 <fatal_2@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-12-21 12:14:34 +0000

    - code reorganisation
    git-svn-id: http://svn.vuze.com/public/client@20699 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/uis/src/org/gudy/azureus2/ui/console/commands/Share.java
+++ trunk/uis/src/org/gudy/azureus2/ui/console/commands/Share.java
@@ -53,147 +53,132 @@ public class Share extends IConsoleCommand {
 		out.println( "> -----" );
 	}
 	
-	public void 
-	execute( 
-		String commandName, 
-		final ConsoleInput ci, 
-		List args ) 
+	public void execute(String commandName,	final ConsoleInput ci, List args ) 
 	{
-		if ( args != null && args.size() == 1 && ((String)args.get(0)).trim().equalsIgnoreCase("list")){
-			
-			try{
-				ShareManager 	share_manager = ci.azureus_core.getPluginManager().getDefaultPluginInterface().getShareManager();
-
-				ShareResource[]	shares = share_manager.getShares();
-				
+		if( args.isEmpty() )
+		{
+			printHelp(ci.out, args);
+			return;
+		}
+		final ShareManager share_manager;
+		try{
+			share_manager = ci.azureus_core.getPluginManager().getDefaultPluginInterface().getShareManager();
+		}catch( ShareException e ){
+			ci.out.println( "ERROR: " + e.getMessage() + " ::");
+			Debug.printStackTrace( e );
+			return;
+		}
+		
+		final String arg = (String) args.remove(0);
+		if ( args.isEmpty() && ("list".equalsIgnoreCase(arg)) ) {
+			ShareResource[]	shares = share_manager.getShares();
+			if( shares.length == 0 )
+				ci.out.println("> No shares found");
+			else
+			{
 				for (int i=0;i<shares.length;i++){
-					
-					ci.out.println( shares[i].getName());
+					ci.out.println( "> " + i + ": " + shares[i].getName());
 				}
-			}catch( Throwable e ){
-				
-				ci.out.println( "ERROR: " + e.getMessage() + " ::");
-				
-				Debug.printStackTrace( e );
-
 			}
-		}else if( args != null && args.size() >= 2 ) {
-			final String[] sub_commands = new String[ args.size() ];
-			args.toArray( sub_commands );
-			for( int i=0; i<sub_commands.length; i++ ) {
-				sub_commands[ i ] = sub_commands[ i ].trim();
-			}
+			return;
+		} 
+		
+		final File path = new File( (String) args.get(0) );
+		if( !path.exists() ) {
+			ci.out.println( "ERROR: path [" +path+ "] does not exist." );
+			return;
+		}
+		
+		String	category = null;
+		
+		if ( args.size() == 2 ){
+			String	properties = (String) args.get(1);
+			int	pos = properties.indexOf("=");
 			
-			final File path = new File( sub_commands[ 1 ] );
-			if( !path.exists() ) {
-				ci.out.println( "ERROR: path [" +path+ "] does not exist." );
+			if ( pos == -1 ){
+				ci.out.println( "ERROR: invalid properties string '" + properties + "'" );
 				return;
 			}
 			
-			String	category = null;
-			
-			if ( sub_commands.length == 3 ){
-			
-				String	properties = sub_commands[2];
-				
-				int	pos = properties.indexOf("=");
-				
-				if ( pos == -1 ){
-					ci.out.println( "ERROR: invalid properties string '" + properties + "'" );
-					return;
-				}
-				
-				category = properties.substring( pos+1 );
-			}
-			
-			try{
-				final ShareManager 	share_manager = ci.azureus_core.getPluginManager().getDefaultPluginInterface().getShareManager();
-				
-				final String		f_category	= category;
-				
-				new AEThread( "shareFile" ) 
-				{
-					public void 
-					runSupport() 
-					{
-						try{
-							ShareResource resource = share_manager.getShare( path );
-							
-							if( sub_commands[ 0 ].equalsIgnoreCase( "file" ) ) {
-		
-								ci.out.println( "File [" +path+ "] share being processed in background..." );
-		
-								if ( resource == null ){
-										
-									resource = share_manager.addFile( path );
-								}
-									
-							}else if( sub_commands[ 0 ].equalsIgnoreCase( "folder" ) ) {
-								
-								ci.out.println( "Folder [" +path+ "] share being processed in background..." );
-		
-								if ( resource == null ){
-									
-									resource = share_manager.addDir( path );	
-								}
-								
-							}else if( sub_commands[ 0 ].equalsIgnoreCase( "contents" ) ) {
-								
-								ci.out.println( "Folder contents [" +path+ "] share being processed in background..." );
-		
-								if ( resource == null ){
-									
-									resource = share_manager.addDirContents( path, false );
-								}
-									
-							}else if( sub_commands[ 0 ].equalsIgnoreCase( "rcontents" ) ) {
-								
-								ci.out.println( "Folder contents recursive [" +path+ "] share being processed in background..." );
-
-								if ( resource == null ){
-									
-									resource = share_manager.addDirContents( path, true );
-								}
-							}else{
-								
-								ci.out.println( "ERROR: type '" + sub_commands[ 0 ] + "' unknown." );
-								
-							}
-							
-							String	cat = f_category;
-							
-							if ( resource != null && cat != null ){
-								
-								if ( cat.length() == 0 ){
-									
-									cat	= null;
-								}
-								
-								TorrentManager tm = ci.azureus_core.getPluginManager().getDefaultPluginInterface().getTorrentManager();
-								
-								resource.setAttribute( tm.getAttribute( TorrentAttribute.TA_CATEGORY), cat );
-							}
-							
-							if ( resource != null ){
-								
-								ci.out.println( "... processing complete" );
-							}
-						}catch( Throwable e ) {
-							
-							ci.out.println( "ERROR: " + e.getMessage() + " ::");
-							
-							Debug.printStackTrace( e );
-						}
-					}
-				}.start();
-				
-			}catch( Throwable e ) {
-				ci.out.println( "ERROR: " + e.getMessage() + " ::");
-				Debug.printStackTrace( e );
-			}
-	
-		}else {
-			printHelp(ci.out, (String)null);
+			category = properties.substring( pos+1 );
 		}
+		
+		final String		f_category	= category;
+			
+		new AEThread( "shareFile" ) 
+		{
+			public void 
+			runSupport() 
+			{
+				try{
+					ShareResource resource = share_manager.getShare( path );
+					
+					if( "file".equalsIgnoreCase( arg ) ) {
+
+						ci.out.println( "File [" +path+ "] share being processed in background..." );
+
+						if ( resource == null ){
+								
+							resource = share_manager.addFile( path );
+						}
+							
+					}else if( "folder".equalsIgnoreCase( arg ) ) {
+						
+						ci.out.println( "Folder [" +path+ "] share being processed in background..." );
+
+						if ( resource == null ){
+							
+							resource = share_manager.addDir( path );	
+						}
+						
+					}else if( "contents".equalsIgnoreCase( arg ) ) {
+						
+						ci.out.println( "Folder contents [" +path+ "] share being processed in background..." );
+
+						if ( resource == null ){
+							
+							resource = share_manager.addDirContents( path, false );
+						}
+							
+					}else if( "rcontents".equalsIgnoreCase( arg ) ) {
+						
+						ci.out.println( "Folder contents recursive [" +path+ "] share being processed in background..." );
+
+						if ( resource == null ){
+							
+							resource = share_manager.addDirContents( path, true );
+						}
+					}else{
+						
+						ci.out.println( "ERROR: type '" + arg + "' unknown." );
+						
+					}
+					
+					String	cat = f_category;
+					
+					if ( resource != null && cat != null ){
+						
+						if ( cat.length() == 0 ){
+							
+							cat	= null;
+						}
+						
+						TorrentManager tm = ci.azureus_core.getPluginManager().getDefaultPluginInterface().getTorrentManager();
+						
+						resource.setAttribute( tm.getAttribute( TorrentAttribute.TA_CATEGORY), cat );
+					}
+					
+					if ( resource != null ){
+						
+						ci.out.println( "... processing complete" );
+					}
+				}catch( Throwable e ) {
+					
+					ci.out.println( "ERROR: " + e.getMessage() + " ::");
+					
+					Debug.printStackTrace( e );
+				}
+			}
+		}.start();
 	}
 }
