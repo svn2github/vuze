7b690b9696d5ba25fadceb5c0f4ba9915ea35fa3
fatal_2 <fatal_2@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-12-30 14:09:02 +0000

    - moved command parsing into separate method - no need for any member variables apart from the reader. thus this probably could be renamed to a CommandParser class and get rid of the Reader
    git-svn-id: http://svn.vuze.com/public/client@20700 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/uis/src/org/gudy/azureus2/ui/console/CommandReader.java
+++ trunk/uis/src/org/gudy/azureus2/ui/console/CommandReader.java
@@ -8,7 +8,7 @@ package org.gudy.azureus2.ui.console;
 
 import java.io.IOException;
 import java.io.Reader;
-
+import java.util.List;
 import java.util.Vector;
 
 /**
@@ -23,11 +23,7 @@ public class CommandReader extends Reader {
   private final int ESCAPE = 4;
   private final int NONQUOTEDESCAPE = 5;
   
-  private Reader in = null;
-  private char[] cb = null;
-  private int state = ENTER;
-  
-  public Vector commandargs =null;
+  private Reader in;
   
   /** Creates a new instance of CommandReader */
   public CommandReader(Reader _in) {
@@ -46,7 +42,6 @@ public class CommandReader extends Reader {
         in.close();
         in = null;
       }
-      cb = null;
     }
   }
   
@@ -65,124 +60,132 @@ public class CommandReader extends Reader {
   }
   
   public String readLine() throws java.io.IOException {
-    synchronized(lock) {
-      ensureOpen();
-      StringBuffer line = new StringBuffer();
-      StringBuffer current = new StringBuffer();
-      Vector args = new Vector();
-      boolean allowEmpty = false;
-      boolean bailout = false;
-      while (!bailout) {
-      	
-      	int ch = in.read();
-      	
-      	if( ch == -1 ){
-      		
-      	  	throw new IOException("stream closed");
-      	}
-      	
-      	char c = (char) ch;
-      	
-      	if (c!='\n'){
-      		
-      		line.append( c );
-      	}
-      	
-        switch (state) {
-          /*case SKIP:
-            switch (c) {
-              case ' ': case '\t':
-                break;
-              case '\"':
-                mode = QUOTE;
-                break;
-              case '&':
-                background = true;
-              case ';':
-                contLine = line.substring(pos +1);
-                pos = line.length();
-                break;
-              default:
-                mode = READ;
-                --pos;
-            }
-            break;*/
-            
-          case ENTER:
-            switch (c) {
-              case '\"':
-                state = QUOTE;
-                break;
-              /*case ' ': case '\t':
-                mode = SKIP;
-                break;*/
-              case  '\\':
-                state = NONQUOTEDESCAPE;
-                break;
-              case '\n':
-                bailout = true;
-                break;
-              case '\r':
-                break;
-              default:
-                current.append(c);
-            }
-            if ((state == ENTER) && ((c==' ') || (c=='\n'))) {
-              String arg = current.toString().trim();
-              if( arg.length() > 0 || allowEmpty )
-              {
-              	args.addElement(arg);
-              	allowEmpty = false;
-              }
-              current = new StringBuffer();
-            }
-            break;
-            
-          case QUOTE:
-            switch (c) {
-              case '\"':
-              	allowEmpty = true;
-                state = ENTER;
-                break;
-              case '\\':
-                state = ESCAPE;
-                break;
-              default:
-                current.append(c);
-            }
-            break;
-            
-          case ESCAPE:
-            switch (c) {
-              case 'n':  c = '\n';  break;
-              case 'r':  c = '\r';  break;
-              case 't':  c = '\t';  break;
-              case 'b':  c = '\b';  break;
-              case 'f':  c = '\f';  break;
-              default: current.append('\\'); break;
-            }
-            state = QUOTE;
-            current.append(c);
-            break;
-          case  NONQUOTEDESCAPE:
-            switch (c) {
-              case  ';':
-                state = ENTER;
-                current.append(c);
-                break;
-              default: // This is not a escaped char.
-                state = ENTER;
-                current.append('\\');
-                current.append(c);
-                break;
-            }
-            break;
-        }
-        
-      }
-      commandargs = args;
-      return line.toString().trim();
-    }
+  	synchronized(lock) {
+  		ensureOpen();
+  		StringBuffer line = new StringBuffer();
+  		int ch;
+  		while( (char)(ch = in.read()) != '\n' )
+  		{
+  			if( ch == -1 )
+  			{
+  				throw new IOException("stream closed");
+  			}
+  			line.append((char)ch);
+  		}
+  		return line.toString().trim();
+  	}
   }
-  
+  public List parseCommandLine( String commandLine )
+  {
+  	StringBuffer current = new StringBuffer();
+  	Vector args = new Vector();
+  	boolean allowEmpty = false;
+  	boolean bailout = false;
+  	int index = 0;
+    int state = ENTER;
+
+  	while (!bailout) {
+  		
+  		int ch = commandLine.charAt(index++);
+  		bailout = (index == commandLine.length());
+  		char c = (char) ch;
+  		
+//  		if (c!='\n'){
+//  			
+//  			line.append( c );
+//  		}
+//  		
+  		switch (state) {
+  		/*case SKIP:
+  		 switch (c) {
+  		 case ' ': case '\t':
+  		 break;
+  		 case '\"':
+  		 mode = QUOTE;
+  		 break;
+  		 case '&':
+  		 background = true;
+  		 case ';':
+  		 contLine = line.substring(pos +1);
+  		 pos = line.length();
+  		 break;
+  		 default:
+  		 mode = READ;
+  		 --pos;
+  		 }
+  		 break;*/
+  		
+  		case ENTER:
+  			switch (c) {
+  			case '\"':
+  				state = QUOTE;
+  				break;
+  				/*case ' ': case '\t':
+  				 mode = SKIP;
+  				 break;*/
+  			case  '\\':
+  				state = NONQUOTEDESCAPE;
+  				break;
+//  			case '\n':
+//  				bailout = true;
+//  				break;
+  			case '\r':
+  				break;
+  			default:
+  				current.append(c);
+  			}
+  			if ((state == ENTER) && ((c==' ') || (bailout))) {
+  				String arg = current.toString().trim();
+  				if( arg.length() > 0 || allowEmpty )
+  				{
+  					args.addElement(arg);
+  					allowEmpty = false;
+  				}
+  				current = new StringBuffer();
+  			}
+  			break;
+  			
+  		case QUOTE:
+  			switch (c) {
+  			case '\"':
+  				allowEmpty = true;
+  				state = ENTER;
+  				break;
+  			case '\\':
+  				state = ESCAPE;
+  				break;
+  			default:
+  				current.append(c);
+  			}
+  			break;
+  			
+  		case ESCAPE:
+  			switch (c) {
+  			case 'n':  c = '\n';  break;
+  			case 'r':  c = '\r';  break;
+  			case 't':  c = '\t';  break;
+  			case 'b':  c = '\b';  break;
+  			case 'f':  c = '\f';  break;
+  			default: current.append('\\'); break;
+  			}
+  			state = QUOTE;
+  			current.append(c);
+  			break;
+  		case  NONQUOTEDESCAPE:
+  			switch (c) {
+  			case  ';':
+  				state = ENTER;
+  				current.append(c);
+  				break;
+  			default: // This is not a escaped char.
+  				state = ENTER;
+  			current.append('\\');
+  			current.append(c);
+  			break;
+  			}
+  			break;
+  		}
+    }
+  	return args;
+  }
 }
