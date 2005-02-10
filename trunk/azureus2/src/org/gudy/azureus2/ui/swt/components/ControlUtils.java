ad1aa3cdbd0a3bdb77933d16e6c61ba9d2d4f7ad
crazyalchemist <crazyalchemist@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-02-10 10:38:17 +0000

    Added Licensing Header
    git-svn-id: http://svn.vuze.com/public/client@5612 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/components/ControlUtils.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/components/ControlUtils.java
@@ -1,5 +1,27 @@
 package org.gudy.azureus2.ui.swt.components;
 
+/*
+ * Created on 8-Feb-2005
+ * Created by James Yeh
+ * Copyright (C) 2004 Aelitis, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ * AELITIS, SARL au capital de 30,000 euros
+ * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
+ *
+ */
+
 import org.gudy.azureus2.core3.util.Constants;
 import org.gudy.azureus2.core3.config.COConfigurationManager;
 
