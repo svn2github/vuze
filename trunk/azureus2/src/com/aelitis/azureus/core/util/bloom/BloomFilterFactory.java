d42831ce4017a15914f5d0e394964346340f2ab7
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-04-29 14:38:52 +0000

    bit of doc
    git-svn-id: http://svn.vuze.com/public/client@6248 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/util/bloom/BloomFilterFactory.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/util/bloom/BloomFilterFactory.java
@@ -27,10 +27,22 @@ import com.aelitis.azureus.core.util.bloom.impl.BloomFilterImpl;
 public class 
 BloomFilterFactory 
 {
+		/**
+		 * Creates a new bloom filter. 
+		 * @param max_entries The filter size.
+		 * 	a size of 10 * expected entries gives a false-positive of around 0.01%
+		 *  17* -> 0.001
+		 *  29* -> 0.0001
+		 * Each entry takes 4 bits  
+		 * So, if 0.01% is acceptable and expected max entries is 100, use a filter
+		 * size of 1000.
+		 * @return
+		 */
+	
 	public static BloomFilter
 	create(
-		int		max_entries )
+		int		filter_size )
 	{
-		return( new BloomFilterImpl( max_entries ));
+		return( new BloomFilterImpl( filter_size ));
 	}
 }
