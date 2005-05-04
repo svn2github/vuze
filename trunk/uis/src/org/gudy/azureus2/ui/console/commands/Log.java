4001dea4b2cf3778358ba3acf88ed4bdeb7feba2
fatal_2 <fatal_2@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-05-04 21:51:36 +0000

    added ability to log to a file bit of a quick hack though - could use more work)
    git-svn-id: http://svn.vuze.com/public/client@20720 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/uis/src/org/gudy/azureus2/ui/console/commands/Log.java
+++ trunk/uis/src/org/gudy/azureus2/ui/console/commands/Log.java
@@ -10,32 +10,78 @@
  */
 package org.gudy.azureus2.ui.console.commands;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 import org.apache.log4j.Appender;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.apache.log4j.varia.DenyAllFilter;
 import org.gudy.azureus2.ui.console.ConsoleInput;
 
 /**
  * @author Tobias Minich
  */
-public class Log extends IConsoleCommand {
+public class Log extends OptionsConsoleCommand {
 
 	public Log()
 	{
 		super( new String[] { "log", "l" });
 	}
-	public void execute(String commandName, ConsoleInput ci, List args) {
+	
+	protected Options getOptions()
+	{
+		Options options = new Options();
+		options.addOption( new Option("f", "filename", true, "filename to write log to"));
+		return options;
+	}
+	
+	public void execute(String commandName, ConsoleInput ci, CommandLine commandLine)
+	{
 		Appender con = Logger.getRootLogger().getAppender("ConsoleAppender");
-		if ((con != null) && (args != null) && (!args.isEmpty())) {
+		List args = commandLine.getArgList();
+		if ((con != null) && (!args.isEmpty())) {
 			String subcommand = (String) args.get(0);
 			if ("off".equalsIgnoreCase(subcommand) ) {
 				con.addFilter(new DenyAllFilter());
 				ci.out.println("> Console logging off");
 			} else if ("on".equalsIgnoreCase(subcommand) ) {
+				if( commandLine.hasOption('f') )
+				{
+					// send log output to a file
+					String filename = commandLine.getOptionValue('f');
+					
+					try
+					{
+						Appender newAppender = new FileAppender(new PatternLayout("%d{ISO8601} %c{1}-%p: %m%n"), filename, true);
+						newAppender.setName("ConsoleAppender");
+						Logger.getRootLogger().removeAppender(con);
+						Logger.getRootLogger().addAppender(newAppender);
+						ci.out.println("> Logging to filename: " + filename);
+					} catch (IOException e)
+					{
+						ci.out.println("> Unable to log to file: " + filename + ": " + e);
+					}					
+				}
+				else
+				{
+					if( ! (con instanceof ConsoleAppender) )
+					{
+						Logger.getRootLogger().removeAppender(con);
+						con = new ConsoleAppender(new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN));
+						con.setName("ConsoleAppender");
+					    Logger.getRootLogger().addAppender(con);
+					}
+					// switch back to console appender
+					ci.out.println("> Console logging on");
+				}
 				con.clearFilters();
-				ci.out.println("> Console logging on");
+				
 			} else {
 				ci.out.println("> Command 'log': Subcommand '" + subcommand + "' unknown.");
 			}
@@ -50,6 +96,6 @@ public class Log extends IConsoleCommand {
 
 	public String getCommandDescriptions()
 	{
-		return("log (on|off)\t\t\tl\tTurn on/off console logging");
+		return("log [-f filename] (on|off)\t\t\tl\tTurn on/off console logging");
 	}
 }
