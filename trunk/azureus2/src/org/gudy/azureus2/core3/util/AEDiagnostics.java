fa21de5c16ad44bef12fb9b518c7b96a8226cc4d
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-05-16 17:31:25 +0000

    Re-enable DBBPool tracing.
    git-svn-id: http://svn.vuze.com/public/client@6384 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/AEDiagnostics.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/AEDiagnostics.java
@@ -47,8 +47,8 @@ AEDiagnostics
 	public static final boolean	DEBUG_MONITOR_SEM_USAGE			= false;
 	
 	public static final boolean	TRACE_DIRECT_BYTE_BUFFERS		= false;
-	public static final boolean	TRACE_DBB_POOL_USAGE			= false;
-	public static final boolean	PRINT_DBB_POOL_USAGE			= false;
+	public static final boolean	TRACE_DBB_POOL_USAGE			= true;
+	public static final boolean	PRINT_DBB_POOL_USAGE			= true;
 	
 	static{
 		if ( ALWAYS_PASS_HASH_CHECKS ){
