e74a610686f14a6718d67f369b512c5987d9bbce
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-05-29 08:48:48 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@6449 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/update/CorePatchChecker.java
+++ trunk/azureus2/src/org/gudy/azureus2/update/CorePatchChecker.java
@@ -55,7 +55,7 @@ CorePatchChecker
 		plugin_interface	= _plugin_interface;
 		
 		plugin_interface.getPluginProperties().setProperty( "plugin.version", 	"1.0" );
-		plugin_interface.getPluginProperties().setProperty( "plugin.name", 		"Core Patcher" );
+		plugin_interface.getPluginProperties().setProperty( "plugin.name", 		"Core Patcher (level=" + CorePatchLevel.getCurrentPatchLevel() + ")" );
 
 		if ( TESTING || !Constants.isCVSVersion()){
 		
