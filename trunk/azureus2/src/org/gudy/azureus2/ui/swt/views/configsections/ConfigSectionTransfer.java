796022d9049453c3105fec60ce846487d0420d6b
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-07-02 15:56:56 +0000

    Alternate upload rate when only seeding option.
    git-svn-id: http://svn.vuze.com/public/client@6622 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionTransfer.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionTransfer.java
@@ -26,10 +26,8 @@
 package org.gudy.azureus2.ui.swt.views.configsections;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.FormAttachment;
-import org.eclipse.swt.layout.FormData;
-import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
@@ -38,6 +36,8 @@ import org.gudy.azureus2.plugins.ui.config.ConfigSectionSWT;
 import org.gudy.azureus2.ui.swt.config.*;
 import org.gudy.azureus2.ui.swt.Messages;
 import org.gudy.azureus2.core3.config.COConfigurationManager;
+
+
 
 public class ConfigSectionTransfer implements ConfigSectionSWT {
   public String configSectionGetParentSection() {
@@ -56,226 +56,192 @@ public class ConfigSectionTransfer implements ConfigSectionSWT {
   
 
   public Composite configSectionCreate(final Composite parent) {
-    FormData formData;
-    FormLayout layout;
-    Label label;
+    GridData gridData;
+    GridLayout layout;
 
-    Composite cTransfer = new Composite(parent, SWT.NULL);
-
-    GridData gridData = new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL);
+    Composite cTransfer = new Composite(parent, SWT.WRAP);
+    gridData = new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.HORIZONTAL_ALIGN_FILL);
     cTransfer.setLayoutData(gridData);
-    layout = new FormLayout();   
-    try {
-      layout.spacing = 5;
-    } catch (NoSuchFieldError e) {
-      /* Ignore for Pre 3.0 SWT.. */
-    }
+    layout = new GridLayout();
+    layout.numColumns = 4;
+    layout.marginHeight = 0;
     cTransfer.setLayout(layout);
 
-    	// max upload speed
     
-    	// store the initial d/l speed so we can do something sensible later
+    //  store the initial d/l speed so we can do something sensible later
+    final int[] manual_max_download_speed = { COConfigurationManager.getIntParameter( "Max Download Speed KBs" )};
     
-    final int[]	manual_max_download_speed = { COConfigurationManager.getIntParameter( "Max Download Speed KBs" )};
     
-    final IntParameter paramMaxUploadSpeed = new IntParameter(cTransfer, "Max Upload Speed KBs", 1, -1, true, true);    
-    formData = new FormData();
-    formData.top = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.left = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.right = new FormAttachment(0,50);
-    paramMaxUploadSpeed.setLayoutData(formData);
+    //  max upload speed
+    gridData = new GridData();
+    gridData.widthHint = 35;
+    final IntParameter paramMaxUploadSpeed = new IntParameter(cTransfer, "Max Upload Speed KBs", 1, -1, true, true);
+    paramMaxUploadSpeed.setLayoutData( gridData );
     
-    label = new Label(cTransfer, SWT.NULL);
+    gridData = new GridData();
+    gridData.horizontalSpan = 3;
+    Label label = new Label(cTransfer, SWT.NULL);
+    label.setLayoutData( gridData );
     Messages.setLanguageText(label, "ConfigView.label.maxuploadspeed");
-    formData = new FormData();
-    formData.top = new FormAttachment(0,5);
-    formData.left = new FormAttachment(paramMaxUploadSpeed.getControl());
-    formData.right = new FormAttachment(100, 0);  // 2 params for Pre SWT 3.0
-    label.setLayoutData(formData);
-    
-    /*
-    // max upload speed when seeding
-    
-    final IntParameter paramMaxUploadSpeedSeeding = new IntParameter(cTransfer, "Max Upload Speed Seeding KBs", 5, -1, true);    
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxUploadSpeed.getControl());  // 2 params for Pre SWT 3.0
-    formData.left = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.right = new FormAttachment(0,50);
-    paramMaxUploadSpeedSeeding.setLayoutData(formData);
-    
+
+
+    //  max upload speed when seeding
     label = new Label(cTransfer, SWT.NULL);
-    Messages.setLanguageText(label, "ConfigView.label.maxuploadspeedseeding");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxUploadSpeed.getControl(),5);
-    formData.left = new FormAttachment(paramMaxUploadSpeedSeeding.getControl());
-    formData.right = new FormAttachment(100, 0);  // 2 params for Pre SWT 3.0
-    label.setLayoutData(formData);
-    */
+    gridData = new GridData();
+    gridData.horizontalIndent = 20;
+    BooleanParameter enable_seeding_rate = new BooleanParameter( cTransfer, "enable.seedingonly.upload.rate", false, "ConfigView.label.maxuploadspeedseeding" );
+    enable_seeding_rate.setLayoutData( gridData );
     
-    	// max download speed
-    
-    final IntParameter paramMaxDownSpeed = new IntParameter(cTransfer, "Max Download Speed KBs", 0, -1, true, true);    
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxUploadSpeed.getControl());
-    formData.left = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.right = new FormAttachment(0,50);
-    paramMaxDownSpeed.setLayoutData(formData);
-    
+    gridData = new GridData();
+    gridData.widthHint = 35;
+    IntParameter paramMaxUploadSpeedSeeding = new IntParameter(cTransfer, "Max Upload Speed Seeding KBs", 1, -1, true, false);   
+    paramMaxUploadSpeedSeeding.setLayoutData( gridData );
+    enable_seeding_rate.setAdditionalActionPerformer( new ChangeSelectionActionPerformer( paramMaxUploadSpeedSeeding.getControl() ) );
     label = new Label(cTransfer, SWT.NULL);
+
+    
+    // max download speed
+    gridData = new GridData();
+    gridData.widthHint = 35;
+    final IntParameter paramMaxDownSpeed = new IntParameter(cTransfer, "Max Download Speed KBs", 0, -1, true, true);
+    paramMaxDownSpeed.setLayoutData( gridData );
+
+    gridData = new GridData();
+    gridData.horizontalSpan = 3;
+    label = new Label(cTransfer, SWT.NULL);
+    label.setLayoutData( gridData );
     Messages.setLanguageText(label, "ConfigView.label.maxdownloadspeed");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxUploadSpeed.getControl(),5);
-    formData.left = new FormAttachment(paramMaxDownSpeed.getControl());
-    formData.right = new FormAttachment(100, 0);  // 2 params for Pre SWT 3.0
-    label.setLayoutData(formData);
     
-    	// max upload/download limit dependencies
+
     
+    // max upload/download limit dependencies
     paramMaxUploadSpeed.addChangeListener(
-    	new ParameterChangeListener()
-		{
-    		public void
-			parameterChanged(
-				Parameter	p,
-				boolean		internal )
-			{
-      			int	up_val 		= paramMaxUploadSpeed.getValue();
-   				int	down_val 	= paramMaxDownSpeed.getValue();
-    			   			
-    			if ( up_val != 0 && up_val < COConfigurationManager.CONFIG_DEFAULT_MIN_MAX_UPLOAD_SPEED ){
-    				
-    				if ( ( down_val==0 ) || down_val > (up_val*2) ){
-    					
-    					paramMaxDownSpeed.setValue( up_val*2 );
-    				}
-    			}else{
-    				
-    				if ( down_val != manual_max_download_speed[0] ){
-    					
-    					paramMaxDownSpeed.setValue( manual_max_download_speed[0] );
-    				}
-    			}
-    		}
-    	});
+        new ParameterChangeListener()
+        {
+            public void
+            parameterChanged(
+                Parameter   p,
+                boolean     internal )
+            {
+                int up_val      = paramMaxUploadSpeed.getValue();
+                int down_val    = paramMaxDownSpeed.getValue();
+                            
+                if ( up_val != 0 && up_val < COConfigurationManager.CONFIG_DEFAULT_MIN_MAX_UPLOAD_SPEED ){
+                    
+                    if ( ( down_val==0 ) || down_val > (up_val*2) ){
+                        
+                        paramMaxDownSpeed.setValue( up_val*2 );
+                    }
+                }else{
+                    
+                    if ( down_val != manual_max_download_speed[0] ){
+                        
+                        paramMaxDownSpeed.setValue( manual_max_download_speed[0] );
+                    }
+                }
+            }
+        });
     
     paramMaxDownSpeed.addChangeListener(
-    	new ParameterChangeListener()
-		{
-    		public void
-			parameterChanged(
-				Parameter	p,
-				boolean		internal )
-			{
-       			int	up_val 		= paramMaxUploadSpeed.getValue();
-   				int	down_val 	= paramMaxDownSpeed.getValue();
-   	   		
-   				if ( !internal ){
-   					
-   					manual_max_download_speed[0] = down_val;
-   				}
-   				  	   		   			     			
-    			if ( up_val < COConfigurationManager.CONFIG_DEFAULT_MIN_MAX_UPLOAD_SPEED ){
-    				
-    				if ( up_val != 0 && up_val < (down_val*2)){
-    					
-    					paramMaxUploadSpeed.setValue((down_val+1)/2 );
-    					
-    				}else if ( down_val == 0 ){
-    					
-    					paramMaxUploadSpeed.setValue( 0 );
-    				}
-    			}   			
-    		}
-    	});
+        new ParameterChangeListener()
+        {
+            public void
+            parameterChanged(
+                Parameter   p,
+                boolean     internal )
+            {
+                int up_val      = paramMaxUploadSpeed.getValue();
+                int down_val    = paramMaxDownSpeed.getValue();
+            
+                if ( !internal ){
+                    
+                    manual_max_download_speed[0] = down_val;
+                }
+                                                        
+                if ( up_val < COConfigurationManager.CONFIG_DEFAULT_MIN_MAX_UPLOAD_SPEED ){
+                    
+                    if ( up_val != 0 && up_val < (down_val*2)){
+                        
+                        paramMaxUploadSpeed.setValue((down_val+1)/2 );
+                        
+                    }else if ( down_val == 0 ){
+                        
+                        paramMaxUploadSpeed.setValue( 0 );
+                    }
+                }               
+            }
+        });
         
+
     
     	// max uploads
+    gridData = new GridData();
+    gridData.widthHint = 35;
+    IntParameter paramMaxUploads = new IntParameter(cTransfer, "Max Uploads", 2, -1, false, false);
+    paramMaxUploads.setLayoutData( gridData );
 
-    IntParameter paramMaxUploads = new IntParameter(cTransfer, "Max Uploads", 2, -1, false, false); 
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxDownSpeed.getControl());
-    formData.left = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.right = new FormAttachment(0,50);
-    paramMaxUploads.setLayoutData(formData);
-
+    gridData = new GridData();
+    gridData.horizontalSpan = 3;
     label = new Label(cTransfer, SWT.NULL);
+    label.setLayoutData( gridData );
     Messages.setLanguageText(label, "ConfigView.label.maxuploads");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxDownSpeed.getControl(),5);
-    formData.left = new FormAttachment(paramMaxUploads.getControl());
-    formData.right = new FormAttachment(100, 0);  // 2 params for Pre SWT 3.0
-    label.setLayoutData(formData);
     
-    
-    
-    
-    IntParameter paramMaxClients = new IntParameter(cTransfer, "Max.Peer.Connections.Per.Torrent");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxUploads.getControl());
-    formData.left = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.right = new FormAttachment(0,50);
-    paramMaxClients.setLayoutData(formData);
-    
-    label = new Label(cTransfer, SWT.NULL);
-    Messages.setLanguageText(label, "ConfigView.label.max_peers_per_torrent");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxUploads.getControl(),5);
-    formData.left = new FormAttachment(paramMaxClients.getControl());
-    formData.right = new FormAttachment(100, 0);  // 2 params for Pre SWT 3.0
-    label.setLayoutData(formData);
-    
-    
-    IntParameter paramMaxClientsTotal = new IntParameter(cTransfer, "Max.Peer.Connections.Total");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxClients.getControl());
-    formData.left = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.right = new FormAttachment(0,50);
-    paramMaxClientsTotal.setLayoutData(formData);
-    
-    label = new Label(cTransfer, SWT.NULL);
-    Messages.setLanguageText(label, "ConfigView.label.max_peers_total");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxClients.getControl(),5);
-    formData.left = new FormAttachment(paramMaxClientsTotal.getControl());
-    formData.right = new FormAttachment(100, 0);  // 2 params for Pre SWT 3.0
-    label.setLayoutData(formData);
 
-     
+    gridData = new GridData();
+    gridData.widthHint = 35;
+    IntParameter paramMaxClients = new IntParameter(cTransfer, "Max.Peer.Connections.Per.Torrent");
+    paramMaxClients.setLayoutData( gridData );
+    
+    gridData = new GridData();
+    gridData.horizontalSpan = 3;
+    label = new Label(cTransfer, SWT.NULL);
+    label.setLayoutData( gridData );
+    Messages.setLanguageText(label, "ConfigView.label.max_peers_per_torrent");
+    
+    
+    gridData = new GridData();
+    gridData.widthHint = 35;
+    IntParameter paramMaxClientsTotal = new IntParameter(cTransfer, "Max.Peer.Connections.Total");
+    paramMaxClientsTotal.setLayoutData( gridData );
+
+    gridData = new GridData();
+    gridData.horizontalSpan = 3;
+    label = new Label(cTransfer, SWT.NULL);
+    label.setLayoutData( gridData );
+    Messages.setLanguageText(label, "ConfigView.label.max_peers_total");
+    
+    
+    gridData = new GridData();
+    gridData.horizontalSpan = 4;
     BooleanParameter allowSameIP = new BooleanParameter(cTransfer, "Allow Same IP Peers", false, "ConfigView.label.allowsameip");
-    formData = new FormData();
-    formData.top = new FormAttachment(paramMaxClientsTotal.getControl(), 10);
-    allowSameIP.setLayoutData(formData);
+    allowSameIP.setLayoutData( gridData );
     
-    	// prioritise 1st piece
-    
+    // prioritise 1st piece
+    gridData = new GridData();
+    gridData.horizontalSpan = 4;
     BooleanParameter firstPiece = new BooleanParameter(cTransfer, "Prioritize First Piece", false, "ConfigView.label.prioritizefirstpiece");
-    formData = new FormData();
-    formData.top = new FormAttachment(allowSameIP.getControl());
-    firstPiece.setLayoutData(formData);
+    firstPiece.setLayoutData( gridData );
     
 	// prioritise most completed files
-    
+    gridData = new GridData();
+    gridData.horizontalSpan = 4;
     BooleanParameter mostCompletedFiles = new BooleanParameter(cTransfer, "Prioritize Most Completed Files", false, "ConfigView.label.prioritizemostcompletedfiles");
-    formData = new FormData();
-    formData.top = new FormAttachment(firstPiece.getControl());
-    mostCompletedFiles.setLayoutData(formData);
+    mostCompletedFiles.setLayoutData(gridData);
     
-   		// ignore ports
     
-    StringParameter ignore_ports = new StringParameter(cTransfer, "Ignore.peer.ports","0"); 
-    formData = new FormData();
-    formData.top = new FormAttachment(mostCompletedFiles.getControl());
-    formData.left = new FormAttachment(0, 0);  // 2 params for Pre SWT 3.0
-    formData.right = new FormAttachment(0,100);
-    ignore_ports.setLayoutData(formData);
-
+    // ignore ports
+    
+    gridData = new GridData();
+    gridData.horizontalSpan = 3;
     label = new Label(cTransfer, SWT.NULL);
+    label.setLayoutData( gridData );
     Messages.setLanguageText(label, "ConfigView.label.transfer.ignorepeerports");
-    formData = new FormData();
-    formData.top = new FormAttachment(mostCompletedFiles.getControl(),5);
-    formData.left = new FormAttachment(ignore_ports.getControl());
-    formData.right = new FormAttachment(100, 0);  // 2 params for Pre SWT 3.0
-    label.setLayoutData(formData);
+    
+    gridData = new GridData();
+    gridData.widthHint = 125;
+    StringParameter ignore_ports = new StringParameter(cTransfer, "Ignore.peer.ports","0"); 
+    ignore_ports.setLayoutData( gridData );
+    
 
     return cTransfer;
   }
