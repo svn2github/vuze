8bd12bef3cd5e8f68f997ad7b8f1ae6b25c6be53
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-07-23 20:01:15 +0000

    interpolation of stepped system time values + some drift protection
    git-svn-id: http://svn.vuze.com/public/client@6737 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/SystemTime.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/SystemTime.java
@@ -30,24 +30,94 @@ public class SystemTime {
   
   public static final long TIME_GRANULARITY_MILLIS = 20;   //internal update time ms
   
+  private static final int STEPS_PER_SECOND = (int)(1000/TIME_GRANULARITY_MILLIS);
+  
   private static final SystemTime instance = new SystemTime();
   
   
   private final Thread updater;
-  private volatile long stepped_time;
   
-  private SystemTime() {
+  private volatile long 	stepped_time;
+  private volatile long		last_approximate_time;
+  
+  private volatile int		access_count;
+  private volatile int 		slice_access_count;
+  private volatile int		access_average_per_slice;
+  private volatile int		drift_adjusted_granularity;
+ 
+  private 
+  SystemTime() 
+  {
     stepped_time = System.currentTimeMillis();
 
-    updater = new Thread("SystemTime") {
-      public void run() {
-        while( true ) {
-          stepped_time = System.currentTimeMillis();
+    updater = 
+    	new Thread("SystemTime") 
+    	{
+    		public void 
+    		run() 
+    		{
+    			Average access_average 	= Average.getInstance( 1000, 10 );
+    			Average drift_average 	= Average.getInstance( 1000, 10 );
+
+    			long	last_second	= 0;
+    			
+    			int	tick_count = 0;
+    			
+    			while( true ) {
+             				
+    				stepped_time = System.currentTimeMillis();  
           
-          try{  Thread.sleep( TIME_GRANULARITY_MILLIS );  }catch(Exception e) {Debug.printStackTrace( e );}
-        }
-      }
-    };
+    				if ( last_second == 0 ){
+    					
+    					last_second	= stepped_time - 1000;
+    				}else{
+    					
+    					if ( stepped_time < last_second ){
+    						
+    							// clock's changed
+    						
+    						last_approximate_time	= 0;
+    						
+    						last_second	= stepped_time - 1000;
+    					}
+    				}
+    				
+    				tick_count++;
+    				    				
+  					if ( tick_count == STEPS_PER_SECOND ){
+
+  		   				long drift = stepped_time - last_second -1000;
+  		   			 
+  		   				last_second	= stepped_time;
+  		   				
+  		   				drift_average.addValue( drift );
+  		   				
+  		   				drift_adjusted_granularity	= (int)( TIME_GRANULARITY_MILLIS + ( drift_average.getAverage() / STEPS_PER_SECOND ));
+  		   				
+     					access_average.addValue( access_count );
+    	   						
+    					access_average_per_slice	= (int)( access_average.getAverage() / STEPS_PER_SECOND );
+    						
+    					// System.out.println( "access count = " + access_count + ", average = " + access_average.getAverage() + ", per slice = " + access_average_per_slice + ", drift = " + drift +", average = " + drift_average.getAverage() + ", dag =" + drift_adjusted_granularity );
+    						
+    					access_count = 0;
+    						
+    	    			tick_count = 0;
+  					}
+  					
+					slice_access_count	= 0; 
+
+    				try{  
+    					Thread.sleep( TIME_GRANULARITY_MILLIS );
+    					
+    				}catch(Exception e){
+    					
+    					Debug.printStackTrace( e );
+    				}
+    			}
+    		}
+    	};
+    	
     updater.setDaemon( true );
     
     // we don't want this thread to lag much as it'll stuff up the upload/download rate mechanisms (for example)
@@ -56,13 +126,100 @@ public class SystemTime {
     updater.start();
   }
 
+  private long
+  getApproximateTime()
+  {
+	  long	adjusted_time = stepped_time;
+
+	  if ( access_average_per_slice > 0 ){
+	  
+		  long	x = (drift_adjusted_granularity*slice_access_count)/access_average_per_slice;
+		  
+		  if ( x >= drift_adjusted_granularity ){
+			  
+			  x = drift_adjusted_granularity-1;
+		  }
+				  
+		  adjusted_time += x;
+	  }
+	  
+	  access_count++;
+	  
+	  slice_access_count++;
+	  
+	  	// make sure we don't go backwards
+	  
+	  if ( adjusted_time < last_approximate_time ){
+		  
+		  adjusted_time	= last_approximate_time;
+		  
+	  }else{
+		  
+		  last_approximate_time = adjusted_time;
+	  }
+	  	  
+	  return( adjusted_time );
+  }
   
   /**
    * Get the current system time.
    * @return time like System.currentTimeMillis()
    */
-  public static long getCurrentTime() {
-    return instance.stepped_time;
+  public static long 
+  getCurrentTime() 
+  {
+    return( instance.getApproximateTime());
   }
-
+  
+  public static void
+  main(
+	String[]	args )
+  {
+	  for (int i=0;i<1;i++){
+		  
+		  final int f_i = i;
+		  
+		  new Thread()
+		  {
+			  public void
+			  run()
+			  {
+				  Average access_average 	= Average.getInstance( 1000, 10 );
+						 
+				  long	last = SystemTime.getCurrentTime();
+				  
+				  int	count = 0;
+				  
+				  while( true ){
+					  
+					  long	now = SystemTime.getCurrentTime();
+					  
+					  long	diff = now - last;
+					  
+					  System.out.println( "diff=" + diff );
+					  
+					  last	= now;
+					  
+					  access_average.addValue( diff );
+					  
+					  count++;
+					  
+					  if ( count == 33 ){
+						  
+						  System.out.println( "AVERAGE " + f_i + " = " + access_average.getAverage());
+						  
+						  count = 0;
+					  }
+					  
+					  try{
+						  Thread.sleep( 3 );
+						  
+					  }catch( Throwable e ){
+						  
+					  }
+				  }
+			  }
+		  }.start();
+	  }
+  }
 }
