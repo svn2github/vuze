194acbc4fde94655feb1c5016dba941f88717879
fatal_2 <fatal_2@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-09-06 13:45:34 +0000

    split out SocketServer class. add support for multi users
    git-svn-id: http://svn.vuze.com/public/client@20733 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/uis/src/org/gudy/azureus2/ui/telnet/UI.java
+++ trunk/uis/src/org/gudy/azureus2/ui/telnet/UI.java
@@ -24,13 +24,9 @@ package org.gudy.azureus2.ui.telnet;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 import java.util.StringTokenizer;
 
@@ -41,6 +37,10 @@ import org.gudy.azureus2.core3.util.FileUtil;
 import org.gudy.azureus2.ui.common.IUserInterface;
 import org.gudy.azureus2.ui.common.UIConst;
 import org.gudy.azureus2.ui.console.ConsoleInput;
+import org.gudy.azureus2.ui.console.UserProfile;
+import org.gudy.azureus2.ui.console.multiuser.MultiUserConsoleInput;
+import org.gudy.azureus2.ui.console.multiuser.UserManager;
+import org.gudy.azureus2.ui.console.multiuser.commands.UserCommand;
 
 /**
  * this is a telnet UI that starts up a server socket that listens for new connections 
@@ -51,33 +51,46 @@ import org.gudy.azureus2.ui.console.ConsoleInput;
  */
 public class UI extends org.gudy.azureus2.ui.common.UITemplateHeadless implements IUserInterface 
 {
-	private ServerSocket serverSocket;
-
 	public String[] processArgs(String[] args) {
 		return args;
 	}
+	
+	private UserManager userManager;
+	
 	/**
 	 * start up a server socket thread on an appropriate port as obtained from the configuration manager.
 	 */
 	public void startUI() {
-		super.startUI();
-		if( ! isStarted() || serverSocket == null || serverSocket.isClosed() )
+		if( ! isStarted() )
 		{
 			try {
 				int telnetPort = COConfigurationManager.getIntParameter("Telnet_iPort", 57006);
-				String allowedHostsStr = COConfigurationManager.getStringParameter("Telnet_allowedHosts", "127.0.0.1,titan");
+				String allowedHostsStr = COConfigurationManager.getStringParameter("Telnet_sAllowedHosts", "127.0.0.1,titan");				
 				StringTokenizer st = new StringTokenizer(allowedHostsStr, ",");
 				Set allowedHosts = new HashSet();
 				while( st.hasMoreTokens() )
 					allowedHosts.add(st.nextToken().toLowerCase());
-				
-				Thread thread = new Thread(new SocketServer(telnetPort, allowedHosts), "Telnet Socket Server Thread");
+				int maxLoginAttempts = COConfigurationManager.getIntParameter("Telnet_iMaxLoginAttempts", 3);
+				userManager = initUserManager();
+				Thread thread = new Thread(new SocketServer(this, telnetPort, allowedHosts, userManager, maxLoginAttempts), "Telnet Socket Server Thread");
 				thread.setDaemon(true);
 				thread.start();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+		super.startUI();
+	}
+	
+	/**
+	 * @return user manager instance if multi user is enabled, otherwise null 
+	 */
+	private UserManager initUserManager()
+	{
+		if( System.getProperty("azureus.console.multiuser") != null )
+			return UserManager.getInstance(UIConst.getAzureusCore().getPluginManager().getDefaultPluginInterface());
+		else
+			return null;
 	}
 	
 	/**
@@ -114,83 +127,21 @@ public class UI extends org.gudy.azureus2.ui.common.UITemplateHeadless implement
 	 * @param consoleName
 	 * @param inputStream
 	 * @param outputStream
+	 * @param profile
 	 */
-	public static void createNewConsoleInput(String consoleName, InputStream inputStream, PrintStream outputStream)
+	public void createNewConsoleInput(String consoleName, InputStream inputStream, PrintStream outputStream, UserProfile profile)
 	{
-	     ConsoleInput console = new ConsoleInput(consoleName, UIConst.getAzureusCore(), inputStream, outputStream, Boolean.FALSE);
-	     console.printconsolehelp();
-	}
-	
-	private static final class SocketServer implements Runnable
-	{
-		private final ServerSocket serverSocket;
-		private final Set allowedHosts;
-		public SocketServer(int port, Set allowedHosts) throws IOException
+		ConsoleInput console; 
+		if( userManager != null )
 		{
-			this.allowedHosts = allowedHosts;
-			serverSocket = new ServerSocket(port);
+			MultiUserConsoleInput muc = new MultiUserConsoleInput(consoleName, UIConst.getAzureusCore(), new InputStreamReader(inputStream), outputStream, Boolean.FALSE, profile);
+			muc.registerCommand( new UserCommand(userManager) );
+			console = muc;
 		}
-		/**
-		 * start up the server socket and when a new connection is received, check that
-		 * the source address is in our permitted list and if so, start a new console input
-		 * on that socket.
-		 */
-		public void run()
+		else
 		{
-			int threadNum = 1;
-			System.out.println("Telnet server started. Listening on port: " + serverSocket.getLocalPort());
-			while(true)
-			{
-				try {
-					Socket socket = serverSocket.accept();
-					InetSocketAddress addr = (InetSocketAddress) socket.getRemoteSocketAddress();
-					if( addr.isUnresolved() || ! isAllowed(addr) )
-					{
-						System.out.println("rejecting connection: " + addr);
-						socket.close();
-					}
-					else
-					{
-						System.out.println("accepting connection: " + addr);
-						createNewConsoleInput("Telnet Console " + threadNum++, socket.getInputStream(), new PrintStream(socket.getOutputStream()));
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-					break;
-				}
-			}
-		}
-		/**
-		 * check that the specified host/ip is allowed
-		 * @param addr
-		 * @return
-		 */
-		private boolean isAllowed(InetSocketAddress addr) {
-			InetAddress address = addr.getAddress();
-			if( checkHost(address.getHostAddress()) )
-				return true;
-			else if( checkHost(address.getHostName()))
-				return true;
-			else
-				return false;
-		}
-		/**
-		 * compare the specified host (might be a hostname or an IP - dont really care) 
-		 * and see if it is a match against one of the allowed hosts
-		 * @param hostName
-		 * @return true if this hostname matches one in our allowed hosts
-		 */
-		private boolean checkHost(String hostName) {
-			if( hostName == null )
-				return false;
-			hostName = hostName.toLowerCase();
-//			System.out.println("checking host: " + hostName);
-			for (Iterator iter = allowedHosts.iterator(); iter.hasNext();) {
-				String allowedHost = (String) iter.next();
-				if( hostName.equals(allowedHost) )
-					return true;
-			}
-			return false;
-		}
+			console = new ConsoleInput(consoleName, UIConst.getAzureusCore(), new InputStreamReader(inputStream), outputStream, Boolean.FALSE, profile);
+		}	
+		console.printconsolehelp();
 	}
 }
