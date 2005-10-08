0df537313aacf23f9914caa015af8166127ba25d
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-10-08 11:22:04 +0000

    added explicit "encoding=utf-8" to UPnP actions
    git-svn-id: http://svn.vuze.com/public/client@7078 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/net/upnp/impl/device/UPnPActionInvocationImpl.java
+++ trunk/azureus2/src/com/aelitis/net/upnp/impl/device/UPnPActionInvocationImpl.java
@@ -71,7 +71,7 @@ UPnPActionInvocationImpl
 		
 		try{
 			String	request =
-				"<?xml version=\"1.0\"?>"+
+				"<?xml version=\"1.0\" encoding=\"utf-8\"?>"+
 				"<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">"+
 					"<s:Body>";
 													
