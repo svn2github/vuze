05cb7db136ab8faa3c58a9b548d109e95b11c8fc
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-10-24 05:22:38 +0000

    JavaDoc changes only
    git-svn-id: http://svn.vuze.com/public/client@7192 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/plugins/ui/config/Parameter.java
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/ui/config/Parameter.java
@@ -21,42 +21,121 @@
  
 package org.gudy.azureus2.plugins.ui.config;
 
+import org.gudy.azureus2.plugins.config.ConfigParameter;
+
 /**
  * represents a genric parameter description
  * @author Olivier
  *
  */
-
-import org.gudy.azureus2.plugins.config.ConfigParameter;
-
 public interface
 Parameter
 	extends ConfigParameter
 {
+	/**
+	 * Sets whether the UI object for this parameter is enabled (changeable) or
+	 * disabled (not changeable, and usualy grayed out)
+	 * 
+	 * @param enabled The new enabled state
+	 * 
+	 * @since 2.3.0.0
+	 */
 	public void
 	setEnabled(
 		boolean	enabled );
 	
+	/**
+	 * Retrieves the enabled state for the UI object for this parameter
+	 * 
+	 * @return The enabled state
+	 * 
+	 * @since 2.3.0.0
+	 */
 	public boolean
 	isEnabled();
+
 	
+	/**
+	 * Sets whether the UI object for this parameter is visble to the user
+	 * 
+	 * @param visible The new visibility state
+	 * 
+	 * @since 2.3.0.4
+	 */
 	public void
 	setVisible(
 		boolean	visible );
 	
+	/**
+	 * Retrieves the visiblility state for the UI object for this parameter
+	 * 
+	 * @return The visibility state
+	 * 
+	 * @since 2.3.0.4
+	 */
 	public boolean
 	isVisible();
 	
+	/**
+	 * Adds a listener triggered when the parameter is changed by the user
+	 * 
+	 * @param l Listener to add
+	 * 
+	 * @since 2.1.0.2
+	 */
 	public void
 	addListener(
 		ParameterListener	l );
 	
+	/**
+	 * Removes a previously added listener
+	 * 
+	 * @param l Listener to remove.
+	 * 
+	 * @since 2.1.0.2
+	 */
 	public void
 	removeListener(
 		ParameterListener	l );
 	
+	/**
+	 * Retrieve the actual text of the label associated with this parameter.
+	 * This is the text after it has been looked up in the language bundle.
+	 * 
+	 * @return The label's text
+	 * 
+	 * @since 2.3.0.6
+	 */
 	public String getLabelText();
+	
+	/**
+	 * Set the text of the label associated to with this parameter to the literal
+	 * text supplied.
+	 * 
+	 * @param sText The actual text to assign to the label
+	 * 
+	 * @since 2.3.0.6
+	 */
 	public void setLabelText(String sText);
+	
+	/**
+	 * Retrieve the language bundle key for the label associated with this 
+	 * parameter.
+	 * 
+	 * @return The language bundle key, or null if the label is using literal
+	 *          text
+	 * 
+	 * @since 2.3.0.6
+	 */
 	public String getLabelKey();
+	
+	/**
+	 * Set the label to use the supplied language bundle key for the label
+	 * associated with this parameter
+	 * 
+	 * @param sLabelKey The language bundle key to use.
+	 * 
+	 * @since 2.3.0.6
+	 */
 	public void setLabelKey(String sLabelKey);
 }
