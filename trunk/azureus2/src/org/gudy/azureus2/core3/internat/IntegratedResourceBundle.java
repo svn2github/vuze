e90f07385e38f64c2f73972cc7e4bbbb74aaea61
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-10-31 17:00:21 +0000

    fix for plugin locale not getting picked up on start
    git-svn-id: http://svn.vuze.com/public/client@7242 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/internat/IntegratedResourceBundle.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/internat/IntegratedResourceBundle.java
@@ -10,6 +10,7 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.ListResourceBundle;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 
@@ -18,56 +19,69 @@ import java.util.ResourceBundle;
  */
 public class IntegratedResourceBundle extends ListResourceBundle {
 
-  public Object[][] getContents() {
-    return contents;
-  }
+	private Locale	locale;
 
   private Object[][] contents;
 
-  public IntegratedResourceBundle(ResourceBundle main, Map localizationPaths) {
-    contents = integrateBundles(main, localizationPaths);
+  public 
+  IntegratedResourceBundle(
+		ResourceBundle main, 
+		Map localizationPaths) 
+  {
+	  locale = main.getLocale();
+	  
+	  Hashtable messages = new Hashtable();
+	  
+	  addResourceMessages(main, messages);
+
+	  for (Iterator iter = localizationPaths.keySet().iterator(); iter.hasNext();) {
+	      String localizationPath = (String) iter.next();
+	      ClassLoader classLoader = (ClassLoader) localizationPaths.get(localizationPath);
+	      ResourceBundle newResourceBundle = null;
+	      try {
+	        if(classLoader != null)
+	          newResourceBundle = ResourceBundle.getBundle(localizationPath, locale ,classLoader);
+	        else
+	        newResourceBundle = ResourceBundle.getBundle(localizationPath, locale,IntegratedResourceBundle.class.getClassLoader());
+	      } catch (Exception e) {
+	        //        System.out.println(localizationPath+": no resource bundle for " +
+					// main.getLocale());
+	        try {
+	          if(classLoader != null)
+	            newResourceBundle = ResourceBundle.getBundle(localizationPath, MessageText.LOCALE_DEFAULT,classLoader);
+	          else 
+	          newResourceBundle = ResourceBundle.getBundle(localizationPath, MessageText.LOCALE_DEFAULT,IntegratedResourceBundle.class.getClassLoader());
+	        } catch (Exception e2) {
+	          System.out.println(localizationPath + ": no default resource bundle");
+	          continue;
+	        }
+	      }
+	      addResourceMessages(newResourceBundle, messages);
+	  }
+
+	 contents = new Object[messages.size()][2];
+	  
+	 int i = 0;
+	  
+	 for (Enumeration enumeration = messages.keys(); enumeration.hasMoreElements();) {
+	      String key = (String) enumeration.nextElement();
+	      contents[i][0] = key;
+	      contents[i++][1] = messages.get(key);
+	 }
   }
 
-  private static Object[][] integrateBundles(ResourceBundle main, Map localizationPaths) {
-    Hashtable messages = new Hashtable();
-    addResourceMessages(main, messages);
-
-    for (Iterator iter = localizationPaths.keySet().iterator(); iter.hasNext();) {
-      String localizationPath = (String) iter.next();
-      ClassLoader classLoader = (ClassLoader) localizationPaths.get(localizationPath);
-      ResourceBundle newResourceBundle = null;
-      try {
-        if(classLoader != null)
-          newResourceBundle = ResourceBundle.getBundle(localizationPath, main.getLocale(),classLoader);
-        else
-        newResourceBundle = ResourceBundle.getBundle(localizationPath, main.getLocale(),IntegratedResourceBundle.class.getClassLoader());
-      } catch (Exception e) {
-        //        System.out.println(localizationPath+": no resource bundle for " +
-				// main.getLocale());
-        try {
-          if(classLoader != null)
-            newResourceBundle = ResourceBundle.getBundle(localizationPath, MessageText.LOCALE_DEFAULT,classLoader);
-          else 
-          newResourceBundle = ResourceBundle.getBundle(localizationPath, MessageText.LOCALE_DEFAULT,IntegratedResourceBundle.class.getClassLoader());
-        } catch (Exception e2) {
-          System.out.println(localizationPath + ": no default resource bundle");
-          continue;
-        }
-      }
-      addResourceMessages(newResourceBundle, messages);
-    }
-
-    Object[][] messageArray = new Object[messages.size()][2];
-    int i = 0;
-    for (Enumeration enumeration = messages.keys(); enumeration.hasMoreElements();) {
-      String key = (String) enumeration.nextElement();
-      messageArray[i][0] = key;
-      messageArray[i++][1] = messages.get(key);
-    }
-    return messageArray;
+  public Locale getLocale() 
+  {
+      return locale;
+  }
+  
+  public Object[][] getContents() {
+	    return contents;
   }
 
-  private static void addResourceMessages(ResourceBundle bundle, Hashtable messages) {
+  private void 
+  addResourceMessages(ResourceBundle bundle, Hashtable messages) 
+  {
     if (bundle != null) {
       for (Enumeration enumeration = bundle.getKeys(); enumeration.hasMoreElements();) {
         String key = (String) enumeration.nextElement();
