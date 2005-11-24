294b947ff6f7c0be74a3a25d056f609e3c31d532
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2005-11-24 02:12:31 +0000

    Implement "boolean setAnnounceURL()". add function to copy torrent file to save directory.
    git-svn-id: http://svn.vuze.com/public/client@7380 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/TorrentUtils.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/TorrentUtils.java
@@ -1287,11 +1287,11 @@ TorrentUtils
 			return( delegate.getAnnounceURL());
 		}
 
-		public void
+		public boolean
 		setAnnounceURL(
 			URL		url )
 		{
-			delegate.setAnnounceURL( url );
+			return delegate.setAnnounceURL( url );
 		}
 			
 		
@@ -1622,5 +1622,62 @@ TorrentUtils
 		{
 			delegate.print();
 		}
+	}
+
+	/**
+	 * Copy a file to the Torrent Save Directory, taking into account all the
+	 * user config options related to that.
+	 * <p>
+	 * Also makes the directory if it doesn't exist.
+	 *  
+	 * @param f File to copy
+	 * @param persistent Whether the torrent is persistent
+	 * @return File after it's been copied (may be the same as f)
+	 * @throws IOException
+	 */
+	public static File copyTorrentFileToSaveDir(File f, boolean persistent)
+			throws IOException {
+		File torrentDir;
+		boolean saveTorrents = persistent
+				&& COConfigurationManager.getBooleanParameter("Save Torrent Files",
+						true);
+		if (saveTorrents)
+			torrentDir = new File(COConfigurationManager
+					.getDirectoryParameter("General_sDefaultTorrent_Directory"));
+		else
+			torrentDir = new File(f.getParent());
+
+		//if the torrent is already in the completed files dir, use this
+		//torrent instead of creating a new one in the default dir
+		boolean moveWhenDone = COConfigurationManager.getBooleanParameter(
+				"Move Completed When Done", false);
+		String completedDir = COConfigurationManager.getStringParameter(
+				"Completed Files Directory", "");
+		if (moveWhenDone && completedDir.length() > 0) {
+			File cFile = new File(completedDir, f.getName());
+			if (cFile.exists()) {
+				//set the torrentDir to the completedDir
+				torrentDir = new File(completedDir);
+			}
+		}
+
+		torrentDir.mkdirs();
+
+		File fDest = new File(torrentDir, f.getName().replaceAll("%20", "."));
+		if (fDest.equals(f)) {
+			return f;
+		}
+
+		while (fDest.exists()) {
+			fDest = new File(torrentDir, "_" + fDest.getName());
+		}
+
+		fDest.createNewFile();
+
+		if (!FileUtil.copyFile(f, fDest)) {
+			throw new IOException("File copy failed");
+		}
+
+		return fDest;
 	}
 }
