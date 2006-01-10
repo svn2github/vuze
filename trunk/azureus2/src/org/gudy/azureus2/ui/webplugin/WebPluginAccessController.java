7874e260708a02f187862a3ac33235385b84e058
amc1 <amc1@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-01-10 19:38:48 +0000

    We now use the new RPException subclasses.
    git-svn-id: http://svn.vuze.com/public/client@7881 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/webplugin/WebPluginAccessController.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/webplugin/WebPluginAccessController.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *
  * AELITIS, SARL au capital de 30,000 euros
  * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
  *
@@ -24,90 +24,89 @@ package org.gudy.azureus2.ui.webplugin;
 
 import org.gudy.azureus2.plugins.*;
 import org.gudy.azureus2.pluginsimpl.remote.*;
+import org.gudy.azureus2.pluginsimpl.remote.rpexceptions.*;
 
 /**
  * @author parg
  *
  */
 
-public class 
-WebPluginAccessController 
-	implements RPRequestAccessController
+public class
+WebPluginAccessController
+    implements RPRequestAccessController
 {
-	protected boolean	view_mode;
-	
-	public
-	WebPluginAccessController(
-		PluginInterface		pi )
-	{
-		String mode_str = pi.getPluginconfig().getPluginStringParameter( WebPlugin.CONFIG_MODE, ((WebPlugin)pi.getPlugin()).CONFIG_MODE_DEFAULT );
-		
-		view_mode = !mode_str.equalsIgnoreCase( WebPlugin.CONFIG_MODE_FULL );
-	}
-	
-	public void
-	checkUploadAllowed()
-	{
-		if ( view_mode ){
-			
-			throw( new RPException( "Access Denied" ));
-		}
-	}
-	
-	public void
-	checkAccess(
-		String			name,
-		RPRequest		request )
-	{
-		String	method = request.getMethod();
-		
-		// System.out.println( "request: " + name + "/" + method );
-		
-		if ( view_mode ){
-			
-			/*
-			request: PluginInterface/getDownloadManager
-			request: PluginInterface/getPluginconfig
-			request: PluginConfig/getPluginStringParameter[String,String]
-			request: DownloadManager/getDownloads
-			request: PluginConfig/getPluginIntParameter[String,int]
-			request: PluginInterface/getIPFilter
-			request: PluginConfig/setPluginParameter[String,int]
-			request: PluginConfig/save
-			*/
-			
-			boolean	ok = false;
-			
-			if ( name.equals( "PluginInterface" )){
-				
-				ok	= 	method.equals( "getPluginconfig" ) ||
-						method.equals( "getDownloadManager" ) ||
-						method.equals( "getIPFilter" );
-				
-			}else if ( name.equals( "DownloadManager" )){
-				
-				ok	= 	method.equals( "getDownloads" );
-				
-			}else if ( name.equals( "PluginConfig" )){
-				
-				if ( 	method.startsWith( "getPlugin") || 
-						method.equals( "save" )){
-					
-					ok	= true;
-					
-				}else if ( method.equals( "setPluginParameter[String,int]" )){
-					
-					String	param = (String)request.getParams()[0];
-					
-					ok = param.equals( "MDConfigModel:refresh_period" );
-				}	
-			}
-			
-			
-			if ( !ok ){
-				
-				throw( new RPException( "Access Denied" ));
-			}
-		}
-	}
+    protected boolean   view_mode;
+
+    public
+    WebPluginAccessController(
+        PluginInterface     pi )
+    {
+        String mode_str = pi.getPluginconfig().getPluginStringParameter( WebPlugin.CONFIG_MODE, ((WebPlugin)pi.getPlugin()).CONFIG_MODE_DEFAULT );
+
+        view_mode = !mode_str.equalsIgnoreCase( WebPlugin.CONFIG_MODE_FULL );
+    }
+
+    public void
+    checkUploadAllowed()
+    {
+        if ( view_mode ){
+            throw new RPMethodAccessDeniedException();
+        }
+    }
+
+    public void
+    checkAccess(
+        String          name,
+        RPRequest       request )
+    {
+        String  method = request.getMethod();
+
+        // System.out.println( "request: " + name + "/" + method );
+
+        if ( view_mode ){
+
+            /*
+            request: PluginInterface/getDownloadManager
+            request: PluginInterface/getPluginconfig
+            request: PluginConfig/getPluginStringParameter[String,String]
+            request: DownloadManager/getDownloads
+            request: PluginConfig/getPluginIntParameter[String,int]
+            request: PluginInterface/getIPFilter
+            request: PluginConfig/setPluginParameter[String,int]
+            request: PluginConfig/save
+            */
+
+            boolean ok = false;
+
+            if ( name.equals( "PluginInterface" )){
+
+                ok  =   method.equals( "getPluginconfig" ) ||
+                        method.equals( "getDownloadManager" ) ||
+                        method.equals( "getIPFilter" );
+
+            }else if ( name.equals( "DownloadManager" )){
+
+                ok  =   method.equals( "getDownloads" );
+
+            }else if ( name.equals( "PluginConfig" )){
+
+                if (    method.startsWith( "getPlugin") ||
+                        method.equals( "save" )){
+
+                    ok  = true;
+
+                }else if ( method.equals( "setPluginParameter[String,int]" )){
+
+                    String  param = (String)request.getParams()[0];
+
+                    ok = param.equals( "MDConfigModel:refresh_period" );
+                }
+            }
+
+
+            if ( !ok ){
+                throw new RPMethodAccessDeniedException();
+            }
+        }
+    }
 }
