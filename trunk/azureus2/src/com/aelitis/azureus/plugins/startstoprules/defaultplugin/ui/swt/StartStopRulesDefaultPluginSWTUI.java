a45601b2c45a690ff5e029c60627dfd2441fd1c4
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-01-10 07:51:45 +0000

    More descriptive debug info
    git-svn-id: http://svn.vuze.com/public/client@7863 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/plugins/startstoprules/defaultplugin/ui/swt/StartStopRulesDefaultPluginSWTUI.java
+++ trunk/azureus2/src/com/aelitis/azureus/plugins/startstoprules/defaultplugin/ui/swt/StartStopRulesDefaultPluginSWTUI.java
@@ -22,19 +22,119 @@
 
 package com.aelitis.azureus.plugins.startstoprules.defaultplugin.ui.swt;
 
-import org.gudy.azureus2.plugins.PluginInterface;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class 
-StartStopRulesDefaultPluginSWTUI 
-{
-	public
-	StartStopRulesDefaultPluginSWTUI(
-		PluginInterface		plugin_interface )
-	{
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+import org.gudy.azureus2.core3.util.TimerEvent;
+import org.gudy.azureus2.plugins.PluginInterface;
+import org.gudy.azureus2.plugins.download.Download;
+import org.gudy.azureus2.plugins.ui.menus.MenuItem;
+import org.gudy.azureus2.plugins.ui.menus.MenuItemListener;
+import org.gudy.azureus2.plugins.ui.tables.TableContextMenuItem;
+import org.gudy.azureus2.plugins.ui.tables.TableManager;
+import org.gudy.azureus2.plugins.ui.tables.TableRow;
+
+import com.aelitis.azureus.plugins.startstoprules.defaultplugin.DefaultRankCalculator;
+import com.aelitis.azureus.plugins.startstoprules.defaultplugin.StartStopRulesDefaultPlugin;
+
+public class StartStopRulesDefaultPluginSWTUI {
+	public StartStopRulesDefaultPluginSWTUI(PluginInterface plugin_interface) {
 		plugin_interface.addConfigSection(new ConfigSectionQueue());
-	    plugin_interface.addConfigSection(new ConfigSectionSeeding());
-	    plugin_interface.addConfigSection(new ConfigSectionSeedingAutoStarting());
-	    plugin_interface.addConfigSection(new ConfigSectionSeedingFirstPriority());
-	    plugin_interface.addConfigSection(new ConfigSectionSeedingIgnore());
+		plugin_interface.addConfigSection(new ConfigSectionSeeding());
+		plugin_interface.addConfigSection(new ConfigSectionSeedingAutoStarting());
+		plugin_interface.addConfigSection(new ConfigSectionSeedingFirstPriority());
+		plugin_interface.addConfigSection(new ConfigSectionSeedingIgnore());
+	}
+
+	public static void openDebugWindow(final DefaultRankCalculator dlData) {
+		final Shell shell = new Shell(Display.getCurrent(), SWT.ON_TOP
+				| SWT.SHELL_TRIM | SWT.TOOL);
+
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 3;
+		GridData gd;
+		shell.setLayout(layout);
+
+		shell.setText("Debug for " + dlData.getDownloadObject().getName());
+
+		final Text txtFP = new Text(shell, SWT.BORDER | SWT.MULTI | SWT.H_SCROLL);
+		gd = new GridData(GridData.FILL_BOTH);
+		gd.horizontalSpan = 3;
+		txtFP.setLayoutData(gd);
+
+		final Button btnAutoRefresh = new Button(shell, SWT.CHECK);
+		btnAutoRefresh.setText("Auto-Refresh");
+		btnAutoRefresh.setLayoutData(new GridData());
+
+		final Button btnRefresh = new Button(shell, SWT.NONE);
+		btnRefresh.setLayoutData(new GridData());
+		btnRefresh.setText("Refresh");
+
+		final Label lbl = new Label(shell, SWT.NONE);
+		gd = new GridData(GridData.FILL_HORIZONTAL);
+		lbl.setLayoutData(gd);
+
+		final TimerTask task = new TimerTask() {
+			String lastText = "";
+
+			public String formatString() {
+				return "FP:\n" + dlData.sExplainFP + "\n" + "SR:" + dlData.sExplainSR
+						+ "\n" + "TRACE:\n" + dlData.sTrace;
+			}
+
+			public void setText(final String s) {
+				lastText = s;
+
+				txtFP.setText(s);
+			}
+
+			public void run() {
+				if (shell.isDisposed())
+					return;
+
+				shell.getDisplay().asyncExec(new Runnable() {
+					public void run() {
+						String s = formatString();
+						if (s.compareTo(lastText) != 0) {
+							if (lastText.length() == 0 || btnAutoRefresh.getSelection()
+									|| btnRefresh.getData("Pressing") != null)
+								setText(s);
+							else
+								lbl.setText("Information is outdated.  Press refresh.");
+						} else {
+							lbl.setText("");
+						}
+					}
+				});
+			}
+		};
+		btnAutoRefresh.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				if (btnAutoRefresh.getSelection())
+					lbl.setText("");
+				task.run();
+			}
+		});
+
+		btnRefresh.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				btnRefresh.setData("Pressing", "1");
+				task.run();
+				btnRefresh.setData("Pressing", null);
+			}
+		});
+
+		shell.setSize(550, 350);
+		shell.open();
+
+		Timer timer = new Timer(true);
+		timer.schedule(task, 0, 2000);
 	}
 }
