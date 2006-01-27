7156c43689ab7648afd0c0ddd601d176b9545a45
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-01-27 12:48:02 +0000

    Fix NPE.
    git-svn-id: http://svn.vuze.com/public/client@8041 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/download/impl/DownloadManagerImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/download/impl/DownloadManagerImpl.java
@@ -2505,7 +2505,7 @@ DownloadManagerImpl
  	  byte[] id1 = getIdentity();
  	  byte[] id2 = other.getIdentity();
        
- 	  if ( id2 == null || id2 == null ){
+ 	  if ( id1 == null || id2 == null ){
        	
  		return( false );	// broken torrents - treat as different so shown
  							// as broken
