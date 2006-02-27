990b944e774211290295a540fc8802aac735e486
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-02-27 04:26:00 +0000

    Document/javadoc LoggerChannel, format to Java Standards (w/tabs)
    git-svn-id: http://svn.vuze.com/public/client@8531 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/plugins/logging/LoggerChannel.java
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/logging/LoggerChannel.java
@@ -22,60 +22,92 @@
 package org.gudy.azureus2.plugins.logging;
 
 /**
- * @author parg
- *
+ * Manipulation of a subsection (a channel) of Azureus' logging.
+ * 
+ * A logger channel is created or retrieve via {@link Logger}.  Typically,
+ * a plugin has it's own channel which it can manipulate.  All channels
+ * are part of Azureus logging as a whole, meaning anything logged here will
+ * also be fed to any functionality that operates on logging data.
+ * 
+ * @since 2.0.7.0
  */
-public interface 
-LoggerChannel 
-{
-	public static final int	LT_INFORMATION	= 1;
-	public static final int LT_WARNING		= 2;
-	public static final int LT_ERROR		= 3;
-		
-	public String
-	getName();
-	
-		/**
-		 * Indicates whether or not logging is enabled - use to optimise calls to the log methods that
-		 * require resources to construct the message to be logged
-		 * Note that this doesn't apply to alerts - these will always be handled
-		 * @return
-		 */
-		
-	public boolean
-	isEnabled();
-	
-		/**
-		 * This causes the channel to also write to logs/<name> files in a cyclic fashion (c.f. the debug_1/2._log
-		 * files)
-		 */
-	
-	public void
-	setDiagnostic();
-	
-	public void
-	log(
-		int		log_type,
-		String	data );
-	
-		/**
-		 * log string with implicit type "information"
-		 * @param data
-		 */
-	
-	public void
-	log(
-		String	data );
-	
-	public void
-	log(
-		Throwable 	error );
-	
-	public void
-	log(
-		String		data,
-		Throwable 	error );
-	
+public interface LoggerChannel {
+	/** Information Log Type */
+	public static final int LT_INFORMATION = 1;
+
+	/** Warning Log Type */
+	public static final int LT_WARNING = 2;
+
+	/** Error Log Type */
+	public static final int LT_ERROR = 3;
+
+	/**
+	 * Returns the name of the Logger Channel
+	 * 
+	 * @return Logger channel name
+	 * @since 2.0.7.0
+	 */
+	public String getName();
+
+	/**
+	 * Indicates whether or not logging is enabled - use to optimise calls to 
+	 * the log methods that require resources to construct the message to be 
+	 * logged.
+	 * 
+	 * Note that this doesn't apply to alerts - these will always be handled
+	 * 
+	 * @return Enabled state of logging
+	 * 
+	 * @since 2.3.0.2
+	 */
+	public boolean isEnabled();
+
+	/**
+	 * This causes the channel to also write to logs/<i>name</i> files in a cyclic 
+	 * fashion (c.f. the debug_1/2._log files)
+	 * 
+	 * @since 2.4.0.2
+	 */
+	public void setDiagnostic();
+
+	/**
+	 * Log a message of a specific type to this channel's logger
+	 * 
+	 * @param log_type LT_* constant
+	 * @param data text to log
+	 * 
+	 * @since 2.0.7.0
+	 */
+	public void log(int log_type, String data);
+
+	/**
+	 * log text with implicit type {@link #LT_INFORMATION}
+	 * 
+	 * @param data text to log
+	 * 
+	 * @since 2.1.0.0
+	 */
+	public void log(String data);
+
+	/**
+	 * log an error with implicit type of {@link #LT_ERROR}
+	 * 
+	 * @param error Throwable object to log
+	 * 
+	 * @since 2.0.7.0
+	 */
+	public void log(Throwable error);
+
+	/**
+	 * log an error with implicit type of {@link #LT_ERROR}
+	 * 
+	 * @param data text to log
+	 * @param error Throwable object to log
+	 * 
+	 * @since 2.0.7.0
+	 */
+	public void log(String data, Throwable error);
+
 	/**
 	 * Log a string against a list of objects
 	 * 
@@ -99,7 +131,7 @@ LoggerChannel
 	 * @since 2.3.0.7
 	 */
 	public void log(Object relatedTo, int log_type, String data);
-	
+
 	/**
 	 * Log an error against an object.
 	 * 
@@ -110,9 +142,9 @@ LoggerChannel
 	 * 
 	 * @since 2.3.0.7
 	 */
-	
+
 	public void log(Object relatedTo, String data, Throwable error);
-	
+
 	/**
 	 * Log an error against a list of objects
 	 * 
@@ -131,44 +163,70 @@ LoggerChannel
 	 * So if you raise an identical alert the second + subsequent messages will not be
 	 * shown. Thus, if you want "identical" messages to be shown, prefix them with something
 	 * unique like a timestamp.
-	 * @param alert_type
-	 * @param message
+	 * 
+	 * @param alert_type LT_* constant
+	 * @param message text to alert user with
+	 * 
+	 * @since 2.0.8.0
 	 */
-	
-	public void
-	logAlert(
-		int			alert_type,
-		String		message );
-	
-	public void
-	logAlert(
-		String		message,
-		Throwable 	e );
-	
-		/**
-		 * These variants will always generate an alert (i.e. duplicates won't be filtered)
-		 * @param alert_type
-		 * @param message
-		 */
-	public void
-	logAlertRepeatable(
-		int			alert_type,
-		String		message );
-	
-	public void
-	logAlertRepeatable(
-		String		message,
-		Throwable 	e );
-	
-	public void
-	addListener(
-		LoggerChannelListener	l );
-	
-	public void
-	removeListener(
-		LoggerChannelListener	l );
-	
-	public Logger
-	getLogger();
-	
+	public void logAlert(int alert_type, String message);
+
+	/**
+	 * Alert the user of an error
+	 * 
+	 * @param message text to alert user with
+	 * @param e Error that will be attached to the alert
+	 * 
+	 * @since 2.1.0.2
+	 */
+	public void logAlert(String message, Throwable e);
+
+	/**
+	 * Raise an alert to the user, if UI present. Subsequent, identical messages
+	 * will always generate an alert (i.e. duplicates won't be filtered)
+	 * 
+	 * @param alert_type LT_* constant
+	 * @param message text to alert user with
+	 * 
+	 * @since 2.1.0.2
+	 */
+	public void logAlertRepeatable(int alert_type, String message);
+
+	/**
+	 * Raise an alert to the user, if UI present. Subsequent, identical messages
+	 * will always generate an alert (i.e. duplicates won't be filtered)
+	 * 
+	 * @param message text to alert user with
+	 * @param e Error that will be attached to the alert
+	 * 
+	 * @since 2.1.0.2
+	 */
+	public void logAlertRepeatable(String message, Throwable e);
+
+	/**
+	 * Add a LoggerChannelListener to this LoggerChannel
+	 * 
+	 * @param l Listener to add
+	 * 
+	 * @since 2.0.8.0
+	 */
+	public void addListener(LoggerChannelListener l);
+
+	/**
+	 * Remove a reviously added LoggerChannelListener
+	 * 
+	 * @param l Listener to remove.
+	 * 
+	 * @since 2.0.8.0
+	 */
+	public void removeListener(LoggerChannelListener l);
+
+	/**
+	 * Retrieve the parent Logger object for this LoggerChannel.
+	 * 
+	 * @return Logger object
+	 * 
+	 * @since 2.3.0.0
+	 */
+	public Logger getLogger();
 }
