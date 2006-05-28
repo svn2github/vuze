1274f8fd8fb85fdd44de09305d8aee1c32851142
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-05-28 06:00:53 +0000

    javadocs
    git-svn-id: http://svn.vuze.com/public/client@8951 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/plugins/peers/PeerStats.java
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/peers/PeerStats.java
@@ -22,36 +22,112 @@
 package org.gudy.azureus2.plugins.peers;
 
 /**
- * @author parg
- *
+ * Statistical information for a Peer.
  */
-public interface 
-PeerStats 
-{	
+public interface PeerStats
+{
+	/**
+	 * Get the the average bytes-per-second speed that we are receiving piece 
+	 * data from the peer.
+	 * 
+	 * @return average speed.
+	 * 
+	 * @since 2.0.6.0
+	 */
 	public int getDownloadAverage();
 
+	/**
+	 * Get the the longer-average bytes-per-second speed at which the peer is 
+	 * uploading data to us.
+	 * 
+	 * @return average speed
+	 * 
+	 * @since 2.0.6.0
+	 */
 	public int getReception();
 
+	/**
+	 * Get the the average bytes-per-second speed that we are sending piece data 
+	 * to the peer.
+	 * 
+	 * @return average speed.
+	 * 
+	 * @since 2.0.6.0
+	 */
 	public int getUploadAverage();
-   
+
+	/**
+	 * Get the estimated total download rate of the peer.
+	 * @return estimated rate in bytes-per-second
+	 */
 	public int getTotalAverage();
-   
+
+	/**
+	 * Get the total number of discarded bytes received from the peer.
+	 * @return total discarded
+	 * 
+	 * @since 2.0.6.0
+	 */
 	public long getTotalDiscarded();
- 
+
+	/**
+	 * Get the total number of data (payload) bytes sent to the peer.
+	 * @return total
+	 * 
+	 * @since 2.0.6.0
+	 */
 	public long getTotalSent();
-  
+
+	/**
+	 * Get the total number of data (payload) bytes received from the peer.
+	 * @return total
+	 * 
+	 * @since 2.0.6.0
+	 */
 	public long getTotalReceived();
- 
+
+	/**
+	 * Get the estimated total upload rate of the peer.
+	 * @return estimated rate in bytes-per-second
+	 * 
+	 * @since 2.0.6.0
+	 */
 	public int getStatisticSentAverage();
-	
-	public void
-	received(
-		int		bytes );
-	
-	public void
-	discarded(
-		int		bytes );
-	
-	public long
-	getTimeSinceConnectionEstablished();
+
+	/**
+	 * The given number of data (payload) bytes have been received from the peer.
+	 * This number gets added to the total and is used to calculate the rate.
+	 * <p>
+	 * Use this if you are talking to the peer outside of Azureus' API, and
+	 * want your stats added into Azureus'
+	 * 
+	 * @param bytes
+	 * 
+	 * @since 2.1.0.0
+	 */
+	public void received(int bytes);
+
+	/**
+	 * The given number of bytes received from the peer were discarded.
+	 * This number gets added to the total and is used to calculate rates that
+	 * include disgarded inforamtion.
+	 * <p>
+	 * Use this if you are talking to the peer outside of Azureus' API, and
+	 * want your stats added into Azureus'
+	 * 
+	 * @param bytes
+	 * 
+	 * @since 2.1.0.0
+	 */
+	public void discarded(int bytes);
+
+	/**
+	 * Get the amount of time that has elapsed since the connection with the
+	 * peer has been established.
+	 * 
+	 * @return Amount of time in ms.
+	 * 
+	 * @since 2.4.0.0
+	 */
+	public long getTimeSinceConnectionEstablished();
 }
