1d7f9cf92a85b2a06ea0d0df546f271d119e4991
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-07-02 16:19:14 +0000

    more options for printing strings to GC
    git-svn-id: http://svn.vuze.com/public/client@9166 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/shells/GCStringPrinter.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/shells/GCStringPrinter.java
@@ -30,90 +30,120 @@ import org.eclipse.swt.graphics.Point;
  * @author Olivier Chalouhi
  *
  */
-public class GCStringPrinter {
-  
-  public static boolean printString(GC gc,String string,Rectangle printArea) {
-    int x0 = printArea.x;
-    int y0 = printArea.y;
-    int height = 0;
-    Rectangle oldClipping = gc.getClipping();
+public class GCStringPrinter
+{
 
-    //Protect the GC from drawing outside the drawing area
-    gc.setClipping(printArea);    
-    
-    //We need to add some cariage return ...
-    String sTabsReplaced = string.replaceAll("\t", "  ");
+	public static boolean printString(GC gc, String string, Rectangle printArea) {
+		return printString(gc, string, printArea, false, false);
+	}
 
-    StringBuffer outputLine = new StringBuffer();
+	public static boolean printString(GC gc, String string, Rectangle printArea,
+			boolean skipClip, boolean fullLinesOnly)
+	{
+		int x0 = printArea.x;
+		int y0 = printArea.y;
+		int height = 0;
 
-    // Process string line by line
-    StringTokenizer stLine = new StringTokenizer(sTabsReplaced,"\n");
-    while(stLine.hasMoreElements()) {      
-      int iLineHeight = 0;
-      String sLine = stLine.nextToken();
-      if (gc.stringExtent(sLine).x > printArea.width) {
-        //System.out.println("Line: "+ sLine);
-        StringTokenizer stWord = new StringTokenizer(sLine, " ");
-        String space = "";  
-        int iLineLength = 0;
-        iLineHeight = gc.stringExtent(" ").y;
-  
-        // Process line word by word
-        while(stWord.hasMoreElements()) {      
-          String word = stWord.nextToken();
-          
-          // check if word is longer than our print area, and split it
-          Point ptWordSize = gc.stringExtent(word + " ");
-          while (ptWordSize.x > printArea.width) {
-          	int endIndex = word.length() - 1;
-          	do {
-          		endIndex--;
-          		ptWordSize = gc.stringExtent(word.substring(0, endIndex) + " ");
-          	} while (endIndex > 3 && ptWordSize.x + iLineLength > printArea.width);
-          	// append part that will fit
-          	outputLine.append(space)
-          			  .append(word.substring(0, endIndex))
-          			  .append("\n");
-            height += ptWordSize.y;
+		Rectangle oldClipping = null;
+		if (!skipClip) {
+			oldClipping = gc.getClipping();
 
-            // setup word as the remaining part that didn't fit
-          	word = word.substring(endIndex);
-          	ptWordSize = gc.stringExtent(word + " ");
-          	iLineLength = 0;
-          }
-          iLineLength += ptWordSize.x;
-          //System.out.println(outputLine + " : " + word + " : " + iLineLength);
-          if(iLineLength > printArea.width) {
-            iLineLength = ptWordSize.x;
-            height += iLineHeight;
-            iLineHeight = ptWordSize.y;
-            space = "\n";
-          }
-          if (iLineHeight < ptWordSize.y)
-            iLineHeight = ptWordSize.y;
-           
-          outputLine.append(space).append(word);            
-          space = " ";
-        }
-      } else {
-        outputLine.append(sLine);
-        iLineHeight = gc.stringExtent(sLine).y;
-      }
-      outputLine.append("\n");
-      height += iLineHeight;
-    }
-    
-    String sOutputLine = outputLine.toString();
-    gc.drawText(sOutputLine,x0,y0,true);        
-    gc.setClipping(oldClipping);
-    return height <= printArea.height;
-  }
-  
-  private static int getAdvanceWidth(GC gc,String s) {
-    int result = 0;
-    for(int i = 0 ; i < s.length() ; i++) {
-      result += gc.getAdvanceWidth(s.charAt(i)) - 1;
-    }
-    return result;
- }
+			// Protect the GC from drawing outside the drawing area
+			gc.setClipping(printArea);
+		}
+
+		//We need to add some cariage return ...
+		String sTabsReplaced = string.replaceAll("\t", "  ");
+
+		StringBuffer outputLine = new StringBuffer();
+
+		// Process string line by line
+		StringTokenizer stLine = new StringTokenizer(sTabsReplaced, "\n");
+		while (stLine.hasMoreElements()) {
+			int iLineHeight = 0;
+			String sLine = stLine.nextToken();
+			if (gc.stringExtent(sLine).x > printArea.width) {
+				//System.out.println("Line: "+ sLine);
+				StringTokenizer stWord = new StringTokenizer(sLine, " ");
+				String space = "";
+				int iLineLength = 0;
+				iLineHeight = gc.stringExtent(" ").y;
+
+				// Process line word by word
+				while (stWord.hasMoreElements()) {
+					String word = stWord.nextToken();
+
+					// check if word is longer than our print area, and split it
+					Point ptWordSize = gc.stringExtent(word + " ");
+					while (ptWordSize.x > printArea.width) {
+						int endIndex = word.length() - 1;
+						do {
+							endIndex--;
+							ptWordSize = gc.stringExtent(word.substring(0, endIndex) + " ");
+						} while (endIndex > 3
+								&& ptWordSize.x + iLineLength > printArea.width);
+						// append part that will fit
+						height += ptWordSize.y;
+						if (fullLinesOnly && height > printArea.height) {
+							break;
+						}
+						outputLine.append(space).append(word.substring(0, endIndex)).append(
+								"\n");
+						if (height > printArea.height) {
+							break;
+						}
+
+						// setup word as the remaining part that didn't fit
+						word = word.substring(endIndex);
+						ptWordSize = gc.stringExtent(word + " ");
+						iLineLength = 0;
+					}
+					if (height > printArea.height) {
+						break;
+					}
+
+					iLineLength += ptWordSize.x;
+					//System.out.println(outputLine + " : " + word + " : " + iLineLength);
+					if (iLineLength > printArea.width) {
+						iLineLength = ptWordSize.x;
+						height += iLineHeight;
+						iLineHeight = ptWordSize.y;
+						space = "\n";
+						if (fullLinesOnly && height > printArea.height) {
+							break;
+						}
+					}
+					if (iLineHeight < ptWordSize.y)
+						iLineHeight = ptWordSize.y;
+
+					outputLine.append(space).append(word);
+					space = " ";
+				}
+				
+				if (height > printArea.height) {
+					break;
+				}
+			} else {
+				outputLine.append(sLine);
+				iLineHeight = gc.stringExtent(sLine).y;
+			}
+			outputLine.append("\n");
+			height += iLineHeight;
+		}
+
+		String sOutputLine = outputLine.toString();
+		gc.drawText(sOutputLine, x0, y0, true);
+		if (!skipClip) {
+			gc.setClipping(oldClipping);
+		}
+		return height <= printArea.height;
+	}
+
+	private static int getAdvanceWidth(GC gc, String s) {
+		int result = 0;
+		for (int i = 0; i < s.length(); i++) {
+			result += gc.getAdvanceWidth(s.charAt(i)) - 1;
+		}
+		return result;
+	}
 }
