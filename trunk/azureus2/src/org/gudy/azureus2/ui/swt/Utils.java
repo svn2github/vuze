68c0fcd773c4b83a709099405f32adb7b5381682
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-10-27 00:32:04 +0000

    do a bit of rounding for pixelsToPoint, and add a double option for pixels param
    git-svn-id: http://svn.vuze.com/public/client@9824 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/Utils.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/Utils.java
@@ -856,7 +856,11 @@ public class Utils {
 	}
 	
 	public static int pixelsToPoint(int pixels, int dpi) {
-    return (pixels * 72) / dpi;
+    return (int) Math.round((pixels * 72.0) / dpi);
+	}
+	
+	public static int pixelsToPoint(double pixels, int dpi) {
+    return (int) Math.round((pixels * 72.0) / dpi);
 	}
 
 	public static boolean drawImage(GC gc, Image image, Rectangle dstRect,
