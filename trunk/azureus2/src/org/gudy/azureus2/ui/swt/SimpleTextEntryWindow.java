e4106f72dc2b864f14b4b7b92383a6b23e240287
amc1 <amc1@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-11-13 22:39:11 +0000

    Reworked this class so it fits in with the new UIInputValidator plugin interface (it also works fine outside the context of a plugin).
    git-svn-id: http://svn.vuze.com/public/client@9912 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/SimpleTextEntryWindow.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/SimpleTextEntryWindow.java
@@ -29,55 +29,90 @@ import org.eclipse.swt.widgets.*;
 import org.gudy.azureus2.core3.internat.MessageText;
 import org.gudy.azureus2.core3.util.Constants;
 import org.gudy.azureus2.core3.util.Debug;
+import org.gudy.azureus2.plugins.ui.UIInputValidator;
+import org.gudy.azureus2.pluginsimpl.local.ui.AbstractUIInputReceiver;
 import org.gudy.azureus2.ui.swt.components.ControlUtils;
+import org.gudy.azureus2.ui.swt.pluginsimpl.AbstractUISWTInputReceiver;
+import org.eclipse.swt.widgets.MessageBox;
 
 /**
  * @author amc1
  * Based on CategoryAdderWindow.
  */
-public class SimpleTextEntryWindow {
+public class SimpleTextEntryWindow extends AbstractUISWTInputReceiver {
 	
-	public static interface TextEntrySubmissionListener {
-		public Object dataEntered(String value);
+	/**
+	 * This is here just to make it more straight-forward for code that wants
+	 * to add a validator, without having to import a plugin API interface.
+	 */
+	public abstract static class Validator implements UIInputValidator {}
+	
+	private Display display;
+	
+	public SimpleTextEntryWindow(final Display display) {
+		this.display = display;
 	}
 	
-	private Object parsed_data = null;
-	private boolean data_entered = false;
-	
-	public SimpleTextEntryWindow(final Display display, final String title_key, final String message_key, final String default_value, final TextEntrySubmissionListener handler) {
-	    final Shell shell = org.gudy.azureus2.ui.swt.components.shell.ShellFactory.createShell(display, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
-	    if (title_key != null) {
-	    	shell.setText(MessageText.getString(title_key));
-	    }
-	    if(! Constants.isOSX) {
+	protected void promptForInput() {
+		final Shell shell = org.gudy.azureus2.ui.swt.components.shell.ShellFactory.createShell(display, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
+
+		if (this.title != null) {
+			shell.setText(this.title);
+		}
+	    
+		if(!Constants.isOSX) {
 	      shell.setImage(ImageRepository.getImage("azureus"));
 	    }
+		
 	    GridLayout layout = new GridLayout();
 	    shell.setLayout(layout);
-
-	    Label label = new Label(shell, SWT.NONE);
-	    if (message_key != null) {
-	    	Messages.setLanguageText(label, message_key);
-	    }
-	    GridData gridData = new GridData();
-	    gridData.widthHint = 330;
-	    label.setLayoutData(gridData);
-
-	    if ((message_key != null) && MessageText.keyExists(message_key + ".2")) {
+	    
+	    // Default width hint is 330.
+	    int width_hint = (this.width_hint == -1) ? 330 : this.width_hint;
+	    
+	    // Process any messages.
+	    Label label = null;
+	    GridData gridData = null;
+	    for (int i=0; i<this.messages.length; i++) {
 	    	label = new Label(shell, SWT.NONE);
-    		Messages.setLanguageText(label, message_key + ".2");
+	    	label.setText(this.messages[i]);
+	    	
+	    	// 330 is the current default width.
 	    	gridData = new GridData();
-	    	gridData.widthHint = 330;
-	    	label.setLayoutData(gridData);
+	    	gridData.widthHint = width_hint;
+		    label.setLayoutData(gridData);
+	    }
+
+	    // We may, at a later date, allow more customisable behaviour w.r.t. to this.
+	    // (e.g. "Should we wrap this, should we provide H_SCROLL capabilities" etc.)
+	    int text_entry_flags = SWT.BORDER;
+	    if (this.multiline_mode) {
+	    	text_entry_flags |= SWT.MULTI | SWT.V_SCROLL | SWT.WRAP; 
+	    }
+	    else {
+	    	text_entry_flags |= SWT.SINGLE;
 	    }
 	    
-	    final Text text_entry = new Text(shell, SWT.BORDER);
-	    if (default_value != null) {
-	    	text_entry.setText(default_value);
-	    	text_entry.selectAll();
+	    // Create Text object with pre-entered text.
+	    final Text text_entry = new Text(shell, text_entry_flags);
+	    if (this.preentered_text != null) {
+	    	text_entry.setText(this.preentered_text);
+	    	if (this.select_preentered_text) {
+	    		text_entry.selectAll();
+	    	}
 	    }
+	    
+	    // Default behaviour - single mode results in default height of 1 line,
+	    // multiple lines has default height of 3.
+	    int line_height = this.line_height;
+	    if (line_height == -1) {
+	    	line_height = (this.multiline_mode) ? 3 : 1;
+	    }
+	    
 	    gridData = new GridData();
-	    gridData.widthHint = 330;
+	    gridData.widthHint = width_hint;
+	    gridData.minimumHeight = text_entry.getLineHeight() * line_height;
+	    gridData.heightHint = gridData.minimumHeight;
 	    text_entry.setLayoutData(gridData);
 
 	    Composite panel = new Composite(shell, SWT.NULL);
@@ -101,19 +136,47 @@ public class SimpleTextEntryWindow {
 	    Button cancel = createAlertButton(panel, "Button.cancel");
 
 	    ok.addListener(SWT.Selection, new Listener() {
+	    	
+	    	private void showError(String text) {
+	    		  String error_title = SimpleTextEntryWindow.this.title;
+	    		  if (error_title == null) {error_title = "";}
+
+	    		  MessageBox mb = new MessageBox(shell, SWT.ICON_ERROR | SWT.OK);
+	    		  mb.setText(error_title);
+	    		  mb.setMessage(text);
+	    		  mb.open();
+	    	}
+	    	
 	      /* (non-Javadoc)
 	       * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
 	       */
 	      public void handleEvent(Event event) {
 	    	  try {
-	    		  String entered_data = text_entry.getText().trim();
-	    		  Object result = entered_data;
-	    		  if (handler != null) {result = handler.dataEntered(entered_data);}
-	    		  SimpleTextEntryWindow.this.parsed_data = result;
-	    		  SimpleTextEntryWindow.this.data_entered = true;
+	    		  String entered_data = text_entry.getText();
+	    		  if (!SimpleTextEntryWindow.this.maintain_whitespace) {
+	    			  entered_data = entered_data.trim();
+	    		  }
+	    		  
+    		  
+	    		  
+	    		  if (!SimpleTextEntryWindow.this.allow_empty_input && entered_data.length() == 0) {
+	    			  showError(MessageText.getString("UI.cannot_submit_blank_text"));
+	    			  return;
+	    		  }
+	    		  
+	    		  UIInputValidator validator = SimpleTextEntryWindow.this.validator;
+	    		  if (validator != null) {
+	    			  String validate_result = validator.validate(entered_data);
+	    			  if (validate_result != null) {
+		    			  showError(MessageText.getString(validate_result));
+		    			  return;	    				  
+	    			  }
+	    		  }
+	    		  SimpleTextEntryWindow.this.recordUserInput(entered_data);
 	    	  }
 	    	  catch (Exception e) {
 	    		  Debug.printStackTrace(e);
+	    		  SimpleTextEntryWindow.this.recordUserAbort();
 	    	  }
 	    	  shell.dispose();
 	      }
@@ -124,7 +187,8 @@ public class SimpleTextEntryWindow {
 	         * @see org.eclipse.swt.widgets.Listener#handleEvent(org.eclipse.swt.widgets.Event)
 	         */
 	        public void handleEvent(Event event) {
-	          shell.dispose();
+	        	SimpleTextEntryWindow.this.recordUserAbort();
+	            shell.dispose();
 	        }
 	      });
 
@@ -133,6 +197,7 @@ public class SimpleTextEntryWindow {
 		shell.addListener(SWT.Traverse, new Listener() {	
 			public void handleEvent(Event e) {
 				if ( e.character == SWT.ESC){
+					SimpleTextEntryWindow.this.recordUserAbort();
 					shell.dispose();
 				}
 			}
@@ -159,7 +224,4 @@ public class SimpleTextEntryWindow {
       return button;
   }
 
-  public Object getStoredValue() {return this.parsed_data;}
-  public String getStoredString() {return (String)this.parsed_data;}
-  public boolean wasDataSubmitted() {return this.data_entered;}
 }
