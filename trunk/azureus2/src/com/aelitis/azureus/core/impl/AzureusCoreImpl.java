54818ea48b0fcf9003868a9d83dd0e3338724ddf
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2006-12-27 20:49:56 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@10189 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/impl/AzureusCoreImpl.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/impl/AzureusCoreImpl.java
@@ -546,7 +546,17 @@ AzureusCoreImpl
 				p_man.hasCapability( PlatformManagerCapabilities.TestNativeAvailability )){
 		
 	
-			String[]	dlls = { "niphk", "nvappfilter", "netdog", "vlsp", "imon", "sarah", "MxAVLsp", "mclsp", "radhslib", "nl_lsp" };
+			String[]	dlls = { 	"niphk", 
+									"nvappfilter", 
+									"netdog", 
+									"vlsp", 
+									"imon", 
+									"sarah", 
+									"MxAVLsp", 
+									"mclsp", 
+									"radhslib", 
+									"winsflt",
+									"nl_lsp" };
 			
 			for (int i=0;i<dlls.length;i++){
 				
