6fc1fdd609f1f7ade69a253bf471e5ce84f64e4f
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-01-11 21:54:52 +0000

    more info
    git-svn-id: http://svn.vuze.com/public/client@10317 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/Sleak.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/Sleak.java
@@ -27,28 +27,43 @@ package org.gudy.azureus2.ui.swt;
  * http://www.eclipse.org/legal/cpl-v10.html
  */
 
-import org.eclipse.swt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.*;
-import java.io.*;
 
 /**
  * Code to detect swt leak
  *
  */
-public class Sleak {
+public class Sleak
+{
 	Display display;
+
 	Shell shell;
+
 	List list;
+
 	Canvas canvas;
+
 	Button start, stop, check;
+
 	Text text;
-	Label label;
+
+	Text label;
 
 	Object[] oldObjects = new Object[0];
+
 	Error[] oldErrors = new Error[0];
+
 	Object[] objects = new Object[0];
+
 	Error[] errors = new Error[0];
+	
+	ArrayList oldNonResources = new ArrayList();
 
 	public void open() {
 		display = Display.getCurrent();
@@ -88,7 +103,7 @@ public class Sleak {
 				refreshDifference();
 			}
 		});
-		label = new Label(shell, SWT.BORDER);
+		label = new Text(shell, SWT.BORDER | SWT.READ_ONLY + SWT.MULTI);
 		label.setText("0 object(s)");
 		shell.addListener(SWT.Resize, new Listener() {
 			public void handleEvent(Event e) {
@@ -103,38 +118,54 @@ public class Sleak {
 	}
 
 	void refreshLabel() {
-		int colors = 0,
-			cursors = 0,
-			fonts = 0,
-			gcs = 0,
-			images = 0,
-			regions = 0;
+		int colors = 0, cursors = 0, fonts = 0, gcs = 0, images = 0, regions = 0, others = 0, composites = 0, labels = 0;
 		for (int i = 0; i < objects.length; i++) {
 			Object object = objects[i];
-			if (object instanceof Color)
+			if (object instanceof Color) {
 				colors++;
-			if (object instanceof Cursor)
+			} else if (object instanceof Cursor) {
 				cursors++;
-			if (object instanceof Font)
+			} else if (object instanceof Font) {
 				fonts++;
-			if (object instanceof GC)
+			} else if (object instanceof GC) {
 				gcs++;
-			if (object instanceof Image)
+			} else if (object instanceof Image) {
 				images++;
-			if (object instanceof Region)
+			} else if (object instanceof Region) {
 				regions++;
+			} else if (object instanceof Composite) {
+				composites++;
+			} else if (object instanceof Label) {
+				labels++;
+			} else {
+				others++;
+			}
 		}
 		String string = "";
-		if (colors != 0)
+		if (colors != 0) {
 			string += colors + " Color(s)\n";
-		if (cursors != 0)
+		}
+		if (cursors != 0) {
 			string += cursors + " Cursor(s)\n";
-		if (fonts != 0)
+		}
+		if (fonts != 0) {
 			string += fonts + " Font(s)\n";
-		if (gcs != 0)
+		}
+		if (gcs != 0) {
 			string += gcs + " GC(s)\n";
-		if (images != 0)
+		}
+		if (images != 0) {
 			string += images + " Image(s)\n";
+		}
+		if (composites != 0) {
+			string += composites + " composite(s)\n";
+		}
+		if (labels != 0) {
+			string += labels + " label(s)\n";
+		}
+		if (others != 0) {
+			string += others + " Other(s)\n";
+		}
 		/* Currently regions are not counted. */
 		//	if (regions != 0) string += regions + " Region(s)\n";
 		if (string.length() != 0) {
@@ -146,51 +177,97 @@ public class Sleak {
 	void refreshDifference() {
 		DeviceData info = display.getDeviceData();
 		if (!info.tracking) {
-			MessageBox dialog =
-				new MessageBox(shell, SWT.ICON_WARNING | SWT.OK);
+			MessageBox dialog = new MessageBox(shell, SWT.ICON_WARNING | SWT.OK);
 			dialog.setText(shell.getText());
-			dialog.setMessage(
-				"Warning: Device is not tracking resource allocation");
+			dialog.setMessage("Warning: Device is not tracking resource allocation");
 			dialog.open();
 		}
 		Object[] newObjects = info.objects;
 		Error[] newErrors = info.errors;
 		Object[] diffObjects = new Object[newObjects.length];
 		Error[] diffErrors = new Error[newErrors.length];
-		int count = 0;
+		int countResourceType = 0;
 		for (int i = 0; i < newObjects.length; i++) {
 			int index = 0;
 			while (index < oldObjects.length) {
-				if (newObjects[i] == oldObjects[index])
+				if (newObjects[i] == oldObjects[index]) {
 					break;
+				}
 				index++;
 			}
 			if (index == oldObjects.length) {
-				diffObjects[count] = newObjects[i];
-				diffErrors[count] = newErrors[i];
-				count++;
+				diffObjects[countResourceType] = newObjects[i];
+				diffErrors[countResourceType] = newErrors[i];
+				countResourceType++;
 			}
 		}
-		objects = new Object[count];
-		errors = new Error[count];
-		System.arraycopy(diffObjects, 0, objects, 0, count);
-		System.arraycopy(diffErrors, 0, errors, 0, count);
+
+		Shell[] shells = display.getShells();
+		ArrayList nonResourceList = new ArrayList();
+		for (int i = 0; i < shells.length; i++) {
+			Shell shell = shells[i];
+			if (shell != this.shell) {
+				buildObjectList(shell, nonResourceList);
+			}
+		}
+		oldNonResources = nonResourceList;
+		Object[] nonResources = nonResourceList.toArray();
+		int countNonResources = nonResources.length;
+
+		int total = countResourceType + countNonResources;
+
+		objects = new Object[total];
+		errors = new Error[total];
+		System.arraycopy(diffObjects, 0, objects, 0, countResourceType);
+		System.arraycopy(diffErrors, 0, errors, 0, countResourceType);
+		System.arraycopy(nonResources, 0, objects, countResourceType,
+				countNonResources);
 		list.removeAll();
 		text.setText("");
 		canvas.redraw();
 		for (int i = 0; i < objects.length; i++) {
 			list.add(objectName(objects[i]));
 		}
+		System.out.println(countResourceType);
 		refreshLabel();
 		layout();
 	}
 
+	/**
+	 * @param shell2
+	 * @param list2
+	 */
+	private void buildObjectList(Control control, ArrayList list) {
+		if (!oldNonResources.contains(control)) {
+			list.add(control);
+		}
+
+		if (control instanceof Composite) {
+			Composite c = (Composite) control;
+			Control[] children = c.getChildren();
+			for (int i = 0; i < children.length; i++) {
+				Control control2 = children[i];
+				buildObjectList(control2, list);
+			}
+		}
+	}
+
 	String objectName(Object object) {
 		String string = object.toString();
-		int index = string.lastIndexOf('.');
-		if (index == -1)
+		if (object instanceof Resource) {
 			return string;
-		return string.substring(index + 1, string.length());
+		}
+
+		int index = string.indexOf(' ');
+		if (index == -1) {
+			return string;
+		}
+		string = string.substring(0, index);
+		if (object instanceof Composite) {
+			Control[] children = ((Composite) object).getChildren();
+			string += ": " + children.length + " kids";
+		}
+		return string;
 	}
 
 	void toggleStackTrace() {
@@ -201,26 +278,33 @@ public class Sleak {
 	void paintCanvas(Event event) {
 		canvas.setCursor(null);
 		int index = list.getSelectionIndex();
-		if (index == -1)
+		if (index == -1) {
 			return;
+		}
 		GC gc = event.gc;
 		Object object = objects[index];
 		if (object instanceof Color) {
-			if (((Color) object).isDisposed())
+			if (((Color) object).isDisposed()) {
+				gc.drawString("Color disposed", 0, 0);
 				return;
+			}
 			gc.setBackground((Color) object);
 			gc.fillRectangle(canvas.getClientArea());
 			return;
 		}
 		if (object instanceof Cursor) {
-			if (((Cursor) object).isDisposed())
+			if (((Cursor) object).isDisposed()) {
+				gc.drawString("Cursor disposed", 0, 0);
 				return;
+			}
 			canvas.setCursor((Cursor) object);
 			return;
 		}
 		if (object instanceof Font) {
-			if (((Font) object).isDisposed())
+			if (((Font) object).isDisposed()) {
+				gc.drawString("Font disposed", 0, 0);
 				return;
+			}
 			gc.setFont((Font) object);
 			FontData[] array = gc.getFont().getFontData();
 			String string = "";
@@ -230,17 +314,14 @@ public class Sleak {
 				String style = "NORMAL";
 				int bits = data.getStyle();
 				if (bits != 0) {
-					if ((bits & SWT.BOLD) != 0)
+					if ((bits & SWT.BOLD) != 0) {
 						style = "BOLD ";
-					if ((bits & SWT.ITALIC) != 0)
+					}
+					if ((bits & SWT.ITALIC) != 0) {
 						style += "ITALIC";
+					}
 				}
-				string += data.getName()
-					+ " "
-					+ data.getHeight()
-					+ " "
-					+ style
-					+ lf;
+				string += data.getName() + " " + data.getHeight() + " " + style + lf;
 			}
 			gc.drawString(string, 0, 0);
 			return;
@@ -250,25 +331,42 @@ public class Sleak {
 		//		return;
 		//	}
 		if (object instanceof Image) {
-			if (((Image) object).isDisposed())
+			if (((Image) object).isDisposed()) {
+				gc.drawString("Image disposed", 0, 0);
 				return;
+			}
 			gc.drawImage((Image) object, 0, 0);
 			return;
 		}
 		if (object instanceof Region) {
-			if (((Region) object).isDisposed())
+			if (((Region) object).isDisposed()) {
 				return;
+			}
 			String string = ((Region) object).getBounds().toString();
 			gc.drawString(string, 0, 0);
+			return;
+		}
+
+		if (object instanceof Control) {
+			gc.drawString(object.toString(), 0, 0);
+			gc.drawString(((Control) object).getBounds().toString(), 0, 20);
+
+			if (object instanceof Widget) {
+				Object data = ((Widget)object).getData("sleak");
+				if (data != null) {
+					gc.drawString(data.toString(), 0, 35);
+				}
+			}
 			return;
 		}
 	}
 
 	void refreshObject() {
 		int index = list.getSelectionIndex();
-		if (index == -1)
+		if (index == -1) {
 			return;
-		if (check.getSelection()) {
+		}
+		if (check.getSelection() && index < errors.length && errors[index] == null) {
 			ByteArrayOutputStream stream = new ByteArrayOutputStream();
 			PrintStream s = new PrintStream(stream);
 			errors[index].printStackTrace(s);
@@ -285,13 +383,14 @@ public class Sleak {
 	void refreshAll() {
 		oldObjects = new Object[0];
 		oldErrors = new Error[0];
+		oldNonResources = new ArrayList();
 		refreshDifference();
 		oldObjects = objects;
 		oldErrors = errors;
 	}
 
 	void layout() {
-		Rectangle rect = shell.getClientArea();		
+		Rectangle rect = shell.getClientArea();
 		int width = 0;
 		String[] items = list.getItems();
 		GC gc = new GC(list);
@@ -304,10 +403,8 @@ public class Sleak {
 		Point size3 = check.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		Point size4 = label.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		width = Math.max(size1.x, Math.max(size2.x, Math.max(size3.x, width)));
-		width =
-			Math.max(
-				64,
-				Math.max(size4.x, list.computeSize(width, SWT.DEFAULT).x));
+		width = Math.max(64, Math.max(size4.x,
+				list.computeSize(width, SWT.DEFAULT).x));
 		start.setBounds(0, 0, width, size1.y);
 		stop.setBounds(0, size1.y, width, size2.y);
 		check.setBounds(0, size1.y + size2.y, width, size3.y);
@@ -329,7 +426,13 @@ public class Sleak {
 			if (!display.readAndDispatch())
 				display.sleep();
 		}
-		display.dispose();
+		try {
+			if (!display.isDisposed()) {
+				display.dispose();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 	}
 
 }
