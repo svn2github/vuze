91009555f6eb6b6787bc70dbf00de260fb134b4d
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-06-27 04:08:10 +0000

    Split TableCellMouseEvent into a Row and a Cell one (cell inherits row class)
    git-svn-id: http://svn.vuze.com/public/client@11192 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/plugins/ui/tables/TableCellMouseEvent.java
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/ui/tables/TableCellMouseEvent.java
@@ -20,83 +20,18 @@ package org.gudy.azureus2.plugins.ui.tables;
 /**
  * Mouse event information for 
  * {@link org.gudy.azureus2.plugins.ui.tables.TableCellMouseListener}
+ * <br><br>
+ * Note: 3.0.1.7 moved most functions to {@link TableRowMouseEvent}
  * 
  * @author TuxPaper
  * @created Jan 10, 2006
  * @since 2.3.0.7
  */
-public class TableCellMouseEvent {
-	/** 
-	 * eventType is triggered when mouse is pressed down
-	 *  
-	 * @since 2.3.0.7
-	 */
-	public final static int EVENT_MOUSEDOWN = 0;
-
-	/** 
-	 * eventType is triggered when mouse is let go 
-	 *  
-	 * @since 2.3.0.7
-	 */
-	public final static int EVENT_MOUSEUP = 1;
-
-	/** 
-	 * eventType is trigggered when mouse is double clicked 
-	 *  
-	 * @since 2.3.0.7
-	 */
-	public final static int EVENT_MOUSEDOUBLECLICK = 2;
-
-	/**
-	 * EVENT_* constant specifying the type of event that has been triggered 
-	 *  
-	 * @since 2.3.0.7
-	 */
-	public int eventType;
-
+public class TableCellMouseEvent extends TableRowMouseEvent {
 	/**
 	 * TableCell that the mouse trigger applies to
 	 *  
 	 * @since 2.3.0.7
 	 */
 	public TableCell cell;
-
-	/**
-	 * x position of mouse relative to table cell
-	 *  
-	 * @since 2.3.0.7
-	 */
-	public int x;
-
-	/**
-	 * y position of mouse relative to table cell
-	 */
-	public int y;
-
-	/**
-	 * Which button was pressed.  1 = first button (left),
-	 * 2 = second button (middle), 3 = third button (right) 
-	 *  
-	 * @since 2.3.0.7
-	 */
-	public int button;
-
-	/**
-	 * Keyboard state when the mouse event was triggered.
-	 * 
-	 * @TODO Define state constants
-	 *  
-	 * @since 2.3.0.7
-	 */
-	public int keyboardState;
-
-	/**
-	 * Setting this value to true will prevent Azureus from running it's
-	 * core functionality (if any) for the mouse event.
-	 * 
-	 * For example, by default the double click in My Torrents goes to
-	 * the details view.  If your plugin remaps double click to do a different
-	 * function, set skipCoreFunctionality = true.
-	 */
-	public boolean skipCoreFunctionality;
 }
