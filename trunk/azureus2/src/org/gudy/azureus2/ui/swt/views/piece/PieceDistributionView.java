18edce3cd347d17ceec9fc79ca3ea05ad24a4360
the8472 <the8472@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-07-27 22:37:54 +0000

    -i18n -getting rid of those evil GC transforms... ashes to ashes, pixel to pixel... -better coloring and bar formatting
    git-svn-id: http://svn.vuze.com/public/client@11473 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/piece/PieceDistributionView.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/piece/PieceDistributionView.java
@@ -20,15 +20,14 @@
 
 package org.gudy.azureus2.ui.swt.views.piece;
 
-import java.util.Arrays;
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.*;
 import org.gudy.azureus2.core3.disk.DiskManagerPiece;
 import org.gudy.azureus2.core3.download.DownloadManager;
-import org.gudy.azureus2.core3.peer.PEPeerManager;
+import org.gudy.azureus2.core3.internat.MessageText;
+import org.gudy.azureus2.core3.peer.PEPiece;
 import org.gudy.azureus2.ui.swt.mainwindow.Colors;
 import org.gudy.azureus2.ui.swt.views.AbstractIView;
 import org.gudy.azureus2.ui.swt.views.utils.CoordinateTransform;
@@ -37,8 +36,8 @@ import com.aelitis.azureus.core.peermanager.piecepicker.PiecePicker;
 
 
 /**
- * @author TuxPaper
- * @created Feb 26, 2007
+ * @author The8472
+ * @created Jun 28, 2007
  *
  */
 public class PieceDistributionView
@@ -106,8 +105,10 @@ public class PieceDistributionView
 		final int nbPieces = picker.getNumberOfPieces();
 		final int[] availabilties = picker.getAvailability();
 		final DiskManagerPiece[] dmPieces = dlm.getDiskManager().getPieces();
+		final PEPiece[] pePieces = dlm.getPeerManager().getPieces();
 		final int[] piecesPerAvailability = new int[upperBound];
 		final int[] ourPiecesPerAvailability = new int[upperBound];
+		final boolean[] downloading = new boolean[upperBound];
 		
 		int avlPeak = 0;
 		int avlPeakIdx = -1;
@@ -115,7 +116,7 @@ public class PieceDistributionView
 		for(int i=0;i<nbPieces;i++)
 		{
 			if(availabilties[i] >= upperBound) 
-				return; // availability lists and peer lists are OOS, just wait for the next round
+				return; // availability and peer lists are OOS, just wait for the next round
 			final int newPeak;
 			if(avlPeak < (newPeak = ++piecesPerAvailability[availabilties[i]]))
 			{
@@ -124,21 +125,22 @@ public class PieceDistributionView
 			}
 			if(dmPieces[i].isDone())
 				++ourPiecesPerAvailability[availabilties[i]];
+			if(pePieces[i] != null)
+				downloading[availabilties[i]] = true;
 		}
 		
 		img = new Image(pieceDistGC.getDevice(),pieceDistCanvas.getBounds());
 		
 		GC gc = new GC(img);
-		Transform scaler = new Transform(gc.getDevice());
 
 		try
 		{
-			//System.out.println("bound:"+upperBound+" peak:"+avlPeak);
-			
-			scaler.translate(0,rect.height);
-			scaler.scale(1F, -1F);
-			scaler.scale(1F*rect.width/(upperBound), 1F*(rect.height-1F)/(avlPeak+1F));
-			gc.setTransform(scaler);
+			int stepWidthX = (int)Math.floor(rect.width/upperBound);
+			int barGap = 2;
+			int barWidth = stepWidthX-barGap;
+			int barFillingWidth = barWidth-1;
+			int stepWidthY = (int)Math.floor(rect.height/avlPeak);
+			int offsetY = rect.height;
 			
 			gc.setForeground(Colors.green);
 			for(int i=0;i<connected;i++)
@@ -147,17 +149,29 @@ public class PieceDistributionView
 				if(i<seeds)
 					curColor = Colors.green;
 				else
-					curColor = Colors.blue;
+					curColor = Colors.blues[Colors.BLUES_DARKEST];
 				if(i==minAvail)
-					curColor = Colors.red;
+					curColor = Colors.blue;
 				gc.setBackground(curColor);
 				gc.setForeground(curColor);
-				gc.drawLine(i, 0, (i+1), 0);
-				gc.drawRectangle(i, 0, 1, piecesPerAvailability[i]);
-				gc.fillRectangle(i, 0, 1, ourPiecesPerAvailability[i]);
 				
+				if(piecesPerAvailability[i] == 0)
+				{
+					gc.setLineWidth(3);
+					gc.drawLine(stepWidthX*i, offsetY-1, stepWidthX*i+barWidth, offsetY-1);
+				} else
+				{
+					gc.setLineWidth(1);
+					if(downloading[i])
+						gc.setLineStyle(SWT.LINE_DASH);
+					gc.fillRectangle(stepWidthX*i+1, offsetY, barFillingWidth, stepWidthY*ourPiecesPerAvailability[i]*-1);
+					gc.drawRectangle(stepWidthX*i, offsetY, barWidth, (stepWidthY*piecesPerAvailability[i]+1)*-1);
+				}
+				
+				gc.setLineStyle(SWT.LINE_SOLID);
 			}
 			
+			gc.setLineWidth(1);			
 			gc.setTransform(null);
 			
 			CoordinateTransform t = new CoordinateTransform(rect);
@@ -165,45 +179,59 @@ public class PieceDistributionView
 			t.shiftExternal(rect.width,0);
 			t.scale(-1.0, 1.0);
 			
-			gc.setForeground(Colors.green);
-			gc.setBackground(Colors.background);
+			String[] boxContent = new String[] {
+				MessageText.getString("PiecesView.DistributionView.SeedAvl"),
+				MessageText.getString("PiecesView.DistributionView.PeerAvl"),
+				MessageText.getString("PiecesView.DistributionView.RarestAvl",new String[] {piecesPerAvailability[minAvail]+"",minAvail+""}),
+				MessageText.getString("PiecesView.DistributionView.weHave"),
+				MessageText.getString("PiecesView.DistributionView.weDownload")				
+				};
+
 			int charWidth = gc.getFontMetrics().getAverageCharWidth();
 			int charHeight = gc.getFontMetrics().getHeight();
-			
-			String[] boxContent = new String[] {
-				"Seed Avl Contribution",
-				"Peer Avl Contribution",
-				"Rarest Pieces: "+piecesPerAvailability[minAvail]+" (Avl:"+minAvail+")"
-				};
-			
+			int maxBoxOffsetY = charHeight+2;
 			int maxBoxWidth = 0;
-			int maxBoxOffset = 0;
+			int maxBoxOffsetX = 0;
 			for(int i=0;i<boxContent.length;i++)
 				maxBoxWidth = Math.max(maxBoxWidth, boxContent[i].length());
 			
-			maxBoxOffset = (maxBoxWidth+5) * charWidth;
+			maxBoxOffsetX = (maxBoxWidth+5) * charWidth;
 			maxBoxWidth = ++maxBoxWidth * charWidth;
 			
-		
-			gc.drawRectangle(t.x(maxBoxOffset),t.y(charHeight*1),maxBoxWidth,charHeight);
-			gc.drawString(boxContent[0],t.x(maxBoxOffset-5),t.y(charHeight*1),true);
-			
-			gc.setForeground(Colors.blue);
-			gc.drawRectangle(t.x(maxBoxOffset),t.y(charHeight*3),maxBoxWidth,charHeight);
-			gc.drawString(boxContent[1],t.x(maxBoxOffset-5),t.y(charHeight*3),true);
-			
-			gc.setForeground(Colors.red);
-			gc.drawRectangle(t.x(maxBoxOffset),t.y(charHeight*5),maxBoxWidth,charHeight);
-			gc.drawString(boxContent[2],t.x(maxBoxOffset-5),t.y(charHeight*5),true);
 
+			gc.setForeground(Colors.green);
+			gc.setBackground(Colors.background);
+			gc.drawRectangle(t.x(maxBoxOffsetX),t.y(maxBoxOffsetY),maxBoxWidth,charHeight);
+			gc.drawString(boxContent[0],t.x(maxBoxOffsetX-5),t.y(maxBoxOffsetY),true);
 			
+			gc.setForeground(Colors.blues[Colors.BLUES_DARKEST]);
+			gc.drawRectangle(t.x(maxBoxOffsetX),t.y(maxBoxOffsetY*2),maxBoxWidth,charHeight);
+			gc.drawString(boxContent[1],t.x(maxBoxOffsetX-5),t.y(maxBoxOffsetY*2),true);
+
+			gc.setForeground(Colors.blue);
+			gc.drawRectangle(t.x(maxBoxOffsetX),t.y(maxBoxOffsetY*3),maxBoxWidth,charHeight);
+			gc.drawString(boxContent[2],t.x(maxBoxOffsetX-5),t.y(maxBoxOffsetY*3),true);
+			
+			gc.setForeground(Colors.black);
+			gc.setBackground(Colors.black);
+			gc.drawRectangle(t.x(maxBoxOffsetX),t.y(maxBoxOffsetY*4),maxBoxWidth,charHeight);
+			gc.fillRectangle(t.x(maxBoxOffsetX),t.y(maxBoxOffsetY*4),maxBoxWidth/2,charHeight);
+			gc.setForeground(Colors.grey);
+			gc.setBackground(Colors.background);
+			gc.drawString(boxContent[3],t.x(maxBoxOffsetX-5),t.y(maxBoxOffsetY*4),true);
+			
+			gc.setForeground(Colors.black);
+			gc.setLineStyle(SWT.LINE_DASH);
+			gc.drawRectangle(t.x(maxBoxOffsetX),t.y(maxBoxOffsetY*5),maxBoxWidth,charHeight);
+			gc.drawString(boxContent[4],t.x(maxBoxOffsetX-5),t.y(maxBoxOffsetY*5),true);
+			
+			gc.setLineStyle(SWT.LINE_SOLID);
+		
 		} finally
 		{
 			gc.dispose();
 		}
-
-		
-		
+	
 		
 	}
 
