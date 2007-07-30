7143f83e7ab402982454114b1ea42ee866e5138e
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-07-30 16:27:39 +0000

    don't switch image on exit if exiting to parent to propogates, or exiting to children and we propogate
    git-svn-id: http://svn.vuze.com/public/client@16582 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/skin/SWTSkinImageChanger.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/skin/SWTSkinImageChanger.java
@@ -4,9 +4,9 @@
 package com.aelitis.azureus.ui.swt.skin;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.*;
 
 /**
  * @author TuxPaper
@@ -39,6 +39,10 @@ public class SWTSkinImageChanger
 
 	public void handleEvent(Event event) {
 		Control control = (Control) event.widget;
+		
+		if (control == null) {
+			return;
+		}
 
 		try {
 			if (event.type == SWT.MouseExit && lastControl != null) {
@@ -48,9 +52,46 @@ public class SWTSkinImageChanger
 			}
 
 			SWTSkinObject skinObject = (SWTSkinObject) control.getData("SkinObject");
+
+			if (event.type == SWT.MouseExit
+					&& (skinObject instanceof SWTSkinObjectContainer)) {
+				// check if exiting and going into child
+				SWTSkinObjectContainer soContainer = (SWTSkinObjectContainer) skinObject;
+				if (soContainer.getPropogation()) {
+					Point pt = control.toDisplay(event.x, event.y);
+					Composite composite = soContainer.getComposite();
+					Point relPt = composite.toControl(pt);
+					Rectangle bounds = composite.getBounds();
+					if (relPt.x >= 0 && relPt.y >= 0 && relPt.x < bounds.width
+							&& relPt.y < bounds.height) {
+						//System.out.println("skip " + skinObject + " because going into child");
+						return;
+					}
+				}
+			}
+
+			if (event.type == SWT.MouseExit) {
+				// check if exiting and going into parent
+				Composite parent = control.getParent();
+				SWTSkinObject soParent = (SWTSkinObject) parent.getData("SkinObject");
+				if (soParent != null && (soParent instanceof SWTSkinObjectContainer)) {
+					SWTSkinObjectContainer container = (SWTSkinObjectContainer) soParent;
+					if (container.getPropogation()) {
+						Point pt = control.toDisplay(event.x, event.y);
+						Point relPt = container.getComposite().toControl(pt);
+						Rectangle bounds = parent.getBounds();
+						if (relPt.x >= 0 && relPt.y >= 0 && relPt.x < bounds.width
+								&& relPt.y < bounds.height) {
+							//System.out.println("skip " + skinObject + " because going into parent");
+							return;
+						}
+					}
+				}
+			}
+
 			if (skinObject != null) {
 				String sSuffix = (event.type == eventOn) ? suffix : "";
-				//System.out.println(System.currentTimeMillis() + ": " + skinObject + "--" + suffix);
+				//System.out.println(System.currentTimeMillis() + ": " + skinObject + "--" + sSuffix);
 
 				skinObject.switchSuffix(sSuffix, 2, true);
 			}
