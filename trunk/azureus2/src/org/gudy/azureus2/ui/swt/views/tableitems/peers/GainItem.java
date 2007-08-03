294c9f64c2c6f9345847aa931ba39f716fca485a
the8472 <the8472@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-08-03 20:49:38 +0000

    fixed licencing stuff
    git-svn-id: http://svn.vuze.com/public/client@11568 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/peers/GainItem.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/peers/GainItem.java
@@ -1,27 +1,23 @@
-/*
- * File    : UpItem.java
- * Created : 24 nov. 2003
- * By      : Olivier
+/**
+ * Copyright (C) 2007 Aelitis, All Rights Reserved.
  *
- * Copyright (C) 2004, 2005, 2006 Aelitis SAS, All rights Reserved
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License.
- *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details ( see the LICENSE file ).
- *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
- * AELITIS, SAS au capital de 46,603.30 euros,
+ * AELITIS, SAS au capital de 63.529,40 euros
  * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
+ *
  */
- 
+
 package org.gudy.azureus2.ui.swt.views.tableitems.peers;
 
 import org.gudy.azureus2.core3.util.DisplayFormatters;
@@ -31,9 +27,8 @@ import org.gudy.azureus2.plugins.ui.tables.*;
 import org.gudy.azureus2.ui.swt.views.table.utils.CoreTableColumn;
 
 /**
- *
- * @author Olivier
- * @author TuxPaper (2004/Apr/19: modified to TableCellAdapter)
+ * @author The8472
+ * @created Jun 28, 2007
  */
 public class GainItem
        extends CoreTableColumn 
