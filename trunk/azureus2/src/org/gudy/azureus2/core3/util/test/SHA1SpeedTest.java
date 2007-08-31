4a2baedfe9d02649ad03433b8223b2d072c3d4b6
the8472 <the8472@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-08-31 21:23:45 +0000

    recommiting improved SHA1 implementation. If you want to compare speeds with the old version just run SHA1SpeedTest.main
    git-svn-id: http://svn.vuze.com/public/client@11732 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/test/SHA1SpeedTest.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/test/SHA1SpeedTest.java
@@ -23,89 +23,206 @@
 package org.gudy.azureus2.core3.util.test;
 
 import java.nio.*;
+import java.util.Arrays;
+import java.util.Random;
 
-//import org.gudy.azureus2.core3.util.SHA1;
+// import org.gudy.azureus2.core3.util.SHA1;
+import org.gudy.azureus2.core3.util.ByteFormatter;
+import org.gudy.azureus2.core3.util.HashWrapper;
 import org.gudy.azureus2.core3.util.SHA1;
 
 /**
  */
 public class SHA1SpeedTest {
-  
-  private static final int BUFF_MAX_SIZE = 4 * 1024 * 1024;
-  
-  private static final int[] LOOPS = {30000, 15000, 4000, 3000, 2000, 1200,  800};
-  private static final int[] TESTS = {   16,    64,  256,  512, 1024, 2048, 4096};
-  
-	public static void main(String[] args) {
-    
-    //SHA1 sha1Jmule = new SHA1();
-    SHA1 sha1Gudy = new SHA1();
-    
-    ByteBuffer dBuffer = ByteBuffer.allocateDirect( BUFF_MAX_SIZE );
-    ByteBuffer fBuffer = ByteBuffer.allocateDirect( 20 );
-    
-    fBuffer.order(ByteOrder.BIG_ENDIAN);
-    
-    for (int i=0; i < BUFF_MAX_SIZE; i++) {
-      byte b = (byte)(Math.random() * 255);
-      dBuffer.put( b );
-    }
-    
-    //allow time for setting thread to high-priority
-    try { Thread.sleep(10000); } catch (Exception ignore) {}
-    
 
-    for (int t=0; t < TESTS.length; t++) {
-      
-      int buffsize = TESTS[t] * 1024;
-      dBuffer.limit( buffsize );
-      
-      int loops = LOOPS[t];
-    
-    	String info = " [" + buffsize/1024 + "KB, " + loops + "x] = ";
-      
-    	float totalMBytes = ((float)buffsize / (1024 * 1024)) * loops;
-    
-    
-    	System.out.print("JMule SHA1");
-    	long jds = System.currentTimeMillis();
-    	for (int i=0; i < loops; i++) {
-    		dBuffer.position(0);
-        dBuffer.limit( buffsize );
-        fBuffer.position(0);
-        //sha1Jmule.update( dBuffer );
-    		//sha1Jmule.finalDigest( fBuffer );
-    	}
-    	long jde = System.currentTimeMillis();
-  
-    	long jdt = (jde - jds);
-    	float jdspeed = totalMBytes / (jdt / 1000);
-      
-    	System.out.println(info + jdt + " ms @ " + jdspeed + " MB/s");
+	private static final int	BUFF_MAX_SIZE	= 4 * 1024 * 1024;
 
-    
-    	System.out.print("Gudy SHA1 ");
-    	long gds = System.currentTimeMillis();
-    	for (int i=0; i < loops; i++) {
-        dBuffer.position(0);
-        dBuffer.limit( buffsize );
-    		sha1Gudy.reset();
-        sha1Gudy.update( dBuffer );
-    		sha1Gudy.digest();
-    	}
-    	long gde = System.currentTimeMillis();
-    
-    	long gdt = (gde - gds);
-    	float gdspeed = totalMBytes / (gdt / 1000);
-    	System.out.println(info + gdt + " ms @ " + gdspeed + " MB/s");
-   
-      System.out.println();
-    
-    }
-    
-    System.out.println("DONE");
-    
-    
+	private static final int[]	LOOPS			= { 1000000, 30000, 15000, 4000, 3000, 2000, 1200, 800 };
+	private static final int[]	TESTS			= { 1, 16, 64, 256, 512, 1024, 2048, 4096 };
+	
+	private static final int TEST_SPEED_FACTOR = 1; // use larger numbers for less tests
+
+	public static void main(String[] args)
+	{
+		
+		Random rnd = new Random();
+
+		SHA1Old oldsha = new SHA1Old();
+		SHA1 newsha = new SHA1();
+
+		ByteBuffer dBuffer = ByteBuffer.allocateDirect(BUFF_MAX_SIZE);
+		ByteBuffer hBuffer = ByteBuffer.allocate(BUFF_MAX_SIZE);
+
+		for (int i = 0; i < BUFF_MAX_SIZE; i++)
+		{
+			byte b = (byte) (rnd.nextInt()&0xFF);
+			dBuffer.put(b);
+		}
+		
+		dBuffer.rewind();
+		hBuffer.put(dBuffer);
+		hBuffer.rewind();
+		dBuffer.rewind();
+		
+
+		// allow time for setting thread to high-priority
+		try
+		{
+			System.out.println("Setting high thread priority to decrease test jitter");
+			Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
+			Thread.sleep(2000);
+		} catch (Exception ignore)
+		{
+		}
+
+		for (int t = 0; t < TESTS.length; t++)
+		{
+
+			int buffsize = TESTS[t] * 1024;
+
+			dBuffer.position(0);
+			dBuffer.limit(buffsize);
+			hBuffer.position(0);
+			hBuffer.limit(buffsize);
+
+			
+			int loops = LOOPS[t]/TEST_SPEED_FACTOR;
+
+			String info = " [" + buffsize / 1024 + "KB, " + loops + "x] = ";
+
+			double totalMBytes = ((double) buffsize / (1024 * 1024)) * loops;
+
+			
+			long time;
+			double speed;
+
+			
+			System.out.println("direct:");
+			
+			System.out.print("Old SHA1");
+			time = System.currentTimeMillis();
+			for (int i = 0; i < loops; i++)
+			{
+				oldsha.reset();
+				oldsha.digest(dBuffer);
+			}
+			time = System.currentTimeMillis() - time;
+			speed = totalMBytes / (time / (double)1024);
+			System.out.println(info + time + " ms @ " + speed + " MiB/s");
+
+			
+			System.out.print("New SHA1 ");
+			time = System.currentTimeMillis();
+			for (int i = 0; i < loops; i++)
+			{
+				newsha.reset();
+				newsha.digest(dBuffer);
+			}
+			time = System.currentTimeMillis() - time;
+			speed = totalMBytes / (time / (double)1024);
+			System.out.println(info + time + " ms @ " + speed + " MiB/s");
+
+			System.out.println("heap:");
+			
+			System.out.print("Old SHA1");
+			time = System.currentTimeMillis();
+			for (int i = 0; i < loops; i++)
+			{
+				oldsha.reset();
+				oldsha.digest(hBuffer);
+			}
+			time = System.currentTimeMillis() - time;
+			speed = totalMBytes / (time / (double)1024);
+			System.out.println(info + time + " ms @ " + speed + " MiB/s");
+
+			
+			System.out.print("New SHA1 ");
+			time = System.currentTimeMillis();
+			for (int i = 0; i < loops; i++)
+			{
+				newsha.reset();
+				newsha.digest(hBuffer);
+			}
+			time = System.currentTimeMillis() - time;
+			speed = totalMBytes / (time / (double)1024);
+			System.out.println(info + time + " ms @ " + speed + " MiB/s");
+
+			System.out.println();
+		}
+		
+		
+		System.out.println("performing randomized buffer windowing checks, this may take a while");
+		
+		byte[] oldd;
+		byte[] newd;
+		byte[] oldh;
+		byte[] newh;
+		
+		int size;
+		int offset;
+		
+		ByteBuffer dview;
+		ByteBuffer hview;
+		
+		for(int i=0;i<LOOPS[1]/TEST_SPEED_FACTOR;i++)
+		{
+			size = rnd.nextInt(BUFF_MAX_SIZE);
+			offset = rnd.nextInt(BUFF_MAX_SIZE-size-1);
+			
+			hBuffer.limit(offset+size);
+			hBuffer.position(offset);
+			dBuffer.limit(offset+size);
+			dBuffer.position(offset);
+			
+			oldsha.reset();
+			newsha.reset();
+			oldh = oldsha.digest(hBuffer);
+			newh = newsha.digest(hBuffer);
+			oldsha.reset();
+			newsha.reset();
+			oldd = oldsha.digest(dBuffer);
+			newd = newsha.digest(dBuffer);
+			
+			
+			if(!Arrays.equals(oldh, newh) || !Arrays.equals(oldd, newd) || !Arrays.equals(oldd, oldh))
+			{
+				System.out.println("hash mismatch at offset: "+offset+" size: "+size);
+				System.out.println("\t\t"+ByteFormatter.nicePrint(oldh));
+				System.out.println("\t\t"+ByteFormatter.nicePrint(newh));
+				System.out.println("\t\t"+ByteFormatter.nicePrint(oldd));
+				System.out.println("\t\t"+ByteFormatter.nicePrint(newd));
+			}
+				
+			if(hBuffer.limit() != offset+size || dBuffer.limit() != offset+size || hBuffer.position() != offset || dBuffer.position() != offset)
+				System.out.println("buffer does not match its original state");
+			
+			dview = dBuffer.slice();
+			hview = hBuffer.slice();
+			
+			oldsha.reset();
+			newsha.reset();
+			oldh = oldsha.digest(hview);
+			newh = newsha.digest(hview);
+			oldsha.reset();
+			newsha.reset();
+			oldd = oldsha.digest(dview);
+			newd = newsha.digest(dview);
+			
+			
+			if(!Arrays.equals(oldh, newh) || !Arrays.equals(oldd, newd) || !Arrays.equals(oldd, oldh))
+			{
+				System.out.println("(view) hash mismatch at offset: "+offset+" size: "+size);
+				System.out.println("\t\t"+ByteFormatter.nicePrint(oldh));
+				System.out.println("\t\t"+ByteFormatter.nicePrint(newh));
+				System.out.println("\t\t"+ByteFormatter.nicePrint(oldd));
+				System.out.println("\t\t"+ByteFormatter.nicePrint(newd));
+			}
+				
+			if(hview.limit() != hview.capacity() || dview.limit() != dview.capacity() || hview.position() != 0 || dview.position() != 0)
+				System.out.println("view buffer does not match its original state");
+		}
+		System.out.println("DONE");
+
 	}
 
 }
