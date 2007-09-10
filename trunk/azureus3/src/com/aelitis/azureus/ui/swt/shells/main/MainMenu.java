0c9cefee033dc4c09333105a33dbd5f1a4d65632
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-09-10 07:19:12 +0000

    sliding (should be moved out of MainMenu)
    git-svn-id: http://svn.vuze.com/public/client@16687 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/main/MainMenu.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/main/MainMenu.java
@@ -332,18 +332,31 @@ public class MainMenu
 		if (skinObject != null) {
 			final Control control = skinObject.getControl();
 			if (control != null && !control.isDisposed()) {
+				if (control.getData("Sliding") != null) {
+					return;
+				}
 				if (visible) {
-					final Point size = (Point) control.getData("v3.oldHeight");
+					Point size = (Point) control.getData("v3.oldHeight");
+					//System.out.println("oldHeight = " + size + ";v=" + control.getVisible() + ";s=" + control.getSize());
+					if (size == null && control.getSize().y < 2) {
+						size = control.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+					}
+
 					if (size != null) {
 						final FormData fd = (FormData) control.getLayoutData();
 						if (fd != null && (fd.width != size.x || fd.height != size.y)) {
 							slide(control, fd, size);
 						}
 					}
+					control.setData("v3.oldHeight", null);
 				} else {
 					final FormData fd = (FormData) control.getLayoutData();
 					if (fd != null) {
-						control.setData("v3.oldHeight", new Point(fd.width, fd.height));
+						Point oldSize = new Point(fd.width, fd.height);
+						if (oldSize.y <= 0) {
+							oldSize = null;
+						}
+						control.setData("v3.oldHeight", oldSize);
 						final Point size = new Point(0, 0);
 
 						slide(control, fd, size);
@@ -360,29 +373,44 @@ public class MainMenu
 	}
 
 	private static void slide(final Control control, final FormData fd, final Point size) {
+		System.out.println("slid to " + size);
 		AERunnable runnable = new AERunnable() {
+			boolean firstTime = true;
+
 			public void runSupport() {
 				if (control.isDisposed()) {
 					return;
 				}
-				if (true) {
+				if (true && false) {
+					// sliding disabled until we prevent slide in being triggered while
+					// sliding out (or visa-versa)
 					fd.width = size.x;
 					fd.height = size.y;
+					System.out.println(size);
 					control.setLayoutData(fd);
 					Utils.relayout(control);
 					return;
 				}
-				
-				// sliding disabled until we prevent slide in being triggered while
-				// sliding out (or visa-versa)
+				if (firstTime) {
+					firstTime = false;
+					if (control.getData("Sliding") != null) {
+						return;
+					}
+					control.setData("Sliding", "1");
+				}
 
 				int newWidth = (int) (fd.width + (size.x - fd.width) * 0.4);
-				int newHeight = (int) (fd.height + (size.y - fd.height) * 0.4);
-				if (newWidth == fd.width && newHeight == fd.height) {
+				int h = fd.height >= 0 ? fd.height : control.getSize().y;
+				int newHeight = (int) (h + (size.y - h) * 0.4);
+				//System.out.println(control + "] newh=" + newHeight + " to " + size.y);
+				
+				if (newWidth == fd.width && newHeight == h) {
 					fd.width = size.x;
 					fd.height = size.y;
+					//System.out.println(control + "] side to " + size.y + " done");
 					control.setLayoutData(fd);
 					Utils.relayout(control);
+					control.setData("Sliding", null);
 				} else {
 					fd.width = newWidth;
 					fd.height = newHeight;
