070493db4db59e4a6c9869f2052d885cb972dd11
the8472 <the8472@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-09-16 22:45:31 +0000

    refresh gfx-columns to make Tux happy
    git-svn-id: http://svn.vuze.com/public/client@11861 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/table/impl/TableViewSWTImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/table/impl/TableViewSWTImpl.java
@@ -3087,7 +3087,7 @@ public class TableViewSWTImpl
 						TableRowSWT row = (TableRowSWT) iter.next();
 						TableCellSWT cell = row.getTableCellSWT(sColumnID);
 						if (cell != null) {
-							cell.refresh(false,false,false);
+							cell.refresh(true,false,false);
 						}
 					}
 				}
