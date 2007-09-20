13f5c6e5f5cdab299945b876a5e2e5232d62618d
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-09-20 21:08:49 +0000

    3031
    git-svn-id: http://svn.vuze.com/public/client@11881 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
@@ -78,8 +78,8 @@ Constants
     //      2.0.8.3_Bnn       // incremental build
   
   public static final String AZUREUS_NAME	  = "Azureus";
-  public static final String AZUREUS_VERSION  = "3.0.3.0";  //3.0.3.1_CVS
-  public static final byte[] VERSION_ID       = ("-" + "AZ" + "3030" + "-").getBytes();  //MUST be 8 chars long!
+  public static final String AZUREUS_VERSION  = "3.0.3.1_CVS";  //3.0.3.1_CVS
+  public static final byte[] VERSION_ID       = ("-" + "AZ" + "3031" + "-").getBytes();  //MUST be 8 chars long!
   
   
   public static final String  OSName = System.getProperty("os.name");
