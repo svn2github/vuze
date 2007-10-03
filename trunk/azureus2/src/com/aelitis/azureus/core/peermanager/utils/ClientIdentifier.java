35e35f8afb38a29585c03f33a3fb0cf547e110ef
amc1 <amc1@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-10-03 07:48:23 +0000

    Last checkin messed up the logic handling our explicit mismatch exception code, now fixed.
    git-svn-id: http://svn.vuze.com/public/client@11919 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/peermanager/utils/ClientIdentifier.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/peermanager/utils/ClientIdentifier.java
@@ -158,10 +158,10 @@ public class ClientIdentifier {
 		// We allow a client to have a different version number than the one decoded from
 		// the peer ID. Some clients separate version and client name using a forward slash,
 		// so we split on that as well.
-		String client_type_peer = peer_id_name.split(" ", 2)[0].toLowerCase();
-		String client_type_handshake = handshake_name_to_process.split(" ", 2)[0].split("/", 2)[0].toLowerCase();
+		String client_type_peer = peer_id_name.split(" ", 2)[0];
+		String client_type_handshake = handshake_name_to_process.split(" ", 2)[0].split("/", 2)[0];
 		
-		if (client_type_peer.equals(client_type_handshake)) {return handshake_name_to_process;}
+		if (client_type_peer.toLowerCase().equals(client_type_handshake.toLowerCase())) {return handshake_name_to_process;}
 		
 		// Bloody XTorrent.
 		if (handshake_name_to_process.equals("Transmission 0.7-svn") && client_type_peer.equals("Azureus")) {
