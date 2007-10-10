a88e5da5ba7090e0a6f956ea96a64a5296de4f9d
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-10-10 22:37:28 +0000

    show remaining bytes on 2nd line
    git-svn-id: http://svn.vuze.com/public/client@11947 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/mytorrents/SizeItem.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/mytorrents/SizeItem.java
@@ -21,7 +21,7 @@
  * AELITIS, SAS au capital de 46,603.30 euros,
  * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
  */
- 
+
 package org.gudy.azureus2.ui.swt.views.tableitems.mytorrents;
 
 import org.gudy.azureus2.core3.util.DisplayFormatters;
@@ -29,31 +29,70 @@ import org.gudy.azureus2.core3.download.DownloadManager;
 import org.gudy.azureus2.plugins.ui.tables.*;
 import org.gudy.azureus2.ui.swt.views.table.utils.CoreTableColumn;
 
-
 /** Size of Torrent cell
  *
  * @author Olivier
  * @author TuxPaper (2004/Apr/17: modified to TableCellAdapter)
  */
 public class SizeItem
-       extends CoreTableColumn 
-       implements TableCellRefreshListener
+	extends CoreTableColumn
+	implements TableCellRefreshListener
 {
 	public static String COLUMN_ID = "size";
 
-  /** Default Constructor */
+	private static boolean DO_MULTILINE = true;
+
+	/** Default Constructor */
 	public SizeItem(String sTableID) {
 		super(COLUMN_ID, ALIGN_TRAIL, POSITION_LAST, 70, sTableID);
 		setMinWidthAuto(true);
 	}
 
-  public void refresh(TableCell cell) {
-    DownloadManager dm = (DownloadManager)cell.getDataSource();
-    long value = (dm == null) ? 0 : dm.getSize();
+	public void refresh(TableCell cell) {
+		DownloadManager dm = (DownloadManager) cell.getDataSource();
+		sizeitemsort value = (dm == null) ? new sizeitemsort(0, 0)
+				: new sizeitemsort(dm.getSize(), dm.getStats().getRemaining());
 
-    if (!cell.setSortValue(value) && cell.isValid())
-      return;
+		// cell.setSortValue(value) always returns true and if I change it,
+		// I'm afraid something will break.. so use compareTo
+		if (value.compareTo(cell.getSortValue()) == 0 && cell.isValid())
+			return;
 
-    cell.setText(DisplayFormatters.formatByteCountToKiBEtc(value));
-  }
+		cell.setSortValue(value);
+
+		String s = DisplayFormatters.formatByteCountToKiBEtc(value.size);
+
+		if (DO_MULTILINE && cell.getMaxLines() > 1 && value.remaining > 0) {
+			s += "\n"
+					+ DisplayFormatters.formatByteCountToKiBEtc(value.remaining, false,
+							false, 0) + " to go";
+		}
+		cell.setText(s);
+	}
+
+	private class sizeitemsort
+		implements Comparable
+	{
+		private final long size;
+
+		private final long remaining;
+
+		public sizeitemsort(long size, long remaining) {
+			this.size = size;
+			this.remaining = remaining;
+		}
+
+		public int compareTo(Object arg0) {
+			if (!(arg0 instanceof sizeitemsort)) {
+				return 1;
+			}
+
+			sizeitemsort otherObj = (sizeitemsort) arg0;
+			if (size == otherObj.size) {
+				return remaining == otherObj.remaining ? 0
+						: remaining > otherObj.remaining ? 1 : -1;
+			}
+			return size > otherObj.size ? 1 : -1;
+		}
+	}
 }
