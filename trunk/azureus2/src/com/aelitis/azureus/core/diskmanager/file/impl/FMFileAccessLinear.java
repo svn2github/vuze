2deba304c73b719ceb1004fc5e17487695758dac
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-10-29 17:58:35 +0000

    reset buffer positions after access fail
    git-svn-id: http://svn.vuze.com/public/client@11980 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/diskmanager/file/impl/FMFileAccessLinear.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/diskmanager/file/impl/FMFileAccessLinear.java
@@ -145,25 +145,31 @@ FMFileAccessLinear
 			throw( new FMFileManagerException( "read - file is closed"));
 		}
 		
+		int[]	original_positions = null;
+		
 		try{			
 			fc.position(offset);
 			
 			ByteBuffer[]	bbs = new ByteBuffer[buffers.length];
 			
+			original_positions = new int[buffers.length];
+
 			ByteBuffer	last_bb	= null;
 			
 			for (int i=0;i<bbs.length;i++){
 				
-				bbs[i] = buffers[i].getBuffer(DirectByteBuffer.SS_FILE);
+				ByteBuffer bb = bbs[i] = buffers[i].getBuffer(DirectByteBuffer.SS_FILE);
 				
-				if ( bbs[i].position() != bbs[i].limit()){
+				int	pos = original_positions[i] = bb.position();
+				
+				if ( pos != bb.limit()){
 					
 					last_bb	= bbs[i];
 				}
 			}
 			
 			if ( last_bb != null ){
-									  
+									
 				int		loop			= 0;
 				
 					// we sometimes read off the end of the file (when rechecking) so
@@ -208,7 +214,7 @@ FMFileAccessLinear
 					}						
 				}
 			}
-		}catch ( Exception e ){
+		}catch ( Throwable e ){
 			
 			try{
 				Debug.out( "Read failed: " + owner.getString() + ": raf open=" + raf.getChannel().isOpen() + ", len=" + raf.length() + ",off=" + offset );
@@ -217,6 +223,20 @@ FMFileAccessLinear
 			}
 			
 			Debug.printStackTrace( e );
+			
+			if ( original_positions != null ){
+			
+				try{
+					for (int i=0;i<original_positions.length;i++){
+						
+						buffers[i].position( DirectByteBuffer.SS_FILE, original_positions[i] );
+						
+					}
+				}catch( Throwable e2 ){
+					
+					Debug.out( e2 );
+				}
+			}
 			
 			throw( new FMFileManagerException( "read fails", e ));
 		}
@@ -230,123 +250,145 @@ FMFileAccessLinear
 	
 		throws FMFileManagerException
 	{
-		if (raf == null){
+		if ( raf == null){
 			
 			throw( new FMFileManagerException( "write fails: raf is null" ));
 		}
     
 		FileChannel fc = raf.getChannel();
     
+		if ( !fc.isOpen()){
+			
+			Debug.out("FileChannel is closed: " + owner.getName());
+			
+			throw( new FMFileManagerException( "read - file is closed"));
+		}
+
+		int[]	original_positions = null;
+
 		try{
-			if (fc.isOpen()){
-				
-				long	expected_write 	= 0;
-				long	actual_write	= 0;
-				boolean	partial_write	= false;
-				
-				if ( DEBUG ){
-				
-					for (int i=0;i<buffers.length;i++){
-						
-						expected_write += buffers[i].limit(DirectByteBuffer.SS_FILE) - buffers[i].position(DirectByteBuffer.SS_FILE);
-					}
+
+			long	expected_write 	= 0;
+			long	actual_write	= 0;
+			boolean	partial_write	= false;
+			
+			if ( DEBUG ){
+			
+				for (int i=0;i<buffers.length;i++){
+					
+					expected_write += buffers[i].limit(DirectByteBuffer.SS_FILE) - buffers[i].position(DirectByteBuffer.SS_FILE);
 				}
+			}
+			
+			fc.position( position );
+								
+			ByteBuffer[]	bbs = new ByteBuffer[buffers.length];
+			
+			original_positions = new int[buffers.length];
+
+			ByteBuffer	last_bb	= null;
+			
+			for (int i=0;i<bbs.length;i++){
 				
-				fc.position( position );
-									
-				ByteBuffer[]	bbs = new ByteBuffer[buffers.length];
+				ByteBuffer bb = bbs[i] = buffers[i].getBuffer(DirectByteBuffer.SS_FILE);
 				
-				ByteBuffer	last_bb	= null;
+				int	pos = original_positions[i] = bb.position();
 				
-				for (int i=0;i<bbs.length;i++){
+				if ( pos != bb.limit()){
 					
-					bbs[i] = buffers[i].getBuffer(DirectByteBuffer.SS_FILE);
-					
-					if ( bbs[i].position() != bbs[i].limit()){
-						
-						last_bb	= bbs[i];
-					}
+					last_bb	= bbs[i];
 				}
+			}
+			
+			if ( last_bb != null ){
+									  
+				int		loop			= 0;
 				
-				if ( last_bb != null ){
-										  
-					int		loop			= 0;
+				while( last_bb.position() != last_bb.limit()){
 					
-					while( last_bb.position() != last_bb.limit()){
+					long	written = fc.write( bbs );
+					
+					actual_write	+= written;
+					
+					if ( written > 0 ){
 						
-						long	written = fc.write( bbs );
+						loop	= 0;
 						
-						actual_write	+= written;
-						
-						if ( written > 0 ){
+						if ( DEBUG ){
 							
-							loop	= 0;
+							if ( last_bb.position() != last_bb.limit()){
 							
-							if ( DEBUG ){
-								
-								if ( last_bb.position() != last_bb.limit()){
-								
-									partial_write	= true;
-									
-									if ( DEBUG_VERBOSE ){
-										
-										Debug.out( "FMFile::write: **** partial write **** this = " + written + ", total = " + actual_write + ", target = " + expected_write );
-									}
-								}
-							}
-							
-						}else{
-						
-							loop++;
-							
-							if ( loop == WRITE_RETRY_LIMIT ){
-								
-								Debug.out( "FMFile::write: zero length write - abandoning" );
-							
-								throw( new FMFileManagerException( "write fails: retry limit exceeded"));
-								
-							}else{
+								partial_write	= true;
 								
 								if ( DEBUG_VERBOSE ){
 									
-									Debug.out( "FMFile::write: zero length write - retrying" );
-								}
-								
-								try{
-									Thread.sleep( WRITE_RETRY_DELAY*loop );
-									
-								}catch( InterruptedException e ){
-									
-									throw( new FMFileManagerException( "write fails: interrupted" ));
+									Debug.out( "FMFile::write: **** partial write **** this = " + written + ", total = " + actual_write + ", target = " + expected_write );
 								}
 							}
-						}						
-					}
-				}
-				
-				if ( DEBUG ){
-
-					if ( expected_write != actual_write ){
-						
-						Debug.out( "FMFile::write: **** partial write **** failed: expected = " + expected_write + ", actual = " + actual_write );
-
-						throw( new FMFileManagerException( "write fails: expected write/actual write mismatch" ));
-					
-					}else{
-						
-						if ( partial_write && DEBUG_VERBOSE ){
-							
-							Debug.out( "FMFile::write: **** partial write **** completed ok" );
 						}
-					}
-				}
-			}else{
+						
+					}else{
+					
+						loop++;
+						
+						if ( loop == WRITE_RETRY_LIMIT ){
+							
+							Debug.out( "FMFile::write: zero length write - abandoning" );
+						
+							throw( new FMFileManagerException( "write fails: retry limit exceeded"));
+							
+						}else{
+							
+							if ( DEBUG_VERBOSE ){
 								
-				throw( new FMFileManagerException( "write fails, channel not open" ));
+								Debug.out( "FMFile::write: zero length write - retrying" );
+							}
+							
+							try{
+								Thread.sleep( WRITE_RETRY_DELAY*loop );
+								
+							}catch( InterruptedException e ){
+								
+								throw( new FMFileManagerException( "write fails: interrupted" ));
+							}
+						}
+					}						
+				}
 			}
 			
-		}catch (Exception e ){
+			if ( DEBUG ){
+
+				if ( expected_write != actual_write ){
+					
+					Debug.out( "FMFile::write: **** partial write **** failed: expected = " + expected_write + ", actual = " + actual_write );
+
+					throw( new FMFileManagerException( "write fails: expected write/actual write mismatch" ));
+				
+				}else{
+					
+					if ( partial_write && DEBUG_VERBOSE ){
 						
+						Debug.out( "FMFile::write: **** partial write **** completed ok" );
+					}
+				}
+			}
+			
+		}catch ( Throwable e ){
+				
+			if ( original_positions != null ){
+				
+				try{
+					for (int i=0;i<original_positions.length;i++){
+						
+						buffers[i].position( DirectByteBuffer.SS_FILE, original_positions[i] );
+						
+					}
+				}catch( Throwable e2 ){
+					
+					Debug.out( e2 );
+				}
+			}
+			
 			throw( new FMFileManagerException( "write fails", e ));
 		}		
 	}
