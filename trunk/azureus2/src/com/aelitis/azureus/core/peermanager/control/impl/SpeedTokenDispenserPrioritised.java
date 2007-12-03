5a0c2a435fc76f53097c592a6b6caa8393a32fbc
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2007-12-03 16:53:00 +0000

    protect against crazy values
    git-svn-id: http://svn.vuze.com/public/client@12189 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/peermanager/control/impl/SpeedTokenDispenserPrioritised.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/peermanager/control/impl/SpeedTokenDispenserPrioritised.java
@@ -3,6 +3,7 @@ package com.aelitis.azureus.core.peermanager.control.impl;
 import org.gudy.azureus2.core3.config.COConfigurationManager;
 import org.gudy.azureus2.core3.config.ParameterListener;
 import org.gudy.azureus2.core3.disk.DiskManager;
+import org.gudy.azureus2.core3.util.Debug;
 import org.gudy.azureus2.core3.util.SystemTime;
 
 import com.aelitis.azureus.core.peermanager.control.SpeedTokenDispenser;
@@ -21,14 +22,20 @@ SpeedTokenDispenserPrioritised
 				rateKiB = COConfigurationManager.getIntParameter("Max Download Speed KBs");
 				if (!COConfigurationManager.getBooleanParameter("Use Request Limiting") || !FeatureAvailability.isRequestLimitingEnabled())
 					rateKiB = 0;
+				
+					// sanity check 
+				if ( rateKiB < 0 ){	
+					rateKiB = 0;
+				}
+				
 				threshold = Math.max(BUCKET_THRESHOLD_FACTOR*rateKiB, BUCKET_THRESHOLD_LOWER_BOUND);
 				lastTime = currentTime - 1; // shortest possible delta
 				refill(); // cap buffer to threshold in case something accumulated
 			}
 		});
 	}
-	private int		threshold;
-	private int		bucket		= 0;
+	private long	threshold;
+	private long	bucket		= 0;
 	private long	lastTime	= SystemTime.getCurrentTime();
 	private long	currentTime;
 
@@ -44,11 +51,21 @@ SpeedTokenDispenserPrioritised
 	public void refill() {
 		if (lastTime == currentTime || rateKiB == 0)
 			return;
+		
+		if ( lastTime > currentTime ){
+			lastTime = currentTime;
+			return;
+		}
+		
+		if ( bucket < 0 ){
+			Debug.out( "Bucket is more than empty! - " + bucket );
+			bucket = 0;
+		}
 		long delta = currentTime - lastTime;
 		lastTime = currentTime;
 		// upcast to long since we might exceed int-max when rate and delta are
 		// large enough; then downcast again...
-		int tickDelta = (int) (((long) rateKiB * 1024 * delta) / 1000);
+		long tickDelta = ( rateKiB * 1024L * delta) / 1000;
 		//System.out.println("threshold:" + threshold + " update: " + bucket + " time delta:" + delta);
 		bucket += tickDelta;
 		if (bucket > threshold)
@@ -65,7 +82,7 @@ SpeedTokenDispenserPrioritised
 			bucket -= chunkSize * numberOfChunks;
 			return numberOfChunks;
 		}
-		int availableChunks = bucket / chunkSize;
+		int availableChunks = (int)( bucket / chunkSize );
 		bucket -= chunkSize * availableChunks;
 		return availableChunks;
 	}
@@ -76,7 +93,7 @@ SpeedTokenDispenserPrioritised
 
 	public int peek(int chunkSize) {
 		if (rateKiB != 0)
-			return bucket / chunkSize;
+			return (int)( bucket / chunkSize );
 		else
 			return Integer.MAX_VALUE;
 	}
