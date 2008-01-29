5b65aed84f517f30a7bd48c6c7b37fe85ccf5c66
khai_m_nguyen <khai_m_nguyen@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-01-29 21:47:34 +0000

    Top Level Navigation ( AZCLIENT-494 ) New interface to facilitate abstraction so that certain functions in the Classic UI is accessible in the Vuze UI's
    git-svn-id: http://svn.vuze.com/public/client@12497 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/IMainWindow.java
@@ -0,0 +1,18 @@
+package org.gudy.azureus2.ui.swt.mainwindow;
+
+public interface IMainWindow
+{
+
+	public static final int WINDOW_ELEMENT_MENU = 1;
+
+	public static final int WINDOW_ELEMENT_TOOLBAR = 2;
+
+	public static final int WINDOW_ELEMENT_STATUSBAR = 3;
+
+	public static final int WINDOW_ELEMENT_SEARCHBAR = 4;
+
+	public boolean isVisible(int windowElement);
+
+	public void setVisible(int windowElement, boolean value);
+
+}
