ce60c7fba03958bedf69083467aed4acae3faeed
khai_m_nguyen <khai_m_nguyen@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-01-30 04:46:10 +0000

    Added option for blur effect for the background
    git-svn-id: http://svn.vuze.com/public/client@16885 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/LightBoxShell.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/LightBoxShell.java
@@ -1,10 +1,12 @@
 package com.aelitis.azureus.ui.swt.shells;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.*;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.gudy.azureus2.core3.util.Constants;
 
 import com.aelitis.azureus.ui.UIFunctions;
 import com.aelitis.azureus.ui.UIFunctionsManager;
@@ -17,9 +19,19 @@ public class LightBoxShell
 
 	private Shell parentShell = null;
 
-	private Image fadedImage;
-
 	private Rectangle fadedAreaExtent = null;
+
+	private float lightPercentage = .6f;
+
+	private int alpha = 255;
+
+	private int top = 0;
+
+	private int bottom = 0;
+
+	private int left = 0;
+
+	private int right = 0;
 
 	public LightBoxShell() {
 		UIFunctions uif = UIFunctionsManager.getUIFunctions();
@@ -32,33 +44,35 @@ public class LightBoxShell
 		if (null == parentShell) {
 			return;
 		}
+		createControls();
+	}
 
+	public LightBoxShell(Shell parentShell) {
+		this.parentShell = parentShell;
+		createControls();
+	}
+
+	public void setBrightness(float lightPercentage, int alpha) {
+		this.lightPercentage = Math.min(Math.max(lightPercentage, 0f), 1f);
+		this.alpha = Math.max(Math.min(alpha, 0), 255);
+	}
+
+	public void setInsets(int top, int bottom, int left, int right) {
+		this.top = top;
+		this.bottom = bottom;
+		this.left = left;
+		this.right = right;
+	}
+
+	private void createControls() {
 		shell = new Shell(parentShell, SWT.NO_TRIM | SWT.APPLICATION_MODAL);
-
-		//		shell.addListener(SWT.Traverse, new Listener() {
-		//			public void handleEvent(Event e) {
-		//				if (e.detail == SWT.TRAVERSE_ESCAPE) {
-		//					shell.close();
-		//					e.detail = SWT.TRAVERSE_NONE;
-		//					e.doit = false;
-		//				}
-		//			}
-		//		});
-
-		shell.addDisposeListener(new DisposeListener() {
-			public void widgetDisposed(DisposeEvent e) {
-				if (null != fadedImage && false == fadedImage.isDisposed()) {
-					fadedImage.dispose();
-				}
-			}
-		});
-
-		shell.addPaintListener(new PaintListener() {
+		parentShell.addPaintListener(new PaintListener() {
+		
 			public void paintControl(PaintEvent e) {
-				if (null != fadedImage) {
-					e.gc.drawImage(fadedImage, 0, 0);
-				}
+				System.out.println("Painting...");//KN: sysout
+		
 			}
+		
 		});
 	}
 
@@ -66,6 +80,7 @@ public class LightBoxShell
 		if (null != shell) {
 			shell.setBounds(getShadedArea());
 			createFadedImage();
+//			createBlurredImage();
 			shell.open();
 		}
 	}
@@ -78,31 +93,88 @@ public class LightBoxShell
 
 	private Rectangle getShadedArea() {
 		if (null == fadedAreaExtent) {
-			int borderWidth = parentShell.getBorderWidth();
-			borderWidth += 3;
-			fadedAreaExtent = parentShell.getClientArea();
+			/*
+			 * Not entirely sure why this has to be done this way but it seems
+			 * the Windows' shell has a 4 pixel border whereas the OSX's shell has none;
+			 * this offset is used to shift the image to fit the client area exactly
+			 */
 
-			Point newLocation = parentShell.getLocation();
-			fadedAreaExtent.x = newLocation.x + borderWidth;
-			fadedAreaExtent.y = newLocation.y + parentShell.getSize().y
-					- fadedAreaExtent.height - borderWidth;
+			int xyOffset = (true == Constants.isOSX) ? 0 : 4;
+
+			fadedAreaExtent = parentShell.getClientArea();
+			Point parentLocation = parentShell.getLocation();
+			fadedAreaExtent.x = parentLocation.x + xyOffset + left;
+			fadedAreaExtent.y = parentLocation.y + parentShell.getSize().y
+					- fadedAreaExtent.height - xyOffset + top;
+			fadedAreaExtent.width -= right + left;
+			fadedAreaExtent.height -= top + bottom;
 		}
 		return fadedAreaExtent;
+	}
+
+	private void createBlurredImage() {
+		Display display = parentShell.getDisplay();
+		GC gc = new GC(display);
+
+		Image fadedImage = new Image(display, getShadedArea());
+		gc.copyArea(fadedImage, getShadedArea().x, getShadedArea().y);
+		gc.dispose();
+
+		ImageData imageData = fadedImage.getImageData();
+		ImageData imageDataNew = fadedImage.getImageData();
+		PaletteData palette = imageData.palette;
+		imageData.alpha = alpha;
+		if (null != palette) {
+
+			int[] lookup = new int[256];
+			for (int i = 0; i < lookup.length; i++) {
+				lookup[i] = (int) (i * lightPercentage);
+			}
+
+			RGB rgbNew = new RGB(0, 0, 0);
+			RGB rgbMinus = new RGB(0,0,0);
+			RGB rgb = new RGB(0, 0, 0);
+			RGB rgbPlus = new RGB(0,0,0);
+			int[] lineData = new int[imageData.width];
+			for (int y = 0; y < imageData.height; y++) {
+				imageData.getPixels(0, y, imageData.width, lineData, 0);
+
+				for (int x = 1; x < (lineData.length-1); x++) {
+
+					rgbMinus = palette.getRGB(lineData[x-1]);
+					rgb = palette.getRGB(lineData[x]);
+					rgbPlus = palette.getRGB(lineData[x+1]);
+					
+					rgbNew.red = lookup[(rgbMinus.red + rgb.red + rgbPlus.red)/3];
+					rgbNew.green = lookup[(rgbMinus.green + rgb.green + rgbPlus.green)/3];
+					rgbNew.blue = lookup[(rgbMinus.blue + rgb.blue + rgbPlus.blue)/3];
+
+					imageDataNew.setPixel(x, y, palette.getPixel(rgbNew));
+				}
+			}
+		}
+		shell.setBackgroundImage(new Image(display, imageDataNew));
+		fadedImage.dispose();
 	}
 
 	private void createFadedImage() {
 		Display display = parentShell.getDisplay();
 		GC gc = new GC(display);
 
-		fadedImage = new Image(display, getShadedArea());
+		Image fadedImage = new Image(display, getShadedArea());
 		gc.copyArea(fadedImage, getShadedArea().x, getShadedArea().y);
 		gc.dispose();
-
+		
 		ImageData imageData = fadedImage.getImageData();
 		PaletteData palette = imageData.palette;
-
+		imageData.alpha = alpha;
 		if (null != palette) {
-			float brightness = .5f;
+
+			int[] lookup = new int[256];
+			for (int i = 0; i < lookup.length; i++) {
+				lookup[i] = (int) (i * lightPercentage);
+			}
+
 			RGB rgb = new RGB(0, 0, 0);
 
 			int[] lineData = new int[imageData.width];
@@ -112,14 +184,15 @@ public class LightBoxShell
 				for (int x = 0; x < lineData.length; x++) {
 
 					rgb = palette.getRGB(lineData[x]);
-					rgb.red = (int) (rgb.red * brightness);
-					rgb.green = (int) (rgb.green * brightness);
-					rgb.blue = (int) (rgb.blue * brightness);
+					rgb.red = lookup[rgb.red];
+					rgb.green = lookup[rgb.green];
+					rgb.blue = lookup[rgb.blue];
 
 					imageData.setPixel(x, y, palette.getPixel(rgb));
 				}
 			}
 		}
-		fadedImage = new Image(display, imageData);
+		shell.setBackgroundImage(new Image(display, imageData));
+		fadedImage.dispose();
 	}
 }
