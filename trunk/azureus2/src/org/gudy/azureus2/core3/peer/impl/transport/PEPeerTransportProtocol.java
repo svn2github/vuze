d7f58e30043de1d6aa9b8a9eb6dd59835884c349
the8472 <the8472@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-02-03 18:12:33 +0000

    client names repeat often
    git-svn-id: http://svn.vuze.com/public/client@12536 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/peer/impl/transport/PEPeerTransportProtocol.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/peer/impl/transport/PEPeerTransportProtocol.java
@@ -1885,7 +1885,7 @@ implements PEPeerTransport
 		peer_id = handshake.getPeerId();
 
 		// Decode a client identification string from the given peerID
-		this.client_peer_id = this.client = PeerClassifier.getClientDescription( peer_id );
+		this.client_peer_id = this.client = StringInterner.intern(PeerClassifier.getClientDescription( peer_id ));
 
 		//make sure the client type is not banned
 		if( !PeerClassifier.isClientTypeAllowed( client ) ) {
@@ -2154,8 +2154,8 @@ implements PEPeerTransport
   protected void decodeLTHandshake(LTHandshake handshake) {
 	  String lt_handshake_name = handshake.getClientName();
 	  if (lt_handshake_name != null) {
-		  this.client_handshake = lt_handshake_name;
-		  this.client = ClientIdentifier.identifyLTEP(this.client_peer_id, this.client_handshake, this.peer_id);
+		  this.client_handshake = StringInterner.intern(lt_handshake_name);
+		  this.client = StringInterner.intern(ClientIdentifier.identifyLTEP(this.client_peer_id, this.client_handshake, this.peer_id));
 	  }
 	  if(handshake.getTCPListeningPort() > 0 )
 	  {
@@ -2196,9 +2196,9 @@ implements PEPeerTransport
   }
   
   protected void decodeAZHandshake(AZHandshake handshake) {
-		this.client_handshake = handshake.getClient();
-		this.client_handshake_version = handshake.getClientVersion();
-		this.client = ClientIdentifier.identifyAZMP(this.client_peer_id, client_handshake, client_handshake_version, this.peer_id);
+		this.client_handshake = StringInterner.intern(handshake.getClient());
+		this.client_handshake_version = StringInterner.intern(handshake.getClientVersion());
+		this.client = StringInterner.intern(ClientIdentifier.identifyAZMP(this.client_peer_id, client_handshake, client_handshake_version, this.peer_id));
 
 		if (handshake.getTCPListenPort() > 0)
 		{ // use the ports given in handshake
