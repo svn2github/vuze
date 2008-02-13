883be376e22cdd34b21d16f0a844cf344983eb53
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-02-13 02:00:56 +0000

    start of code to support distributed connectivity testing
    git-svn-id: http://svn.vuze.com/public/client@12616 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/plugins/net/netstatus/NetStatusPlugin.java
+++ trunk/azureus2/src/com/aelitis/azureus/plugins/net/netstatus/NetStatusPlugin.java
@@ -21,10 +21,27 @@
 
 package com.aelitis.azureus.plugins.net.netstatus;
 
+import java.net.InetSocketAddress;
+import java.util.*;
+
+import org.gudy.azureus2.core3.util.AEThread2;
+import org.gudy.azureus2.core3.util.BDecoder;
+import org.gudy.azureus2.core3.util.BEncoder;
 import org.gudy.azureus2.plugins.*;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabase;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseContact;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseException;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseKey;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseProgressListener;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseTransferHandler;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseTransferType;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseValue;
 import org.gudy.azureus2.plugins.logging.LoggerChannel;
 import org.gudy.azureus2.plugins.ui.UIInstance;
 import org.gudy.azureus2.plugins.ui.UIManagerListener;
+import org.gudy.azureus2.plugins.ui.config.ActionParameter;
+import org.gudy.azureus2.plugins.ui.config.Parameter;
+import org.gudy.azureus2.plugins.ui.config.ParameterListener;
 import org.gudy.azureus2.plugins.ui.config.StringParameter;
 import org.gudy.azureus2.plugins.ui.model.BasicPluginConfigModel;
 import org.gudy.azureus2.ui.swt.plugins.UISWTInstance;
@@ -33,13 +50,19 @@ import com.aelitis.azureus.plugins.net.netstatus.swt.NetStatusPluginView;
 
 public class 
 NetStatusPlugin
-	implements Plugin
+	implements Plugin, DistributedDatabaseTransferHandler
 {
 	public static final String VIEW_ID = "aznetstatus";
 	
 	private LoggerChannel	logger;
 	
 	private StringParameter ping_target;
+	private StringParameter test_address;
+	
+	private DistributedDatabase	ddb;
+	
+	private testXferType		transfer_type;
+	
 	
 	public void
 	initialize(
@@ -57,9 +80,26 @@ NetStatusPlugin
 		
 		logger.setDiagnostic();
 		
+		transfer_type = new testXferType();
+		
 		BasicPluginConfigModel config = plugin_interface.getUIManager().createBasicPluginConfigModel( name_res );
 		
 		ping_target = config.addStringParameter2( "plugin.aznetstatus.pingtarget", "plugin.aznetstatus.pingtarget", "www.google.com" );
+		
+		test_address = config.addStringParameter2( "plugin.aznetstatus.test_address", "plugin.aznetstatus.test_address", "" );
+
+		ActionParameter test = config.addActionParameter2( "test", "test " );
+		
+		test.addListener(
+			new ParameterListener()
+			{
+				public void
+				parameterChanged(
+					Parameter	param )
+				{
+					runTest();
+				}
+			});
 		
 		plugin_interface.getUIManager().addUIListener(
 			new UIManagerListener()
@@ -86,6 +126,175 @@ NetStatusPlugin
 				{
 				}
 			});
+		
+		plugin_interface.addListener(
+			new PluginListener()
+			{
+				public void
+				initializationComplete()
+				{
+					new AEThread2( "NetstatusPlugin:init", true )
+					{
+						public void
+						run()
+						{
+							try{
+								ddb = plugin_interface.getDistributedDatabase();
+								
+								ddb.addTransferHandler(
+									transfer_type,
+									NetStatusPlugin.this );
+								
+							}catch( Throwable e ){
+								
+								log( "DDB transfer type registration failed", e );
+							}
+						}
+					}.start();
+				}
+				
+				public void
+				closedownInitiated()
+				{				
+				}
+				
+				public void
+				closedownComplete()
+				{				
+				}
+			});
+	}
+	
+	protected void
+	runTest()
+	{
+		try{
+			String str = test_address.getValue();
+	
+			String[]	bits = str.split( ":" );
+			
+			if ( bits.length != 2 ){
+				
+				log( "Invalid address - use <host>:<port> " );
+				
+				return;
+			}
+			
+			InetSocketAddress address = new InetSocketAddress( bits[0].trim(), Integer.parseInt( bits[1].trim()));
+			 
+			DistributedDatabaseContact contact = ddb.importContact( address );
+			
+			Map	request = new HashMap();
+			
+			request.put( "v", new Long(1));
+			
+			Map	reply = sendRequest( contact, request );
+			
+			log( "Reply: " + reply );
+			
+		}catch( Throwable e ){
+			
+			log( "Test failed", e );
+		}
+	}
+	
+	protected Map
+	sendRequest(
+		DistributedDatabaseContact	contact,
+		Map							request )
+	{
+		try{
+			DistributedDatabaseKey key = ddb.createKey( BEncoder.encode( request ));
+			
+			DistributedDatabaseValue value = 
+				contact.read( 
+					new DistributedDatabaseProgressListener()
+					{
+						public void
+						reportSize(
+							long	size )
+						{	
+						}
+						
+						public void
+						reportActivity(
+							String	str )
+						{	
+						}
+						
+						public void
+						reportCompleteness(
+							int		percent )
+						{
+						}
+					},
+					transfer_type,
+					key,
+					20000 );
+			
+			if ( value == null ){
+				
+				return( null );
+			}
+			
+			return( BDecoder.decode((byte[])value.getValue( byte[].class )));
+			
+		}catch( Throwable e ){
+			
+			log( "sendRequest failed", e );
+			
+			return( null );
+		}
+	}
+	
+	protected Map
+	receiveRequest(
+		Map		request )
+	{
+		Map	reply = new HashMap();
+		
+		reply.put( "v", new Long(1));
+		
+		return( reply );
+	}
+	
+	public DistributedDatabaseValue
+	read(
+		DistributedDatabaseContact			contact,
+		DistributedDatabaseTransferType		type,
+		DistributedDatabaseKey				key )
+	
+		throws DistributedDatabaseException
+	{
+		Object	o_key = key.getKey();
+		
+		log( "Received ddb request from " + contact.getName() + ": " + o_key );
+
+		try{
+			Map	request = BDecoder.decode( (byte[])o_key);
+			
+			Map	result = receiveRequest( request );
+			
+			return( ddb.createValue( BEncoder.encode( result )));
+			
+		}catch( Throwable e ){
+			
+			log( "ddb read failed", e );
+			
+			return( null );
+		}
+	}
+	
+	public void
+	write(
+		DistributedDatabaseContact			contact,
+		DistributedDatabaseTransferType		type,
+		DistributedDatabaseKey				key,
+		DistributedDatabaseValue			value )
+	
+		throws DistributedDatabaseException
+	{
+		throw( new DistributedDatabaseException( "not supported" ));
 	}
 	
 	public String
@@ -99,5 +308,20 @@ NetStatusPlugin
 		String		str )
 	{
 		logger.log( str );
+	}
+	
+	public void
+	log(
+		String		str,
+		Throwable	e )
+	{
+		logger.log( str );
+		logger.log( e );
+	}
+	
+	protected class
+	testXferType
+		implements DistributedDatabaseTransferType
+	{	
 	}
 }
