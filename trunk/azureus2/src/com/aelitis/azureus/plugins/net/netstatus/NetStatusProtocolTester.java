2bb6488bf9fd4d834a72c3fd54afc7ea7c3589f6
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-02-16 02:19:58 +0000

    refactoring
    git-svn-id: http://svn.vuze.com/public/client@12639 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/com/aelitis/azureus/plugins/net/netstatus/NetStatusProtocolTester.java
@@ -0,0 +1,466 @@
+/*
+ * Created on Feb 15, 2008
+ * Created by Paul Gardner
+ * 
+ * Copyright 2008 Vuze, Inc.  All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+package com.aelitis.azureus.plugins.net.netstatus;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
+import java.util.*;
+
+
+import org.gudy.azureus2.core3.util.BDecoder;
+import org.gudy.azureus2.core3.util.BEncoder;
+import org.gudy.azureus2.core3.util.ByteFormatter;
+import org.gudy.azureus2.core3.util.HashWrapper;
+import org.gudy.azureus2.core3.util.SimpleTimer;
+import org.gudy.azureus2.core3.util.SystemTime;
+import org.gudy.azureus2.core3.util.TimerEvent;
+import org.gudy.azureus2.core3.util.TimerEventPerformer;
+import org.gudy.azureus2.core3.util.TimerEventPeriodic;
+import org.gudy.azureus2.plugins.PluginInterface;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabase;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseContact;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseException;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseKey;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseProgressListener;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseTransferHandler;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseTransferType;
+import org.gudy.azureus2.plugins.ddb.DistributedDatabaseValue;
+
+import com.aelitis.azureus.core.dht.DHT;
+import com.aelitis.azureus.core.dht.transport.DHTTransportContact;
+import com.aelitis.azureus.plugins.dht.DHTPlugin;
+
+
+public class 
+NetStatusProtocolTester
+	implements DistributedDatabaseTransferHandler
+{
+	private static final int REQUEST_HISTORY_MAX	= 64;
+	
+	private static final int MAX_ACTIVE_TESTS	= 3;
+	private static final int MAX_TEST_TIME		= 120*1000;
+	
+	private static final int TEST_TYPE_BT		= 1;
+	
+	private NetStatusPlugin		plugin;
+	private PluginInterface		plugin_interface;
+
+	private DistributedDatabase	ddb;
+		
+	private DHTPlugin			dht_plugin;
+	
+	
+
+	private testXferType transfer_type = new testXferType();
+
+	private Map	request_history =  
+		new LinkedHashMap(REQUEST_HISTORY_MAX,0.75f,true)
+		{
+			protected boolean 
+			removeEldestEntry(
+		   		Map.Entry eldest) 
+			{
+				return size() > REQUEST_HISTORY_MAX;
+			}
+		};
+		
+	private List active_tests 		= new ArrayList();
+	
+	private TimerEventPeriodic	timer_event	= null;
+		
+	protected
+	NetStatusProtocolTester(
+		NetStatusPlugin		_plugin,
+		PluginInterface		_plugin_interface )
+	{
+		plugin				= _plugin;
+		plugin_interface	= _plugin_interface;
+		
+		try{
+			PluginInterface dht_pi = plugin_interface.getPluginManager().getPluginInterfaceByClass( DHTPlugin.class );
+			
+			if ( dht_pi != null ){
+				
+				dht_plugin = (DHTPlugin)dht_pi.getPlugin();
+			}
+
+			ddb = plugin_interface.getDistributedDatabase();
+			
+			ddb.addTransferHandler( transfer_type, this );
+			
+			log( "DDB transfer type registered" );
+			
+		}catch( Throwable e ){
+			
+			log( "DDB transfer type registration failed", e );
+		}
+	}
+	
+	protected void
+	runTest(
+		String	test_address )
+	{
+		try{
+			if ( test_address.length() == 0 ){
+				
+				DHT[]	dhts = dht_plugin.getDHTs();
+				
+				DHT	cvs_dht = null;
+				
+				for (int i=0;i<dhts.length;i++){
+					
+					if ( dhts[i].getTransport().getNetwork() == DHT.NW_CVS ){
+						
+						cvs_dht = dhts[i];
+					}
+				}
+				
+				DHTTransportContact[] contacts = cvs_dht.getTransport().getReachableContacts();
+				
+				for (int i=0;i<contacts.length;i++){
+					
+					DHTTransportContact dht_contact = contacts[i];
+					
+					DistributedDatabaseContact contact = ddb.importContact( dht_contact.getAddress());
+					
+					Map	request = new HashMap();
+					
+					request.put( "v", new Long(1));
+					
+					Map	reply = sendRequest( contact, request );
+					
+					System.out.println( contact.getName() + " -> " + reply );
+				}
+			}else{
+				
+				String[]	bits = test_address.split( ":" );
+				
+				if ( bits.length != 2 ){
+					
+					log( "Invalid address - use <host>:<port> " );
+					
+					return;
+				}
+				
+				InetSocketAddress address = new InetSocketAddress( bits[0].trim(), Integer.parseInt( bits[1].trim()));
+				 
+				DistributedDatabaseContact contact = ddb.importContact( address );
+				
+				Map	request = new HashMap();
+				
+				request.put( "v", new Long(1));
+				
+				NetStatusProtocolTesterBT bt_tester = new NetStatusProtocolTesterBT( this );
+				
+				try{
+					request.put( "t", new Long( TEST_TYPE_BT ));
+					
+					request.put( "h", bt_tester.getServerHash());
+					
+					Map	reply = sendRequest( contact, request );
+										
+					byte[]	server_hash = (byte[])reply.get( "h" );
+					
+					if ( server_hash != null ){
+						
+						InetSocketAddress hack_address = new InetSocketAddress( "127.0.0.1", Integer.parseInt( bits[1].trim()));
+	
+						bt_tester.testOutbound( hack_address, server_hash, false );
+						
+					}else{
+						
+						bt_tester.destroy();
+					}
+				}finally{
+					
+					if ( !bt_tester.isDestroyed()){
+						
+						addToActive( bt_tester );
+					}
+				}
+			}
+		}catch( Throwable e ){
+			
+			log( "Test failed", e );
+		}
+	}
+	
+	
+	protected Map
+	sendRequest(
+		DistributedDatabaseContact	contact,
+		Map							request )
+	{
+		try{
+			log( "Sending DDB request to " + contact.getName() + " - " + request );
+			
+			DistributedDatabaseKey key = ddb.createKey( BEncoder.encode( request ));
+			
+			DistributedDatabaseValue value = 
+				contact.read( 
+					new DistributedDatabaseProgressListener()
+					{
+						public void
+						reportSize(
+							long	size )
+						{	
+						}
+						
+						public void
+						reportActivity(
+							String	str )
+						{	
+						}
+						
+						public void
+						reportCompleteness(
+							int		percent )
+						{
+						}
+					},
+					transfer_type,
+					key,
+					10000 );
+			
+			if ( value == null ){
+				
+				return( null );
+			}
+			
+			Map reply = BDecoder.decode((byte[])value.getValue( byte[].class ));
+			
+			log( "    received reply - " + reply );
+			
+			return( reply );
+			
+		}catch( Throwable e ){
+			
+			log( "sendRequest failed", e );
+			
+			return( null );
+		}
+	}
+	
+	protected Map
+	receiveRequest(
+		InetSocketAddress	originator,
+		Map					request )
+	{
+		Map	reply = new HashMap();
+		
+		Long	test_type	= (Long)request.get( "t" );
+		
+		reply.put( "v", new Long(1));
+		
+		if ( test_type != null ){
+			
+			if ( test_type.intValue() == TEST_TYPE_BT ){
+				
+					// TODO; see if we support it
+				
+				byte[]	their_hash	= (byte[])request.get( "h" );
+				
+				if ( their_hash != null ){
+					
+					NetStatusProtocolTesterBT bt_tester;
+					
+					synchronized( active_tests ){
+						
+						if ( active_tests.size() > MAX_ACTIVE_TESTS ){
+							
+							log( "Too many active tests" );
+							
+							return( reply );
+							
+						}else{
+							
+							bt_tester = new NetStatusProtocolTesterBT( this );
+							
+							addToActive( bt_tester );
+						}
+					}
+					
+					InetSocketAddress hack_address = new InetSocketAddress( "127.0.0.1", originator.getPort());
+
+					bt_tester.testOutbound( hack_address, their_hash, true );
+					
+					reply.put( "h", bt_tester.getServerHash());
+				}
+			}
+		}		
+		
+		return( reply );
+	}
+	
+	protected void
+	addToActive(
+		NetStatusProtocolTesterBT		tester )
+	{
+		synchronized( active_tests ){
+
+			active_tests.add( tester );
+			
+			if ( timer_event == null ){
+				
+				timer_event = 
+					SimpleTimer.addPeriodicEvent(
+						"NetStatusProtocolTester:timer",
+						30*1000,
+						new TimerEventPerformer()
+						{
+							public void 
+							perform(
+								TimerEvent event )
+							{
+								long	now = SystemTime.getCurrentTime();
+								
+								List	to_remove = new ArrayList();
+								
+								synchronized( active_tests ){
+
+									for (int i=0;i<active_tests.size();i++){
+										
+										NetStatusProtocolTesterBT tester = (NetStatusProtocolTesterBT)active_tests.get(i);
+										
+										long start = tester.getStartTime( now );
+										
+										if ( now - start > MAX_TEST_TIME ){
+											
+											to_remove.add( tester );
+										}
+									}
+								}
+								
+								for ( int i=0;i<to_remove.size();i++ ){
+									
+									removeFromActive( (NetStatusProtocolTesterBT)to_remove.get(i));
+								}
+							}
+						});
+			}
+		}
+	}
+	
+	protected void
+	removeFromActive(
+		NetStatusProtocolTesterBT		tester )
+	{
+		tester.destroy();
+		
+		synchronized( active_tests ){
+			
+			active_tests.remove( tester );
+			
+			if ( active_tests.size() == 0 ){
+				
+				if ( timer_event != null ){
+					
+					timer_event.cancel();
+					
+					timer_event = null;
+				}
+			}
+		}
+	}
+	
+	public DistributedDatabaseValue
+	read(
+		DistributedDatabaseContact			contact,
+		DistributedDatabaseTransferType		type,
+		DistributedDatabaseKey				ddb_key )
+	
+		throws DistributedDatabaseException
+	{
+		Object	o_key = ddb_key.getKey();
+		
+		try{
+			byte[]	key = (byte[])o_key;
+			
+			HashWrapper	hw = new HashWrapper( key );
+			
+			synchronized( request_history ){
+				
+				if ( request_history.containsKey( hw )){
+										
+					return( null );
+				}
+				
+				request_history.put( hw, "" );
+			}
+			
+			Map	request = BDecoder.decode( (byte[])o_key);
+			
+			log( "Received DDB request from " + contact.getName() + " - " + request );
+
+			Map	result = receiveRequest( contact.getAddress(), request );
+			
+			return( ddb.createValue( BEncoder.encode( result )));
+			
+		}catch( Throwable e ){
+			
+			log( "DDB read failed", e );
+			
+			return( null );
+		}
+	}
+	
+	public void
+	write(
+		DistributedDatabaseContact			contact,
+		DistributedDatabaseTransferType		type,
+		DistributedDatabaseKey				key,
+		DistributedDatabaseValue			value )
+	
+		throws DistributedDatabaseException
+	{
+		throw( new DistributedDatabaseException( "not supported" ));
+	}
+	
+	
+	public void
+	log(
+		String		str )
+	{
+		System.out.println( str );
+		
+		plugin.log( str );
+	}
+	
+	public void
+	log(
+		String		str,
+		Throwable	e )
+	{
+		System.out.println( str );
+		e.printStackTrace();
+		
+		plugin.log( str, e );
+	}
+	
+	
+	protected class
+	testXferType
+		implements DistributedDatabaseTransferType
+	{	
+	}
+}
---
+++ trunk/azureus2/src/com/aelitis/azureus/plugins/net/netstatus/NetStatusProtocolTesterBT.java
@@ -0,0 +1,379 @@
+/*
+ * Created on Feb 15, 2008
+ * Created by Paul Gardner
+ * 
+ * Copyright 2008 Vuze, Inc.  All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+package com.aelitis.azureus.plugins.net.netstatus;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
+import java.util.*;
+
+import org.gudy.azureus2.core3.util.ByteFormatter;
+import org.gudy.azureus2.core3.util.HashWrapper;
+import org.gudy.azureus2.core3.util.SystemTime;
+
+import com.aelitis.azureus.core.networkmanager.ConnectionEndpoint;
+import com.aelitis.azureus.core.networkmanager.IncomingMessageQueue;
+import com.aelitis.azureus.core.networkmanager.NetworkConnection;
+import com.aelitis.azureus.core.networkmanager.NetworkManager;
+import com.aelitis.azureus.core.networkmanager.OutgoingMessageQueue;
+import com.aelitis.azureus.core.networkmanager.ProtocolEndpoint;
+import com.aelitis.azureus.core.networkmanager.impl.tcp.ProtocolEndpointTCP;
+import com.aelitis.azureus.core.peermanager.PeerManager;
+import com.aelitis.azureus.core.peermanager.PeerManagerRegistration;
+import com.aelitis.azureus.core.peermanager.PeerManagerRegistrationAdapter;
+import com.aelitis.azureus.core.peermanager.messaging.Message;
+import com.aelitis.azureus.core.peermanager.messaging.bittorrent.BTHandshake;
+import com.aelitis.azureus.core.peermanager.messaging.bittorrent.BTMessage;
+import com.aelitis.azureus.core.peermanager.messaging.bittorrent.BTMessageDecoder;
+import com.aelitis.azureus.core.peermanager.messaging.bittorrent.BTMessageEncoder;
+import com.aelitis.azureus.core.peermanager.messaging.bittorrent.BTMessageFactory;
+
+public class 
+NetStatusProtocolTesterBT 
+{
+	private static Random	random = new SecureRandom();
+	
+	private NetStatusProtocolTester	tester;
+	
+	private byte[]		my_hash;
+	
+	private PeerManagerRegistration		pm_reg;
+
+	private long		start_time	= SystemTime.getCurrentTime();
+	
+	private List		connections	= new ArrayList();
+	
+	private boolean		destroyed;
+	
+	protected
+	NetStatusProtocolTesterBT(
+		NetStatusProtocolTester		_tester )
+	{
+		tester	= _tester;
+		
+		my_hash = new byte[20];
+		
+		random.nextBytes( my_hash );
+		
+		pm_reg = PeerManager.getSingleton().registerLegacyManager(
+			new HashWrapper( my_hash ),
+			new PeerManagerRegistrationAdapter()
+			{
+				public byte[][]
+	          	getSecrets()
+				{
+					return( new byte[][]{ my_hash });
+				}
+	          	
+	          	public boolean
+	          	manualRoute(
+	          		NetworkConnection		connection )
+	          	{
+	          		log( "Got incoming connection from " + connection.getEndpoint().getNotionalAddress());
+	          		
+	          		initialiseConnection( connection, false );
+	          		
+	          		return( true );
+	          	}
+	          	
+	          	public boolean
+	          	isPeerSourceEnabled(
+	          		String					peer_source )
+	          	{
+	          		return( true );
+	          	}
+	          	
+	          	public boolean
+	          	activateRequest(
+	          		InetSocketAddress		remote_address )
+	          	{
+	          		return( true );
+	          	}
+	          	
+	          	public void
+	          	deactivateRequest(
+	          		InetSocketAddress		remote_address )
+	          	{
+	          	}
+	          	
+	          	public String
+	          	getDescription()
+	          	{
+	          		return( "NetStatusPlugin - router" );
+	          	}
+
+			});
+		
+		log( "Incoming routing established for " + ByteFormatter.encodeString( my_hash ));
+	}
+	
+	protected byte[]
+	getServerHash()
+	{
+		return( my_hash );
+	}
+	
+	protected long
+	getStartTime(
+		long	now )
+	{
+		if ( now < start_time ){
+			
+			start_time = now;
+		}
+		
+		return( start_time );
+	}
+	
+	protected void
+	testOutbound(
+		InetSocketAddress		address,
+		byte[]					their_hash,
+		boolean					use_crypto )
+	{
+		log( "Making outbound connection to " + address );
+		
+		boolean	allow_fallback	= false;
+		
+		ProtocolEndpoint	pe = new ProtocolEndpointTCP( address );
+		
+		ConnectionEndpoint connection_endpoint	= new ConnectionEndpoint( address );
+
+		connection_endpoint.addProtocol( pe );
+
+		final NetworkConnection connection = 
+			NetworkManager.getSingleton().createConnection(
+					connection_endpoint, 
+					new BTMessageEncoder(), 
+					new BTMessageDecoder(), 
+					use_crypto, 
+					allow_fallback, 
+					new byte[][]{ their_hash });
+	
+		connection.connect( 
+				true,
+				new NetworkConnection.ConnectionListener() 
+				{
+					public final void 
+					connectStarted() 
+					{
+						log( "Outbound connect start" );
+					}
+
+					public final void 
+					connectSuccess( 
+						ByteBuffer remaining_initial_data ) 
+					{
+						log( "Outbound connect success" );
+						
+						initialiseConnection( connection, true );
+					}
+
+					public final void 
+					connectFailure( 
+						Throwable e ) 
+					{
+						log( "Outbound connect fail", e );
+						
+						connection.close();
+					}
+
+					public final void 
+					exceptionThrown( 
+						Throwable e ) 
+					{
+						log( "Outbound connect fail", e );
+						
+						connection.close();
+					}
+    			
+					public String
+					getDescription()
+					{
+						return( "NetStatusPlugin - outbound" );
+					}
+				});
+	}
+	
+	protected void
+	initialiseConnection(
+		NetworkConnection	connection,
+		boolean				outgoing )
+	{
+		synchronized( this ){
+			
+			if ( destroyed ){
+				
+				log( "Already destroyed" );
+				
+				connection.close();
+				
+				return;
+				
+			}else{
+				
+				connections.add( connection );
+			}
+		}
+		
+		connection.getIncomingMessageQueue().registerQueueListener(
+			new IncomingMessageQueue.MessageQueueListener() 
+			{
+				public boolean 
+				messageReceived( Message message ) 
+				{               
+					String	message_id = message.getID();
+
+					log( "Incoming message received: " + message.getID());
+					
+			        if ( message_id.equals( BTMessage.ID_BT_HANDSHAKE )){
+				
+					}
+			        
+			        return( true );
+				}
+  
+
+				public final void 
+				protocolBytesReceived(
+					int byte_count ) 
+				
+				{
+				}
+
+				public final void 
+				dataBytesReceived( 
+					int byte_count ) 
+				{
+				}
+			});
+
+		connection.getOutgoingMessageQueue().registerQueueListener( 
+			new OutgoingMessageQueue.MessageQueueListener() 
+			{
+				public final boolean 
+				messageAdded( 
+					Message message )
+				{
+					return( true );
+				}
+	
+				public final void 
+				messageQueued( 
+					Message message )
+				{
+				}
+	
+				public final void 
+				messageRemoved( 
+					Message message )
+				{
+					
+				}
+	
+				public final void 
+				messageSent( 
+					Message message ) 
+				{
+					log( "Outgoing message sent: " + message.getID());
+				}
+	
+				public final void 
+				protocolBytesSent( 
+					int byte_count ) 
+				{
+				}
+	
+				public final void 
+				dataBytesSent( 
+					int byte_count ) 
+				{
+				}
+		});
+
+		connection.startMessageProcessing();
+		
+		if ( outgoing ){
+			
+			byte[]	peer_id = new byte[20];
+			
+			random.nextBytes( peer_id );
+			
+			connection.getOutgoingMessageQueue().addMessage(
+				new BTHandshake( my_hash, peer_id, false, BTMessageFactory.MESSAGE_VERSION_INITIAL ),
+				false );
+		}
+	}
+	
+	protected void
+	destroy()
+	{
+		List	to_close	= new ArrayList();
+		
+		synchronized( this ){
+			
+			if ( destroyed ){
+				
+				return;
+			}
+			
+			destroyed = true;
+			
+			to_close.addAll( connections );
+			
+			connections.clear();
+		}
+		
+		for (int i=0;i<to_close.size();i++){
+			
+			NetworkConnection connection = (NetworkConnection)to_close.get(i);
+			
+			connection.close();
+		}
+		
+		pm_reg.unregister();
+		
+		log( "Incoming routing destroyed for " + ByteFormatter.encodeString( my_hash ));
+
+	}
+	
+	protected boolean
+	isDestroyed()
+	{
+		return( destroyed );
+	}
+	
+	protected void
+	log(
+		String	str )
+	{
+		tester.log( str );
+	}
+	
+	protected void
+	log(
+		String		str,
+		Throwable	e )
+	{
+		tester.log( str, e );
+	}
+}
