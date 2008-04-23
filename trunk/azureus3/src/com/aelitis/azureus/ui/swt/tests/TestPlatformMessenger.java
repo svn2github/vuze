e24bb18f4ea57f789ddb8ae12ad431c30c4677b4
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-04-23 19:44:51 +0000

    move SWT out of messenger core
    git-svn-id: http://svn.vuze.com/public/client@17283 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/tests/TestPlatformMessenger.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/tests/TestPlatformMessenger.java
@@ -1,24 +1,22 @@
 /**
- * Copyright (C) 2006 Aelitis, All Rights Reserved.
+ * Created on Apr 23, 2008
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Copyright 2008 Vuze, Inc.  All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- * AELITIS, SAS au capital de 63.529,40 euros
- * 8 Allee Lenotre, La Grille Royale, 78600 Le Mesnil le Roi, France.
- *
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA 
  */
-
-package com.aelitis.azureus.core.messenger;
+ 
+package com.aelitis.azureus.ui.swt.tests;
 
 import java.util.*;
 
@@ -31,6 +29,7 @@ import org.gudy.azureus2.core3.util.SystemTime;
 
 import com.aelitis.azureus.core.AzureusCore;
 import com.aelitis.azureus.core.AzureusCoreFactory;
+import com.aelitis.azureus.core.messenger.*;
 import com.aelitis.azureus.core.messenger.config.PlatformRatingMessenger;
 import com.aelitis.azureus.util.Constants;
 
@@ -39,10 +38,10 @@ import org.gudy.azureus2.plugins.PluginInterface;
 
 /**
  * @author TuxPaper
- * @created Sep 26, 2006
+ * @created Apr 23, 2008
  *
  */
-public class Test
+public class TestPlatformMessenger
 {
 	public void initialize(PluginInterface pi) throws PluginException {
 		Constants.initialize(AzureusCoreFactory.getSingleton());
@@ -121,7 +120,7 @@ public class Test
 		int count = 0;
 		try {
 			AzureusCore core = AzureusCoreFactory.create();
-			Test test = new Test();
+			TestPlatformMessenger test = new TestPlatformMessenger();
 			test.initialize(core.getPluginManager().getDefaultPluginInterface());
 
 			while (!shell.isDisposed()) {
@@ -135,5 +134,4 @@ public class Test
 			Debug.printStackTrace(e);
 		}
 	}
-
 }
