f70dacd19607c9463e3adeafb74e2b0412e46847
khai_m_nguyen <khai_m_nguyen@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-04-27 05:31:55 +0000

    Some more formalization for Buddies-related code
    git-svn-id: http://svn.vuze.com/public/client@13020 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/IMainWindow.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/IMainWindow.java
@@ -20,6 +20,8 @@ public interface IMainWindow
 	public static final int WINDOW_CONTENT_DISPLAY_AREA = 7;
 
 	public static final int WINDOW_ELEMENT_FOOTER = 8;
+	
+	public static final int WINDOW_ELEMENT_BUTTON_BAR = 9;
 
 	public boolean isVisible(int windowElement);
 
