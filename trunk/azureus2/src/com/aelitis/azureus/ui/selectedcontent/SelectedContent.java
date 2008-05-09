7ce9ee76e1d56d9aecef7cc19e445535a8326558
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-05-09 10:14:57 +0000

    Classes for the ability to track currently selected content
    git-svn-id: http://svn.vuze.com/public/client@13073 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/com/aelitis/azureus/ui/selectedcontent/SelectedContent.java
@@ -0,0 +1,56 @@
+/**
+ * Created on May 6, 2008
+ *
+ * Copyright 2008 Vuze, Inc.  All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA 
+ */
+ 
+package com.aelitis.azureus.ui.selectedcontent;
+
+import org.gudy.azureus2.core3.download.DownloadManager;
+import org.gudy.azureus2.core3.torrent.TOTorrentException;
+
+/**
+ * Represents a piece of content (torrent) that is selected
+ * 
+ * @author TuxPaper
+ * @created May 6, 2008
+ *
+ */
+public class SelectedContent
+{
+	public String hash;
+	public DownloadManager dm;
+	public String displayName;
+
+	/**
+	 * @param dm2
+	 */
+	public SelectedContent(DownloadManager dm) {
+		this.dm = dm;
+		try {
+			this.hash = dm.getTorrent().getHashWrapper().toBase32String();
+		} catch (TOTorrentException e) {
+		}
+		displayName = dm.getDisplayName();
+	}
+
+	/**
+	 * 
+	 */
+	public SelectedContent(String hash, String displayName) {
+		this.hash = hash;
+		this.displayName = displayName;
+	}
+}
---
+++ trunk/azureus2/src/com/aelitis/azureus/ui/selectedcontent/SelectedContentManager.java
@@ -0,0 +1,58 @@
+/**
+ * Created on May 6, 2008
+ *
+ * Copyright 2008 Vuze, Inc.  All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA 
+ */
+
+package com.aelitis.azureus.ui.selectedcontent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manages the currently selected content in the visible display
+ * 
+ * @author TuxPaper
+ * @created May 6, 2008
+ *
+ */
+public class SelectedContentManager
+{
+	private static List listeners = new ArrayList();
+
+	private static SelectedContent[] currentlySelectedContent = new SelectedContent[0];
+
+	public static void addCurrentlySelectedContentListener(
+			SelectedContentListener l) {
+		listeners.add(l);
+		l.currentlySectedContentChanged(currentlySelectedContent);
+	}
+
+	public static void changeCurrentlySelectedContent(
+			SelectedContent[] currentlySelectedContent) {
+		SelectedContentManager.currentlySelectedContent = currentlySelectedContent == null
+				? new SelectedContent[0] : currentlySelectedContent;
+
+		Object[] listenerArray = listeners.toArray();
+		for (int i = 0; i < listenerArray.length; i++) {
+			SelectedContentListener l = (SelectedContentListener) listenerArray[i];
+			l.currentlySectedContentChanged(SelectedContentManager.currentlySelectedContent);
+		}
+	}
+
+	public static SelectedContent[] getCurrentlySelectedContent() {
+		return currentlySelectedContent;
+	}
+}
