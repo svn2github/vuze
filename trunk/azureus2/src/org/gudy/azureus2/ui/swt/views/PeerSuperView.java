f3d1116fbe6ba627331e63d5ad3b077dcb66b891
amc1 <amc1@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-05-20 17:08:29 +0000

    Removed unnecessary class check.
    git-svn-id: http://svn.vuze.com/public/client@13195 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/PeerSuperView.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/PeerSuperView.java
@@ -35,7 +35,6 @@ import org.gudy.azureus2.core3.download.DownloadManagerPeerListener;
 import org.gudy.azureus2.core3.peer.PEPeer;
 import org.gudy.azureus2.core3.peer.PEPeerManager;
 import org.gudy.azureus2.plugins.ui.tables.TableManager;
-import org.gudy.azureus2.ui.swt.Utils;
 import org.gudy.azureus2.ui.swt.views.peer.PeerInfoView;
 import org.gudy.azureus2.ui.swt.views.peer.RemotePieceDistributionView;
 import org.gudy.azureus2.ui.swt.views.table.TableViewSWT;
@@ -98,11 +97,7 @@ public class PeerSuperView
   
 	// @see com.aelitis.azureus.ui.common.table.TableLifeCycleListener#tableViewInitialized()
 	public void tableViewInitialized() {
-		if (tv instanceof TableViewSWT) {
-			shell = ((TableViewSWT)tv).getComposite().getShell();
-		} else {
-			shell = Utils.findAnyShell();
-		}
+		shell = tv.getComposite().getShell();
 		registerGlobalManagerListener();
 	}
 		
