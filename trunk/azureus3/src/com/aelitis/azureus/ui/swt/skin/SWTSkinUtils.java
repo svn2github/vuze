08d8dd3be6c648760a48e51a977703558f862a1d
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-06-02 23:01:34 +0000

    fix last redraw drawing the background widgets
    git-svn-id: http://svn.vuze.com/public/client@17774 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/skin/SWTSkinUtils.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/skin/SWTSkinUtils.java
@@ -273,13 +273,12 @@ public class SWTSkinUtils
 				int h = fd.height >= 0 ? fd.height : control.getSize().y;
 				int newHeight = (int) (h + (size.y - h) * pct);
 				pct += 0.01;
-				//System.out.println(control + "] newh=" + newHeight + " to " + size.y);
+				//System.out.println(control + "] newh=" + newHeight + "/" + newWidth + " to " + size.y);
 
 				if (newWidth == fd.width && newHeight == h) {
 					fd.width = size.x;
 					fd.height = size.y;
-					//System.out.println(control + "] side to " + size.y + " done");
-					control.setSize(size);
+					//System.out.println(control + "] side to " + size.y + " done" + size.x);
 					control.setLayoutData(fd);
 					Utils.relayout(control);
 					control.getParent().layout();
