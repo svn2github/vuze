704eab99b056c1ca7b864ec7e30d0f72aec146c8
khai_m_nguyen <khai_m_nguyen@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-06-11 20:19:25 +0000

    Alternate look-n-feel for standard message box shell: added .open(boolean) and _open2() .open() or .open(false) retains existing look-n-feel .open(true) renders in new look-n-feel The only place using the new dialog is the doRemoveBuddy() in AvatarWidget (for now)
    git-svn-id: http://svn.vuze.com/public/client@13506 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/shells/MessageBoxShell.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/shells/MessageBoxShell.java
@@ -1,17 +1,50 @@
 package org.gudy.azureus2.ui.swt.shells;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.browser.*;
-import org.eclipse.swt.events.*;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
-
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.LocationEvent;
+import org.eclipse.swt.browser.LocationListener;
+import org.eclipse.swt.browser.OpenWindowListener;
+import org.eclipse.swt.browser.ProgressEvent;
+import org.eclipse.swt.browser.ProgressListener;
+import org.eclipse.swt.browser.WindowEvent;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.MouseTrackAdapter;
+import org.eclipse.swt.events.MouseTrackListener;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.gudy.azureus2.core3.internat.MessageText;
-import org.gudy.azureus2.core3.util.*;
+import org.gudy.azureus2.core3.util.AERunnable;
+import org.gudy.azureus2.core3.util.Debug;
+import org.gudy.azureus2.core3.util.SimpleTimer;
+import org.gudy.azureus2.core3.util.SystemTime;
+import org.gudy.azureus2.core3.util.TimerEvent;
+import org.gudy.azureus2.core3.util.TimerEventPerformer;
 import org.gudy.azureus2.ui.swt.ImageRepository;
 import org.gudy.azureus2.ui.swt.Utils;
 import org.gudy.azureus2.ui.swt.components.shell.ShellFactory;
+import org.gudy.azureus2.ui.swt.components.shell.StyledShell;
 import org.gudy.azureus2.ui.swt.mainwindow.ClipboardCopy;
 import org.gudy.azureus2.ui.swt.shells.GCStringPrinter.URLInfo;
 
@@ -70,7 +103,7 @@ public class MessageBoxShell
 	private Image imgLeft;
 
 	protected Color urlColor;
-	
+
 	public static int open(final Shell parent, final String title,
 			final String text, final String[] buttons, final int defaultOption) {
 		return open(parent, title, text, buttons, defaultOption, null, false, -1);
@@ -139,6 +172,10 @@ public class MessageBoxShell
 	}
 
 	public int open() {
+		return open(false);
+	}
+
+	public int open(final boolean useCustomShell) {
 		if (rememberID != null) {
 			int rememberedDecision = RememberedDecisionsManager.getRememberedDecision(rememberID);
 			if (rememberedDecision >= 0) {
@@ -153,7 +190,11 @@ public class MessageBoxShell
 
 		Utils.execSWTThread(new AERunnable() {
 			public void runSupport() {
-				result[0] = _open();
+				if (true == useCustomShell) {
+					result[0] = _open2();
+				} else {
+					result[0] = _open();
+				}
 			}
 		}, false);
 
@@ -180,7 +221,7 @@ public class MessageBoxShell
 				| SWT.RESIZE | SWT.APPLICATION_MODAL);
 		shell.setText(title);
 		shell.setBackgroundMode(SWT.INHERIT_DEFAULT);
-		
+
 		GridLayout gridLayout = new GridLayout();
 		shell.setLayout(gridLayout);
 		Utils.setShellIcon(shell);
@@ -193,7 +234,7 @@ public class MessageBoxShell
 
 		FormData formData;
 		GridData gridData;
-		
+
 		Composite textComposite = shell;
 		if (imgLeft != null) {
 			textComposite = new Composite(shell, SWT.NONE);
@@ -211,7 +252,8 @@ public class MessageBoxShell
 		if ((html != null && html.length() > 0)
 				|| (url != null && url.length() > 0)) {
 			try {
-				final Browser browser = new Browser(shell, Utils.getInitialBrowserStyle(SWT.NONE));
+				final Browser browser = new Browser(shell,
+						Utils.getInitialBrowserStyle(SWT.NONE));
 				if (url != null && url.length() > 0) {
 					browser.setUrl(url);
 				} else {
@@ -435,20 +477,20 @@ public class MessageBoxShell
 				}
 			}
 		});
-		
+
 		Listener filterListener = new Listener() {
 			public void handleEvent(Event event) {
 				if (event.detail == SWT.TRAVERSE_ARROW_NEXT) {
 					event.detail = SWT.TRAVERSE_TAB_NEXT;
 					event.doit = true;
 				} else if (event.detail == SWT.TRAVERSE_ARROW_PREVIOUS) {
-					event.detail = SWT.TRAVERSE_TAB_PREVIOUS;						
+					event.detail = SWT.TRAVERSE_TAB_PREVIOUS;
 					event.doit = true;
 				}
 			}
 		};
 		display.addFilter(SWT.Traverse, filterListener);
-		
+
 		if (mouseAdapter != null) {
 			addMouseTrackListener(shell, mouseAdapter);
 		}
@@ -481,7 +523,343 @@ public class MessageBoxShell
 				display.sleep();
 			}
 		}
-		
+
+		if (display != null && !display.isDisposed()) {
+			display.removeFilter(SWT.Traverse, filterListener);
+		}
+
+		return result[0];
+	}
+
+	private int _open2() {
+		final int[] result = {
+			-1
+		};
+
+		if (parent == null || parent.isDisposed()) {
+			parent = Utils.findAnyShell();
+			if (parent == null || parent.isDisposed()) {
+				return result[0];
+			}
+		}
+
+		MouseTrackAdapter mouseAdapter = null;
+		Display display = parent.getDisplay();
+
+		final StyledShell sShell = new StyledShell(parent, 6);
+		final Shell shell = sShell.getShell();
+
+		final Composite content = sShell.getContent();
+		content.setBackgroundMode(SWT.INHERIT_DEFAULT);
+
+		GridLayout gridLayout = new GridLayout();
+		content.setLayout(gridLayout);
+
+		Label titleLabel = new Label(content, SWT.WRAP);
+		titleLabel.setForeground(content.getForeground());
+		titleLabel.setText(title);
+		titleLabel.setLayoutData(new GridData(GridData.FILL_BOTH));
+		Utils.setFontHeight(titleLabel, 12, SWT.NORMAL);
+
+
+		UISkinnableSWTListener[] listeners = UISkinnableManagerSWT.getInstance().getSkinnableListeners(
+				MessageBoxShell.class.toString());
+		for (int i = 0; i < listeners.length; i++) {
+			listeners[i].skinBeforeComponents(shell, this, relatedObjects);
+		}
+
+		FormData formData;
+		GridData gridData;
+
+		Composite textComposite = content;
+		if (imgLeft != null) {
+			textComposite = new Composite(content, SWT.NONE);
+			textComposite.setForeground(content.getForeground());
+			textComposite.setLayout(new GridLayout(2, false));
+			textComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+			Label lblImage = new Label(textComposite, SWT.NONE);
+			lblImage.setImage(imgLeft);
+			lblImage.setLayoutData(new GridData(GridData.VERTICAL_ALIGN_BEGINNING));
+		}
+
+		Control linkControl;
+		linkControl = createLinkLabel(textComposite, text);
+
+		if ((html != null && html.length() > 0)
+				|| (url != null && url.length() > 0)) {
+			try {
+				final Browser browser = new Browser(content,
+						Utils.getInitialBrowserStyle(SWT.NONE));
+				if (url != null && url.length() > 0) {
+					browser.setUrl(url);
+				} else {
+					browser.setText(html);
+				}
+				GridData gd = new GridData(GridData.FILL_BOTH);
+				gd.heightHint = 200;
+				browser.setLayoutData(gd);
+				browser.addProgressListener(new ProgressListener() {
+					public void completed(ProgressEvent event) {
+						browser.addLocationListener(new LocationListener() {
+							public void changing(LocationEvent event) {
+								event.doit = false;
+							}
+
+							public void changed(LocationEvent event) {
+							}
+						});
+						browser.addOpenWindowListener(new OpenWindowListener() {
+							public void open(WindowEvent event) {
+								event.required = true;
+							}
+						});
+					}
+
+					public void changed(ProgressEvent event) {
+					}
+				});
+			} catch (Exception e) {
+				Debug.out(e);
+				if (html != null) {
+					Text text = new Text(content, SWT.BORDER | SWT.READ_ONLY | SWT.WRAP);
+					text.setText(html);
+					GridData gd = new GridData(GridData.FILL_BOTH);
+					gd.heightHint = 200;
+					text.setLayoutData(gd);
+				}
+			}
+
+			gridData = new GridData(GridData.FILL_HORIZONTAL);
+			linkControl.setLayoutData(gridData);
+		} else {
+			gridData = new GridData(GridData.FILL_BOTH);
+			linkControl.setLayoutData(gridData);
+		}
+
+		Label lblPadding = new Label(content, SWT.NONE);
+		lblPadding.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+
+		// Closing in..
+		if (autoCloseInMS > 0) {
+			final Label lblCloseIn = new Label(content, SWT.WRAP);
+			lblCloseIn.setForeground(content.getForeground());
+			lblCloseIn.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+			long endOn = SystemTime.getCurrentTime() + autoCloseInMS;
+			lblCloseIn.setData("CloseOn", new Long(endOn));
+			SimpleTimer.addPeriodicEvent("autoclose", 500, new TimerEventPerformer() {
+				public void perform(TimerEvent event) {
+					if (content.isDisposed()) {
+						event.cancel();
+						return;
+					}
+					Utils.execSWTThread(new AERunnable() {
+						public void runSupport() {
+							if (!content.isDisposed()) {
+								boolean bDelayPaused = lblCloseIn.getData("DelayPaused") != null;
+								if (bDelayPaused) {
+									return;
+								}
+
+								long endOn = ((Long) lblCloseIn.getData("CloseOn")).longValue();
+								if (SystemTime.getCurrentTime() > endOn) {
+									result[0] = defaultOption;
+									autoClosed = true;
+									content.dispose();
+								} else {
+									String sText = "";
+
+									if (lblCloseIn.isDisposed())
+										return;
+
+									if (!bDelayPaused) {
+										long delaySecs = (endOn - SystemTime.getCurrentTime()) / 1000;
+										sText = MessageText.getString("popup.closing.in",
+												new String[] {
+													String.valueOf(delaySecs)
+												});
+									}
+
+									lblCloseIn.setText(sText);
+								}
+							}
+						};
+					});
+				}
+			});
+
+			SimpleTimer.addPeriodicEvent("OverPopup", 100, new TimerEventPerformer() {
+				boolean wasOver = true;
+
+				long lEnterOn = 0;
+
+				public void perform(final TimerEvent event) {
+					if (content.isDisposed()) {
+						event.cancel();
+						return;
+					}
+					Utils.execSWTThread(new AERunnable() {
+						public void runSupport() {
+							if (content.isDisposed()) {
+								event.cancel();
+								return;
+							}
+							boolean isOver = content.getBounds().contains(
+									content.getDisplay().getCursorLocation());
+							if (isOver != wasOver) {
+								wasOver = isOver;
+								if (isOver) {
+									lblCloseIn.setData("DelayPaused", "");
+									lEnterOn = SystemTime.getCurrentTime();
+									lblCloseIn.setText("");
+								} else {
+									lblCloseIn.setData("DelayPaused", null);
+									if (lEnterOn > 0) {
+										long diff = SystemTime.getCurrentTime() - lEnterOn;
+										long endOn = ((Long) lblCloseIn.getData("CloseOn")).longValue()
+												+ diff;
+										lblCloseIn.setData("CloseOn", new Long(endOn));
+									}
+								}
+							}
+						}
+					});
+				}
+			});
+		}
+
+		// Remember Me
+		Button checkRemember = null;
+		if (rememberID != null) {
+			checkRemember = new Button(content, SWT.CHECK);
+			checkRemember.setText(rememberText);
+			checkRemember.setSelection(rememberByDefault);
+
+			checkRemember.addDisposeListener(new DisposeListener() {
+				public void widgetDisposed(DisposeEvent e) {
+					Button checkRemember = (Button) e.widget;
+					if (rememberID != null && checkRemember != null
+							&& checkRemember.getSelection()) {
+						RememberedDecisionsManager.setRemembered(rememberID, result[0]);
+					}
+				}
+			});
+		}
+
+		// Buttons
+
+		Composite cButtons = new Composite(content, SWT.NONE);
+		FormLayout layout = new FormLayout();
+
+		cButtons.setLayout(layout);
+		gridData = new GridData(GridData.HORIZONTAL_ALIGN_CENTER);
+		cButtons.setLayoutData(gridData);
+
+		Control lastButton = null;
+
+		Listener buttonListener = new Listener() {
+
+			public void handleEvent(Event event) {
+				result[0] = ((Integer) event.widget.getData()).intValue();
+				sShell.close();
+			}
+
+		};
+
+		int buttonWidth = 0;
+		Button[] swtButtons = new Button[buttons.length];
+		for (int i = 0; i < buttons.length; i++) {
+			Button button = new Button(cButtons, SWT.PUSH);
+			swtButtons[i] = button;
+			button.setData(new Integer(i));
+			button.setText(buttons[i]);
+			button.addListener(SWT.Selection, buttonListener);
+
+			formData = new FormData();
+			if (lastButton != null) {
+				formData.left = new FormAttachment(lastButton, 5);
+			}
+
+			button.setLayoutData(formData);
+
+			Point size = button.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+			if (size.x > buttonWidth) {
+				buttonWidth = size.x;
+			}
+
+			if (i == defaultOption) {
+				button.setFocus();
+				//				shell.setDefaultButton(button);
+			}
+
+			lastButton = button;
+		}
+
+		if (buttonWidth > 0) {
+			if (buttonWidth < MIN_BUTTON_SIZE) {
+				buttonWidth = MIN_BUTTON_SIZE;
+			}
+			for (int i = 0; i < buttons.length; i++) {
+				Point size = swtButtons[i].computeSize(buttonWidth, SWT.DEFAULT);
+				swtButtons[i].setSize(size);
+				formData = (FormData) swtButtons[i].getLayoutData();
+				formData.width = buttonWidth;
+			}
+		}
+
+		shell.addTraverseListener(new TraverseListener() {
+			public void keyTraversed(TraverseEvent event) {
+				if (event.detail == SWT.TRAVERSE_ESCAPE) {
+					shell.dispose();
+				}
+			}
+		});
+
+		Listener filterListener = new Listener() {
+			public void handleEvent(Event event) {
+				if (event.detail == SWT.TRAVERSE_ARROW_NEXT) {
+					event.detail = SWT.TRAVERSE_TAB_NEXT;
+					event.doit = true;
+				} else if (event.detail == SWT.TRAVERSE_ARROW_PREVIOUS) {
+					event.detail = SWT.TRAVERSE_TAB_PREVIOUS;
+					event.doit = true;
+				}
+			}
+		};
+		display.addFilter(SWT.Traverse, filterListener);
+
+		if (mouseAdapter != null) {
+			addMouseTrackListener(shell, mouseAdapter);
+		}
+
+		shell.pack();
+		Point size = shell.getSize();
+		if (size.x < MIN_SIZE_X) {
+			size.x = MIN_SIZE_X;
+			shell.setSize(size);
+		} else if (size.x > MAX_SIZE_X) {
+			size = shell.computeSize(MAX_SIZE_X, SWT.DEFAULT);
+			shell.setSize(size);
+		}
+
+		if (size.y < MIN_SIZE_Y) {
+			size.y = MIN_SIZE_Y;
+			shell.setSize(size);
+		}
+
+		Utils.centerWindowRelativeTo(shell, parent);
+
+		for (int i = 0; i < listeners.length; i++) {
+			listeners[i].skinAfterComponents(content, this, relatedObjects);
+		}
+
+		sShell.open();
+
+		while (!content.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+
 		if (display != null && !display.isDisposed()) {
 			display.removeFilter(SWT.Traverse, filterListener);
 		}
@@ -510,15 +888,15 @@ public class MessageBoxShell
 				control.addMouseTrackListener(listener);
 		}
 	}
-	
+
 	private Canvas createLinkLabel(final Composite shell, final String text) {
 
 		final Canvas canvas = new Canvas(shell, SWT.None) {
 			public Point computeSize(int wHint, int hHint, boolean changed) {
 				Rectangle area = new Rectangle(0, 0, MAX_SIZE_X, 5000);
 				GC gc = new GC(this);
-				GCStringPrinter sp = new GCStringPrinter(gc, text, area, true,
-						false, SWT.WRAP | SWT.TOP);
+				GCStringPrinter sp = new GCStringPrinter(gc, text, area, true, false,
+						SWT.WRAP | SWT.TOP);
 				sp.calculateMetrics();
 				gc.dispose();
 				Point size = sp.getCalculatedSize();
@@ -532,8 +910,8 @@ public class MessageBoxShell
 			public void handleEvent(Event e) {
 				if (e.type == SWT.Paint) {
 					Rectangle area = canvas.getClientArea();
-					sp = new GCStringPrinter(e.gc, text, area, true, false,
-							SWT.WRAP | SWT.TOP);
+					sp = new GCStringPrinter(e.gc, text, area, true, false, SWT.WRAP
+							| SWT.TOP);
 					sp.setUrlColor(ColorCache.getColor(e.gc.getDevice(), "#0000ff"));
 					if (urlColor != null) {
 						sp.setUrlColor(urlColor);
@@ -576,7 +954,6 @@ public class MessageBoxShell
 
 		return canvas;
 	}
-
 
 	public String getHtml() {
 		return html;
@@ -639,25 +1016,27 @@ public class MessageBoxShell
 	public boolean isAutoClosed() {
 		return autoClosed;
 	}
-	
+
 	// @see com.aelitis.azureus.ui.UIFunctionsUserPrompter#setRelatedObject(java.lang.Object)
 	public void setRelatedObject(Object relatedObject) {
-		this.relatedObjects = new Object[] { relatedObject };
+		this.relatedObjects = new Object[] {
+			relatedObject
+		};
 	}
 
 	// @see com.aelitis.azureus.ui.UIFunctionsUserPrompter#setRelatedObjects(java.lang.Object[])
 	public void setRelatedObjects(Object[] relatedObjects) {
 		this.relatedObjects = relatedObjects;
 	}
-	
+
 	public Object[] getRelatedObjects() {
 		return relatedObjects;
 	}
-	
+
 	public void setLeftImage(Image imgLeft) {
 		this.imgLeft = imgLeft;
 	}
-	
+
 	/**
 	 * Replaces Image on left with icon
 	 * 
@@ -666,39 +1045,32 @@ public class MessageBoxShell
 	 * @since 3.0.1.7
 	 */
 	public void setLeftImage(final int icon) {
-		Utils.execSWTThread(
-			new AERunnable() 
-			{
-				public void 
-				runSupport() 
-				{
-					setLeftImage(Display.getDefault().getSystemImage(icon));
-				}
-			});
+		Utils.execSWTThread(new AERunnable() {
+			public void runSupport() {
+				setLeftImage(Display.getDefault().getSystemImage(icon));
+			}
+		});
 	}
 
-	public void 
-	setIconResource(
-		String resource )
-	{
-		if ( resource.equals( "info" )){
-			
-			setLeftImage( SWT.ICON_INFORMATION );
-			
-		}else if ( resource.equals( "warning" )){
-			
-			setLeftImage( SWT.ICON_WARNING );
-			
-		}else if ( resource.equals( "error" )){
-			
-			setLeftImage( SWT.ICON_ERROR );
-			
-		}else{
-			
-			setLeftImage( ImageRepository.getImage( resource ));
+	public void setIconResource(String resource) {
+		if (resource.equals("info")) {
+
+			setLeftImage(SWT.ICON_INFORMATION);
+
+		} else if (resource.equals("warning")) {
+
+			setLeftImage(SWT.ICON_WARNING);
+
+		} else if (resource.equals("error")) {
+
+			setLeftImage(SWT.ICON_ERROR);
+
+		} else {
+
+			setLeftImage(ImageRepository.getImage(resource));
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Display display = Display.getDefault();
 		Shell shell = new Shell(display, SWT.SHELL_TRIM);
