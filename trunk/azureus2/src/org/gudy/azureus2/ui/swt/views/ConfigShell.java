b51067868593209763afce789439550e62fb371f
khai_m_nguyen <khai_m_nguyen@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-07-10 18:02:26 +0000

    Create a class for popping the ConfigView in a Shell This shell auto-saves the configs when closed There is only one shell opened at any time Will remember it's size and location
    git-svn-id: http://svn.vuze.com/public/client@13755 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/ConfigShell.java
@@ -0,0 +1,103 @@
+package org.gudy.azureus2.ui.swt.views;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.gudy.azureus2.core3.config.COConfigurationManager;
+import org.gudy.azureus2.core3.internat.MessageText;
+import org.gudy.azureus2.ui.swt.Utils;
+import org.gudy.azureus2.ui.swt.components.shell.ShellFactory;
+
+import com.aelitis.azureus.core.AzureusCoreFactory;
+import com.aelitis.azureus.ui.swt.UIFunctionsManagerSWT;
+import com.aelitis.azureus.ui.swt.UIFunctionsSWT;
+
+/**
+ * A shell containing the <code>ConfigView</code>
+ * This is used to pop-up the configs in a Shell as opposed to hosting it in the application
+ * This class is used to ensure that only one shell is opened at any time.
+ * @author khai
+ *
+ */
+public class ConfigShell
+{
+
+	private static ConfigShell instance;
+
+	private Shell shell;
+
+	public static ConfigShell getInstance() {
+		if (null == instance) {
+			instance = new ConfigShell();
+		}
+		return instance;
+	}
+
+	private ConfigShell() {
+	}
+
+	/**
+	 * Opens the <code>ConfigView</code> inside a pop-up <code>Shell</code>.
+	 * If the Shell is opened already then just force it active
+	 * @param width
+	 * @param height
+	 */
+	public void open() {
+		if (null != shell && false == shell.isDisposed()) {
+			if (true == shell.getMinimized()) {
+				shell.setMinimized(false);
+			}
+			shell.forceActive();
+			shell.forceFocus();
+		} else {
+			shell = ShellFactory.createShell(SWT.SHELL_TRIM);
+			shell.setLayout(new GridLayout());
+			shell.setText(MessageText.getString(MessageText.resolveLocalizationKey("ConfigView.title.full")));
+			Utils.setShellIcon(shell);
+			final ConfigView cView = new ConfigView(AzureusCoreFactory.getSingleton());
+			cView.initialize(shell);
+
+			/*
+			 * Set default size and centers the shell if it's configuration does not exist yet
+			 */
+			if (null == COConfigurationManager.getStringParameter(
+					"options.rectangle", null)) {
+				shell.setSize(800, 600);
+				Utils.centerWindowRelativeTo(shell, getMainShell());
+			}
+
+			Utils.linkShellMetricsToConfig(shell, "options");
+
+			/*
+			 * Auto-save when the shell closes
+			 */
+			shell.addListener(SWT.Close, new Listener() {
+				public void handleEvent(Event event) {
+					cView.save();
+					event.doit = true;
+				}
+			});
+
+			shell.open();
+		}
+	}
+
+	public void close() {
+		if (null != shell && false == shell.isDisposed()) {
+			shell.close();
+		}
+	}
+
+	private Shell getMainShell() {
+		UIFunctionsSWT uiFunctions = UIFunctionsManagerSWT.getUIFunctionsSWT();
+		if (null != uiFunctions) {
+			return uiFunctions.getMainShell();
+		}
+
+		throw new IllegalStateException(
+				"No instance of UIFunctionsSWT found; the UIFunctionsManager might not have been initialized properly");
+
+	}
+}
