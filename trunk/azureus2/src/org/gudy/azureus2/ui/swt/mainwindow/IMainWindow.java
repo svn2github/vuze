321e6f1b20932e2d30851c546db2d87e6a48df45
khai_m_nguyen <khai_m_nguyen@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-08-18 23:27:39 +0000

    Clean-up
    git-svn-id: http://svn.vuze.com/public/client@13961 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/IMainWindow.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/IMainWindow.java
@@ -21,10 +21,6 @@ public interface IMainWindow
 	
 	public static final int WINDOW_CONTENT_DISPLAY_AREA = 7;
 
-//	public static final int WINDOW_ELEMENT_FOOTER = 8;
-	
-	public static final int WINDOW_ELEMENT_BUTTON_BAR = 9;
-
 	public boolean isVisible(int windowElement);
 
 	public void setVisible(int windowElement, boolean value);
