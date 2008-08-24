c991ae4695592589e652baa81d1a12d80b120f84
amc1 <amc1@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-08-24 13:51:07 +0000

    Stop using the massive switch statement that I had before for decodeAlphaNumericChar - instead, do the whole char / int mixing thing which makes me feel a bit uncomfortable.
    git-svn-id: http://svn.vuze.com/public/client@13986 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/peermanager/utils/BTPeerIDByteDecoderUtils.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/peermanager/utils/BTPeerIDByteDecoderUtils.java
@@ -61,135 +61,17 @@ class BTPeerIDByteDecoderUtils {
 	}
 
 	public static String decodeAlphaNumericChar(char c) {
-		switch (c) {
-			case '0':
-				return "0";
-			case '1':
-				return "1";
-			case '2':
-				return "2";
-			case '3':
-				return "3";
-			case '4':
-				return "4";
-			case '5':
-				return "5";
-			case '6':
-				return "6";
-			case '7':
-				return "7";
-			case '8':
-				return "8";
-			case '9':
-				return "9";
-			case 'A':
-				return "10";
-			case 'B':
-				return "11";
-			case 'C':
-				return "12";
-			case 'D':
-				return "13";
-			case 'E':
-				return "14";
-			case 'F':
-				return "15";
-			case 'G':
-				return "16";
-			case 'H':
-				return "17";
-			case 'I':
-				return "18";
-			case 'J':
-				return "19";
-			case 'K':
-				return "20";
-			case 'L':
-				return "21";
-			case 'M':
-				return "22";
-			case 'N':
-				return "23";
-			case 'O':
-				return "24";
-			case 'P':
-				return "25";
-			case 'Q':
-				return "26";
-			case 'R':
-				return "27";
-			case 'S':
-				return "28";
-			case 'T':
-				return "29";
-			case 'U':
-				return "30";
-			case 'V':
-				return "31";
-			case 'W':
-				return "32";
-			case 'X':
-				return "33";
-			case 'Y':
-				return "34";
-			case 'Z':
-				return "35";
-			case 'a':
-				return "36";
-			case 'b':
-				return "37";
-			case 'c':
-				return "38";
-			case 'd':
-				return "39";
-			case 'e':
-				return "40";
-			case 'f':
-				return "41";
-			case 'g':
-				return "42";
-			case 'h':
-				return "43";
-			case 'i':
-				return "44";
-			case 'j':
-				return "45";
-			case 'k':
-				return "46";
-			case 'l':
-				return "47";
-			case 'm':
-				return "48";
-			case 'n':
-				return "49";
-			case 'o':
-				return "50";
-			case 'p':
-				return "51";
-			case 'q':
-				return "52";
-			case 'r':
-				return "53";
-			case 's':
-				return "54";
-			case 't':
-				return "55";
-			case 'u':
-				return "56";
-			case 'v':
-				return "57";
-			case 'w':
-				return "58";
-			case 'x':
-				return "59";
-			case 'y':
-				return "60";
-			case 'z':
-				return "61";
-			case '.':
-				return "62";
-			//case '-':
-			//	return "63";
+		if ('0' <= c && c <= '9') {
+			return String.valueOf(c);
+		}
+		else if ('A' <= c && c <= 'Z') {
+			return String.valueOf(10 + (c - 'A'));
+		}
+		else if ('a' <= c && c <= 'z') {
+			return String.valueOf(36 + (c - 'A'));
+		}
+		else if (c == '.') {
+			return "62";
 		}
 		return null;
 	}
