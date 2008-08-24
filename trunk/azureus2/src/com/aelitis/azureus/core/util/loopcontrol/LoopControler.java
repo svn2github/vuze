f8b28c5d0eeb0fd2bea983ef66cc104d42a8c58f
amc1 <amc1@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-08-24 19:27:14 +0000

    Added comment about the fact that this code doesn't appear to be used...
    git-svn-id: http://svn.vuze.com/public/client@13987 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/util/loopcontrol/LoopControler.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/util/loopcontrol/LoopControler.java
@@ -1,5 +1,8 @@
 package com.aelitis.azureus.core.util.loopcontrol;
 
+/**
+ * XXX: I can't find anywhere where this is used...
+ */
 public interface LoopControler {
 	
 	public double updateControler(double error,double position);
