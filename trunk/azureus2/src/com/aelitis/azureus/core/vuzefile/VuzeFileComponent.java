cf28965302001529e3af457dcb4b3e3badb1d24a
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-09-11 19:01:35 +0000

    reworked to avoid generic keys
    git-svn-id: http://svn.vuze.com/public/client@14076 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/vuzefile/VuzeFileComponent.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/vuzefile/VuzeFileComponent.java
@@ -26,13 +26,14 @@ import java.util.*;
 public interface 
 VuzeFileComponent 
 {
-	public static final int COMP_TYPE_NONE					= 0x00000000;
-	public static final int COMP_TYPE_METASEARCH_TEMPLATE	= 0x00000001;
-	public static final int COMP_TYPE_V3_NAVIGATION			= 0x00000002;
-	public static final int COMP_TYPE_V3_CONDITION_CHECK	= 0x00000004;
-	public static final int COMP_TYPE_PLUGIN				= 0x00000008;
-	public static final int COMP_TYPE_SUBSCRIPTION			= 0x00000010;
-	
+	public static final int COMP_TYPE_NONE						= 0x00000000;
+	public static final int COMP_TYPE_METASEARCH_TEMPLATE		= 0x00000001;
+	public static final int COMP_TYPE_V3_NAVIGATION				= 0x00000002;
+	public static final int COMP_TYPE_V3_CONDITION_CHECK		= 0x00000004;
+	public static final int COMP_TYPE_PLUGIN					= 0x00000008;
+	public static final int COMP_TYPE_SUBSCRIPTION				= 0x00000010;
+	public static final int COMP_TYPE_SUBSCRIPTION_SINGLETON	= 0x00000020;
+
 	public int
 	getType();
 	
