de5f05f35fd0d47e12381f5d2a49e3d257c3bef5
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-10-22 16:42:20 +0000

    separate tooltip and item name
    git-svn-id: http://svn.vuze.com/public/client@19191 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/subscriptions/SubscriptionWizard.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/subscriptions/SubscriptionWizard.java
@@ -820,7 +820,8 @@ public class SubscriptionWizard {
 			          int index = subscriptionTable.indexOf (item);
 			          Subscription subscription = subscriptions[index];
 			          item.setImage(rssIcon);
-			          item.setText(0, subscription.getNameEx());
+			          item.setText(0, subscription.getName());
+			          item.setData("tooltip", subscription.getNameEx());
 			          item.setData("popularity", new Long(subscription.getCachedPopularity()));
 			          if(subscription.isSubscribed()) {
 			        	  item.setForeground(display.getSystemColor(SWT.COLOR_GRAY));
