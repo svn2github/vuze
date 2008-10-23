34913a48ef65d22d8474b12439ad4517d3258fb2
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-10-23 21:18:08 +0000

    delay need-saving saves until loading is complete
    git-svn-id: http://svn.vuze.com/public/client@14432 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
@@ -241,7 +241,7 @@ public class GlobalManagerImpl
 	        
 	        determineSaveResumeDataInterval();
 	        
-	        if ((loopFactor % saveResumeLoopCount == 0) || needsSaving) {
+	        if ((loopFactor % saveResumeLoopCount == 0) || ( needsSaving && loadingComplete )) {
 	          	
 	        	saveDownloads( true );
 	        }
