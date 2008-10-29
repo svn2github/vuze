8ee7f7430a897061c39138145e5a36db219a8380
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2008-10-29 17:00:53 +0000

    add asset-hash to rss subscriptions
    git-svn-id: http://svn.vuze.com/public/client@14458 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/plugins/utils/search/SearchResult.java
+++ trunk/azureus2/src/org/gudy/azureus2/plugins/utils/search/SearchResult.java
@@ -44,6 +44,7 @@ SearchResult
 	public static final int	PR_ACCURACY				= 18;	// Long value, 0->100
 	public static final int	PR_VOTES_DOWN			= 19;	// Long
 	public static final int	PR_UID					= 20;	// String
+	public static final int	PR_HASH					= 21;	// byte[] torrent hash
 	
 
 	
