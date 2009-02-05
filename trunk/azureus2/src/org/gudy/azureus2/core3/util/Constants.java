cfc182e1083f428e91d3bbfb0343065fd4c3376f
nolar <nolar@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2009-02-05 06:48:36 +0000

    4103.
    git-svn-id: http://svn.vuze.com/public/client@14758 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
@@ -81,8 +81,8 @@ Constants
   
   public static String APP_NAME = "Vuze";
   public static final String AZUREUS_NAME	  = "Azureus";
-  public static final String AZUREUS_VERSION  = "4.1.0.1_CVS";  //4.1.0.1_CVS
-  public static final byte[] VERSION_ID       = ("-" + "AZ" + "4101" + "-").getBytes();  //MUST be 8 chars long!
+  public static final String AZUREUS_VERSION  = "4.1.0.3_CVS";  //4.1.0.5_CVS
+  public static final byte[] VERSION_ID       = ("-" + "AZ" + "4103" + "-").getBytes();  //MUST be 8 chars long!
   
   
   public static final String  OSName = System.getProperty("os.name");
