af14800016a0e661ea56581eb5f0a51a26a60406
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2009-04-14 23:14:30 +0000

    new constant, yay!
    git-svn-id: http://svn.vuze.com/public/client@14989 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
@@ -95,6 +95,7 @@ Constants
   public static final boolean isWindowsVista 	= OSName.equalsIgnoreCase("Windows Vista");
   public static final boolean isWindows95		= OSName.equalsIgnoreCase("Windows 95");
   public static final boolean isWindows98		= OSName.equalsIgnoreCase("Windows 98");
+  public static final boolean isWindows2000		= OSName.equalsIgnoreCase("Windows 2000");
   public static final boolean isWindowsME		= OSName.equalsIgnoreCase("Windows ME");
   public static final boolean isWindows9598ME	= isWindows95 || isWindows98 || isWindowsME;
   
