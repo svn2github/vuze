00c28a18def05a1716a40130cd162cbabe027597
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2009-07-23 01:10:15 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/client@15120 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/ByteEncodedKeyHashMap.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/ByteEncodedKeyHashMap.java
@@ -26,9 +26,22 @@ package org.gudy.azureus2.core3.util;
  */
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class 
 ByteEncodedKeyHashMap<T,S>
 	extends HashMap<T,S>
 {
+	public
+	ByteEncodedKeyHashMap()
+	{
+		super();
+	}
+	
+	public
+	ByteEncodedKeyHashMap(
+		Map<T,S>	map )
+	{
+		super( map );
+	}
 }
