9752a1f9cee27eab1824d3473ac4ba300b6bb9d5
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2009-08-26 22:30:35 +0000

    1.05 with debug disabled
    git-svn-id: http://svn.vuze.com/public/client@15204 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/build/libOSXAccess/OSXAccess.mm
+++ trunk/azureus2/src/build/libOSXAccess/OSXAccess.mm
@@ -12,7 +12,7 @@
 
 #include "IONotification.h"
 
-#define VERSION "1.04"
+#define VERSION "1.05"
 
 #define assertNot0(a) if (a == 0) { fprintf(stderr, "%s is 0\n", #a); return; }
 void fillServiceInfo(io_service_t service, JNIEnv *env, jobject hashMap, jmethodID methPut);
