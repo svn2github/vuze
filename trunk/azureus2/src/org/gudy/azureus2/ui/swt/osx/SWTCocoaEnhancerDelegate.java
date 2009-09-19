f76963935b0a700b011559684ac6093bbfa8a4a6
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2009-09-19 01:18:38 +0000

    friendly reminder that you can exclude this file from non-osx build
    git-svn-id: http://svn.vuze.com/public/client@15299 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/osx/SWTCocoaEnhancerDelegate.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/osx/SWTCocoaEnhancerDelegate.java
@@ -2,6 +2,9 @@ package org.gudy.azureus2.ui.swt.osx;
 
 import org.eclipse.swt.internal.cocoa.*;
 
+/**
+ * You can exclude this file (or this whole path) for non OSX builds
+ */
 public class SWTCocoaEnhancerDelegate extends NSObject {
 
 	public SWTCocoaEnhancerDelegate() {
