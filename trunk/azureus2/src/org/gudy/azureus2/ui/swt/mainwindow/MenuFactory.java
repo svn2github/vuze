ea6b0a2c2749e2970b45dbfaaa3a0894b97cd144
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2009-10-07 00:40:19 +0000

    Remove Publish menuitem from Main Menu
    git-svn-id: http://svn.vuze.com/public/client@15373 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/MenuFactory.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/mainwindow/MenuFactory.java
@@ -107,10 +107,6 @@ public class MenuFactory
 		return createTopLevelMenuItem(menuParent, MENU_ID_VIEW);
 	}
 
-	public static MenuItem createPublishMenuItem(Menu menuParent) {
-		return createTopLevelMenuItem(menuParent, MENU_ID_PUBLISH);
-	}
-
 	public static MenuItem createAdvancedMenuItem(Menu menuParent) {
 		return createTopLevelMenuItem(menuParent, MENU_ID_ADVANCED);
 	}
