216c28144de81d3d3603031acdea712599cc155d
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2010-01-13 23:53:20 +0000

    three stage pairing window only allow one access code change per open
    git-svn-id: http://svn.vuze.com/public/client@20330 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/RemotePairingWindow.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/RemotePairingWindow.java
@@ -18,6 +18,8 @@
 
 package com.aelitis.azureus.ui.swt.shells;
 
+import java.util.*;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
@@ -28,6 +30,14 @@ import org.eclipse.swt.widgets.Control;
 import org.gudy.azureus2.core3.config.COConfigurationManager;
 import org.gudy.azureus2.core3.util.AERunnable;
 import org.gudy.azureus2.core3.util.Constants;
+import org.gudy.azureus2.core3.util.Debug;
+import org.gudy.azureus2.plugins.PluginException;
+import org.gudy.azureus2.plugins.PluginInterface;
+import org.gudy.azureus2.plugins.PluginManager;
+import org.gudy.azureus2.plugins.installer.*;
+import org.gudy.azureus2.plugins.ui.sidebar.SideBarVitalityImage;
+import org.gudy.azureus2.plugins.update.UpdateCheckInstance;
+import org.gudy.azureus2.pluginsimpl.local.PluginManagerDefaultsImpl;
 import org.gudy.azureus2.ui.swt.Utils;
 import org.gudy.azureus2.ui.swt.mainwindow.Colors;
 import org.gudy.azureus2.ui.swt.shells.CoreWaiterSWT;
@@ -35,12 +45,15 @@ import org.gudy.azureus2.ui.swt.shells.GCStringPrinter;
 import org.gudy.azureus2.ui.swt.shells.CoreWaiterSWT.TriggerInThread;
 
 import com.aelitis.azureus.core.AzureusCore;
+import com.aelitis.azureus.core.AzureusCoreFactory;
 import com.aelitis.azureus.core.AzureusCoreRunningListener;
 import com.aelitis.azureus.core.pairing.*;
 import com.aelitis.azureus.ui.swt.skin.*;
 import com.aelitis.azureus.ui.swt.skin.SWTSkinButtonUtility.ButtonListenerAdapter;
 import com.aelitis.azureus.ui.swt.views.skin.SkinnedDialog;
 import com.aelitis.azureus.ui.swt.views.skin.SkinnedDialog.SkinnedDialogClosedListener;
+import com.aelitis.azureus.ui.swt.views.skin.sidebar.SideBar;
+import com.aelitis.azureus.ui.swt.views.skin.sidebar.SideBarEntrySWT;
 
 /**
  * @author TuxPaper
@@ -49,6 +62,8 @@ import com.aelitis.azureus.ui.swt.views.skin.SkinnedDialog.SkinnedDialogClosedLi
  */
 public class RemotePairingWindow implements PairingManagerListener
 {
+	private static final String PLUGINID_WEBUI = "xmwebui";
+
 	static RemotePairingWindow instance = null;
 
 	private SkinnedDialog skinnedDialog;
@@ -73,6 +88,14 @@ public class RemotePairingWindow implements PairingManagerListener
 
 	private PairingManagerListener pairingManagerListener;
 
+	private SWTSkinObject soFTUX;
+
+	private SWTSkinObject soCode;
+
+	private SWTSkinObject soInstall;
+
+	private PluginInterface piWebUI;
+
 	public static void open() {
 		synchronized (RemotePairingWindow.class) {
 			if (instance == null) {
@@ -89,20 +112,27 @@ public class RemotePairingWindow implements PairingManagerListener
 
 	private void _open() {
 		pairingManager = PairingManagerFactory.getSingleton();
+		piWebUI = AzureusCoreFactory.getSingleton().getPluginManager().getPluginInterfaceByID(
+				PLUGINID_WEBUI, true);
 		if (skinnedDialog == null || skinnedDialog.isDisposed()) {
 			skinnedDialog = new SkinnedDialog("skin3_dlg_remotepairing", "shell",
 					SWT.DIALOG_TRIM);
 
 			skin = skinnedDialog.getSkin();
 			soEnablePairing = (SWTSkinObjectCheckbox) skin.getSkinObject("enable-pairing");
-			soEnablePairing.setChecked(pairingManager.isEnabled());
+			soEnablePairing.setChecked(pairingManager.isEnabled() && piWebUI != null);
 			soEnablePairing.addSelectionListener(new SWTSkinCheckboxListener() {
 				public void checkboxChanged(SWTSkinObjectCheckbox so, boolean checked) {
-					pairingManager.setEnabled(checked);
-					try {
-						accessCode = pairingManager.getAccessCode();
-					} catch (PairingException e) {
-						// ignore.. if error, lastErrorUpdates will trigger
+					if (!pairingManager.isEnabled()) {
+  					pairingManager.setEnabled(checked);
+  					try {
+  						accessCode = pairingManager.getAccessCode();
+  					} catch (PairingException e) {
+  						// ignore.. if error, lastErrorUpdates will trigger
+  					}
+					}
+					if (piWebUI == null) {
+						installWebUI();
 					}
 					control.redraw();
 				}
@@ -171,23 +201,100 @@ public class RemotePairingWindow implements PairingManagerListener
 						// ignore.. if error, lastErrorUpdates will trigger
 					}
 					control.redraw();
+					SWTSkinObject soPairArea = skin.getSkinObject("reset-pair-area");
+					if (soPairArea != null) {
+						soPairArea.setVisible(false);
+					}
 				}
 			});
-		}
-		skinnedDialog.addCloseListener(new SkinnedDialogClosedListener() {
-			public void skinDialogClosed(SkinnedDialog dialog) {
-				pairingManager.removeListener(RemotePairingWindow.this);
-				Utils.disposeSWTObjects(new Object[] { fontCode });
+
+			skinnedDialog.addCloseListener(new SkinnedDialogClosedListener() {
+				public void skinDialogClosed(SkinnedDialog dialog) {
+					pairingManager.removeListener(RemotePairingWindow.this);
+					Utils.disposeSWTObjects(new Object[] { fontCode });
+				}
+			});
+			
+			soFTUX = skin.getSkinObject("pairing-ftux");
+			soCode = skin.getSkinObject("pairing-code");
+			soInstall = skin.getSkinObject("pairing-install");
+			
+			
+			if (piWebUI == null) {
+				soFTUX.getControl().moveAbove(null);
 			}
-		});
+		}
 		skinnedDialog.open();
+
+		if (piWebUI == null) {
+			soFTUX.setVisible(true);
+			soCode.setVisible(false);
+		}
 	}
 
-	/**
-	 * @param status
-	 *
-	 * @since 4.1.0.5
-	 */
+	protected void installWebUI() {
+		final PluginInstaller installer = AzureusCoreFactory.getSingleton().getPluginManager().getPluginInstaller();
+
+		StandardPlugin vuze_plugin = null;
+
+		try {
+			vuze_plugin = installer.getStandardPlugin(PLUGINID_WEBUI);
+
+		} catch (Throwable e) {
+		}
+		
+		if (vuze_plugin == null) {
+			return;
+		}
+		
+		if (vuze_plugin.isAlreadyInstalled()) {
+			PluginInterface plugin = vuze_plugin.getAlreadyInstalledPlugin();
+			plugin.getPluginState().setDisabled(false);
+			return;
+		}
+
+		try {
+			soInstall.setVisible(true);
+			soCode.setVisible(false);
+			soFTUX.setVisible(false);
+
+			Map<Integer, Object> properties = new HashMap<Integer, Object>();
+
+			properties.put(UpdateCheckInstance.PT_UI_STYLE,
+					UpdateCheckInstance.PT_UI_STYLE_SIMPLE);
+
+			properties.put(UpdateCheckInstance.PT_UI_PARENT_SWT_COMPOSITE,
+					soInstall.getControl());
+
+			properties.put(UpdateCheckInstance.PT_UI_DISABLE_ON_SUCCESS_SLIDEY, true);
+
+			installer.install(new InstallablePlugin[] { vuze_plugin }, false, properties,
+					new PluginInstallationListener() {
+						public void completed() {
+							soInstall.setVisible(false);
+							soCode.setVisible(true);
+							soFTUX.setVisible(false);
+						}
+
+						public void 
+						cancelled(){
+							skinnedDialog.close();
+						}
+						
+						public void failed(PluginException e) {
+							
+							Debug.out(e);
+							//Utils.openMessageBox(Utils.findAnyShell(), SWT.OK, "Error",
+							//		e.toString());
+						}
+					});
+
+		} catch (Throwable e) {
+
+			Debug.printStackTrace(e);
+		}
+	}
+
 	private void updateStatusText() {
 		if (soStatus != null) {
 			String s = "Status: " + pairingManager.getStatus();
