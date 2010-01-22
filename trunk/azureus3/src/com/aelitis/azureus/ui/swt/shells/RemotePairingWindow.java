406c18d377103063dcd4b44ea0e462383b765cb1
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2010-01-22 02:01:34 +0000

    Paring Test on showing of access code
    git-svn-id: http://svn.vuze.com/public/client@20352 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/RemotePairingWindow.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/RemotePairingWindow.java
@@ -22,8 +22,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -47,22 +46,24 @@ import org.gudy.azureus2.ui.swt.shells.GCStringPrinter.URLInfo;
 import com.aelitis.azureus.core.AzureusCore;
 import com.aelitis.azureus.core.AzureusCoreFactory;
 import com.aelitis.azureus.core.AzureusCoreRunningListener;
-import com.aelitis.azureus.core.cnetwork.ContentNetwork;
 import com.aelitis.azureus.core.pairing.*;
 import com.aelitis.azureus.ui.swt.skin.*;
 import com.aelitis.azureus.ui.swt.skin.SWTSkinButtonUtility.ButtonListenerAdapter;
+import com.aelitis.azureus.ui.swt.utils.ColorCache;
 import com.aelitis.azureus.ui.swt.views.skin.SkinnedDialog;
 import com.aelitis.azureus.ui.swt.views.skin.SkinnedDialog.SkinnedDialogClosedListener;
-import com.aelitis.azureus.util.ConstantsVuze;
 
 /**
  * @author TuxPaper
  * @created Jan 5, 2010
  *
  */
-public class RemotePairingWindow implements PairingManagerListener
+public class RemotePairingWindow
+	implements PairingManagerListener
 {
 	private static final String PLUGINID_WEBUI = "xmwebui";
+	
+	private static final boolean DEBUG = false;
 
 	static RemotePairingWindow instance = null;
 
@@ -82,7 +83,7 @@ public class RemotePairingWindow implements PairingManagerListener
 
 	private Control control;
 
-	private SWTSkinObjectText soStatus;
+	private SWTSkinObjectText soStatusText;
 
 	private SWTSkinObject soFTUX;
 
@@ -92,18 +93,36 @@ public class RemotePairingWindow implements PairingManagerListener
 
 	private SWTSkinObjectText soToClipboard;
 
+	private boolean hideCode = true;
+	
+	private String fallBackStatusText = "";
+
+	private static testPairingClass testPairingClass;
+	
+	private PairingTest pairingTest;
+
+
 	public static void open() {
+		if (DEBUG) {
+			if (testPairingClass == null) {
+				testPairingClass = new testPairingClass();
+			} else {
+				testPairingClass.inc();
+			}
+		}
+
 		synchronized (RemotePairingWindow.class) {
 			if (instance == null) {
 				instance = new RemotePairingWindow();
 			}
 		}
 
-		CoreWaiterSWT.waitForCore(TriggerInThread.SWT_THREAD, new AzureusCoreRunningListener() {
-			public void azureusCoreRunning(AzureusCore core) {
-				instance._open();
-			}
-		});
+		CoreWaiterSWT.waitForCore(TriggerInThread.SWT_THREAD,
+				new AzureusCoreRunningListener() {
+					public void azureusCoreRunning(AzureusCore core) {
+						instance._open();
+					}
+				});
 	}
 
 	private void _open() {
@@ -116,6 +135,10 @@ public class RemotePairingWindow implements PairingManagerListener
 					SWT.DIALOG_TRIM);
 
 			skin = skinnedDialog.getSkin();
+
+			soCodeArea = skin.getSkinObject("code-area");
+			control = soCodeArea.getControl();
+
 			soEnablePairing = (SWTSkinObjectButton) skin.getSkinObject("enable-pairing");
 			soEnablePairing.addSelectionListener(new ButtonListenerAdapter() {
 				// @see com.aelitis.azureus.ui.swt.skin.SWTSkinButtonUtility.ButtonListenerAdapter#pressed(com.aelitis.azureus.ui.swt.skin.SWTSkinButtonUtility, com.aelitis.azureus.ui.swt.skin.SWTSkinObject, int)
@@ -123,12 +146,12 @@ public class RemotePairingWindow implements PairingManagerListener
 						SWTSkinObject skinObject, int stateMask) {
 					skinObject.getControl().setEnabled(false);
 					if (!pairingManager.isEnabled()) {
-  					pairingManager.setEnabled(true);
-  					try {
-  						accessCode = pairingManager.getAccessCode();
-  					} catch (PairingException e) {
-  						// ignore.. if error, lastErrorUpdates will trigger
-  					}
+						pairingManager.setEnabled(true);
+						try {
+							accessCode = pairingManager.getAccessCode();
+						} catch (PairingException e) {
+							// ignore.. if error, lastErrorUpdates will trigger
+						}
 					}
 					if (piWebUI == null) {
 						installWebUI();
@@ -138,16 +161,28 @@ public class RemotePairingWindow implements PairingManagerListener
 					control.redraw();
 				}
 			});
-			
-			soStatus = (SWTSkinObjectText) skin.getSkinObject("status");
+
+			soStatusText = (SWTSkinObjectText) skin.getSkinObject("status-text");
+			soStatusText.addUrlClickedListener(new SWTSkinObjectText_UrlClickedListener() {
+				public boolean urlClicked(URLInfo urlInfo) {
+					if (urlInfo.url.equals("retry")) {
+						if (DEBUG) {
+							testPairingClass.inc();
+						}
+						testPairing();
+						return true;
+					}
+					return false;
+				}
+			});
 			updateStatusText();
+			
 			pairingManager.addListener(this);
 
-			soCodeArea = skin.getSkinObject("code-area");
-			control = soCodeArea.getControl();
 			Font font = control.getFont();
 			GC gc = new GC(control);
-			fontCode = Utils.getFontWithHeight(font, gc, Constants.isWindows ? 20 : 18, SWT.BOLD);
+			fontCode = Utils.getFontWithHeight(font, gc, Constants.isWindows ? 20
+					: 18, SWT.BOLD);
 			gc.dispose();
 			control.setFont(fontCode);
 
@@ -190,14 +225,17 @@ public class RemotePairingWindow implements PairingManagerListener
 						e.gc.fillRectangle(r);
 						e.gc.setForeground(Colors.blues[Colors.BLUES_DARKEST]);
 						e.gc.drawRectangle(r);
-						e.gc.setForeground(oldColor);
-						GCStringPrinter.printString(e.gc, "" + accessCode.charAt(i), r, false, false, SWT.CENTER);
+						if (!hideCode) {
+							e.gc.setForeground(oldColor);
+							GCStringPrinter.printString(e.gc, "" + accessCode.charAt(i), r,
+									false, false, SWT.CENTER);
+						}
 					}
 				}
 			});
 
 			soToClipboard = (SWTSkinObjectText) skin.getSkinObject("pair-clipboard");
-			
+
 			soToClipboard.addUrlClickedListener(new SWTSkinObjectText_UrlClickedListener() {
 				public boolean urlClicked(URLInfo urlInfo) {
 					if (urlInfo.url.equals("new")) {
@@ -218,7 +256,8 @@ public class RemotePairingWindow implements PairingManagerListener
 					return true;
 				}
 			});
-			SWTSkinButtonUtility btnToClipboard  = new SWTSkinButtonUtility(soToClipboard);
+			SWTSkinButtonUtility btnToClipboard = new SWTSkinButtonUtility(
+					soToClipboard);
 			btnToClipboard.addSelectionListener(new ButtonListenerAdapter() {
 				public void pressed(SWTSkinButtonUtility buttonUtility,
 						SWTSkinObject skinObject, int stateMask) {
@@ -227,15 +266,20 @@ public class RemotePairingWindow implements PairingManagerListener
 
 			skinnedDialog.addCloseListener(new SkinnedDialogClosedListener() {
 				public void skinDialogClosed(SkinnedDialog dialog) {
+					skinnedDialog = null;
 					pairingManager.removeListener(RemotePairingWindow.this);
-					Utils.disposeSWTObjects(new Object[] { fontCode });
+					Utils.disposeSWTObjects(new Object[] {
+						fontCode
+					});
+					if (pairingTest != null) {
+						pairingTest.cancel();
+					}
 				}
 			});
-			
+
 			soFTUX = skin.getSkinObject("pairing-ftux");
 			soCode = skin.getSkinObject("pairing-code");
-			
-			
+
 			if (showFTUX) {
 				soFTUX.getControl().moveAbove(null);
 			}
@@ -248,7 +292,7 @@ public class RemotePairingWindow implements PairingManagerListener
 			switchToCode();
 		}
 	}
-	
+
 	public void switchToFTUX() {
 		SWTSkinObject soPairInstallArea = skin.getSkinObject("pair-install");
 		if (soPairInstallArea != null) {
@@ -260,22 +304,118 @@ public class RemotePairingWindow implements PairingManagerListener
 
 	public void switchToCode() {
 		Utils.execSWTThread(new AERunnable() {
-			
+
 			public void runSupport() {
 
 				if (skinnedDialog.isDisposed()) {
 					return;
 				}
-				
+
 				SWTSkinObject soPairArea = skin.getSkinObject("reset-pair-area");
 				if (soPairArea != null) {
 					soPairArea.getControl().moveAbove(null);
 				}
 				soFTUX.setVisible(false);
 				soCode.setVisible(true);
-				
+
+				testPairing();
 			}
 		});
+	}
+
+	/**
+	 * 
+	 *
+	 * @since 4.1.0.5
+	 */
+	protected void testPairing() {
+		try {
+			hideCode = true;
+			control.redraw();
+			soStatusText.setTextID("remote.pairing.test.running");
+			soStatusText.setTextColor(ColorCache.getColor(control.getDisplay(), "#000000"));
+			SWTSkinObjectImage soImage = (SWTSkinObjectImage) skin.getSkinObject("status-image");
+			if (soImage != null) {
+				soImage.setImageByID("icon.spin", null);
+			}
+
+			PairingTestListener testListener = new PairingTestListener() {
+				public void testStarted(PairingTest test) {
+				}
+
+				public void testComplete(PairingTest test) {
+					if (skinnedDialog.isDisposed()) {
+						return;
+					}
+
+					int outcome = test.getOutcome();
+					String iconID = null;
+					String colorID = "#000000";
+					switch (outcome) {
+						case PairingTest.OT_SUCCESS:
+							fallBackStatusText = MessageText.getString("remote.pairing.test.success");
+							iconID = "icon.success";
+							colorID = "#007305";
+							break;
+
+						case PairingTest.OT_CANCELLED:
+							fallBackStatusText = test.getErrorMessage();
+							iconID = "icon.warning";
+							colorID = "#A97000";
+							break;
+							
+						case PairingTest.OT_SERVER_FAILED:
+						case PairingTest.OT_SERVER_OVERLOADED:
+						case PairingTest.OT_SERVER_UNAVAILABLE:
+							fallBackStatusText = MessageText.getString("remote.pairing.test.unavailable",
+									new String[] {
+										test.getErrorMessage()
+									});
+							iconID = "icon.warning";
+							colorID = "#C98000";
+							break;
+
+						default:
+							fallBackStatusText = MessageText.getString("remote.pairing.test.fail",
+									new String[] {
+										test.getErrorMessage()
+									});
+							iconID = "icon.failure";
+							colorID = "#c90000";
+							break;
+					}
+
+					hideCode = false;
+					final String fIconID = iconID;
+					Utils.execSWTThread(new AERunnable() {
+						public void runSupport() {
+							control.redraw();
+							SWTSkinObjectImage soImage = (SWTSkinObjectImage) skin.getSkinObject("status-image");
+							if (soImage != null) {
+								soImage.setImageByID(fIconID, null);
+							}
+						}
+					});
+					soStatusText.setText(fallBackStatusText);
+					soStatusText.setTextColor(ColorCache.getColor(control.getDisplay(), colorID));
+				}
+			};
+			pairingTest = pairingManager.testService(PLUGINID_WEBUI, testListener);
+			
+			if (DEBUG) {
+				testListener.testComplete(testPairingClass);
+				return;
+			}
+		} catch (PairingException e) {
+			soStatusText.setText(Debug.getNestedExceptionMessage(e));
+			Debug.out(e);
+		}
+		
+		if (pairingTest == null) {
+			hideCode = false;
+			control.redraw();
+			updateStatusText();
+		}
 	}
 
 	protected void installWebUI() {
@@ -288,11 +428,11 @@ public class RemotePairingWindow implements PairingManagerListener
 
 		} catch (Throwable e) {
 		}
-		
+
 		if (vuze_plugin == null) {
 			return;
 		}
-		
+
 		if (vuze_plugin.isAlreadyInstalled()) {
 			PluginInterface plugin = vuze_plugin.getAlreadyInstalledPlugin();
 			plugin.getPluginState().setDisabled(false);
@@ -318,27 +458,27 @@ public class RemotePairingWindow implements PairingManagerListener
 
 			properties.put(UpdateCheckInstance.PT_UI_DISABLE_ON_SUCCESS_SLIDEY, true);
 
-			installer.install(new InstallablePlugin[] { vuze_plugin }, false, properties,
-					new PluginInstallationListener() {
-						public void completed() {
-							if (soLearnMore != null) {
-								soLearnMore.setVisible(true);
-							}
-							switchToCode();
-						}
+			installer.install(new InstallablePlugin[] {
+				vuze_plugin
+			}, false, properties, new PluginInstallationListener() {
+				public void completed() {
+					if (soLearnMore != null) {
+						soLearnMore.setVisible(true);
+					}
+					switchToCode();
+				}
 
-						public void 
-						cancelled(){
-							skinnedDialog.close();
-						}
-						
-						public void failed(PluginException e) {
-							
-							Debug.out(e);
-							//Utils.openMessageBox(Utils.findAnyShell(), SWT.OK, "Error",
-							//		e.toString());
-						}
-					});
+				public void cancelled() {
+					skinnedDialog.close();
+				}
+
+				public void failed(PluginException e) {
+
+					Debug.out(e);
+					//Utils.openMessageBox(Utils.findAnyShell(), SWT.OK, "Error",
+					//		e.toString());
+				}
+			});
 
 		} catch (Throwable e) {
 
@@ -347,10 +487,13 @@ public class RemotePairingWindow implements PairingManagerListener
 	}
 
 	private void updateStatusText() {
-		if (soStatus != null) {
-			String s =  "";
+		if (hideCode || soStatusText.isDisposed()) {
+			return;
+		}
+		if (soStatusText != null) {
+			String s = "";
 			if (pairingManager.hasActionOutstanding()) {
-			 s += "Status: " + pairingManager.getStatus();
+				s += "Status: " + pairingManager.getStatus();
 			}
 			String lastServerError = pairingManager.getLastServerError();
 			if (lastServerError != null && lastServerError.length() > 0) {
@@ -359,14 +502,64 @@ public class RemotePairingWindow implements PairingManagerListener
 				}
 				s += "Last Error: " + lastServerError;
 			}
-			soStatus.setText(s);
+			if (s.length() == 0) {
+				s = fallBackStatusText;
+			} else {
+				soStatusText.setTextColor(ColorCache.getColor(control.getDisplay(),
+						"#666666"));
+			}
+			soStatusText.setText(s);
 		}
 	}
 
 	// @see com.aelitis.azureus.core.pairing.PairingManagerListener#somethingChanged(com.aelitis.azureus.core.pairing.PairingManager)
 	public void somethingChanged(PairingManager pm) {
+		if (skinnedDialog.isDisposed()) {
+			return;
+		}
+
 		updateStatusText();
 		
-		accessCode = pairingManager.peekAccessCode();
+		String newAccessCode = pairingManager.peekAccessCode();
+		if (newAccessCode == null) {
+			newAccessCode = "";
+		}
+		if (!newAccessCode.equals(accessCode)) {
+			accessCode = newAccessCode;
+			if (accessCode.length() > 0) {
+				testPairing();
+			}
+		}
+	}
+	
+	public static class testPairingClass implements PairingTest {
+			int curOutcome = 0;
+			int[] testOutcomes = { OT_SUCCESS, OT_FAILED, OT_CANCELLED, OT_SERVER_FAILED, OT_SERVER_OVERLOADED, OT_SERVER_UNAVAILABLE };
+			String[] testErrs = {
+				"Success",
+				"Could Not Connect blah blah technical stuff",
+				"You Cancelled (unpossible!)",
+				"Server Failed",
+				"Server Overloaded",
+				"Server Unavailable",
+			};
+			
+			public void inc() {
+				curOutcome++;
+				if (curOutcome == testOutcomes.length) {
+					curOutcome = 0;
+				}
+			}
+			
+			public int getOutcome() {
+				return testOutcomes[curOutcome];
+			}
+			
+			public String getErrorMessage() {
+				return testErrs[curOutcome];
+			}
+			
+			public void cancel() {
+			}
 	}
 }
