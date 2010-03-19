62a1ece52503e01c43c6da31587e73362f78003a
tuxpaper <tuxpaper@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2010-03-19 17:19:55 +0000

    double fix FakeTableCell's plugin datasource bug
    git-svn-id: http://svn.vuze.com/public/client@20545 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/skin/FakeTableCell.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/skin/FakeTableCell.java
@@ -303,8 +303,8 @@ public class FakeTableCell implements TableCell
 		cell.setCursorID(cursorID);
 	}
 
-	public void setDataSources(Object cds, Object pds) {
-		cell.setDataSources(cds, pds);
+	public void setDataSource(Object cds) {
+		cell.setDataSource(cds);
 	}
 
 	public void setFillCell(boolean fillCell) {
