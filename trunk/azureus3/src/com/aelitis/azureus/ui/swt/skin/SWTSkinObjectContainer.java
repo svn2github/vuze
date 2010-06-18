25917ada59527788f1e03da415a36ee24cdd1348
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2010-06-18 20:31:43 +0000

    Extract out CompositeMinSize so it can be used elsewhere
    git-svn-id: http://svn.vuze.com/public/client@24994 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/skin/SWTSkinObjectContainer.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/skin/SWTSkinObjectContainer.java
@@ -34,6 +34,8 @@ import org.gudy.azureus2.core3.util.AERunnable;
 import org.gudy.azureus2.core3.util.AERunnableObject;
 import org.gudy.azureus2.core3.util.Debug;
 import org.gudy.azureus2.ui.swt.Utils;
+import org.gudy.azureus2.ui.swt.components.CompositeMinSize;
+
 
 /**
  * A SWTSkinObject that contains other SWTSkinObjects
@@ -110,55 +112,10 @@ public class SWTSkinObjectContainer
 					|| !sTypeParams[1].equalsIgnoreCase("group")) {
   			// Lovely SWT has a default size of 64x64 if no children have sizes.
   			// Let's fix that..
-  			parentComposite = new Composite(createOn, style) {
-  				public Point computeSize(int wHint, int hHint, boolean changed) {
-  					try {
-    					Point size = super.computeSize(wHint, hHint, changed);
-    					return betterComputeSize(this, size, wHint, hHint, changed);
-  					} catch (Throwable t) {
-  						Debug.out(t);
-							return new Point(wHint == -1 ? 10 : wHint, hHint == -1 ? 10
-									: hHint);
-  					}
-  				}
-  				
-  				public Point computeSize(int wHint, int hHint) {
-  					try {
-    					Point size = super.computeSize(wHint, hHint);
-    					return betterComputeSize(this, size, wHint, hHint);
-  					} catch (Throwable t) {
-  						Debug.out(t);
-							return new Point(wHint == -1 ? 10 : wHint, hHint == -1 ? 10
-									: hHint);
-  					}
-  				}
-  			};
+  			parentComposite = new CompositeMinSize(createOn, style);
+  			((CompositeMinSize) parentComposite).setMinSize(new Point(minWidth, minHeight));
 			} else {
-  			parentComposite = new Group(createOn, style) {
-  				protected void checkSubclass() {};
-  				
-  				public Point computeSize(int wHint, int hHint, boolean changed) {
-  					try {
-    					Point size = super.computeSize(wHint, hHint, changed);
-    					return betterComputeSize(this, size, wHint, hHint, changed);
-  					} catch (Throwable t) {
-  						Debug.out(t);
-							return new Point(wHint == -1 ? 10 : wHint, hHint == -1 ? 10
-									: hHint);
-  					}
-  				}
-  				
-  				public Point computeSize(int wHint, int hHint) {
-  					try {
-    					Point size = super.computeSize(wHint, hHint);
-    					return betterComputeSize(this, size, wHint, hHint);
-  					} catch (Throwable t) {
-  						Debug.out(t);
-							return new Point(wHint == -1 ? 10 : wHint, hHint == -1 ? 10
-									: hHint);
-  					}
-  				}
-  			};
+  			parentComposite = new Group(createOn, style);
 			}
 		}
 		
@@ -172,55 +129,6 @@ public class SWTSkinObjectContainer
 		setControl(control);
 	}
 	
-	protected Point betterComputeSize(Composite c, Point size, int wHint,
-			int hHint) {
-		if (c.getChildren().length == 0 && (size.x == 64 || size.y == 64)) {
-			Object ld = c.getLayoutData();
-			if (ld instanceof FormData) {
-				FormData fd = (FormData) ld;
-				if (fd.width != 0 && fd.height != 0) {
-					Rectangle trim = c.computeTrim (0, 0, fd.width, fd.height);
-					return new Point(trim.width, trim.height);
-				}
-			}
-			return new Point(1, 1);
-		}
-		if (size.x == 0 && size.y == 0) {
-			return size;
-		}
-		if (minWidth > 0 && size.x < minWidth) {
-			size.x = minWidth;
-		}
-		if (minHeight > 0 && size.y < minHeight) {
-			size.y = minHeight;
-		}
-		return size;
-	}
-
-	protected Point betterComputeSize(Composite c, Point size, int wHint, int hHint, boolean changed) {
-		if (c.getChildren().length == 0 && (size.x == 64 || size.y == 64)) {
-			Object ld = c.getLayoutData();
-			if (ld instanceof FormData) {
-				FormData fd = (FormData) ld;
-				if (fd.width != 0 && fd.height != 0) {
-					Rectangle trim = c.computeTrim (0, 0, fd.width, fd.height);
-					return new Point(trim.width, trim.height);
-				}
-			}
-			return new Point(1, 1);
-		}
-		if (size.x == 0 && size.y == 0) {
-			return size;
-		}
-		if (minWidth > 0 && size.x < minWidth) {
-			size.x = minWidth;
-		}
-		if (minHeight > 0 && size.y < minHeight) {
-			size.y = minHeight;
-		}
-		return size;
-	}
-
 	// @see com.aelitis.azureus.ui.swt.skin.SWTSkinObjectBasic#setControl(org.eclipse.swt.widgets.Control)
 	public void setControl(Control control) {
 		bPropogateDown = properties.getIntValue(sConfigID + ".propogateDown", 1) == 1;
