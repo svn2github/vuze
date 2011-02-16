2a71b5acc15c8d4d6131b41705f295ec0d39fc71
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2011-02-16 18:44:36 +0000

    git-svn-id: http://svn.vuze.com/public/client@26484 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/skin/sidebar/SideBar.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/skin/sidebar/SideBar.java
@@ -74,7 +74,6 @@ import com.aelitis.azureus.util.ContentNetworkUtils;
  */
 public class SideBar
 	extends BaseMDI
-	implements ObfusticateImage
 {
 	protected static final boolean END_INDENT = Constants.isLinux
 			|| Constants.isWindows2000 || Constants.isWindows9598ME;
@@ -1581,21 +1580,5 @@ public class SideBar
 		}
 		
 		return super.updateLanguage(skinObject, params);
-	}
-
-	// @see org.gudy.azureus2.ui.swt.debug.ObfusticateImage#obfusticatedImage(org.eclipse.swt.graphics.Image)
-	public Image obfusticatedImage(Image image) {
-
-		Rectangle treeBounds = tree.getBounds();
-		SideBarEntrySWT[] sideBarEntries = (SideBarEntrySWT[]) mapIdToEntry.values().toArray(
-				new SideBarEntrySWT[0]);
-		for (SideBarEntrySWT entry : sideBarEntries) {
-			Rectangle entryBounds = entry.swt_getBounds();
-			if (treeBounds.intersects(entryBounds)) {
-				
-			}
-			System.out.println(entry.getId() + ":" + entryBounds + ";" + treeBounds.intersects(entryBounds));
-		}
-		return image;
 	}
 }
