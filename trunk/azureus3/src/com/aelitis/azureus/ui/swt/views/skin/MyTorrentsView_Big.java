30ea591cbf7e7d74dbcf55c11ddf3aa15543bebb
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2011-03-23 16:42:32 +0000

    remove dead code
    git-svn-id: http://svn.vuze.com/public/client@26575 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/skin/MyTorrentsView_Big.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/skin/MyTorrentsView_Big.java
@@ -1,6 +1,5 @@
 package com.aelitis.azureus.ui.swt.views.skin;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
 
@@ -8,10 +7,7 @@ import org.gudy.azureus2.core3.download.DownloadManager;
 import org.gudy.azureus2.plugins.download.Download;
 import org.gudy.azureus2.plugins.download.DownloadTypeComplete;
 import org.gudy.azureus2.plugins.download.DownloadTypeIncomplete;
-import org.gudy.azureus2.plugins.ui.tables.TableManager;
 import org.gudy.azureus2.ui.swt.views.MyTorrentsView;
-import org.gudy.azureus2.ui.swt.views.table.TableViewSWT;
-import org.gudy.azureus2.ui.swt.views.table.impl.TableViewSWTImpl;
 
 import com.aelitis.azureus.core.AzureusCore;
 import com.aelitis.azureus.core.torrent.PlatformTorrentUtils;
@@ -75,41 +71,6 @@ public class MyTorrentsView_Big
 		return super.isOurDownloadManager(dm);
 	}
 
-	protected TableViewSWT<?> createTableView(TableColumnCore[] basicItems) {
-		String tableID;
-		Class<?> forDataSourceType;
-		switch (torrentFilterMode) {
-			case SBC_LibraryView.TORRENTS_COMPLETE:
-				tableID = TableManager.TABLE_MYTORRENTS_COMPLETE_BIG;
-				forDataSourceType = DownloadTypeComplete.class;
-				break;
-
-			case SBC_LibraryView.TORRENTS_INCOMPLETE:
-				tableID = TableManager.TABLE_MYTORRENTS_INCOMPLETE_BIG;
-				forDataSourceType = DownloadTypeIncomplete.class;
-				break;
-				
-			case SBC_LibraryView.TORRENTS_UNOPENED:
-				tableID = TableManager.TABLE_MYTORRENTS_UNOPENED_BIG;
-				forDataSourceType = Download.class;
-				break;
-				
-			case SBC_LibraryView.TORRENTS_ALL:
-				tableID = TableManager.TABLE_MYTORRENTS_ALL_BIG;
-				forDataSourceType = Download.class;
-				break;
-
-			default:
-				tableID = "bad";
-				forDataSourceType = null;
-				break;
-		}
-		TableViewSWTImpl<?> tv = new TableViewSWTImpl<Object>(forDataSourceType,
-				tableID, "MyTorrentsView_Big", basicItems, "#", SWT.MULTI
-						| SWT.FULL_SELECTION | SWT.VIRTUAL | SWT.BORDER);
-		return tv;
-	}
-	
 	// @see org.gudy.azureus2.ui.swt.views.MyTorrentsView#defaultSelected(com.aelitis.azureus.ui.common.table.TableRowCore[])
 	public void defaultSelected(TableRowCore[] rows, int stateMask) {
 		SBC_LibraryTableView.doDefaultClick(rows, stateMask, !isSeedingView);
