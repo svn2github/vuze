ecbf4f679851e4be0b6025a58b556b6d47e9d3f3
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2011-08-25 00:15:34 +0000

    derp-a-swt-thread
    git-svn-id: http://svn.vuze.com/public/client@26687 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/pieces/BlocksItem.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/pieces/BlocksItem.java
@@ -29,6 +29,8 @@ import org.eclipse.swt.graphics.Image;
 import org.gudy.azureus2.core3.disk.DiskManager;
 import org.gudy.azureus2.core3.peer.PEPiece;
 import org.gudy.azureus2.core3.torrent.TOTorrent;
+import org.gudy.azureus2.core3.util.AERunnable;
+import org.gudy.azureus2.ui.swt.Utils;
 import org.gudy.azureus2.ui.swt.mainwindow.Colors;
 import org.gudy.azureus2.ui.swt.mainwindow.SWTThread;
 import org.gudy.azureus2.ui.swt.plugins.UISWTGraphic;
@@ -92,14 +94,20 @@ public class BlocksItem
 		cell.setFillCell(true);
 	}
 
-	public void dispose(TableCell cell) {
-		Image img = ((TableCellSWT) cell).getGraphicSWT();
-		if (img != null && !img.isDisposed())
-			img.dispose();
+	public void dispose(final TableCell cell) {
+		Utils.execSWTThread(new AERunnable() {
+
+			public void runSupport() {
+				Image img = ((TableCellSWT) cell).getGraphicSWT();
+				if (img != null && !img.isDisposed()) {
+					img.dispose();
+				}
+			}
+		});
 	}
 
-	public void refresh(TableCell cell) {
-		PEPiece pePiece = (PEPiece) cell.getDataSource();
+	public void refresh(final TableCell cell) {
+		final PEPiece pePiece = (PEPiece) cell.getDataSource();
 		if (pePiece == null) {
 			cell.setSortValue(0);
 			dispose(cell);
@@ -108,126 +116,135 @@ public class BlocksItem
 		}
 
 		cell.setSortValue(pePiece.getNbWritten());
-		long lNumBlocks = pePiece.getNbBlocks();
 
-		int newWidth = cell.getWidth();
-		if (newWidth <= 0) {
-			dispose(cell);
-			cell.setGraphic(null);
-			return;
-		}
-		int newHeight = cell.getHeight();
+		Utils.execSWTThread(new AERunnable() {
 
-		int x1 = newWidth - 2;
-		int y1 = newHeight - 3;
-		if (x1 < 10 || y1 < 3) {
-			dispose(cell);
-			cell.setGraphic(null);
-			return;
-		}
-		Image image = new Image(SWTThread.getInstance().getDisplay(), newWidth,
-				newHeight);
-		Color color;
-		GC gcImage = new GC(image);
-		gcImage.setForeground(Colors.grey);
-		gcImage.drawRectangle(0, 0, x1 + 1, y1 + 1);
-		int blocksPerPixel = 0;
-		int iPixelsPerBlock = 0;
-		int pxRes = 0;
-		long pxBlockStep = 0;
-		int factor = 4;
+			public void runSupport() {
 
-		while (iPixelsPerBlock <= 0) {
-			blocksPerPixel++;
-			iPixelsPerBlock = (int) ((x1 + 1) / (lNumBlocks / blocksPerPixel));
-		}
+				long lNumBlocks = pePiece.getNbBlocks();
 
-		pxRes = (int) (x1 - ((lNumBlocks / blocksPerPixel) * iPixelsPerBlock)); // kolik mi zbyde
-		if (pxRes <= 0)
-			pxRes = 1;
-		pxBlockStep = (lNumBlocks * factor) / pxRes; // kolikaty blok na +1 k sirce
-		long addBlocks = (lNumBlocks * factor) / pxBlockStep;
-		if ((addBlocks * iPixelsPerBlock) > pxRes)
-			pxBlockStep += 1;
+				int newWidth = cell.getWidth();
+				if (newWidth <= 0) {
+					dispose(cell);
+					cell.setGraphic(null);
+					return;
+				}
+				int newHeight = cell.getHeight();
 
-		/*      String msg = "iPixelsPerBlock = "+iPixelsPerBlock + ", blocksPerPixel = " + blocksPerPixel;
-		      msg += ", pxRes = " + pxRes + ", pxBlockStep = " + pxBlockStep + ", addBlocks = " + addBlocks + ", x1 = " + x1;
-		      Debug.out(msg);*/
+				int x1 = newWidth - 2;
+				int y1 = newHeight - 3;
+				if (x1 < 10 || y1 < 3) {
+					dispose(cell);
+					cell.setGraphic(null);
+					return;
+				}
+				Image image = new Image(SWTThread.getInstance().getDisplay(), newWidth,
+						newHeight);
+				Color color;
+				GC gcImage = new GC(image);
+				gcImage.setForeground(Colors.grey);
+				gcImage.drawRectangle(0, 0, x1 + 1, y1 + 1);
+				int blocksPerPixel = 0;
+				int iPixelsPerBlock = 0;
+				int pxRes = 0;
+				long pxBlockStep = 0;
+				int factor = 4;
 
-		TOTorrent torrent = pePiece.getManager().getDiskManager().getTorrent();
+				while (iPixelsPerBlock <= 0) {
+					blocksPerPixel++;
+					iPixelsPerBlock = (int) ((x1 + 1) / (lNumBlocks / blocksPerPixel));
+				}
 
-		boolean[] written = pePiece.getDMPiece().getWritten();
-		boolean piece_written = pePiece.isWritten();
-		int drawnWidth = 0;
-		int blockStep = 0;
+				pxRes = (int) (x1 - ((lNumBlocks / blocksPerPixel) * iPixelsPerBlock)); // kolik mi zbyde
+				if (pxRes <= 0)
+					pxRes = 1;
+				pxBlockStep = (lNumBlocks * factor) / pxRes; // kolikaty blok na +1 k sirce
+				long addBlocks = (lNumBlocks * factor) / pxBlockStep;
+				if ((addBlocks * iPixelsPerBlock) > pxRes)
+					pxBlockStep += 1;
 
-		int pieceNumber = pePiece.getPieceNumber();
-		long[] offsets = new long[(int) lNumBlocks];
-		long[] lengths = (long[]) offsets.clone();
-		Arrays.fill(offsets,
-				(long) pePiece.getManager().getDiskManager().getPieceLength()
-						* (long) pieceNumber);
-		for (int i = 0; i < lNumBlocks; lengths[i] = pePiece.getBlockSize(i), offsets[i] += DiskManager.BLOCK_SIZE * i, i++)
-			;
+				/*      String msg = "iPixelsPerBlock = "+iPixelsPerBlock + ", blocksPerPixel = " + blocksPerPixel;
+				      msg += ", pxRes = " + pxRes + ", pxBlockStep = " + pxBlockStep + ", addBlocks = " + addBlocks + ", x1 = " + x1;
+				      Debug.out(msg);*/
 
-		boolean[] isCached = cacheStats == null ? null
-				: cacheStats.getBytesInCache(torrent, offsets, lengths);
+				TOTorrent torrent = pePiece.getManager().getDiskManager().getTorrent();
 
-		for (int i = 0; i < lNumBlocks; i += blocksPerPixel) {
-			int nextWidth = iPixelsPerBlock;
+				boolean[] written = pePiece.getDMPiece().getWritten();
+				boolean piece_written = pePiece.isWritten();
+				int drawnWidth = 0;
+				int blockStep = 0;
 
-			blockStep += blocksPerPixel * factor;
-			if (blockStep >= pxBlockStep) { // pokud jsem prelezl dany pocet bloku, zvys tomuhle sirku
-				nextWidth += (int) (blockStep / pxBlockStep);
-				blockStep -= pxBlockStep;
+				int pieceNumber = pePiece.getPieceNumber();
+				long[] offsets = new long[(int) lNumBlocks];
+				long[] lengths = (long[]) offsets.clone();
+				Arrays.fill(offsets,
+						(long) pePiece.getManager().getDiskManager().getPieceLength()
+								* (long) pieceNumber);
+				for (int i = 0; i < lNumBlocks; lengths[i] = pePiece.getBlockSize(i), offsets[i] += DiskManager.BLOCK_SIZE
+						* i, i++)
+					;
+
+				boolean[] isCached = cacheStats == null ? null
+						: cacheStats.getBytesInCache(torrent, offsets, lengths);
+
+				for (int i = 0; i < lNumBlocks; i += blocksPerPixel) {
+					int nextWidth = iPixelsPerBlock;
+
+					blockStep += blocksPerPixel * factor;
+					if (blockStep >= pxBlockStep) { // pokud jsem prelezl dany pocet bloku, zvys tomuhle sirku
+						nextWidth += (int) (blockStep / pxBlockStep);
+						blockStep -= pxBlockStep;
+					}
+
+					if (i >= lNumBlocks - blocksPerPixel) { // pokud je posledni, at zasahuje az na konec
+						nextWidth = x1 - drawnWidth;
+					}
+					color = Colors.white;
+
+					if ((written == null && piece_written)
+							|| (written != null && written[i])) {
+
+						color = colors[COLOR_WRITTEN];
+
+					} else if (pePiece.isDownloaded(i)) {
+
+						color = colors[COLOR_DOWNLOADED];
+
+					} else if (pePiece.isRequested(i)) {
+
+						color = colors[COLOR_REQUESTED];
+					}
+
+					gcImage.setBackground(color);
+					gcImage.fillRectangle(drawnWidth + 1, 1, nextWidth, y1);
+
+					if (isCached[i]) {
+						gcImage.setBackground(colors[COLOR_INCACHE]);
+						gcImage.fillRectangle(drawnWidth + 1, 1, nextWidth, 3);
+
+					}
+
+					drawnWidth += nextWidth;
+
+				}
+				gcImage.dispose();
+
+				Image oldImage = null;
+				Graphic graphic = cell.getGraphic();
+				if (graphic instanceof UISWTGraphic) {
+					oldImage = ((UISWTGraphic) graphic).getImage();
+				}
+
+				if (cell instanceof TableCellSWT) {
+					((TableCellSWT) cell).setGraphic(image);
+				} else {
+					cell.setGraphic(new UISWTGraphicImpl(image));
+				}
+				if (oldImage != null && !oldImage.isDisposed())
+					oldImage.dispose();
+
+				gcImage.dispose();
 			}
-
-			if (i >= lNumBlocks - blocksPerPixel) { // pokud je posledni, at zasahuje az na konec
-				nextWidth = x1 - drawnWidth;
-			}
-			color = Colors.white;
-
-			if ((written == null && piece_written) || (written != null && written[i])) {
-
-				color = colors[COLOR_WRITTEN];
-
-			} else if (pePiece.isDownloaded(i)) {
-
-				color = colors[COLOR_DOWNLOADED];
-
-			} else if (pePiece.isRequested(i)) {
-
-				color = colors[COLOR_REQUESTED];
-			}
-
-			gcImage.setBackground(color);
-			gcImage.fillRectangle(drawnWidth + 1, 1, nextWidth, y1);
-
-			if (isCached[i]) {
-				gcImage.setBackground(colors[COLOR_INCACHE]);
-				gcImage.fillRectangle(drawnWidth + 1, 1, nextWidth, 3);
-
-			}
-
-			drawnWidth += nextWidth;
-
-		}
-		gcImage.dispose();
-		
-		Image oldImage = null;
-		Graphic graphic = cell.getGraphic();
-		if (graphic instanceof UISWTGraphic) {
-			oldImage = ((UISWTGraphic)graphic).getImage();
-		}
-
-		if (cell instanceof TableCellSWT) {
-			((TableCellSWT) cell).setGraphic(image);
-		} else {
-			cell.setGraphic(new UISWTGraphicImpl(image));
-		}
-		if (oldImage != null && !oldImage.isDisposed())
-			oldImage.dispose();
-
-		gcImage.dispose();
+		});
 	}
 }
