6433e4e8e3d4090c663712f04ed5c7185e98322a
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2012-07-19 22:06:41 +0000

    Prep next release (4713) cycle.
    git-svn-id: http://svn.vuze.com/public/client@27280 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/util/Constants.java
@@ -119,10 +119,10 @@ Constants
   
   public static String APP_NAME = "Vuze";
   public static final String AZUREUS_NAME	  = "Azureus";
-  public static final String AZUREUS_VERSION  = "4.7.1.2";  //4.7.1.3_CVS
+  public static final String AZUREUS_VERSION  = "4.7.1.3_CVS";  //4.7.1.3_CVS
   public static final String BUILD_VERSION  = "@build.version@";   //Ant replace
   public static final String AZUREUS_SUBVER	  = "";
-  public static final byte[] VERSION_ID       = ("-" + "AZ" + "4712" + "-").getBytes();  //MUST be 8 chars long!
+  public static final byte[] VERSION_ID       = ("-" + "AZ" + "4713" + "-").getBytes();  //MUST be 8 chars long!
 
   private static final boolean FORCE_NON_CVS = System.getProperty( "az.force.noncvs", "0" ).equals( "1" );
   
