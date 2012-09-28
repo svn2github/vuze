ec0a1a5e5ba0b83d82ffe9263dfcc06713d264b6
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2012-09-28 23:16:33 +0000

    async api
    git-svn-id: http://svn.vuze.com/public/client@27494 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/ViewTitleInfoBetaP.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/views/ViewTitleInfoBetaP.java
@@ -5,12 +5,13 @@ import java.net.URL;
 import java.util.Map;
 
 import org.gudy.azureus2.core3.config.COConfigurationManager;
-import org.gudy.azureus2.core3.util.FileUtil;
+import org.gudy.azureus2.core3.util.*;
 import org.gudy.azureus2.plugins.PluginInterface;
 import org.gudy.azureus2.plugins.utils.resourcedownloader.ResourceDownloader;
 import org.gudy.azureus2.pluginsimpl.local.PluginInitializer;
 
 import com.aelitis.azureus.ui.common.viewtitleinfo.ViewTitleInfo;
+import com.aelitis.azureus.ui.common.viewtitleinfo.ViewTitleInfoManager;
 import com.aelitis.azureus.util.JSONUtils;
 import com.aelitis.azureus.util.MapUtils;
 
@@ -18,29 +19,40 @@ public class ViewTitleInfoBetaP
 	implements ViewTitleInfo
 {
 	private static final String PARAM_LASTPOSTCOUNT = "betablog.numPosts";
+
 	long numNew = 0;
+
 	private long postCount = 0;
 
 	@SuppressWarnings("rawtypes")
 	public ViewTitleInfoBetaP() {
-		long lastPostCount = COConfigurationManager.getLongParameter(PARAM_LASTPOSTCOUNT, 0);
-		PluginInterface pi = PluginInitializer.getDefaultInterface();
-		try {
-			ResourceDownloader rd = pi.getUtilities().getResourceDownloaderFactory().create(
-					new URL("http://api.tumblr.com/v2/blog/devblog.vuze.com/info?api_key=C5a8UGiSwPflOrVecjcvwGiOWVsLFF22pC9SgUIKSuQfjAvDAY"));
-			InputStream download = rd.download();
-			Map json = JSONUtils.decodeJSON(FileUtil.readInputStreamAsString(download, 65535));
-			Map mapResponse = MapUtils.getMapMap(json, "response", null);
-			if (mapResponse != null) {
-				Map mapBlog = MapUtils.getMapMap(mapResponse, "blog", null);
-				if (mapBlog != null) {
-					postCount = MapUtils.getMapLong(mapBlog, "posts", 0);
-					numNew = postCount - lastPostCount;
-				}
-			}
-			
-		} catch (Exception e) {
-		}
+		SimpleTimer.addEvent("devblog", SystemTime.getCurrentTime(),
+				new TimerEventPerformer() {
+					public void perform(TimerEvent event) {
+						long lastPostCount = COConfigurationManager.getLongParameter(
+								PARAM_LASTPOSTCOUNT, 0);
+						PluginInterface pi = PluginInitializer.getDefaultInterface();
+						try {
+							ResourceDownloader rd = pi.getUtilities().getResourceDownloaderFactory().create(
+									new URL(
+											"http://api.tumblr.com/v2/blog/devblog.vuze.com/info?api_key=C5a8UGiSwPflOrVecjcvwGiOWVsLFF22pC9SgUIKSuQfjAvDAY"));
+							InputStream download = rd.download();
+							Map json = JSONUtils.decodeJSON(FileUtil.readInputStreamAsString(
+									download, 65535));
+							Map mapResponse = MapUtils.getMapMap(json, "response", null);
+							if (mapResponse != null) {
+								Map mapBlog = MapUtils.getMapMap(mapResponse, "blog", null);
+								if (mapBlog != null) {
+									postCount = MapUtils.getMapLong(mapBlog, "posts", 0);
+									numNew = postCount - lastPostCount;
+									ViewTitleInfoManager.refreshTitleInfo(ViewTitleInfoBetaP.this);
+								}
+							}
+
+						} catch (Exception e) {
+						}
+					}
+				});
 	}
 
 	public Object getTitleInfoProperty(int propertyID) {
