cf18604414af2088b219401d9d904245fd88fb24
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2012-12-28 16:35:27 +0000

    keep alive
    git-svn-id: http://svn.vuze.com/public/client@27810 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/net/upnpms/impl/UPNPMSBrowserImpl.java
+++ trunk/azureus2/src/com/aelitis/net/upnpms/impl/UPNPMSBrowserImpl.java
@@ -198,6 +198,9 @@ UPNPMSBrowserImpl
 		ResourceDownloader rd = new ResourceDownloaderFactoryImpl().create( url, post_data );
 		
 		try{
+			rd.setProperty( "URL_Connection", "Keep-Alive" );
+			rd.setProperty( "URL_Read_Timeout", 10*60*1000 );
+			rd.setProperty( "URL_Connect_Timeout", 5*60*1000 );
 			rd.setProperty( "URL_SOAPAction", "\"" + soap_action + "\"");
 			rd.setProperty( "URL_X-AV-Client-Info", "av=1.0; cn=\"Azureus Software, Inc.\"; mn=\"" + client_name + "\"; mv=\""+ Constants.AZUREUS_VERSION + "\"" );
 			rd.setProperty( "URL_Content-Type", "text/xml; charset=\"utf-8\"" );
