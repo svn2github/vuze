fb229eadc82087ea8e831b66f0d0020b5b1e8809
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2013-03-23 01:01:00 +0000

    git-svn-id: http://svn.vuze.com/public/client@28156 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/com/aelitis/azureus/core/tag/impl/TagManagerImpl.java
+++ trunk/azureus2/src/com/aelitis/azureus/core/tag/impl/TagManagerImpl.java
@@ -21,21 +21,31 @@
 
 package com.aelitis.azureus.core.tag.impl;
 
+import java.lang.ref.WeakReference;
 import java.util.*;
 
+import org.gudy.azureus2.core3.util.AERunnable;
 import org.gudy.azureus2.core3.util.Debug;
+import org.gudy.azureus2.core3.util.FileUtil;
+import org.gudy.azureus2.core3.util.FrequencyLimitedDispatcher;
 
+import com.aelitis.azureus.core.AzureusCore;
+import com.aelitis.azureus.core.AzureusCoreFactory;
+import com.aelitis.azureus.core.AzureusCoreLifecycleAdapter;
 import com.aelitis.azureus.core.tag.TagManager;
 import com.aelitis.azureus.core.tag.TagType;
 import com.aelitis.azureus.core.tag.Taggable;
 import com.aelitis.azureus.core.tag.TaggableLifecycleHandler;
 import com.aelitis.azureus.core.tag.TaggableResolver;
 import com.aelitis.azureus.core.util.CopyOnWriteList;
+import com.aelitis.azureus.util.MapUtils;
 
 public class 
 TagManagerImpl
 	implements TagManager
 {
+	private static final String	CONFIG_FILE 				= "tag.config";
+
 	private static TagManagerImpl	singleton = new TagManagerImpl();
 	
 	public static TagManagerImpl
@@ -48,9 +58,41 @@ TagManagerImpl
 	
 	private Map<Integer,TagType>	tag_type_map = new HashMap<Integer, TagType>();
 	
+	
+	private FrequencyLimitedDispatcher dirty_dispatcher = 
+		new FrequencyLimitedDispatcher(
+			new AERunnable()
+			{
+				public void
+				runSupport()
+				{
+					writeConfig();
+				}
+			},
+			30*1000 );
+	
+	
+	private Map					config;
+	private WeakReference<Map>	config_ref;
+	
+	private boolean				config_dirty;
+	
+	
+	
+	
 	private
 	TagManagerImpl()
 	{
+		AzureusCoreFactory.getSingleton().addLifecycleListener(
+			new AzureusCoreLifecycleAdapter()
+			{
+				public void
+				stopped(
+					AzureusCore		core )
+				{
+					destroy();
+				}
+			});
 	}
 	
 	public void
@@ -112,6 +154,92 @@ TagManagerImpl
 			});
 	}
 	
+	private void
+	destroy()
+	{
+		writeConfig();
+	}
+	
+	private void
+	setDirty()
+	{
+		synchronized( this ){
+			
+			if ( !config_dirty ){
+		
+				config_dirty = true;
+	
+				dirty_dispatcher.dispatch();
+			}
+		}
+	}
+	
+	private Map
+	readConfig()
+	{
+		Map map;
+		
+		if ( FileUtil.resilientConfigFileExists( CONFIG_FILE )){
+			
+			map = FileUtil.readResilientConfigFile( CONFIG_FILE );
+			
+		}else{
+			
+			map = new HashMap();
+		}
+		
+		return( map );
+	}
+	
+	private Map
+	getConfig()
+	{
+		synchronized( this ){
+			
+			if ( config != null ){
+				
+				return( config );
+			}
+			
+			if ( config_ref != null ){
+					
+				config = config_ref.get();
+				
+				if ( config != null ){
+					
+					return( config );
+				}
+			}
+							
+			config = readConfig();
+			
+			return( config );
+		}
+	}
+	
+	private void
+	writeConfig()
+	{
+		synchronized( this ){
+			
+			if ( !config_dirty ){
+				
+				return;
+			}
+	
+			config_dirty = false;
+			
+			if ( config != null ){
+							
+				FileUtil.writeResilientConfigFile( CONFIG_FILE, config );
+				
+				config_ref = new WeakReference<Map>( config );
+				
+				config = null;
+			}
+		}
+	}
+	
 	protected long
 	readLongAttribute(
 		TagTypeBase	tag_type,
@@ -119,7 +247,40 @@ TagManagerImpl
 		String		attr,
 		long		def )
 	{
-		return( def );
+		try{
+			synchronized( this ){
+				
+				Map m = getConfig();
+				
+				Map tt = (Map)m.get( String.valueOf( tag_type.getTagType()));
+				
+				if ( tt == null ){
+					
+					return( def );
+				}
+				
+				Map t = (Map)tt.get( String.valueOf( tag.getTagID()));
+				
+				if ( t == null ){
+					
+					return( def );
+				}
+				
+				Map conf = (Map)t.get( "c" );
+				
+				if ( conf == null ){
+					
+					return( def );
+				}
+				
+				return( MapUtils.getMapLong( conf, attr, def ));
+			}
+		}catch( Throwable e ){
+			
+			Debug.out( e );
+			
+			return( def );
+		}
 	}
 	
 	protected void
@@ -129,5 +290,56 @@ TagManagerImpl
 		String			attr,
 		long			value )
 	{
+		try{
+			synchronized( this ){
+				
+				Map m = getConfig();
+				
+				String tt_key = String.valueOf( tag_type.getTagType());
+				
+				Map tt = (Map)m.get( tt_key );
+				
+				if ( tt == null ){
+					
+					tt = new HashMap();
+					
+					m.put( tt_key, tt );
+				}
+				
+				String t_key = String.valueOf( tag.getTagID());
+				
+				Map t = (Map)tt.get( t_key );
+				
+				if ( t == null ){
+					
+					t = new HashMap();
+					
+					tt.put( t_key, t );
+				}
+				
+				Map conf = (Map)t.get( "c" );
+				
+				if ( conf == null ){
+					
+					conf= new HashMap();
+					
+					t.put( "c", conf );
+				}
+				
+				long old = MapUtils.getMapLong( conf, attr, 0 );
+				
+				if ( old == value && conf.containsKey( attr )){
+					
+					return;
+				}
+				
+				conf.put( attr, value );
+				
+				setDirty();
+			}
+		}catch( Throwable e ){
+			
+			Debug.out( e );
+		}
 	}	
 }
