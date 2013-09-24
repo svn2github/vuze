d4fcb8c4da7fa292ac87bd2a978f18be1bd481c9
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2013-09-24 17:40:40 +0000

    limit offsetting
    git-svn-id: http://svn.vuze.com/public/client@29075 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/opentorrent/OpenTorrentOptionsWindow.java
+++ trunk/azureus3/src/com/aelitis/azureus/ui/swt/shells/opentorrent/OpenTorrentOptionsWindow.java
@@ -1277,12 +1277,32 @@ public class OpenTorrentOptionsWindow
 				
 				Shell shell = dlg.getShell();
 				
+				Rectangle displayArea;
+				
+				try{
+					displayArea = shell.getMonitor().getClientArea();
+					
+				}catch (NoSuchMethodError e){
+					
+					displayArea = shell.getDisplay().getClientArea();
+				}	
+				
 				Rectangle rect = shell.getBounds();
 				
-				rect.x = max_x + 16;
-				rect.y = max_y + 16;
+				final int OFFSET = 16;
 				
-				dlg.getShell().setBounds( rect );
+				rect.x = max_x + OFFSET;
+				rect.y = max_y + OFFSET;
+				
+				if ( 	rect.x < displayArea.width - OFFSET &&
+						rect.y < displayArea.height - OFFSET ){
+				
+					shell.setBounds( rect );
+					
+				}else{
+					
+					Utils.centreWindow( shell );
+				}
 			}
 			
 			dlg.addCloseListener(new SkinnedDialogClosedListener() {
