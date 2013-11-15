fe5166aef09702d8e07f25aa4bf88da174a86067
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2013-11-15 16:54:44 +0000

    display current OS snd/rcv buff sizes when not overridden
    git-svn-id: http://svn.vuze.com/public/client@39531 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionConnectionAdvanced.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionConnectionAdvanced.java
@@ -20,12 +20,14 @@
 
 package org.gudy.azureus2.ui.swt.views.configsections;
 
+import java.net.Socket;
+import java.nio.channels.SocketChannel;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
-
 import org.gudy.azureus2.core3.config.COConfigurationManager;
 import org.gudy.azureus2.core3.config.impl.ConfigurationManager;
 import org.gudy.azureus2.core3.internat.MessageText;
@@ -115,7 +117,7 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		gridData = new GridData(GridData.VERTICAL_ALIGN_FILL | GridData.FILL_HORIZONTAL);
 		gSocket.setLayoutData(gridData);
 		GridLayout glayout = new GridLayout();
-		glayout.numColumns = 2;
+		glayout.numColumns = 3;
 		gSocket.setLayout(glayout);
 
 			// max simultaneous
@@ -128,6 +130,7 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		IntParameter max_connects = new IntParameter(gSocket,
 				"network.max.simultaneous.connect.attempts", 1, 100);    
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		max_connects.setLayoutData(gridData);
 
 			// // max pending
@@ -140,6 +143,7 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		IntParameter max_pending_connects = new IntParameter(gSocket,
 				"network.tcp.max.connections.outstanding", 1, 65536 );    
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		max_pending_connects.setLayoutData(gridData);
 		
 		
@@ -148,11 +152,13 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		
 		Label lbind = new Label(gSocket, SWT.NULL);
 		Messages.setLanguageText(lbind, "ConfigView.label.bindip" );
-		lbind.setLayoutData(new GridData());
+		gridData = new GridData();
+		lbind.setLayoutData(gridData);
 		
 		StringParameter bindip = new StringParameter(gSocket, "Bind IP", "", false);
 		gridData = new GridData();
 		gridData.widthHint = 100;
+		gridData.horizontalSpan = 2;
 		bindip.setLayoutData(gridData);
 
 		Text lbind2 = new Text(gSocket, SWT.READ_ONLY | SWT.MULTI);
@@ -162,23 +168,23 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 				"ConfigView.label.bindip.details",
 				new String[] { "\t" + NetworkAdmin.getSingleton().getNetworkInterfacesAsString().replaceAll( "\\\n", "\n\t" ) });
 		gridData = new GridData();
-		gridData.horizontalSpan = 2;
+		gridData.horizontalSpan = 3;
 		lbind2.setLayoutData(gridData);
 
 		
 		BooleanParameter check_bind = new BooleanParameter(gSocket, "Check Bind IP On Start","network.check.ipbinding");
 		gridData = new GridData();
-		gridData.horizontalSpan = 2;
+		gridData.horizontalSpan = 3;
 		check_bind.setLayoutData(gridData);
 
 		BooleanParameter force_bind = new BooleanParameter(gSocket, "Enforce Bind IP","network.enforce.ipbinding");
 		gridData = new GridData();
-		gridData.horizontalSpan = 2;
+		gridData.horizontalSpan = 3;
 		force_bind.setLayoutData(gridData);
 		
 		BooleanParameter bind_icon = new BooleanParameter(gSocket, "Show IP Bindings Icon", "network.ipbinding.icon.show");
 		gridData = new GridData();
-		gridData.horizontalSpan = 2;
+		gridData.horizontalSpan = 3;
 		bind_icon.setLayoutData(gridData);
 		
 		Label lpbind = new Label(gSocket, SWT.NULL);
@@ -186,6 +192,7 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		final IntParameter port_bind = new IntParameter(gSocket,
 				"network.bind.local.port", 0, 65535);
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		port_bind.setLayoutData(gridData);
 		
 		
@@ -194,28 +201,109 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		final IntParameter mtu_size = new IntParameter(gSocket,"network.tcp.mtu.size");
 		mtu_size.setMaximumValue(512 * 1024);
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		mtu_size.setLayoutData(gridData);
 
-
+			// sndbuf
+		
 		Label lsend = new Label(gSocket, SWT.NULL);
 		Messages.setLanguageText(lsend, CFG_PREFIX + "SO_SNDBUF");
 		final IntParameter SO_SNDBUF = new IntParameter(gSocket,	"network.tcp.socket.SO_SNDBUF");
 		gridData = new GridData();
 		SO_SNDBUF.setLayoutData(gridData);
+		
+		final Label lsendcurr =  new Label(gSocket, SWT.NULL);
+		gridData = new GridData( GridData.FILL_HORIZONTAL);
+		gridData.horizontalIndent = 10;
+		lsendcurr.setLayoutData( gridData );
 
-
+			// rcvbuf
+		
 		Label lreceiv = new Label(gSocket, SWT.NULL);
 		Messages.setLanguageText(lreceiv, CFG_PREFIX + "SO_RCVBUF");
 		final IntParameter SO_RCVBUF = new IntParameter(gSocket,	"network.tcp.socket.SO_RCVBUF");
 		gridData = new GridData();
 		SO_RCVBUF.setLayoutData(gridData);
+
+		final Label lreccurr =  new Label(gSocket, SWT.NULL);
+		gridData = new GridData( GridData.FILL_HORIZONTAL);
+		gridData.horizontalIndent = 10;
+		lreccurr.setLayoutData( gridData );
 		
+		final Runnable buff_updater = 
+			new Runnable() 
+			{			
+				public void 
+				run()
+				{
+					SocketChannel	sc = null;
+					
+					int	snd_val = 0;
+					int	rec_val = 0;
+					
+					try{
+						sc = SocketChannel.open();
+					
+						Socket socket = sc.socket();
+						
+						if ( SO_SNDBUF.getValue() == 0 ){
+							
+							snd_val = socket.getSendBufferSize();
+						}
+						
+						if ( SO_RCVBUF.getValue() == 0 ){
+							
+							rec_val = socket.getReceiveBufferSize();
+						}
+					}catch( Throwable e ){
+						
+					}finally{
+						
+						try{
+							sc.close();
+							
+						}catch( Throwable e ){
+						}
+					}
+					
+					if ( snd_val == 0 ){
+						lsendcurr.setText( "" );
+					}else{
+						Messages.setLanguageText( lsendcurr, "label.current.equals", new String[]{ String.valueOf(snd_val) });
+					}
+					
+					if ( rec_val == 0 ){
+						lreccurr.setText( "" );
+					}else{
+						Messages.setLanguageText( lreccurr, "label.current.equals", new String[]{ String.valueOf(rec_val) });
+					}
+				}
+			};
+				
+		buff_updater.run();
+			
+		ParameterChangeAdapter buff_listener =
+			new ParameterChangeAdapter()
+			{
+				public void 
+				parameterChanged(
+					Parameter 	p, 
+					boolean 	caused_internally) 
+				{
+					buff_updater.run();
+				}
+			};
+		
+		SO_RCVBUF.addChangeListener( buff_listener );
+		SO_SNDBUF.addChangeListener( buff_listener );
+			
 
 		Label ltos = new Label(gSocket, SWT.NULL);
 		Messages.setLanguageText(ltos, CFG_PREFIX + "IPDiffServ");
 		final StringParameter IPDiffServ = new StringParameter(gSocket,	"network.tcp.socket.IPDiffServ");
 		gridData = new GridData();
 		gridData.widthHint = 100;
+		gridData.horizontalSpan = 2;
 		IPDiffServ.setLayoutData(gridData);
 
 
@@ -262,12 +350,14 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		Messages.setLanguageText(lreadsel, CFG_PREFIX + "read_select", new String[]{ String.valueOf( COConfigurationManager.getDefault("network.tcp.read.select.time"))});
 		final IntParameter read_select = new IntParameter(gSocket,	"network.tcp.read.select.time", 10, 250);
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		read_select.setLayoutData(gridData);
 		
 		Label lreadselmin = new Label(gSocket, SWT.NULL);
 		Messages.setLanguageText(lreadselmin, CFG_PREFIX + "read_select_min", new String[]{ String.valueOf( COConfigurationManager.getDefault("network.tcp.read.select.min.time"))});
 		final IntParameter read_select_min = new IntParameter(gSocket,	"network.tcp.read.select.min.time", 0, 100 );
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		read_select_min.setLayoutData(gridData);
 
 			// write select
@@ -276,12 +366,14 @@ public class ConfigSectionConnectionAdvanced implements UISWTConfigSection {
 		Messages.setLanguageText(lwritesel, CFG_PREFIX + "write_select", new String[]{ String.valueOf( COConfigurationManager.getDefault("network.tcp.write.select.time"))});
 		final IntParameter write_select = new IntParameter(gSocket,	"network.tcp.write.select.time", 10, 250);
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		write_select.setLayoutData(gridData);
 		
 		Label lwriteselmin = new Label(gSocket, SWT.NULL);
 		Messages.setLanguageText(lwriteselmin, CFG_PREFIX + "write_select_min", new String[]{ String.valueOf( COConfigurationManager.getDefault("network.tcp.write.select.min.time"))});
 		final IntParameter write_select_min = new IntParameter(gSocket,	"network.tcp.write.select.min.time", 0, 100 );
 		gridData = new GridData();
+		gridData.horizontalSpan = 2;
 		write_select_min.setLayoutData(gridData);
 
 		new BooleanParameter( cSection, "IPV6 Enable Support", "network.ipv6.enable.support"  );
