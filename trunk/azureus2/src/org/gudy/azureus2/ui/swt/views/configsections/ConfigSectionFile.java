b012b01585c857eeefd8eb889c16bd4d0be6fcee
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2014-03-10 16:56:37 +0000

    grey out best-guess when not applicable
    git-svn-id: http://svn.vuze.com/public/client@39933 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionFile.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/configsections/ConfigSectionFile.java
@@ -32,8 +32,8 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.*;
-
 import org.gudy.azureus2.core3.config.COConfigurationManager;
+import org.gudy.azureus2.core3.config.ParameterListener;
 import org.gudy.azureus2.core3.config.impl.ConfigurationDefaults;
 import org.gudy.azureus2.core3.internat.MessageText;
 import org.gudy.azureus2.core3.torrent.TOTorrent;
@@ -93,7 +93,7 @@ public class ConfigSectionFile
 		int userMode = COConfigurationManager.getIntParameter("User Mode");
 
 		// Default Dir Section
-		Group gDefaultDir = new Group(gFile, SWT.NONE);
+		final Group gDefaultDir = new Group(gFile, SWT.NONE);
 		Messages.setLanguageText(gDefaultDir,
 				"ConfigView.section.file.defaultdir.section");
 		layout = new GridLayout();
@@ -182,20 +182,20 @@ public class ConfigSectionFile
 			gridData = new GridData(GridData.FILL_HORIZONTAL);
 			gridData.horizontalSpan = 3;
 			autoSaveAutoRename.setLayoutData(gridData);
-			IAdditionalActionPerformer aapDefaultDirStuff3 = new ChangeSelectionActionPerformer(
-					autoSaveAutoRename.getControls(), false);
+			//IAdditionalActionPerformer aapDefaultDirStuff3 = new ChangeSelectionActionPerformer(
+			//		autoSaveAutoRename.getControls(), false);
 
 			// def dir: best guess
 			sCurConfigID = "DefaultDir.BestGuess";
 			allConfigIDs.add(sCurConfigID);
-			BooleanParameter bestGuess = new BooleanParameter(gDefaultDir,
+			final BooleanParameter bestGuess = new BooleanParameter(gDefaultDir,
 					sCurConfigID, "ConfigView.section.file.defaultdir.bestguess");
 			gridData = new GridData(GridData.FILL_HORIZONTAL);
 			gridData.horizontalSpan = 3;
 			bestGuess.setLayoutData(gridData);
 
-			IAdditionalActionPerformer aapDefaultDirStuff = new ChangeSelectionActionPerformer(
-					bestGuess.getControls(), true);
+			//IAdditionalActionPerformer aapDefaultDirStuff = new ChangeSelectionActionPerformer(
+			//		bestGuess.getControls(), true);
 
 			// def dir: auto update
 			sCurConfigID = "DefaultDir.AutoUpdate";
@@ -206,8 +206,27 @@ public class ConfigSectionFile
 			gridData.horizontalSpan = 3;
 			autoUpdateSaveDir.setLayoutData(gridData);
 
-			IAdditionalActionPerformer aapDefaultDirStuff2 = new ChangeSelectionActionPerformer(
-					autoUpdateSaveDir.getControls(), true);
+			//IAdditionalActionPerformer aapDefaultDirStuff2 = new ChangeSelectionActionPerformer(
+			//		autoUpdateSaveDir.getControls(), true);
+			
+			COConfigurationManager.addAndFireParameterListener(
+					"Default save path",
+					new ParameterListener() {
+						
+						public void parameterChanged(String parameterName) {
+						
+							if ( gDefaultDir.isDisposed()){
+								
+								COConfigurationManager.removeParameterListener(parameterName, this );
+								
+							}else{
+								
+								String dsp = COConfigurationManager.getStringParameter( parameterName );
+								
+								bestGuess.setEnabled( dsp == null || dsp.trim().length() == 0 );
+							}
+						}
+					});
 		}
 
 		new Label(gFile, SWT.NONE);
