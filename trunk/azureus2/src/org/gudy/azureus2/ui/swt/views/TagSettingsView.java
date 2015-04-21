ac6c5733275f17b05045cb169946c8cf6801dbee
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2015-04-21 21:37:16 +0000

    Tag File Settings and constraint
    git-svn-id: http://svn.vuze.com/public/client@42015 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/TagSettingsView.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/TagSettingsView.java
@@ -18,14 +18,17 @@
 
 package org.gudy.azureus2.ui.swt.views;
 
+import java.io.File;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.events.ModifyEvent;
-import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
+import org.gudy.azureus2.core3.config.COConfigurationManager;
 import org.gudy.azureus2.core3.internat.MessageText;
 import org.gudy.azureus2.core3.util.AERunnable;
 import org.gudy.azureus2.core3.util.Debug;
@@ -39,6 +42,8 @@ import org.gudy.azureus2.ui.swt.plugins.UISWTViewEvent;
 import org.gudy.azureus2.ui.swt.pluginsimpl.UISWTViewCoreEventListener;
 
 import com.aelitis.azureus.core.tag.*;
+import com.aelitis.azureus.core.tag.TagFeatureProperties.TagProperty;
+import com.aelitis.azureus.ui.swt.imageloader.ImageLoader;
 import com.aelitis.azureus.ui.swt.utils.FontUtils;
 
 /**
@@ -77,6 +82,14 @@ public class TagSettingsView
 		public GenericFloatParameter min_sr;
 
 		public GenericFloatParameter max_sr;
+
+		public folderOption initalSaveFolder;
+
+		public folderOption moveOnCompleteFolder;
+
+		public folderOption copyOnCompleteFolder;
+
+		public Text constraints;
 	}
 
 	private Params params = null;
@@ -196,7 +209,6 @@ public class TagSettingsView
 			gridLayout.marginHeight = gridLayout.marginWidth = 0;
 			cMainComposite.setLayout(gridLayout);
 
-
 			Composite cSection1 = new Composite(cMainComposite, SWT.NONE);
 			gridLayout = new GridLayout(4, false);
 			gridLayout.marginHeight = 0;
@@ -209,8 +221,6 @@ public class TagSettingsView
 			cSection2.setLayout(gridLayout);
 			gd = new GridData(SWT.FILL, SWT.FILL, true, false);
 			cSection2.setLayoutData(gd);
-			
-			
 
 			Label label;
 
@@ -257,8 +267,8 @@ public class TagSettingsView
 			label = new Label(cSection1, SWT.NONE);
 			Messages.setLanguageText(label, "label.color");
 			int[] color = tag.getColor();
-			params.tagColor = new ColorParameter(cSection1, null, color[0],
-					color[1], color[2]) {
+			params.tagColor = new ColorParameter(cSection1, null, color[0], color[1],
+					color[2]) {
 				// @see org.gudy.azureus2.ui.swt.config.ColorParameter#newColorChosen(org.eclipse.swt.graphics.RGB)
 				public void newColorChosen(RGB newColor) {
 					tag.setColor(new int[] {
@@ -310,6 +320,8 @@ public class TagSettingsView
 				params.isPublic.setLayoutData(gd);
 			}
 
+			////////////////////
+
 			Group gTransfer = new Group(cMainComposite, SWT.NONE);
 			gTransfer.setText("Transfer Settings");
 			gridLayout = new GridLayout(4, false);
@@ -317,10 +329,11 @@ public class TagSettingsView
 
 			gd = new GridData(SWT.NONE, SWT.NONE, false, false, 4, 1);
 			gTransfer.setLayoutData(gd);
-			
+
 			if (tag instanceof TagFeatureRateLimit) {
 				final TagFeatureRateLimit rl = (TagFeatureRateLimit) tag;
-				String k_unit = DisplayFormatters.getRateUnitBase10(DisplayFormatters.UNIT_KB).trim();
+				String k_unit = DisplayFormatters.getRateUnitBase10(
+						DisplayFormatters.UNIT_KB).trim();
 
 				if (rl.supportsTagDownloadLimit()) {
 					// Field: Download Limit
@@ -361,117 +374,307 @@ public class TagSettingsView
 					}
 				}
 
-					// Upload Limit
-					if (rl.supportsTagUploadLimit()) {
-						gd = new GridData();
-						label = new Label(gTransfer, SWT.NULL);
-						label.setLayoutData(gd);
-						label.setText(k_unit + " " + MessageText.getString(
-								"GeneralView.label.maxuploadspeed.tooltip"));
+				// Upload Limit
+				if (rl.supportsTagUploadLimit()) {
+					gd = new GridData();
+					label = new Label(gTransfer, SWT.NULL);
+					label.setLayoutData(gd);
+					label.setText(k_unit + " " + MessageText.getString(
+							"GeneralView.label.maxuploadspeed.tooltip"));
 
-						gd = new GridData();
-						//gd.horizontalSpan = 3;
-						params.maxUploadSpeed = new GenericIntParameter(
-								new GenericParameterAdapter() {
-									public int getIntValue(String key) {
-										int limit = rl.getTagUploadLimit();
-										return limit < 0 ? limit : limit / 1024;
-									}
+					gd = new GridData();
+					//gd.horizontalSpan = 3;
+					params.maxUploadSpeed = new GenericIntParameter(
+							new GenericParameterAdapter() {
+								public int getIntValue(String key) {
+									int limit = rl.getTagUploadLimit();
+									return limit < 0 ? limit : limit / 1024;
+								}
 
-									public int getIntValue(String key, int def) {
-										return getIntValue(key);
-									}
+								public int getIntValue(String key, int def) {
+									return getIntValue(key);
+								}
 
-									public void setIntValue(String key, int value) {
-										if (value == -1) {
-											rl.setTagUploadLimit(value);
-										} else {
-											rl.setTagUploadLimit(value * 1024);
-										}
+								public void setIntValue(String key, int value) {
+									if (value == -1) {
+										rl.setTagUploadLimit(value);
+									} else {
+										rl.setTagUploadLimit(value * 1024);
 									}
+								}
 
-									public boolean resetIntDefault(String key) {
-										return false;
-									}
-								}, gTransfer, null, -1, Integer.MAX_VALUE);
-						params.maxUploadSpeed.setLayoutData(gd);
+								public boolean resetIntDefault(String key) {
+									return false;
+								}
+							}, gTransfer, null, -1, Integer.MAX_VALUE);
+					params.maxUploadSpeed.setLayoutData(gd);
+				}
+
+				// Field: Upload Priority
+				if (rl.getTagUploadPriority() >= 0) {
+					params.uploadPriority = new GenericBooleanParameter(
+							new GenericParameterAdapter() {
+								public boolean getBooleanValue(String key) {
+									return rl.getTagUploadPriority() > 0;
+								}
+
+								public boolean getBooleanValue(String key, boolean def) {
+									return getBooleanValue(key);
+								}
+
+								public void setBooleanValue(String key, boolean value) {
+									rl.setTagUploadPriority(value ? 1 : 0);
+								}
+							}, gTransfer, null, "cat.upload.priority");
+					gd = new GridData();
+					gd.horizontalSpan = 4;
+					params.uploadPriority.setLayoutData(gd);
+				}
+
+				// Field: Min Share
+				if (rl.getTagMinShareRatio() >= 0) {
+					label = new Label(gTransfer, SWT.NONE);
+					Messages.setLanguageText(label, "TableColumn.header.min_sr");
+					gd = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+					label.setLayoutData(gd);
+
+					params.min_sr = new GenericFloatParameter(
+							new GenericParameterAdapter() {
+								public float getFloatValue(String key) {
+									return rl.getTagMinShareRatio() / 1000f;
+								}
+
+								public void setFloatValue(String key, float value) {
+									rl.setTagMinShareRatio((int) (value * 1000));
+								}
+							}, gTransfer, null, 0, Float.MAX_VALUE, true, 3);
+					gd = new GridData();
+					//gd.horizontalSpan = 3;
+					gd.widthHint = 50;
+					params.min_sr.setLayoutData(gd);
+				}
+
+				// Field: Max Share
+				if (rl.getTagMaxShareRatio() >= 0) {
+					label = new Label(gTransfer, SWT.NONE);
+					Messages.setLanguageText(label, "TableColumn.header.max_sr");
+					gd = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
+					label.setLayoutData(gd);
+
+					params.max_sr = new GenericFloatParameter(
+							new GenericParameterAdapter() {
+								public float getFloatValue(String key) {
+									return rl.getTagMaxShareRatio() / 1000f;
+								}
+
+								public void setFloatValue(String key, float value) {
+									rl.setTagMaxShareRatio((int) (value * 1000));
+								}
+							}, gTransfer, null, 0, Float.MAX_VALUE, true, 3);
+					gd = new GridData();
+					//gd.horizontalSpan = 3;
+					gd.widthHint = 50;
+					params.max_sr.setLayoutData(gd);
+				}
+			}
+
+			/////////////////////////////////
+
+			if (tag instanceof TagFeatureFileLocation) {
+				final TagFeatureFileLocation fl = (TagFeatureFileLocation) tag;
+
+				if (fl.supportsTagCopyOnComplete() || fl.supportsTagInitialSaveFolder()
+						|| fl.supportsTagMoveOnComplete()) {
+
+					Group gFiles = new Group(cMainComposite, SWT.NONE);
+					gFiles.setText("File Settings");
+					gridLayout = new GridLayout(4, false);
+					gFiles.setLayout(gridLayout);
+
+					gd = new GridData(SWT.FILL, SWT.NONE, true, false, 4, 1);
+					gFiles.setLayoutData(gd);
+
+					if (fl.supportsTagInitialSaveFolder()) {
+						params.initalSaveFolder = new folderOption(gFiles,
+								"label.init.save.loc") {
+							public void setFolder(File folder) {
+								fl.setTagInitialSaveFolder(folder);
+							}
+
+							public File getFolder() {
+								return fl.getTagInitialSaveFolder();
+							}
+						};
 					}
 
-					// Field: Upload Priority
-					if (rl.getTagUploadPriority() >= 0) {
-  					params.uploadPriority = new GenericBooleanParameter(
-  							new GenericParameterAdapter() {
-  								public boolean getBooleanValue(String key) {
-  									return rl.getTagUploadPriority() > 0;
-  								}
-  
-  								public boolean getBooleanValue(String key, boolean def) {
-  									return getBooleanValue(key);
-  								}
-  
-  								public void setBooleanValue(String key, boolean value) {
-  									rl.setTagUploadPriority(value ? 1 : 0);
-  								}
-  							}, gTransfer, null, "cat.upload.priority");
-  					gd = new GridData();
-  					gd.horizontalSpan = 4;
-  					params.uploadPriority.setLayoutData(gd);
+					if (fl.supportsTagMoveOnComplete()) {
+						params.moveOnCompleteFolder = new folderOption(gFiles,
+								"label.move.on.comp") {
+							public void setFolder(File folder) {
+								fl.setTagMoveOnCompleteFolder(folder);
+							}
+
+							public File getFolder() {
+								return fl.getTagMoveOnCompleteFolder();
+							}
+						};
 					}
 
-					// Field: Min Share
-					if (rl.getTagMinShareRatio() >= 0) {
-  					label = new Label(gTransfer, SWT.NONE);
-  					Messages.setLanguageText(label, "TableColumn.header.min_sr" );
-  					gd = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
-  					label.setLayoutData(gd);
-  
-  					params.min_sr = new GenericFloatParameter(
-  							new GenericParameterAdapter() {
-  								public float getFloatValue(String key) {
-  									return rl.getTagMinShareRatio() / 1000f;
-  								}
-  								
-  								public void setFloatValue(String key, float value) {
-  									rl.setTagMinShareRatio((int) (value * 1000));
-  								}
-  							}, gTransfer, null, 0, Float.MAX_VALUE,
-  							true, 3);
-  					gd = new GridData();
-  					//gd.horizontalSpan = 3;
-  					gd.widthHint = 50;
-  					params.min_sr.setLayoutData(gd);
-					}
+					if (fl.supportsTagCopyOnComplete()) {
+						params.copyOnCompleteFolder = new folderOption(gFiles,
+								"label.copy.on.comp") {
+							public void setFolder(File folder) {
+								fl.setTagCopyOnCompleteFolder(folder);
+							}
 
-					// Field: Max Share
-					if (rl.getTagMaxShareRatio() >= 0) {
-  					label = new Label(gTransfer, SWT.NONE);
-  					Messages.setLanguageText(label, "TableColumn.header.max_sr" );
-  					gd = new GridData(SWT.BEGINNING, SWT.CENTER, false, false);
-  					label.setLayoutData(gd);
-  
-  					params.max_sr = new GenericFloatParameter(
-  							new GenericParameterAdapter() {
-  								public float getFloatValue(String key) {
-  									return rl.getTagMaxShareRatio() / 1000f;
-  								}
-  								
-  								public void setFloatValue(String key, float value) {
-  									rl.setTagMaxShareRatio((int) (value * 1000));
-  								}
-  							}, gTransfer, null, 0, Float.MAX_VALUE,
-  							true, 3);
-  					gd = new GridData();
-  					//gd.horizontalSpan = 3;
-  					gd.widthHint = 50;
-  					params.max_sr.setLayoutData(gd);
+							public File getFolder() {
+								return fl.getTagCopyOnCompleteFolder();
+							}
+						};
 					}
 				}
+			}
+
+			///////////////////////////////
+
+			if (tag.getTagType().hasTagTypeFeature(TagFeature.TF_PROPERTIES)
+					&& (tag instanceof TagFeatureProperties)) {
+				TagFeatureProperties tfp = (TagFeatureProperties) tag;
+
+				final TagProperty propConstraint = tfp.getProperty(
+						TagFeatureProperties.PR_CONSTRAINT);
+				if (propConstraint != null) {
+					Group gConstraint = new Group(cMainComposite, SWT.NONE);
+					Messages.setLanguageText(gConstraint, "tag.property.constraint");
+					gridLayout = new GridLayout(3, false);
+					gConstraint.setLayout(gridLayout);
+
+					gd = new GridData(SWT.FILL, SWT.NONE, true, false, 4, 1);
+					gConstraint.setLayoutData(gd);
+
+					params.constraints = new Text(gConstraint,
+							SWT.WRAP | SWT.BORDER | SWT.MULTI);
+					gd = new GridData(SWT.FILL, SWT.NONE, true, false, 3, 1);
+					gd.heightHint = 40;
+					params.constraints.setLayoutData(gd);
+					params.constraints.addKeyListener(new KeyListener() {
+						public void keyReleased(KeyEvent e) {
+						}
+
+						public void keyPressed(KeyEvent e) {
+							params.constraints.setData("skipset", 1);
+						}
+					});
+
+					Button btnSave = new Button(gConstraint, SWT.PUSH);
+					btnSave.addListener(SWT.Selection, new Listener() {
+						public void handleEvent(Event event) {
+							propConstraint.setStringList(new String[] {
+								params.constraints.getText()
+							});
+						}
+					});
+					Messages.setLanguageText(btnSave, "Button.save");
+
+					Button btnReset = new Button(gConstraint, SWT.PUSH);
+					btnReset.addListener(SWT.Selection, new Listener() {
+						public void handleEvent(Event event) {
+							params.constraints.setData("skipset", null);
+							swt_updateFields();
+						}
+					});
+					Messages.setLanguageText(btnReset, "Button.reset");
+
+					Link lblAboutConstraint = new Link(gConstraint, SWT.WRAP);
+					lblAboutConstraint.setLayoutData(
+							Utils.getWrappableLabelGridData(1, GridData.GRAB_HORIZONTAL));
+					lblAboutConstraint.setText(
+							MessageText.getString("tag.constraints.info"));
+					lblAboutConstraint.addListener(SWT.Selection, new Listener() {
+						public void handleEvent(Event event) {
+							if (event.text != null && (event.text.startsWith("http://")
+									|| event.text.startsWith("https://"))) {
+								Utils.launch(event.text);
+							}
+						}
+					});
+
+				}
+			}
 
 			swt_updateFields();
 		}
 
-		cMainComposite.layout();;
+		cMainComposite.layout();
 		sc.setMinSize(cMainComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+	}
+
+	private static abstract class folderOption
+	{
+		private Button btnClear;
+
+		private Label lblValue;
+
+		public folderOption(final Composite parent, String labelTextID) {
+			ImageLoader imageLoader = ImageLoader.getInstance();
+			Image imgOpenFolder = imageLoader.getImage("openFolderButton");
+
+			GridData gd = new GridData();
+			Label label = new Label(parent, SWT.NONE);
+			label.setLayoutData(gd);
+			Messages.setLanguageText(label, labelTextID);
+
+			Button browse = new Button(parent, SWT.PUSH);
+			browse.setImage(imgOpenFolder);
+			imgOpenFolder.setBackground(browse.getBackground());
+			browse.setToolTipText(MessageText.getString("ConfigView.button.browse"));
+
+			browse.addListener(SWT.Selection, new Listener() {
+				public void handleEvent(Event event) {
+					DirectoryDialog dialog = new DirectoryDialog(parent.getShell(),
+							SWT.APPLICATION_MODAL);
+					String filterPath;
+					File tagInitialSaveFolder = getFolder();
+					filterPath = tagInitialSaveFolder != null
+							? tagInitialSaveFolder.toString()
+							: COConfigurationManager.getStringParameter("Default save path");
+					dialog.setFilterPath(filterPath);
+					dialog.setMessage(MessageText.getString("label.init.save.loc"));
+					dialog.setText(MessageText.getString("label.init.save.loc"));
+					String path = dialog.open();
+					if (path != null) {
+						setFolder(new File(path));
+					}
+				}
+			});
+
+			lblValue = new Label(parent, SWT.WRAP);
+			gd = Utils.getWrappableLabelGridData(1, GridData.FILL_HORIZONTAL);
+			gd.verticalAlignment = SWT.CENTER;
+			lblValue.setLayoutData(gd);
+
+			btnClear = new Button(parent, SWT.PUSH);
+			Messages.setLanguageText(btnClear, "Button.clear");
+			btnClear.addListener(SWT.Selection, new Listener() {
+				public void handleEvent(Event event) {
+					setFolder(null);
+				}
+			});
+		}
+
+		public void update() {
+			File folder = getFolder();
+			if (folder == null) {
+				Messages.setLanguageText(lblValue, "label.none.assigned");
+			} else {
+				lblValue.setText(folder.toString());
+			}
+			btnClear.setVisible(folder != null);
+		}
+
+		public abstract File getFolder();
+
+		public abstract void setFolder(File folder);
 	}
 
 	private String getFullTitle() {
@@ -516,7 +719,7 @@ public class TagSettingsView
 		if (params.maxUploadSpeed != null) {
 			params.maxUploadSpeed.resetToDefault();
 		}
-		
+
 		if (params.uploadPriority != null) {
 			params.uploadPriority.refresh();
 		}
@@ -527,6 +730,34 @@ public class TagSettingsView
 			params.max_sr.refresh();
 		}
 
+		if (params.initalSaveFolder != null) {
+			params.initalSaveFolder.update();
+		}
+		if (params.moveOnCompleteFolder != null) {
+			params.moveOnCompleteFolder.update();
+		}
+		if (params.copyOnCompleteFolder != null) {
+			params.copyOnCompleteFolder.update();
+		}
+		if (params.constraints != null
+				&& params.constraints.getData("skipset") == null) {
+			String text = "";
+			if (tag.getTagType().hasTagTypeFeature(TagFeature.TF_PROPERTIES)
+					&& (tag instanceof TagFeatureProperties)) {
+				TagFeatureProperties tfp = (TagFeatureProperties) tag;
+
+				TagProperty propConstraint = tfp.getProperty(
+						TagFeatureProperties.PR_CONSTRAINT);
+				if (propConstraint != null) {
+					String[] stringList = propConstraint.getStringList();
+					// constraint only has one entry
+					if (stringList.length == 1 && stringList.length > 0) {
+						text = stringList[0];
+					}
+				}
+			}
+			params.constraints.setText(text);
+		}
 	}
 
 	// @see com.aelitis.azureus.core.tag.TagTypeListener#tagTypeChanged(com.aelitis.azureus.core.tag.TagType)
