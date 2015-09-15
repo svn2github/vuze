df0bab2b029dcf770d4cba400712cfec0f20e435
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2015-09-15 19:37:43 +0000

    try/catch setText
    git-svn-id: http://svn.vuze.com/public/client@42536 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/config/StringParameter.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/config/StringParameter.java
@@ -24,6 +24,7 @@ import org.eclipse.swt.widgets.*;
 
 import org.gudy.azureus2.core3.config.COConfigurationManager;
 import org.gudy.azureus2.core3.util.AERunnable;
+import org.gudy.azureus2.core3.util.Debug;
 import org.gudy.azureus2.ui.swt.Utils;
 
 /**
@@ -80,7 +81,11 @@ public class StringParameter extends Parameter{
     	}
     };
     String value = COConfigurationManager.getStringParameter(name, defaultValue);
-    inputField.setText(value);
+    try {
+    	inputField.setText(value);
+    } catch (IllegalArgumentException e) {
+    	Debug.out("IllegalArgumentException for value of " + name);
+    }
     inputField.addListener(SWT.Verify, new Listener() {
         public void handleEvent(Event e) {
           e.doit = COConfigurationManager.verifyParameter(name, e.text );
