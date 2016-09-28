c6d072d303ca6b17e083ffd9afb8598c142e7104
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2016-09-28 23:04:18 +0000

    git-svn-id: http://svn.vuze.com/public/client@43603 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/global/impl/GlobalManagerImpl.java
@@ -4173,9 +4173,9 @@ public class GlobalManagerImpl
 								tag_active.addTaggable( dm );
 								
 								tag_inactive.removeTaggable( dm );
-								
-								dm.getDownloadState().setLongAttribute( DownloadManagerState.AT_LAST_ADDED_TO_ACTIVE_TAG, SystemTime.getCurrentTime());
 							}
+							
+							dm.getDownloadState().setLongAttribute( DownloadManagerState.AT_LAST_ADDED_TO_ACTIVE_TAG, SystemTime.getCurrentTime());
 						}
 					}
 				}
