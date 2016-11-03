58fc728ff9acbfe7aba5b374e7b2fdca98669007
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2016-11-03 23:01:30 +0000

    documentation
    git-svn-id: http://svn.vuze.com/public/client@43765 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/core3/security/impl/SESecurityManagerImpl.java
+++ trunk/azureus2/src/org/gudy/azureus2/core3/security/impl/SESecurityManagerImpl.java
@@ -1012,6 +1012,81 @@ SESecurityManagerImpl
 		}
 	}
 	
+	/*package org.gudy.azureus2.core3.security.impl;
+
+	import java.net.Socket;
+	import java.security.cert.CertificateException;
+	import java.security.cert.X509Certificate;
+
+	import javax.net.ssl.SSLEngine;
+	import javax.net.ssl.X509ExtendedTrustManager;
+	import javax.net.ssl.X509TrustManager;
+
+	public class 
+	SETrustingManager 
+		extends X509ExtendedTrustManager
+	{
+		private X509TrustManager		delegate;
+		
+		public
+		SETrustingManager(
+			X509TrustManager	_delegate )
+		{
+			delegate = _delegate;
+		}
+		
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType)
+				throws CertificateException {
+			if ( delegate != null ){
+				delegate.checkClientTrusted( chain, authType );
+			}
+		}
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType,
+				Socket socket) throws CertificateException {
+			if ( delegate != null ){
+				delegate.checkClientTrusted( chain, authType );
+			}
+		}
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType,
+				SSLEngine engine) throws CertificateException {
+			if ( delegate != null ){
+				delegate.checkClientTrusted( chain, authType );
+			}		
+		}
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType)
+				throws CertificateException {
+			if ( delegate != null ){
+				delegate.checkServerTrusted( chain, authType );
+			}
+		}
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType,
+				Socket socket) throws CertificateException {
+			if ( delegate != null ){
+				delegate.checkServerTrusted( chain, authType );
+			}	
+		}
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType,
+				SSLEngine engine) throws CertificateException {
+			if ( delegate != null ){
+				delegate.checkServerTrusted( chain, authType );
+			}		
+		}
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			if ( delegate != null ){
+				return( delegate.getAcceptedIssuers());
+			}
+			return null;
+		}
+	}*/
+	
+	
 	private SSLSocketFactory
 	installServerCertificates(
 		final URL	https_url,
