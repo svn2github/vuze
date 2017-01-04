2d29764c95f35f0a116518f11871ca5304ae36c2
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2017-01-04 18:25:07 +0000

    Tooltip on Peer's Download column with protocol/data/total bytes info
    git-svn-id: http://svn.vuze.com/public/client@43931 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/peers/DownItem.java
+++ trunk/azureus2/src/org/gudy/azureus2/ui/swt/views/tableitems/peers/DownItem.java
@@ -22,13 +22,17 @@
 
 package org.gudy.azureus2.ui.swt.views.tableitems.peers;
 
+import java.text.NumberFormat;
+
 import org.gudy.azureus2.core3.config.COConfigurationManager;
 import org.gudy.azureus2.core3.config.ParameterListener;
+import org.gudy.azureus2.core3.internat.MessageText;
 import org.gudy.azureus2.core3.peer.PEPeer;
 import org.gudy.azureus2.core3.util.DisplayFormatters;
 
 import org.gudy.azureus2.plugins.ui.tables.TableCell;
 import org.gudy.azureus2.plugins.ui.tables.TableCellRefreshListener;
+import org.gudy.azureus2.plugins.ui.tables.TableCellToolTipListener;
 import org.gudy.azureus2.plugins.ui.tables.TableColumnInfo;
 import org.gudy.azureus2.ui.swt.views.table.CoreTableColumnSWT;
 
@@ -39,7 +43,7 @@ import org.gudy.azureus2.ui.swt.views.table.CoreTableColumnSWT;
  * @author TuxPaper (2004/Apr/19: modified to TableCellAdapter)
  */
 public class DownItem extends CoreTableColumnSWT implements
-		TableCellRefreshListener
+		TableCellRefreshListener, TableCellToolTipListener
 {
 	public static final String COLUMN_ID = "download";
 
@@ -93,5 +97,41 @@ public class DownItem extends CoreTableColumnSWT implements
 
 		cell.setText(DisplayFormatters.formatDataProtByteCountToKiBEtc(data_value,
 				prot_value));
+	}
+
+	/* (non-Javadoc)
+	 * @see org.gudy.azureus2.plugins.ui.tables.TableCellToolTipListener#cellHover(org.gudy.azureus2.plugins.ui.tables.TableCell)
+	 */
+	public void cellHover(TableCell cell) {
+		Object ds = cell.getDataSource();
+		if (ds instanceof PEPeer) {
+			PEPeer peer = (PEPeer) ds;
+			
+			long data_value = peer.getStats().getTotalDataBytesReceived();
+			long prot_value = peer.getStats().getTotalProtocolBytesReceived();
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append(DisplayFormatters.formatByteCountToKiBEtc(data_value));
+			sb.append(' ');
+			sb.append(MessageText.getString("label.transfered.data"));
+			sb.append('\n');
+			sb.append(DisplayFormatters.formatByteCountToKiBEtc(prot_value));
+			sb.append(' ');
+			sb.append(MessageText.getString("label.transfered.protocol"));
+			sb.append('\n');
+			sb.append(DisplayFormatters.formatByteCountToKiBEtc(prot_value + data_value));
+			sb.append(' ');
+			sb.append(MessageText.getString("label.transfered.total"));
+			sb.append('\n');
+			
+			cell.setToolTip(sb.toString());
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.gudy.azureus2.plugins.ui.tables.TableCellToolTipListener#cellHoverComplete(org.gudy.azureus2.plugins.ui.tables.TableCell)
+	 */
+	public void cellHoverComplete(TableCell cell) {
+		cell.setToolTip(null);
 	}
 }
